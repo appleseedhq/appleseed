aba64878dec2e6d797d667511ef91a9a43278fb6
Francois Beaune <beaune@aist.enst.fr>
2010-07-06 09:11:45 +0200

    updated the packaging script.

--- scripts/make-package.bat
+++ scripts/make-package.bat
@@ -4,13 +4,15 @@ set platform=win32.vs90
 set configuration=Ship
 
 set package-name=appleseed-VERSION-MATURITY-%platform%.zip
+set package-path=..\archives\%package-name%
+
 set log-file=make-package.log
 
-echo Starting to build %package-name%
+echo Starting to build %package-path%
 echo.
 
 echo Removing left over package...
-del ..\%package-name% 2>nul
+del %package-path% 2>nul
 
 echo Removing left over staging directory...
 rmdir /S /Q appleseed 2>nul
@@ -21,7 +23,7 @@ del %log-file% 2>nul
 echo Archiving sandbox from HEAD...
 pushd ..\sandbox >>%log-file% 2>&1
 call git archive -o sandbox.zip HEAD
-popd >>%log-file% 2>&1
+popd
 
 echo Unzipping sandbox archive to staging directory...
 mkdir appleseed >>%log-file% 2>&1
@@ -30,6 +32,8 @@ del ..\sandbox\sandbox.zip >>%log-file% 2>&1
 
 echo Removing unwanted files from staging directory...
 rmdir /S /Q appleseed\scenes\cyberdemon >>%log-file% 2>&1
+del "appleseed\scenes\killeroo\killeroo ao.appleseed" >>%log-file% 2>&1
+del "appleseed\scenes\killeroo\killeroo ao close up.appleseed" >>%log-file% 2>&1
 rmdir /S /Q appleseed\scenes\smoke >>%log-file% 2>&1
 rmdir /S /Q appleseed\scenes\tests\invalid >>%log-file% 2>&1
 rmdir /S /Q appleseed\scenes\tests\scope >>%log-file% 2>&1
@@ -53,7 +57,7 @@ copy ..\LICENSE.txt appleseed\ >>%log-file% 2>&1
 copy ..\README.txt appleseed\ >>%log-file% 2>&1
 
 echo Building final zip file from staging directory...
-tools\7-zip\7z.exe a -xr!*.gitignore ..\%package-name% appleseed >>%log-file% 2>&1
+tools\7-zip\7z.exe a -xr!*.gitignore %package-path% appleseed >>%log-file% 2>&1
 
 echo Removing staging directory...
 rmdir /S /Q appleseed >>%log-file% 2>&1
