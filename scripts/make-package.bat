de9e9242b7cdce5c26443f32ffc47376ef6d977d
Francois Beaune <beaune@aist.enst.fr>
2010-07-06 08:43:40 +0200

    updated the packaging script.

--- scripts/make-package.bat
+++ scripts/make-package.bat
@@ -1,27 +1,67 @@
 @echo off
 
-echo Packaging Release...
+set platform=win32.vs90
+set configuration=Ship
 
-rmdir /S /Q tmp 2>nul
-del appleseed-package.zip 2>nul
+set package-name=appleseed-VERSION-MATURITY-%platform%.zip
+set log-file=make-package.log
 
-mkdir tmp
-cd tmp
+echo Starting to build %package-name%
+echo.
 
-svn export https://svn.appleseedhq.net/trunk/sandbox
+echo Removing left over package...
+del ..\%package-name% 2>nul
 
-copy ..\..\sandbox\schemas\* sandbox\schemas\
+echo Removing left over staging directory...
+rmdir /S /Q appleseed 2>nul
 
-copy ..\..\sandbox\bin\win32.vs90\Ship\*.exe sandbox\bin\
-copy ..\..\sandbox\bin\win32.vs90\Ship\*.dll sandbox\bin\
+echo Clearing log file...
+del %log-file% 2>nul
 
-copy C:\Qt\2009.03-win32.vs90\qt\bin\QtCore4.dll sandbox\bin\
-copy C:\Qt\2009.03-win32.vs90\qt\bin\QtGui4.dll sandbox\bin\
+echo Archiving sandbox from HEAD...
+pushd ..\sandbox >>%log-file% 2>&1
+call git archive -o sandbox.zip HEAD
+popd >>%log-file% 2>&1
 
-move sandbox appleseed
-tools\7-zip\7z.exe a ..\appleseed-package.zip appleseed
+echo Unzipping sandbox archive to staging directory...
+mkdir appleseed >>%log-file% 2>&1
+tools\7-zip\7z.exe x -oappleseed ..\sandbox\sandbox.zip >>%log-file% 2>&1
+del ..\sandbox\sandbox.zip >>%log-file% 2>&1
 
-cd ..
-rmdir /S /Q tmp 2>nul
+echo Removing unwanted files from staging directory...
+rmdir /S /Q appleseed\scenes\cyberdemon >>%log-file% 2>&1
+rmdir /S /Q appleseed\scenes\smoke >>%log-file% 2>&1
+rmdir /S /Q appleseed\scenes\tests\invalid >>%log-file% 2>&1
+rmdir /S /Q appleseed\scenes\tests\scope >>%log-file% 2>&1
+rmdir /S /Q "appleseed\scenes\tests\self intersections" >>%log-file% 2>&1
+rmdir /S /Q appleseed\scenes\winosi\renders >>%log-file% 2>&1
+
+echo Adding local schemas to staging directory...
+mkdir appleseed\schemas >>%log-file% 2>&1
+copy ..\sandbox\schemas\* appleseed\schemas\ >>%log-file% 2>&1
+
+echo Adding local binaries to staging directory...
+copy ..\sandbox\bin\%platform%\%configuration%\*.exe appleseed\bin\ >>%log-file% 2>&1
+copy ..\sandbox\bin\%platform%\%configuration%\*.dll appleseed\bin\ >>%log-file% 2>&1
+
+echo Adding dependencies to staging directory...
+copy C:\Qt\2009.03-%platform%\qt\bin\QtCore4.dll appleseed\bin\ >>%log-file% 2>&1
+copy C:\Qt\2009.03-%platform%\qt\bin\QtGui4.dll appleseed\bin\ >>%log-file% 2>&1
+
+echo Adding LICENSE.txt and README.txt...
+copy ..\LICENSE.txt appleseed\ >>%log-file% 2>&1
+copy ..\README.txt appleseed\ >>%log-file% 2>&1
+
+echo Building final zip file from staging directory...
+tools\7-zip\7z.exe a -xr!*.gitignore ..\%package-name% appleseed >>%log-file% 2>&1
+
+echo Removing staging directory...
+rmdir /S /Q appleseed >>%log-file% 2>&1
+
+echo Done.
+echo.
+
+echo Check %log-file% for errors.
+echo.
 
 pause
