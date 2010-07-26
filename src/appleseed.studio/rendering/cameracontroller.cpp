appleseedhq/appleseed

commit fd0f292d61b5d8dd4fda00f20018da0aae1d8f47

the camera controller (appleseed::studio::CameraController) now uses the center of the scene's bounding box as a target point, if no target point is available in the camera.

src/appleseed.studio/rendering/cameracontroller.cpp
@@ -31,6 +31,8 @@
 
 // appleseed.renderer headers.
 #include "renderer/api/camera.h"
+#include "renderer/api/scene.h"
+#include "renderer/api/utility.h"
 
 // appleseed.foundation headers.
 #include "foundation/math/transform.h"
@@ -52,17 +54,14 @@ namespace studio {
 
 CameraController::CameraController(
     QWidget*    render_widget,
-    Camera*     camera)
+    Scene*      scene)
   : m_render_widget(render_widget)
-  , m_camera(camera)
+  , m_camera(scene->get_camera())
 {
     m_controller.set_transform(
         m_camera->get_transform().get_local_to_parent());
 
-    m_controller.set_target(
-        m_camera->get_parameters().get_optional<Vector3d>(
-            "controller_target",
-            m_controller.get_target()));
+    set_controller_target(scene);
 
     m_render_widget->installEventFilter(this);
 }
@@ -100,6 +99,26 @@ bool CameraController::eventFilter(QObject* object, QEvent* event)
     }
 
     return QObject::eventFilter(object, event);
+}
+
+void CameraController::set_controller_target(const Scene* scene)
+{
+    if (m_camera->get_parameters().strings().exist("controller_target"))
+    {
+        m_controller.set_target(
+            m_camera->get_parameters().get_optional<Vector3d>(
+                "controller_target",
+                Vector3d(0.0)));
+    }
+    else
+    {
+        const GAABB3 scene_bbox =
+            compute_parent_bbox<GAABB3>(
+                scene->assembly_instances().begin(),
+                scene->assembly_instances().end());
+
+        m_controller.set_target(Vector3d(scene_bbox.center()));
+    }
 }
 
 Vector2d CameraController::get_mouse_position(const QMouseEvent* event) const

src/appleseed.studio/rendering/cameracontroller.h
@@ -38,6 +38,8 @@
 
 // Forward declarations.
 namespace renderer  { class Camera; }
+namespace renderer  { class Scene; }
+
 class QEvent;
 class QMouseEvent;
 class QWidget;
@@ -54,7 +56,7 @@ class CameraController
     // Constructor.
     CameraController(
         QWidget*            render_widget,
-        renderer::Camera*   camera);
+        renderer::Scene*    scene);
 
     // Destructor.
     ~CameraController();
@@ -72,6 +74,8 @@ class CameraController
     ControllerType      m_controller;
 
     virtual bool eventFilter(QObject* object, QEvent* event);
+
+    void set_controller_target(const renderer::Scene* scene);
 
     foundation::Vector2d get_mouse_position(const QMouseEvent* event) const;
 

src/appleseed.studio/rendering/renderingmanager.cpp
@@ -139,7 +139,7 @@ void RenderingManager::start_rendering(
     m_camera_controller.reset(
         new CameraController(
             m_render_widget,
-            m_project->get_scene()->get_camera()));
+            m_project->get_scene()));
 
     connect(
         m_camera_controller.get(), SIGNAL(signal_camera_changed()),

commit ecc1ddd53fcf67bf40f6e7c7ca901e6b29d9b01b

fixed rendering of empty scene, broken after commit fd0f292d61b5d8dd4fda00f20018da0aae1d8f47.

src/appleseed.studio/rendering/cameracontroller.cpp
@@ -117,7 +117,10 @@ void CameraController::set_controller_target(const Scene* scene)
                 scene->assembly_instances().begin(),
                 scene->assembly_instances().end());
 
-        m_controller.set_target(Vector3d(scene_bbox.center()));
+        m_controller.set_target(
+            scene_bbox.is_valid()
+                ? Vector3d(scene_bbox.center())
+                : Vector3d(0.0));
     }
 }
 