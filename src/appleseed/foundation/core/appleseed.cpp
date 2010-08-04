36f3134cb640a625e5602fe8dd60fa8456e1fd50
Francois Beaune <beaune@aist.enst.fr>
2010-08-04 16:58:45 +0200

    modified foundation/core/appleseed.cpp to ease regular-expression parsing.
    fixed the return value of foundation::Appleseed::get_lib_name() on Mac OS X.

--- src/appleseed/foundation/core/appleseed.cpp
+++ src/appleseed/foundation/core/appleseed.cpp
@@ -33,10 +33,9 @@ namespace foundation
 {
 
 //
-// Library information.
+// Appleseed class implementation.
 //
 
-// Return the name of the library.
 const char* Appleseed::get_lib_name()
 {
 // Windows.
@@ -47,7 +46,7 @@ const char* Appleseed::get_lib_name()
 // Mac OS X.
 #elif defined __APPLE__
 
-    return "appleseed.dylib";
+    return "libappleseed.dylib";
 
 // Other operating system.
 #else
@@ -57,30 +56,24 @@ const char* Appleseed::get_lib_name()
 #endif
 }
 
-// Return the version string of the library.
 const char* Appleseed::get_lib_version()
 {
-    return "1.1.0-UNOFFICIAL";
+    static const char* LibVersionToken = "1.1.0-UNOFFICIAL"
+    return LibVersionToken;
 }
 
-// Return the maturity level of the library.
 const char* Appleseed::get_lib_maturity_level()
 {
-    return "alpha-2";
+    static const char* LibMaturityLevelToken = "alpha-2"
+    return LibMaturityLevelToken;
 }
 
-// Return the build number of the library.
 size_t Appleseed::get_lib_build_number()
 {
-    // This works as follow: the build number is stored in a static variable
-    // whose name is unique (the text after build_number_ is a GUID).
-    // Before compilation begins, a tool searches for this variable declaration
-    // (in this file) and increases the build number by 1.
-    static const size_t build_number_335A07D9_68C1_4E21_86FA_60C64BD6FE6C = 7618;
-    return build_number_335A07D9_68C1_4E21_86FA_60C64BD6FE6C;
+    static const size_t LibBuildNumberToken = 7618;
+    return LibBuildNumberToken;
 }
 
-// Return the configuration of the library.
 const char* Appleseed::get_lib_configuration()
 {
 #ifdef DEBUG
@@ -90,13 +83,11 @@ const char* Appleseed::get_lib_configuration()
 #endif
 }
 
-// Return the compilation date of the library.
 const char* Appleseed::get_lib_compilation_date()
 {
     return __DATE__;
 }
 
-// Return the compilation time of the library.
 const char* Appleseed::get_lib_compilation_time()
 {
     return __TIME__;
