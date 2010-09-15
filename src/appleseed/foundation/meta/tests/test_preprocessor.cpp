eea72edc0da1afb2f12dfeec4ca62a3a82923ecd
Francois Beaune <beaune@aist.enst.fr>
2010-09-15 12:26:54 +0200

    added foundation::Preprocessor, a simple text preprocessor implementing a subset of a standard C preprocessor.

---
+++ src/appleseed/foundation/meta/tests/test_preprocessor.cpp
@@ -0,0 +1,246 @@
+
+//
+// This source file is part of appleseed.
+// Visit http://appleseedhq.net/ for additional information and resources.
+//
+// This software is released under the MIT license.
+//
+// Copyright (c) 2010 Francois Beaune
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+// appleseed.foundation headers.
+#include "foundation/utility/preprocessor.h"
+#include "foundation/utility/test.h"
+
+// Standard headers.
+#include <string>
+
+using namespace foundation;
+using namespace std;
+
+FOUNDATION_TEST_SUITE(Foundation_Utility_Preprocessor)
+{
+    FOUNDATION_TEST_CASE(Process_GivenEmptyString_ReturnsEmptyString)
+    {
+        const string InputText = "";
+
+        Preprocessor preprocessor;
+        preprocessor.process(InputText.c_str());
+
+        FOUNDATION_ASSERT_TRUE(preprocessor.succeeded());
+        FOUNDATION_EXPECT_EQ(InputText, preprocessor.get_processed_text());
+    }
+
+    FOUNDATION_TEST_CASE(Process_GivenPlainString_ReturnsInputString)
+    {
+        const string InputText = "hello world";
+
+        Preprocessor preprocessor;
+        preprocessor.process(InputText.c_str());
+
+        FOUNDATION_ASSERT_TRUE(preprocessor.succeeded());
+        FOUNDATION_EXPECT_EQ(InputText, preprocessor.get_processed_text());
+    }
+
+    FOUNDATION_TEST_CASE(Process_GivenPlainMultilineString_ReturnsInputString)
+    {
+        const string InputText =
+            "hello\n"
+            "world";
+
+        Preprocessor preprocessor;
+        preprocessor.process(InputText.c_str());
+
+        FOUNDATION_ASSERT_TRUE(preprocessor.succeeded());
+        FOUNDATION_EXPECT_EQ(InputText, preprocessor.get_processed_text());
+    }
+
+    FOUNDATION_TEST_CASE(Process_GivenPlainStringWithLeadingEOL_ReturnsInputString)
+    {
+        const string InputText = "\nhello world";
+
+        Preprocessor preprocessor;
+        preprocessor.process(InputText.c_str());
+
+        FOUNDATION_ASSERT_TRUE(preprocessor.succeeded());
+        FOUNDATION_EXPECT_EQ(InputText, preprocessor.get_processed_text());
+    }
+
+    FOUNDATION_TEST_CASE(Process_GivenPlainStringWithTrailingEOL_ReturnsInputString)
+    {
+        const string InputText = "hello world\n";
+
+        Preprocessor preprocessor;
+        preprocessor.process(InputText.c_str());
+
+        FOUNDATION_ASSERT_TRUE(preprocessor.succeeded());
+        FOUNDATION_EXPECT_EQ(InputText, preprocessor.get_processed_text());
+    }
+
+    FOUNDATION_TEST_CASE(Process_GivenStringWithEOLOnly_ReturnsInputString)
+    {
+        const string InputText = "\n\n\n";
+
+        Preprocessor preprocessor;
+        preprocessor.process(InputText.c_str());
+
+        FOUNDATION_ASSERT_TRUE(preprocessor.succeeded());
+        FOUNDATION_EXPECT_EQ(InputText, preprocessor.get_processed_text());
+    }
+
+    FOUNDATION_TEST_CASE(Process_IfdefWithUndefinedSymbol_SkipsIfdefSection)
+    {
+        const string InputText =
+            "#ifdef X\n"
+            "ignore\n"
+            "#endif\n"
+            "more\n";
+
+        const string ExpectedText = "more\n";
+
+        Preprocessor preprocessor;
+        preprocessor.process(InputText.c_str());
+
+        FOUNDATION_ASSERT_TRUE(preprocessor.succeeded());
+        FOUNDATION_EXPECT_EQ(ExpectedText, preprocessor.get_processed_text());
+    }
+
+    FOUNDATION_TEST_CASE(Process_IfdefWithPredefinedSymbol_KeepsIfdefSection)
+    {
+        const string InputText =
+            "#ifdef X\n"
+            "keep\n"
+            "#endif\n"
+            "more\n";
+
+        const string ExpectedText =
+            "keep\n"
+            "more\n";
+
+        Preprocessor preprocessor;
+        preprocessor.define_symbol("X");
+        preprocessor.process(InputText.c_str());
+
+        FOUNDATION_ASSERT_TRUE(preprocessor.succeeded());
+        FOUNDATION_EXPECT_EQ(ExpectedText, preprocessor.get_processed_text());
+    }
+
+    FOUNDATION_TEST_CASE(Process_IfdefWithDefinedSymbol_KeepsIfdefSection)
+    {
+        const string InputText =
+            "#define X\n"
+            "#ifdef X\n"
+            "keep\n"
+            "#endif\n"
+            "more\n";
+
+        const string ExpectedText =
+            "keep\n"
+            "more\n";
+
+        Preprocessor preprocessor;
+        preprocessor.process(InputText.c_str());
+
+        FOUNDATION_ASSERT_TRUE(preprocessor.succeeded());
+        FOUNDATION_EXPECT_EQ(ExpectedText, preprocessor.get_processed_text());
+    }
+
+    FOUNDATION_TEST_CASE(Process_IfdefWithValuedSymbol_KeepsIfdefSection)
+    {
+        const string InputText =
+            "#define X 42\n"
+            "#ifdef X\n"
+            "keep\n"
+            "#endif\n"
+            "more\n";
+
+        const string ExpectedText =
+            "keep\n"
+            "more\n";
+
+        Preprocessor preprocessor;
+        preprocessor.process(InputText.c_str());
+
+        FOUNDATION_ASSERT_TRUE(preprocessor.succeeded());
+        FOUNDATION_EXPECT_EQ(ExpectedText, preprocessor.get_processed_text());
+    }
+
+    FOUNDATION_TEST_CASE(Process_UnknownKeyword_GeneratesError)
+    {
+        const string InputText = "#stuff X Y";
+
+        Preprocessor preprocessor;
+        preprocessor.process(InputText.c_str());
+
+        FOUNDATION_ASSERT_FALSE(preprocessor.succeeded());
+        FOUNDATION_EXPECT_EQ(string("Unknown directive: #stuff"), preprocessor.get_error_message());
+        FOUNDATION_EXPECT_EQ(1, preprocessor.get_error_location());
+    }
+
+    FOUNDATION_TEST_CASE(Process_MissingEndIf_GeneratesError)
+    {
+        const string InputText =
+            "#ifdef X\n"
+            "keep\n";
+
+        Preprocessor preprocessor;
+        preprocessor.process(InputText.c_str());
+
+        FOUNDATION_ASSERT_FALSE(preprocessor.succeeded());
+        FOUNDATION_EXPECT_EQ(string("Expected directive: #endif"), preprocessor.get_error_message());
+        FOUNDATION_EXPECT_EQ(2, preprocessor.get_error_location());
+    }
+
+    FOUNDATION_TEST_CASE(Process_GivenSymbolDefinition_SubstitutesSymbolWithValue)
+    {
+        const string InputText =
+            "#define X 42\n"
+            "foo X barX\n"
+            "XX\n";
+
+        const string ExpectedText =
+            "foo 42 bar42\n"
+            "4242\n";
+
+        Preprocessor preprocessor;
+        preprocessor.process(InputText.c_str());
+
+        FOUNDATION_ASSERT_TRUE(preprocessor.succeeded());
+        FOUNDATION_EXPECT_EQ(ExpectedText, preprocessor.get_processed_text());
+    }
+
+    FOUNDATION_TEST_CASE(Process_GivenSymbolDefinitionUsingAnotherSymbol_SubstitutesInChain)
+    {
+        const string InputText =
+            "#define X 42\n"
+            "#define Y X\n"
+            "Y\n";
+
+        const string ExpectedText =
+            "42\n";
+
+        Preprocessor preprocessor;
+        preprocessor.process(InputText.c_str());
+
+        FOUNDATION_ASSERT_TRUE(preprocessor.succeeded());
+        FOUNDATION_EXPECT_EQ(ExpectedText, preprocessor.get_processed_text());
+    }
+}
---
+++ src/appleseed/foundation/utility/preprocessor.cpp
@@ -0,0 +1,396 @@
+
+//
+// This source file is part of appleseed.
+// Visit http://appleseedhq.net/ for additional information and resources.
+//
+// This software is released under the MIT license.
+//
+// Copyright (c) 2010 Francois Beaune
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+// Interface header.
+#include "preprocessor.h"
+
+// appleseed.foundation headers.
+#include "foundation/core/exception.h"
+#include "foundation/utility/containers/dictionary.h"
+#include "foundation/utility/foreach.h"
+#include "foundation/utility/string.h"
+#include "foundation/utility/test.h"
+
+// Standard headers.
+#include <cassert>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+namespace foundation
+{
+
+//
+// Preprocessor class implementation.
+//
+
+namespace
+{
+    bool is_directive(const string& line)
+    {
+        const string::size_type i = line.find_first_not_of(Blanks);
+
+        return i != string::npos && line[i] == '#';
+    }
+
+    void split_line(const string& line, string& keyword, string& arguments)
+    {
+        string::size_type cursor = line.find_first_not_of(Blanks);
+
+        const string::size_type keyword_begin = cursor;
+        cursor = line.find_first_of(Blanks, cursor);
+
+        keyword =
+            cursor == string::npos
+                ? line.substr(keyword_begin)
+                : line.substr(keyword_begin, cursor - keyword_begin);
+
+        arguments.clear();
+
+        if (cursor == string::npos)
+            return;
+
+        cursor = line.find_first_not_of(Blanks, cursor);
+
+        if (cursor == string::npos)
+            return;
+
+        const string::size_type arguments_begin = cursor;
+        cursor = line.find_last_not_of(Blanks);
+
+        arguments = line.substr(arguments_begin, cursor - arguments_begin + 1);
+    }
+
+    void split_directive(const string& line, string& keyword, string& arguments)
+    {
+        const string::size_type i = line.find_first_not_of(Blanks);
+
+        assert(i != string::npos);
+        assert(line[i] == '#');
+
+        split_line(line.substr(i + 1), keyword, arguments);
+    }
+
+    FOUNDATION_TEST_SUITE(Foundation_Utility_Preprocessor_Impl)
+    {
+        FOUNDATION_TEST_CASE(SplitDirective_GivenKeyword_ReturnsKeyword)
+        {
+            string keyword, arguments;
+            split_directive("#keyword", keyword, arguments);
+
+            FOUNDATION_EXPECT_EQ("keyword", keyword);
+        }
+
+        FOUNDATION_TEST_CASE(SplitDirective_GivenSpacesBeforeHashCharacter_ReturnsKeyword)
+        {
+            string keyword, arguments;
+            split_directive("   #keyword", keyword, arguments);
+
+            FOUNDATION_EXPECT_EQ("keyword", keyword);
+        }
+
+        FOUNDATION_TEST_CASE(SplitDirective_GivenSpacesAfterHashCharacter_ReturnsKeyword)
+        {
+            string keyword, arguments;
+            split_directive("#   keyword", keyword, arguments);
+
+            FOUNDATION_EXPECT_EQ("keyword", keyword);
+        }
+
+        FOUNDATION_TEST_CASE(SplitDirective_GivenSpacesBeforeAndAfterHashCharacter_ReturnsKeyword)
+        {
+            string keyword, arguments;
+            split_directive("   #   keyword", keyword, arguments);
+
+            FOUNDATION_EXPECT_EQ("keyword", keyword);
+        }
+
+        FOUNDATION_TEST_CASE(SplitDirective_GivenSpacesAfterKeyword_ReturnsKeyword)
+        {
+            string keyword, arguments;
+            split_directive("#keyword   ", keyword, arguments);
+
+            FOUNDATION_EXPECT_EQ("keyword", keyword);
+        }
+
+        FOUNDATION_TEST_CASE(SplitDirective_GivenNoArgument_ReturnsEmptyArguments)
+        {
+            string keyword, arguments;
+            split_directive("#keyword", keyword, arguments);
+
+            FOUNDATION_EXPECT_EQ("", arguments);
+        }
+
+        FOUNDATION_TEST_CASE(SplitDirective_GivenArguments_ReturnsArguments)
+        {
+            string keyword, arguments;
+            split_directive("#keyword arg1 arg2", keyword, arguments);
+
+            FOUNDATION_EXPECT_EQ("arg1 arg2", arguments);
+        }
+
+        FOUNDATION_TEST_CASE(SplitDirective_GivenSpacesAfterArguments_ReturnsArguments)
+        {
+            string keyword, arguments;
+            split_directive("#keyword arg1 arg2   ", keyword, arguments);
+
+            FOUNDATION_EXPECT_EQ("arg1 arg2", arguments);
+        }
+    }
+}
+
+struct Preprocessor::Impl
+{
+    struct ExceptionParseError
+      : public Exception
+    {
+        const size_t m_line_number;
+
+        ExceptionParseError(const string& message, const size_t line_number)
+          : Exception(message.c_str())
+          , m_line_number(line_number)
+        {
+        }
+    };
+
+    bool                    m_succeeded;
+    string                  m_error_message;
+    size_t                  m_error_location;
+
+    StringDictionary        m_symbols;
+
+    vector<string>          m_input_lines;
+    size_t                  m_current_input_line;
+
+    string                  m_result;
+    size_t                  m_current_output_line;
+
+    void process_text(const string& text)
+    {
+        assert(m_result.empty());
+
+        m_succeeded = true;
+        m_error_location = 0;
+        m_current_input_line = 0;
+        m_current_output_line = 0;
+
+        split(text, "\n", m_input_lines);
+
+        parse();
+
+        if (!text.empty() && text[text.size() - 1] == '\n')
+            m_result += '\n';
+    }
+
+    void parse()
+    {
+        while (!is_end_of_input_text())
+        {
+            const string& line = get_next_input_line();
+
+            if (is_directive(line))
+            {
+                string keyword, arguments;
+                split_directive(line, keyword, arguments);
+                parse_directive(keyword, arguments);
+            }
+            else
+            {
+                process_line(line);
+            }
+        }
+    }
+
+    void parse_directive(const string& keyword, const string& arguments)
+    {
+        if (keyword == "define")
+        {
+            parse_define_directive(arguments);
+        }
+        else if (keyword == "ifdef")
+        {
+            parse_ifdef_directive(arguments);
+        }
+        else
+        {
+            parse_error(string("Unknown directive: #") + keyword);
+        }
+    }
+
+    void parse_define_directive(const string& arguments)
+    {
+        string symbol, value;
+        split_line(arguments, symbol, value);
+
+        value = perform_symbol_substitutions(value);
+
+        m_symbols.insert(symbol, value);
+    }
+
+    void parse_ifdef_directive(const string& arguments)
+    {
+        const bool condition_value = evaluate_condition(arguments);
+
+        while (true)
+        {
+            if (is_end_of_input_text())
+            {
+                parse_error("Expected directive: #endif");
+                break;
+            }
+
+            const string& line = get_next_input_line();
+
+            if (is_directive(line))
+            {
+                string keyword, arguments;
+                split_directive(line, keyword, arguments);
+
+                if (keyword == "endif")
+                    break;
+                else parse_directive(keyword, arguments);
+            }
+            else
+            {
+                if (condition_value)
+                    process_line(line);
+            }
+        }
+    }
+
+    bool evaluate_condition(const string& condition) const
+    {
+        return m_symbols.exist(condition);
+    }
+
+    void process_line(const string& line)
+    {
+        emit_line(perform_symbol_substitutions(line));
+    }
+
+    string perform_symbol_substitutions(const string& line) const
+    {
+        string s = line;
+
+        for (const_each<StringDictionary> i = m_symbols; i; ++i)
+            s = replace(s, i->name(), i->value());
+
+        return s;
+    }
+
+    void parse_error(const string& message)
+    {
+        throw ExceptionParseError(message, m_current_input_line);
+    }
+
+    bool is_end_of_input_text() const
+    {
+        return m_current_input_line == m_input_lines.size();
+    }
+
+    const string& get_next_input_line()
+    {
+        assert(m_current_input_line < m_input_lines.size());
+
+        return m_input_lines[m_current_input_line++];
+    }
+
+    void emit_line(const string& line)
+    {
+        if (m_current_output_line > 0)
+            m_result += '\n';
+
+        m_result += line;
+
+        ++m_current_output_line;
+    }
+};
+
+Preprocessor::Preprocessor()
+  : impl(new Impl())
+{
+}
+
+void Preprocessor::define_symbol(const char* name)
+{
+    assert(name);
+
+    impl->m_symbols.insert(name, "");
+}
+
+void Preprocessor::define_symbol(const char* name, const char* value)
+{
+    assert(name);
+    assert(value);
+
+    impl->m_symbols.insert(name, value);
+}
+
+void Preprocessor::process(const char* text)
+{
+    assert(text);
+
+    try
+    {
+        impl->process_text(text);
+    }
+    catch (const Impl::ExceptionParseError& e)
+    {
+        impl->m_succeeded = false;
+        impl->m_error_message = e.what();
+        impl->m_error_location = e.m_line_number;
+    }
+}
+
+const char* Preprocessor::get_processed_text() const
+{
+    return impl->m_result.c_str();
+}
+
+bool Preprocessor::succeeded() const
+{
+    return impl->m_succeeded;
+}
+
+bool Preprocessor::failed() const
+{
+    return !succeeded();
+}
+
+const char* Preprocessor::get_error_message() const
+{
+    return failed() ? impl->m_error_message.c_str() : 0;
+}
+
+size_t Preprocessor::get_error_location() const
+{
+    return failed() ? impl->m_error_location : ~size_t(0);
+}
+
+}   // namespace foundation
