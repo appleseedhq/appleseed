2570c541dc40a1562ea965bbf53b2d5b127e92e2
Francois Beaune <beaune@aist.enst.fr>
2010-09-29 10:20:33 +0200

    renamed FOUNDATION_FLOOR_SSE() to FLOOR_SSE().

--- src/appleseed/foundation/platform/sse.h
+++ src/appleseed/foundation/platform/sse.h
@@ -120,10 +120,10 @@ typedef __m128d sse2d;
 //   http://www.masm32.com/board/index.php?topic=9515.msg78719#msg78719
 //
 
-#define FOUNDATION_FLOOR_SSE(x)                             \
-    _mm_cvtepi32_ps(                                        \
-        _mm_sub_epi32(                                      \
-            _mm_cvttps_epi32(x),                            \
+#define FLOOR_SSE(x)                                    \
+    _mm_cvtepi32_ps(                                    \
+        _mm_sub_epi32(                                  \
+            _mm_cvttps_epi32(x),                        \
             _mm_srli_epi32(_mm_castps_si128(x), 31)))
 
 }       // namespace foundation
