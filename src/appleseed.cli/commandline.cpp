eae7e2b21e48eeb08b9899a04f1e1019bd78df48
Francois Beaune <beaune@aist.enst.fr>
2011-01-13 14:31:05 +0100

    updated copyright notices.

--- src/appleseed.cli/commandline.cpp
+++ src/appleseed.cli/commandline.cpp
@@ -5,7 +5,7 @@
 //
 // This software is released under the MIT license.
 //
-// Copyright (c) 2010 Francois Beaune
+// Copyright (c) 2010-2011 Francois Beaune
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -230,7 +230,7 @@ void CommandLine::print_version_information(SuperLogger& logger) const
         logger,
         "appleseed.cli version %s %s, build " FMT_SIZE_T ", %s configuration\n"
         "compiled on %s at %s using %s version %s\n"
-        "copyright (c) 2010 francois beaune.\n"
+        "copyright (c) 2010-2011 francois beaune.\n"
         "this software is released under the MIT license (http://www.opensource.org/licenses/mit-license.php).\n"
         "visit http://appleseedhq.net/ for additional information and resources.",
         Appleseed::get_lib_version(),
