appleseedhq/appleseed

commit 17fa8f2c4efb7ca4efaaf550b5263ac7a9fb3fdb

fixed a bug in the master renderer and refactored the code.
the bug: the rendering time was not reset when the camera was moved or a shading override was set.

the refactoring: got rid of the rendering modes (renderer::MasterRenderer::Mode). what now distinguishes "render once" from "render continuously" is that "render once" will actually terminate at some point (using the new renderer::IRendererController::TerminateRendering status value) whereas "render continuously" won't (unless it's aborted).

src/appleseed.studio/mainwindow/rendering/renderingmanager.cpp
@@ -98,8 +98,11 @@ RenderingManager::RenderingManager(StatusBar& status_bar)
 {
     connect(
         &m_renderer_controller, SIGNAL(signal_frame_begin()),
-        this, SLOT(slot_frame_begin()),
-        Qt::DirectConnection);
+        this, SLOT(slot_frame_begin()));
+
+    connect(
+        &m_renderer_controller, SIGNAL(signal_frame_end()),
+        this, SLOT(slot_frame_end()));
 
     connect(
         &m_renderer_controller, SIGNAL(signal_rendering_begin()),
@@ -128,7 +131,7 @@ RenderingManager::RenderingManager(StatusBar& status_bar)
 void RenderingManager::start_rendering(
     Project*                    project,
     const ParamArray&           params,
-    const MasterRenderer::Mode  mode,
+    const bool                  highlight_tiles,
     RenderWidget*               render_widget)
 {
     m_project = project;
@@ -147,8 +150,6 @@ void RenderingManager::start_rendering(
         m_camera_controller.get(), SIGNAL(signal_camera_changed()),
         this, SIGNAL(signal_camera_changed()));
 
-    const bool highlight_tiles = mode == MasterRenderer::RenderOnce;
-
     m_tile_callback_factory.reset(
         new QtTileCallbackFactory(
             m_render_widget,
@@ -158,7 +159,6 @@ void RenderingManager::start_rendering(
         new MasterRenderer(
             *m_project,
             params,
-            mode,
             &m_renderer_controller,
             m_tile_callback_factory.get()));
 
@@ -237,6 +237,57 @@ void RenderingManager::archive_frame_to_disk()
         autosave_path.directory_string().c_str());
 }
 
+void RenderingManager::slot_rendering_begin()
+{
+    assert(m_master_renderer.get());
+
+    if (m_override_shading)
+    {
+        m_master_renderer->get_parameters()
+            .push("shading_engine")
+            .push("override_shading")
+            .insert("mode", m_override_shading_mode);
+    }
+    else
+    {
+        m_master_renderer->get_parameters()
+            .push("shading_engine")
+            .dictionaries().remove("override_shading");
+    }
+
+    const int UpdateRate = 15;
+    m_render_widget_update_timer.start(1000 / UpdateRate, this);
+}
+
+void RenderingManager::slot_rendering_end()
+{
+    m_render_widget_update_timer.stop();
+
+    m_render_widget->update();
+
+    print_rendering_time();
+    print_average_luminance();
+    archive_frame_to_disk();
+
+    // Prevent manipulation of the camera after rendering has ended.
+    m_camera_controller.reset();
+}
+
+void RenderingManager::slot_frame_begin()
+{
+    m_renderer_controller.set_status(IRendererController::ContinueRendering);
+
+    m_camera_controller->update_camera_transform();
+
+    m_rendering_timer.start();
+    m_status_bar.start_rendering_time_display(&m_rendering_timer);
+}
+
+void RenderingManager::slot_frame_end()
+{
+    m_status_bar.stop_rendering_time_display();
+}
+
 void RenderingManager::slot_clear_shading_override()
 {
     m_override_shading = false;
@@ -258,54 +309,6 @@ void RenderingManager::slot_set_shading_override()
 void RenderingManager::slot_camera_changed()
 {
     restart_rendering();
-}
-
-void RenderingManager::slot_rendering_begin()
-{
-    assert(m_master_renderer.get());
-
-    if (m_override_shading)
-    {
-        m_master_renderer->get_parameters()
-            .push("shading_engine")
-            .push("override_shading")
-            .insert("mode", m_override_shading_mode);
-    }
-    else
-    {
-        m_master_renderer->get_parameters()
-            .push("shading_engine")
-            .dictionaries().remove("override_shading");
-    }
-
-    m_rendering_timer.start();
-    m_status_bar.start_rendering_time_display(&m_rendering_timer);
-
-    const int UpdateRate = 15;
-    m_render_widget_update_timer.start(1000 / UpdateRate, this);
-}
-
-void RenderingManager::slot_rendering_end()
-{
-    m_render_widget_update_timer.stop();
-
-    m_status_bar.stop_rendering_time_display();
-
-    m_render_widget->update();
-
-    print_rendering_time();
-    print_average_luminance();
-    archive_frame_to_disk();
-
-    // Prevent manipulation of the camera after rendering has ended.
-    m_camera_controller.reset();
-}
-
-void RenderingManager::slot_frame_begin()
-{
-    m_renderer_controller.set_status(IRendererController::ContinueRendering);
-
-    m_camera_controller->update_camera_transform();
 }
 
 }   // namespace studio

src/appleseed.cli/main.cpp
@@ -337,7 +337,6 @@ namespace
         MasterRenderer renderer(
             project,
             params,
-            MasterRenderer::RenderOnce,
             &renderer_controller);
 
         // Render the frame.

src/appleseed.studio/mainwindow/mainwindow.cpp
@@ -538,15 +538,10 @@ void MainWindow::start_rendering(const bool interactive)
     params.merge(m_settings);
     params.merge(configuration->get_parameters());
 
-    const MasterRenderer::Mode mode =
-        interactive
-            ? MasterRenderer::RenderContinuously
-            : MasterRenderer::RenderOnce;
-
     m_rendering_manager.start_rendering(
         m_project_manager.get_project(),
         params,
-        mode,
+        !interactive,
         m_render_widgets["RGB"]->m_render_widget);
 }
 

src/appleseed.studio/mainwindow/rendering/renderingmanager.h
@@ -70,7 +70,7 @@ class RenderingManager
     void start_rendering(
         renderer::Project*                      project,
         const renderer::ParamArray&             params,
-        const renderer::MasterRenderer::Mode    mode,
+        const bool                              highlight_tiles,
         RenderWidget*                           render_widget);
 
     // Return true if currently rendering, false otherwise.
@@ -122,10 +122,11 @@ class RenderingManager
     void archive_frame_to_disk();
 
   private slots:
-    void slot_camera_changed();
     void slot_rendering_begin();
     void slot_rendering_end();
     void slot_frame_begin();
+    void slot_frame_end();
+    void slot_camera_changed();
 };
 
 }       // namespace studio

src/appleseed/renderer/kernel/rendering/irenderercontroller.h
@@ -64,6 +64,7 @@ class RENDERERDLL IRendererController
     enum Status
     {
         ContinueRendering = 0,
+        TerminateRendering,
         AbortRendering,
         RestartRendering,
         ReinitializeRendering

src/appleseed/renderer/kernel/rendering/masterrenderer.cpp
@@ -81,12 +81,10 @@ namespace renderer
 MasterRenderer::MasterRenderer(
     Project&                project,
     const ParamArray&       params,
-    const Mode              mode,
     IRendererController*    renderer_controller,
     ITileCallbackFactory*   tile_callback_factory)
   : m_project(project)
   , m_params(params)
-  , m_mode(mode)
   , m_renderer_controller(renderer_controller)
   , m_tile_callback_factory(tile_callback_factory)
 {
@@ -129,29 +127,29 @@ void MasterRenderer::do_render()
 {
     while (true)
     {
-        // Notify the rendering controller that rendering is beginning.
         m_renderer_controller->on_rendering_begin();
 
-        // Initialize the rendering components and render until completed or aborted.
-        const IRendererController::Status status = render_from_scratch();
+        const IRendererController::Status status = initialize_and_render_frame_sequence();
 
-        // In non-continuous rendering mode, stop after one frame.
-        if (m_mode == RenderOnce)
+        switch (status)
         {
+          case IRendererController::TerminateRendering:
             m_renderer_controller->on_rendering_success();
-            break;
-        }
+            return;
 
-        // If rendering was aborted, stop.
-        if (status == IRendererController::AbortRendering)
-        {
+          case IRendererController::AbortRendering:
             m_renderer_controller->on_rendering_abort();
+            return;
+
+          case IRendererController::ReinitializeRendering:
             break;
+
+          assert_otherwise;
         }
     }
 }
 
-IRendererController::Status MasterRenderer::render_from_scratch()
+IRendererController::Status MasterRenderer::initialize_and_render_frame_sequence()
 {
     assert(m_project.get_scene());
     assert(m_project.get_frame());
@@ -248,7 +246,58 @@ IRendererController::Status MasterRenderer::render_from_scratch()
     }
 
     // Execute the main rendering loop.
-    return render_until_completed_or_aborted(frame_renderer.get());
+    return render_frame_sequence(frame_renderer.get());
+}
+
+IRendererController::Status MasterRenderer::render_frame_sequence(IFrameRenderer* frame_renderer)
+{
+    while (true) 
+    {
+        assert(!frame_renderer->is_rendering());
+
+        m_renderer_controller->on_frame_begin();
+
+        on_frame_begin();
+
+        const IRendererController::Status status = render_frame(frame_renderer);
+        assert(!frame_renderer->is_rendering());
+
+        on_frame_end();
+
+        m_renderer_controller->on_frame_end();
+
+        switch (status)
+        {
+          case IRendererController::TerminateRendering:
+          case IRendererController::AbortRendering:
+          case IRendererController::ReinitializeRendering:
+            return status;
+
+          case IRendererController::RestartRendering:
+            break;
+
+          assert_otherwise;
+        }
+    }
+}
+
+IRendererController::Status MasterRenderer::render_frame(IFrameRenderer* frame_renderer)
+{
+    frame_renderer->start_rendering();
+
+    while (frame_renderer->is_rendering())
+    {
+        const IRendererController::Status status = m_renderer_controller->on_progress();
+
+        if (status == IRendererController::ContinueRendering)
+            continue;
+
+        frame_renderer->stop_rendering();
+
+        return status;
+    }
+
+    return IRendererController::TerminateRendering;
 }
 
 bool MasterRenderer::bind_inputs() const
@@ -256,62 +305,6 @@ bool MasterRenderer::bind_inputs() const
     InputBinder input_binder;
     input_binder.bind(*m_project.get_scene());
     return input_binder.get_error_count() == 0;
-}
-
-IRendererController::Status MasterRenderer::render_until_completed_or_aborted(
-    IFrameRenderer* frame_renderer)
-{
-    while (true) 
-    {
-        assert(!frame_renderer->is_rendering());
-
-        // Notify the rendering controller that a frame is about to be rendered.
-        m_renderer_controller->on_frame_begin();
-
-        // Perform pre-frame rendering actions.
-        on_frame_begin();
-
-        // Start rendering the frame.
-        frame_renderer->start_rendering();
-
-        // Wait until rendering of the frame is complete.
-        const IRendererController::Status status = wait_until_frame_complete(frame_renderer);
-        assert(!frame_renderer->is_rendering());
-
-        // Perform post-frame rendering actions.
-        on_frame_end();
-
-        // Notify the rendering controller that a frame is finished rendering.
-        m_renderer_controller->on_frame_end();
-
-        // In non-continuous rendering mode, return after one frame.
-        if (m_mode == RenderOnce)
-            return IRendererController::ContinueRendering;
-
-        // If rendering was aborted or reinitialized, return.
-        if (status == IRendererController::AbortRendering ||
-            status == IRendererController::ReinitializeRendering)
-            return status;
-    }
-}
-
-IRendererController::Status MasterRenderer::wait_until_frame_complete(
-    IFrameRenderer* frame_renderer)
-{
-    while (frame_renderer->is_rendering())
-    {
-        // Report progress and retrieve status.
-        const IRendererController::Status status = m_renderer_controller->on_progress();
-
-        // Return if rendering was aborted or restarted.
-        if (status != IRendererController::ContinueRendering)
-        {
-            frame_renderer->stop_rendering();
-            return status;
-        }
-    }
-
-    return IRendererController::ContinueRendering;
 }
 
 namespace

src/appleseed/renderer/kernel/rendering/masterrenderer.h
@@ -49,18 +49,10 @@ class RENDERERDLL MasterRenderer
   : public foundation::NonCopyable
 {
   public:
-    // Rendering modes.
-    enum Mode
-    {
-        RenderOnce = 0,
-        RenderContinuously
-    };
-
     // Constructor.
     MasterRenderer(
         Project&                project,
         const ParamArray&       params,
-        const Mode              mode,
         IRendererController*    renderer_controller,
         ITileCallbackFactory*   tile_callback_factory = 0);
 
@@ -74,24 +66,23 @@ class RENDERERDLL MasterRenderer
   private:
     Project&                    m_project;
     ParamArray                  m_params;
-    const Mode                  m_mode;
     IRendererController*        m_renderer_controller;
     ITileCallbackFactory*       m_tile_callback_factory;
 
-    // Continuously render from scratch.
+    // Render frame sequences, each time reinitializing the rendering components.
     void do_render();
 
-    // Initialize the rendering components and render until completed or aborted.
-    IRendererController::Status render_from_scratch();
+    // Initialize the rendering components and render a frame sequence.
+    IRendererController::Status initialize_and_render_frame_sequence();
+
+    // Render a frame sequence until the sequence is completed or rendering is aborted.
+    IRendererController::Status render_frame_sequence(IFrameRenderer* frame_renderer);
+
+    // Render a frame until until the frame is completed or rendering is aborted.
+    IRendererController::Status render_frame(IFrameRenderer* frame_renderer);
 
     // Bind all scene entities inputs. Return true on success, false otherwise.
     bool bind_inputs() const;
-
-    // Render (one or many frames) until rendering is completed or aborted.
-    IRendererController::Status render_until_completed_or_aborted(IFrameRenderer* frame_renderer);
-
-    // Wait until the frame being rendered is complete, or rendering was aborted.
-    IRendererController::Status wait_until_frame_complete(IFrameRenderer* frame_renderer);
 
     // Perform pre-frame rendering actions.
     void on_frame_begin() const;

commit fbaeb8945e838888c3017e3b19eb7d14f8f6f450

fixed a regression introduced in the previous commit (17fa8f2c4efb7ca4efaaf550b5263ac7a9fb3fdb).
all the connections between a signal in the renderer controller and a slot in the rendering manager must be of type Qt::BlockingQueuedConnection to ensure causality.

src/appleseed.studio/mainwindow/rendering/renderingmanager.cpp
@@ -96,13 +96,28 @@ RenderingManager::RenderingManager(StatusBar& status_bar)
   , m_render_widget(0)
   , m_override_shading(false)
 {
+    //
+    // The connections below are using the Qt::BlockingQueuedConnection connection type.
+    //
+    // They are using a queued connection because the emitting thread is different from
+    // the receiving thread (the emitting thread is the master renderer thread, and the
+    // receiving thread is the UI thread of the main window (presumably).
+    //
+    // They are using a blocking queue connection because we need the receiving slot to
+    // have returned in the receiving thread before the emitting thread can continue.
+    //
+    // See http://doc.trolltech.com/4.6/qt.html#ConnectionType-enum for more details.
+    //
+
     connect(
         &m_renderer_controller, SIGNAL(signal_frame_begin()),
-        this, SLOT(slot_frame_begin()));
+        this, SLOT(slot_frame_begin()),
+        Qt::BlockingQueuedConnection);
 
     connect(
         &m_renderer_controller, SIGNAL(signal_frame_end()),
-        this, SLOT(slot_frame_end()));
+        this, SLOT(slot_frame_end()),
+        Qt::BlockingQueuedConnection);
 
     connect(
         &m_renderer_controller, SIGNAL(signal_rendering_begin()),