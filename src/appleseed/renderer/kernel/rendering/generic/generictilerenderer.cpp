appleseedhq/appleseed

commit caeec82fe7e36ac7aa920f1341975f9c74b89491

changed the definition of normalized device coordinates throughout appleseed.renderer.
until now, normalized device coordinates (NDC) were defined in the domain [-0.5,0.5) x (-0.5,0.5] with (-0.5,-0.5) located in the bottom-left corner in the image and (0.5,0.5) located in the top-right corner. not only was this definition non-symmetrical, but the fact that it considered points with y=0.5 to be part of the domain made it sometimes necessary to clamp y coordinates to prevent them from overflowing.

now, they are simply defined in [0,1)^2 with (0,0) located in the top-left corner of the image and (1,1) located in the bottom-right corner.

src/appleseed/renderer/kernel/rendering/generic/generictilerenderer.cpp
@@ -197,17 +197,17 @@ namespace
                         for (size_t sx = 0; sx < m_sqrt_max_samples; ++sx)
                         {
                             // Compute the sample position in sample space and the instance number.
-                            Vector2d sample_position;
+                            Vector2d s;
                             size_t instance;
                             m_pixel_sampler.sample(
                                 base_sx + sx,
                                 base_sy + sy,
-                                sample_position,
+                                s,
                                 instance);
 
-                            // Transform the sample position from sample space to NDC.
-                            sample_position[0] = sample_position[0] * m_rcp_sample_canvas_width - 0.5;
-                            sample_position[1] = 0.5 - sample_position[1] * m_rcp_sample_canvas_height;
+                            // Compute the sample position in NDC.
+                            const Vector2d sample_position =
+                                frame.get_sample_position(s.x, s.y);
 
                             // Create a sampling context.
                             SamplingContext sampling_context(

src/appleseed/renderer/kernel/rendering/generic/pixelsampler.h
@@ -56,8 +56,7 @@ class PixelSampler
     // Compute the position of a pixel sample and the initial instance number
     // of the corresponding sampling context, given the integer coordinates
     // of the subpixel grid cell containing the sample. The coordinates of the
-    // pixel sample are expressed in sample space, i.e. for a WxH image with
-    // N samples per pixels, in [0..W*N)x[0..H*N).
+    // pixel sample are expressed in sample space (http://appleseedhq.net/conventions).
     void sample(
         const size_t            sx,
         const size_t            sy,

src/appleseed/renderer/kernel/rendering/generic/genericsamplegenerator.cpp
@@ -94,12 +94,10 @@ namespace
             const size_t                    sequence_index,
             SampleVector&                   samples)
         {
-            // Compute the sample coordinates in [0,1)^2.
+            // Compute the sample position in NDC.
             const size_t Bases[2] = { 2, 3 };
-            const Vector2d s = halton_sequence<double, 2>(Bases, sequence_index);
-
-            // Compute the sample position, in NDC.
-            const Vector2d sample_position = m_frame.get_sample_position(s.x, s.y);
+            const Vector2d sample_position =
+                halton_sequence<double, 2>(Bases, sequence_index);
 
             // Create a sampling context.
             SamplingContext sampling_context(

src/appleseed/renderer/kernel/rendering/isamplerenderer.h
@@ -47,7 +47,6 @@ class RENDERERDLL ISampleRenderer
 {
   public:
     // Render a sample at a given point on the image plane.
-    // The point is expressed in normalized device coordinates (NDC).
     // See renderer::Camera class documentation for details.
     virtual void render_sample(
         SamplingContext&                sampling_context,

src/appleseed/renderer/kernel/rendering/lighttracing/lighttracingsamplegenerator.cpp
@@ -175,8 +175,8 @@ namespace
                     m_camera.get_transform().transform_point_to_parent(Vector3d(0.0));
 
                 // Reject vertices not belonging on the image plane of the camera.
-                if (sample_position_ndc[0] < -0.5 || sample_position_ndc[0] > 0.5 ||
-                    sample_position_ndc[1] < -0.5 || sample_position_ndc[1] > 0.5)
+                if (sample_position_ndc[0] < 0.0 || sample_position_ndc[0] >= 1.0 ||
+                    sample_position_ndc[1] < 0.0 || sample_position_ndc[1] >= 1.0)
                     return;
 
                 // Compute the transmission factor between this vertex and the camera.

src/appleseed/renderer/kernel/rendering/progressiveframebuffer.cpp
@@ -95,8 +95,8 @@ void ProgressiveFrameBuffer::store_samples(
 
     while (sample_ptr < sample_end)
     {
-        const double fx = (0.5 + sample_ptr->m_position.x) * fb_width;
-        const double fy = (0.5 - sample_ptr->m_position.y) * fb_height;
+        const double fx = sample_ptr->m_position.x * fb_width;
+        const double fy = sample_ptr->m_position.y * fb_height;
 
         const size_t x = truncate<size_t>(fx);
         const size_t y = truncate<size_t>(fy);

src/appleseed/renderer/modeling/camera/camera.cpp
@@ -174,7 +174,7 @@ void Camera::get_focal_distance(
     Vector2d&           autofocus_target,
     double&             focal_distance) const
 {
-    const Vector2d DefaultAFTarget(0.0);        // in NDC
+    const Vector2d DefaultAFTarget(0.0);        // in [-0.5,0.5)^2
     const double DefaultFocalDistance = 1.0;    // in meters
 
     if (has_param("focal_distance"))

src/appleseed/renderer/modeling/camera/camera.h
@@ -73,14 +73,11 @@ class RENDERERDLL Camera
     virtual void on_frame_end(
         const Project&              project);
 
-    // Generate a ray, originating from a given point on the lens surface of the camera,
-    // and directed toward a given point on the film plane, at a given time.
-    // uv is a point in the unit square [0,1)^2; it is mapped to the surface of the
-    // camera lens. point is the target point on the film plane, expressed in normalized
-    // device coordinates [-0.5,0.5]^2, with (-0.5,-0.5) being at the lower left corner
-    // of the image plane and (0.5,0.5) being at the upper right corner, regardless of
-    // the image resolution. time is the time at which the ray must be generated.
-    // The generated ray is expressed in world space.
+    // Generate a ray originating from a given point on the lens surface of the camera
+    // and directed toward a given point on the film plane, at a given time. point is
+    // the target point on the film plane expressed in normalized device coordinates
+    // (http://appleseedhq.net/conventions). time is the time at which the ray must be
+    // generated. The generated ray is expressed in world space.
     virtual void generate_ray(
         SamplingContext&            sampling_context,
         const foundation::Vector2d& point,              // point on film plane, in NDC

commit 3ca670761eab542bc18ff66a0d30999f8a32260a

fixed a regression introduced in caeec82fe7e36ac7aa920f1341975f9c74b89491 ("changed the definition of normalized device coordinates throughout appleseed.renderer") breaking final frame rendering.

src/appleseed/CMakeLists.txt
@@ -984,6 +984,7 @@ set(renderer_meta_tests_sources
     renderer/meta/tests/test_lightsampler.cpp
     renderer/meta/tests/test_paramarray.cpp
     renderer/meta/tests/test_pinholecamera.cpp
+    renderer/meta/tests/test_pixelsampler.cpp
     renderer/meta/tests/test_projectfilereader.cpp
     renderer/meta/tests/test_samplecounter.cpp
 )

src/appleseed/renderer/kernel/rendering/generic/generictilerenderer.cpp
@@ -86,7 +86,6 @@ namespace
             // Generate pixel ordering.
             vector<size_t> ordering;
             ordering.reserve(num_pixels);
-//          linear_ordering(ordering, num_pixels);
             hilbert_ordering(
                 ordering,
                 properties.m_tile_width,
@@ -105,17 +104,20 @@ namespace
                 m_pixel_ordering[i].y = static_cast<uint16>(y);
             }
 
-            // Initialize the pixel sampler.
-            m_pixel_sampler.initialize(m_params.m_max_samples);
-
             // Compute the approximate size of one side of the subpixel grid inside a pixel.
             m_sqrt_max_samples =
-                static_cast<size_t>(sqrt(
-                    static_cast<double>(m_params.m_max_samples)));
+                round<size_t>(sqrt(static_cast<double>(m_params.m_max_samples)));
+            RENDERER_LOG_INFO(
+                "effective subpixel grid size: " FMT_SIZE_T "x" FMT_SIZE_T,
+                m_sqrt_max_samples,
+                m_sqrt_max_samples);
+
+            // Initialize the pixel sampler.
+            m_pixel_sampler.initialize(m_sqrt_max_samples);
 
             m_rcp_sample_canvas_width = 1.0 / (properties.m_canvas_width * m_sqrt_max_samples);
             m_rcp_sample_canvas_height = 1.0 / (properties.m_canvas_height * m_sqrt_max_samples);
-            m_rcp_sample_count = 1.0f / m_params.m_max_samples;
+            m_rcp_sample_count = 1.0f / (m_sqrt_max_samples * m_sqrt_max_samples);
         }
 
         virtual void release()

src/appleseed/renderer/kernel/rendering/generic/pixelsampler.h
@@ -51,7 +51,9 @@ class PixelSampler
 {
   public:
     // Initialize the pixel sampler for a given number of samples per pixel.
-    void initialize(const size_t sample_count);
+    // Note that the input to this method is actually the square root of
+    // the number of samples per pixel.
+    void initialize(const size_t sqrt_sample_count);
 
     // Compute the position of a pixel sample and the initial instance number
     // of the corresponding sampling context, given the integer coordinates
@@ -64,11 +66,12 @@ class PixelSampler
         size_t&                 initial_instance);
 
   private:
-    double                      m_rcp_period;           // 1.0 / m_period
-    size_t                      m_log_period;           // log2(m_period)
-    size_t                      m_period;               // m_sigma.size()
-    size_t                      m_period_mask;          // m_period - 1
-    std::vector<size_t>         m_sigma;                // 2^N * radical_inverse_base2(0..N-1)
+    double                      m_rcp_sqrt_sample_count;    // 1.0 / sqrt_sample_count
+    double                      m_rcp_period;               // 1.0 / m_period
+    size_t                      m_log_period;               // log2(m_period)
+    size_t                      m_period;                   // m_sigma.size()
+    size_t                      m_period_mask;              // m_period - 1
+    std::vector<size_t>         m_sigma;                    // 2^N * radical_inverse_base2(0..N-1)
 };
 
 
@@ -90,8 +93,8 @@ FORCE_INLINE void PixelSampler::sample(
     initial_instance = (j << m_log_period) + sigma_k;
 
     // Compute the sample coordinates in image space.
-    sample_position[0] = sx + sigma_k * m_rcp_period;
-    sample_position[1] = sy + sigma_j * m_rcp_period;
+    sample_position[0] = (sx + sigma_k * m_rcp_period) * m_rcp_sqrt_sample_count;
+    sample_position[1] = (sy + sigma_j * m_rcp_period) * m_rcp_sqrt_sample_count;
 }
 
 }       // namespace renderer

src/appleseed/renderer/meta/tests/test_pixelsampler.cpp
@@ -26,51 +26,49 @@
 // THE SOFTWARE.
 //
 
-// Interface header.
-#include "pixelsampler.h"
+// appleseed.renderer headers.
+#include "renderer/kernel/rendering/generic/pixelsampler.h"
 
 // appleseed.foundation headers.
-#include "foundation/math/scalar.h"
+#include "foundation/math/vector.h"
+#include "foundation/utility/test.h"
+
+// Standard headers.
+#include <cstddef>
 
 using namespace foundation;
+using namespace renderer;
+using namespace std;
 
-namespace renderer
+TEST_SUITE(Rendering_Generic_PixelSampler)
 {
-
-//
-// PixelSampler class implementation.
-//
-
-void PixelSampler::initialize(const size_t sqrt_sample_count)
-{
-    m_rcp_sqrt_sample_count = 1.0 / sqrt_sample_count;
-
-    const size_t sample_count = sqrt_sample_count * sqrt_sample_count;
-    m_log_period = log2(sample_count * 32);
-
-    if (m_log_period > 16)
-        m_log_period = 16;
-
-    m_period = 1 << m_log_period;
-    m_sigma.resize(m_period);
-
-    // Precompute the first N values of 2^N * radical_inverse_base2(0..N-1).
-    for (size_t i = 0; i < m_period; ++i)
+    struct Fixture
     {
-        size_t b = m_period;
-        size_t x = 0;
+        PixelSampler m_sampler;
 
-        for (size_t n = i; n; n >>= 1)
+        Fixture()
         {
-            b >>= 1;
-            x += (n & 1) * b;
+            // 2x2 samples per pixels.
+            m_sampler.initialize(2);
         }
+    };
 
-        m_sigma[i] = x;
+    TEST_CASE_WITH_FIXTURE(Sample_SubPixelAtTopLeftCorner, Fixture)
+    {
+        Vector2d sample_position;
+        size_t initial_instance;
+        m_sampler.sample(0, 0, sample_position, initial_instance);
+
+        EXPECT_EQ(Vector2d(0.0), sample_position);
     }
 
-    m_rcp_period = 1.0 / m_period;
-    m_period_mask = m_period - 1;
-}
+    TEST_CASE_WITH_FIXTURE(Sample_SubPixelAtBottomRightCorner, Fixture)
+    {
+        // Assume a 32x32 pixels image with 2x2 samples per pixels.
+        Vector2d sample_position;
+        size_t initial_instance;
+        m_sampler.sample(32 * 2 - 1, 32 * 2 - 1, sample_position, initial_instance);
 
-}   // namespace renderer
+        EXPECT_EQ(Vector2d(31.9921875), sample_position);
+    }
+}