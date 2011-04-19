c01a8a508067a310159fa0232e00dcdf71430814
Francois Beaune <beaune@aist.enst.fr>
2011-04-19 11:42:09 +0200

    new component foundation/math/frustum.h.

---
+++ src/appleseed/foundation/math/frustum.h
@@ -0,0 +1,175 @@
+
+//
+// This source file is part of appleseed.
+// Visit http://appleseedhq.net/ for additional information and resources.
+//
+// This software is released under the MIT license.
+//
+// Copyright (c) 2010-2011 Francois Beaune
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+#ifndef APPLESEED_FOUNDATION_MATH_FRUSTUM_H
+#define APPLESEED_FOUNDATION_MATH_FRUSTUM_H
+
+// appleseed.foundation headers.
+#include "foundation/math/vector.h"
+
+// Standard headers.
+#include <cassert>
+#include <cstddef>
+
+namespace foundation
+{
+
+//
+// A view pyramid defined by the intersection of four negative half spaces.
+// Half spaces are defined by the normal vector to their plane. The points
+// inside the planes are considered to be part of the negative half spaces.
+//
+
+template <typename T>
+class Pyramid3
+{
+  public:
+    typedef T ValueType;
+    typedef Vector<T, 3> VectorType;
+
+    enum Plane
+    {
+        TopPlane    = 0,
+        BottomPlane = 1,
+        LeftPlane   = 2,
+        RightPlane  = 3
+    };
+
+    // Set a given plane of the pyramid. n must be normalized.
+    void set_plane(const Plane plane, const VectorType& n);
+
+    // Get a given plane of the pyramid.
+    const VectorType& get_plane(const Plane plane) const;
+
+    // Clip a line segment against a plane. n must be normalized.
+    // Returns false if the line segment was entirely clipped away.
+    static bool clip(
+        const VectorType&   n,
+        VectorType&         a,
+        VectorType&         b);
+
+    // Clip a line segment against the pyramid.
+    // Returns false if the line segment was entirely clipped away.
+    bool clip(VectorType& a, VectorType& b) const;
+
+  private:
+    VectorType  m_planes[4];
+};
+
+
+//
+// Full specializations.
+//
+
+typedef Pyramid3<float> Pyramid3f;
+typedef Pyramid3<double> Pyramid3d;
+
+
+//
+// A view frustum defined by six planes containing the origin.
+// todo: implement. 
+//
+
+template <typename T>
+class Frustum3
+{
+};
+
+
+//
+// Full specializations.
+//
+
+typedef Frustum3<float> Frustum3f;
+typedef Frustum3<double> Frustum3d;
+
+
+//
+// Pyramid3 class implementation.
+//
+
+template <typename T>
+inline void Pyramid3<T>::set_plane(const Plane plane, const VectorType& n)
+{
+    assert(plane < 4);
+    assert(is_normalized(n));
+    m_planes[plane] = n;
+}
+
+template <typename T>
+inline const Vector<T, 3>& Pyramid3<T>::get_plane(const Plane plane) const
+{
+    assert(plane < 4);
+    return m_planes[plane];
+}
+
+template <typename T>
+inline bool Pyramid3<T>::clip(
+    const VectorType&   n,
+    VectorType&         a,
+    VectorType&         b)
+{
+    assert(is_normalized(n));
+
+    const VectorType u = b - a;
+    const ValueType dot_un = dot(u, n);
+    const ValueType dot_an = dot(a, n);
+
+    if (dot_un == ValueType(0.0))
+        return dot_an <= ValueType(0.0);
+
+    const ValueType t = -dot_an / dot_un;
+
+    if (t < ValueType(0.0))
+        return false;
+
+    if (t > ValueType(1.0))
+        return true;
+
+    if (dot_an < ValueType(0.0))
+        b = a + t * u;
+    else a += t * u;
+
+    return true;
+}
+
+template <typename T>
+inline bool Pyramid3<T>::clip(VectorType& a, VectorType& b) const
+{
+    for (size_t i = 0; i < 4; ++i)
+    {
+        if (!clip(m_planes[i], a, b))
+            return false;
+    }
+
+    return true;
+}
+
+}       // namespace foundation
+
+#endif  // !APPLESEED_FOUNDATION_MATH_FRUSTUM_H
---
+++ src/appleseed/foundation/meta/tests/test_frustum.cpp
@@ -0,0 +1,126 @@
+
+//
+// This source file is part of appleseed.
+// Visit http://appleseedhq.net/ for additional information and resources.
+//
+// This software is released under the MIT license.
+//
+// Copyright (c) 2010-2011 Francois Beaune
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+// appleseed.foundation headers.
+#include "foundation/math/frustum.h"
+#include "foundation/math/vector.h"
+#include "foundation/utility/iostreamop.h"
+#include "foundation/utility/test.h"
+
+using namespace foundation;
+
+TEST_SUITE(Foundation_Math_Frustum_Pyramid3)
+{
+    TEST_CASE(Clip_GivenSegmentParallelToPlaneInsideNegativeHalfSpace_ReturnsTrueAndLeavesSegmentUnchanged)
+    {
+        const Vector3d N(1.0, 0.0, 0.0);
+        const Vector3d OriginalA(-1.0, 0.0, +1.0);
+        const Vector3d OriginalB(-1.0, 0.0, -1.0);
+
+        Vector3d a = OriginalA, b = OriginalB;
+        const bool inside = Pyramid3d::clip(N, a, b);
+
+        EXPECT_TRUE(inside);
+        EXPECT_EQ(OriginalA, a);
+        EXPECT_EQ(OriginalB, b);
+    }
+
+    TEST_CASE(Clip_GivenSegmentParallelToPlaneInsidePositiveHalfSpace_ReturnsFalseAndLeavesSegmentUnchanged)
+    {
+        const Vector3d N(1.0, 0.0, 0.0);
+        const Vector3d OriginalA(1.0, 0.0, +1.0);
+        const Vector3d OriginalB(1.0, 0.0, -1.0);
+
+        Vector3d a = OriginalA, b = OriginalB;
+        const bool inside = Pyramid3d::clip(N, a, b);
+
+        EXPECT_FALSE(inside);
+        EXPECT_EQ(OriginalA, a);
+        EXPECT_EQ(OriginalB, b);
+    }
+
+    TEST_CASE(Clip_GivenSegmentFullyInsideNegativeHalfSpace_ReturnsTrueAndLeavesSegmentUnchanged)
+    {
+        const Vector3d N(1.0, 0.0, 0.0);
+        const Vector3d OriginalA(-2.0, 0.0, 0.0);
+        const Vector3d OriginalB(-1.0, 0.0, 0.0);
+
+        Vector3d a = OriginalA, b = OriginalB;
+        const bool inside = Pyramid3d::clip(N, a, b);
+
+        EXPECT_TRUE(inside);
+        EXPECT_EQ(OriginalA, a);
+        EXPECT_EQ(OriginalB, b);
+    }
+
+    TEST_CASE(Clip_GivenSegmentFullyInsidePositiveHalfSpace_ReturnsFalseAndLeavesSegmentUnchanged)
+    {
+        const Vector3d N(1.0, 0.0, 0.0);
+        const Vector3d OriginalA(1.0, 0.0, 0.0);
+        const Vector3d OriginalB(2.0, 0.0, 0.0);
+
+        Vector3d a = OriginalA, b = OriginalB;
+        const bool inside = Pyramid3d::clip(N, a, b);
+
+        EXPECT_FALSE(inside);
+        EXPECT_EQ(OriginalA, a);
+        EXPECT_EQ(OriginalB, b);
+    }
+
+    TEST_CASE(Clip_GivenSegmentStraddlingPlane_ReturnsTrueAndClipSegmentAgainstPlane)
+    {
+        const Vector3d N(1.0, 0.0, 0.0);
+        const Vector3d OriginalA(-1.0, 0.0, 0.0);
+        const Vector3d OriginalB(+1.0, 0.0, 0.0);
+
+        Vector3d a = OriginalA, b = OriginalB;
+        const bool inside = Pyramid3d::clip(N, a, b);
+
+        EXPECT_TRUE(inside);
+        EXPECT_EQ(OriginalA, a);
+        EXPECT_FEQ(Vector3d(0.0, 0.0, 0.0), b);
+    }
+
+    TEST_CASE(Clip_GivenSegment_ReturnsTrueAndClipSegmentAgainstPyramid)
+    {
+        Pyramid3d pyramid;
+        pyramid.set_plane(Pyramid3d::TopPlane, normalize(Vector3d(0.0, 1.0, 1.0)));
+        pyramid.set_plane(Pyramid3d::BottomPlane, normalize(Vector3d(0.0, -1.0, 1.0)));
+        pyramid.set_plane(Pyramid3d::LeftPlane, normalize(Vector3d(-1.0, 0.0, 1.0)));
+        pyramid.set_plane(Pyramid3d::RightPlane, normalize(Vector3d(1.0, 0.0, 1.0)));
+
+        Vector3d a(-3.0, 0.0, -1.0);
+        Vector3d b(+3.0, 0.0, -1.0);
+
+        const bool inside = pyramid.clip(a, b);
+
+        EXPECT_TRUE(inside);
+        EXPECT_EQ(Vector3d(-1.0, 0.0, -1.0), a);
+        EXPECT_FEQ(Vector3d(1.0, 0.0, -1.0), b);
+    }
+}
