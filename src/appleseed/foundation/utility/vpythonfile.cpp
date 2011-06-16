8bdb504f2837d32a91f6a7d16cffffe4ab5c3da4
Francois Beaune <beaune@aist.enst.fr>
2011-06-16 09:00:01 +0200

    new component foundation/utility/vpythonfile.{h,cpp}.

---
+++ src/appleseed/foundation/utility/vpythonfile.cpp
@@ -0,0 +1,110 @@
+
+//
+// This source file is part of appleseed.
+// Visit http://appleseedhq.net/ for additional information and resources.
+//
+// This software is released under the MIT license.
+//
+// Copyright (c) 2010-2011 Francois Beaune
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+// Interface header.
+#include "vpythonfile.h"
+
+// appleseed.foundation headers.
+#include "foundation/platform/types.h"
+
+// Standard headers.
+#include <sstream>
+
+using namespace std;
+
+namespace foundation
+{
+
+//
+// VPythonFile class implementation.
+//
+
+VPythonFile::VPythonFile(const string& filename)
+{
+    m_file = fopen(filename.c_str(), "wt");
+
+    if (m_file == 0)
+        throw ExceptionIOError();
+
+    fprintf(m_file, "from __future__ import division\n");
+    fprintf(m_file, "from visual import *\n");
+}
+
+VPythonFile::~VPythonFile()
+{
+    fclose(m_file);
+}
+
+namespace
+{
+    string points_to_string(const size_t point_count, const Vector3d points[])
+    {
+        stringstream sstr;
+
+        for (size_t i = 0; i < point_count; ++i)
+        {
+            const Vector3d& point = points[i];
+
+            if (i > 0)
+                sstr << ",";
+            
+            sstr << "(" << point.x << "," << point.y << "," << point.z << ")";
+        }
+
+        return sstr.str();
+    }
+}
+
+void VPythonFile::draw_point(
+    const Vector3d&     point,
+    const string&       color,
+    const size_t        size)
+{
+    fprintf(
+        m_file,
+        "points(pos=[(%f,%f,%f)], size=" FMT_SIZE_T ", color=color.%s)",
+        point.x, point.y, point.z,
+        size,
+        color.c_str());
+}
+
+void VPythonFile::draw_points(
+    const size_t        point_count,
+    const Vector3d      points[],
+    const string&       color,
+    const size_t        size)
+{
+    fprintf(
+        m_file,
+        "points(pos=[%s], size=" FMT_SIZE_T ", color=color.%s)",
+        points_to_string(point_count, points).c_str(),
+        size,
+        color.c_str());
+}
+
+}   // namespace foundation
---
+++ src/appleseed/foundation/utility/vpythonfile.h
@@ -0,0 +1,86 @@
+
+//
+// This source file is part of appleseed.
+// Visit http://appleseedhq.net/ for additional information and resources.
+//
+// This software is released under the MIT license.
+//
+// Copyright (c) 2010-2011 Francois Beaune
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+#ifndef APPLESEED_FOUNDATION_UTILITY_VPYTHONFILE_H
+#define APPLESEED_FOUNDATION_UTILITY_VPYTHONFILE_H
+
+// appleseed.foundation headers.
+#include "foundation/core/concepts/noncopyable.h"
+#include "foundation/core/exceptions/exception.h"
+#include "foundation/math/vector.h"
+
+// Standard headers.
+#include <cstddef>
+#include <cstdio>
+#include <string>
+
+namespace foundation
+{
+
+//
+// VPython program file.
+//
+// See http://vpython.org/ for more information about VPython.
+//
+
+class VPythonFile
+  : public NonCopyable
+{
+  public:
+    // Exception thrown when an I/O error occur.
+    struct ExceptionIOError
+      : public Exception
+    {
+    };
+
+    // Constructor, opens the file for writing.
+    explicit VPythonFile(const std::string& filename);
+
+    // Destructor, closes the file.
+    ~VPythonFile();
+
+    // Output a single point.
+    void draw_point(
+        const Vector3d&     point,
+        const std::string&  color = "white",
+        const size_t        size = 5);
+
+    // Output a collection of points.
+    void draw_points(
+        const size_t        point_count,
+        const Vector3d      points[],
+        const std::string&  color = "white",
+        const size_t        size = 5);
+
+  private:
+    std::FILE* m_file;
+};
+
+}       // namespace foundation
+
+#endif  // !APPLESEED_FOUNDATION_UTILITY_VPYTHONFILE_H
