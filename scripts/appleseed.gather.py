0fa54a6d608f7200d38e9720a7ab2a558fb9159d
Francois Beaune <beaune@aist.enst.fr>
2011-07-17 13:50:04 +0200

    appleseed.gather version 1.2.
    - detect and report unexpected dependencies.
    - include relative file paths in output.
    - don't include files that end up being empty after stripping (but report them)
    - apply custom stripping commands just before scanning a file for immediate dependencies.

--- scripts/appleseed.gather.py
+++ scripts/appleseed.gather.py
@@ -28,7 +28,7 @@
 #
 
 # Settings.
-VersionString = "1.1"
+VersionString = "1.2"
 
 # Imports.
 import glob
@@ -45,14 +45,33 @@ from xml.etree.ElementTree import ElementTree
 
 NEWLINE_REGEX = r"[\r\n]+"
 
+def info(message):
+    print("INFO: {0}".format(message))
+
+def warning(message):
+    print("WARNING: {0}".format(message))
+
 def fatal(message):
-    print
-    print("FATAL: " + message + ", aborting.")
+    print("\nFATAL: {0}, aborting.".format(message))
     sys.exit(1)
 
 def safe_make_directory(path):
     if not os.path.isdir(path):
         os.makedirs(path)
+
+def back_to_forward_slashes(path):
+    return path.replace('\\', '/')
+
+def make_relative_path(file_path, base):
+    npath = os.path.normpath(os.path.normcase(file_path))
+    nbase = os.path.normpath(os.path.normcase(base))
+    if npath.startswith(nbase):
+        result = npath[len(nbase):]
+        if result.startswith("/") or result.startswith("\\"):
+            result = result[1:]
+        return result
+    else:
+        return file_path
 
 def stable_unique(seq):
     seen = {}
@@ -79,7 +98,7 @@ def load_file(file_path):
 
 class Manifest:
     def __init__(self, manifest_path):
-        print "Loading manifest file '" + manifest_path + "'..."
+        print("Loading manifest file '" + manifest_path + "'...")
 
         try:
             tree = ElementTree()
@@ -95,6 +114,8 @@ class Manifest:
         for files_element in tree.findall("input/files"):
             file_mask = os.path.join(self.input_root_path, files_element.text)
             self.input_file_paths.extend(glob.glob(file_mask))
+        for i in range(len(self.input_file_paths)):
+            self.input_file_paths[i] = back_to_forward_slashes(self.input_file_paths[i])
 
         # Extract the output base path and base filename.
         self.output_base_path = tree.find("output/base_path").text
@@ -123,6 +144,16 @@ class Manifest:
     def get_output_source_file_path(self):
         return os.path.join(self.output_base_path, self.output_base_filename + ".cpp")
 
+    def strip_custom_literals(self, text):
+        for literal in self.literals_to_strip:
+            text = text.replace(literal, "")
+        return text
+
+    def strip_custom_regexes(self, text):
+        for regex in self.regexes_to_strip:
+            text = re.sub(regex, "", text, 0, re.MULTILINE)
+        return text
+
 
 #
 # DependencyFinder class.
@@ -139,9 +170,6 @@ class DependencyFinder:
             if os.path.splitext(file_path)[1].lower() == ".h":
                 files.extend(self.__gather_recursive_user_deps(file_path))
 
-        for i in range(len(files)):
-            files[i] = files[i].replace('\\', '/')
-
         return stable_unique(files)
 
     def gather_source_files(self):
@@ -150,9 +178,6 @@ class DependencyFinder:
         for file_path in self.manifest.input_file_paths:
             if os.path.splitext(file_path)[1].lower() == ".cpp":
                 files.append(file_path)
-
-        for i in range(len(files)):
-            files[i] = files[i].replace('\\', '/')
 
         return files
 
@@ -173,7 +198,7 @@ class DependencyFinder:
             sub_dep_file_path = os.path.join(self.manifest.input_root_path, sub_dep)
             deps.extend(self.__gather_recursive_user_deps(sub_dep_file_path))
 
-        deps.append(file_path)
+        deps.append(back_to_forward_slashes(file_path))
 
         return deps
 
@@ -185,6 +210,8 @@ class DependencyFinder:
 
     def __gather_immediate_deps(self, file_path, opening_marker, closing_marker):
         text = load_file(file_path)
+        text = self.manifest.strip_custom_literals(text)
+        text = self.manifest.strip_custom_regexes(text)
 
         deps = []
         pattern = re.compile(r"^#include " + opening_marker +
@@ -210,86 +237,97 @@ class FileGenerator:
 
     def generate_output_header_file(self):
         output_file_path = self.manifest.get_output_header_file_path()
-        print "Generating '" + output_file_path + "'..."
+        print("Generating '" + output_file_path + "'...")
+
+        header_files = self.depfinder.gather_header_files()
+
+        # Report unexpected dependencies.
+        for header_file in header_files:
+            if header_file not in self.manifest.input_file_paths:
+                warning("unexpected dependency: {0}".format(header_file))
 
         # Open file for writing.
         safe_make_directory(self.manifest.output_base_path)
         output_file = open(output_file_path, "w")
 
-        # Write header.
+        # Write the file header.
         output_file.write(self.manifest.header)
 
-        # Generate the header guard.
+        # Generate and write the header guard.
         header_guard_token = os.path.basename(output_file_path).replace("/", "_").replace("\\", "_").replace(".", "_").upper()
         output_file.write("#ifndef " + header_guard_token + "\n")
         output_file.write("#define " + header_guard_token + "\n\n")
 
-        # Include all the platform headers.
-        header_files = self.depfinder.gather_header_files()
-        platform_deps = self.depfinder.gather_platform_deps(header_files)
-        if len(platform_deps) > 0:
-            output_file.write("// Standard and platform headers.\n")
-            for dep in platform_deps:
-                output_file.write("#include <" + dep + ">\n")
-            output_file.write("\n")
+        # Include all required platform headers.
+        self.__write_platform_headers(output_file, self.depfinder.gather_platform_deps(header_files))
 
         # Concatenate the content of all header files.
         self.__write_files(output_file, header_files)
 
         # Close the header guard.
-        output_file.write("#endif  // !" + header_guard_token + "\n")
+        output_file.write("\n#endif  // !" + header_guard_token + "\n")
 
         output_file.close()
 
     def generate_output_source_file(self):
         output_file_path = self.manifest.get_output_source_file_path()
-        print "Generating '" + output_file_path + "'..."
+        print("Generating '" + output_file_path + "'...")
+
+        source_files = self.depfinder.gather_source_files()
 
         # Open file for writing.
         safe_make_directory(self.manifest.output_base_path)
         output_file = open(output_file_path, "w")
 
-        # Write header.
+        # Write the file header.
         output_file.write(self.manifest.header)
 
-        # Include the header file.
+        # Include the interface header file.
         output_file.write("// Interface header.\n")
         output_file.write("#include \"" + self.manifest.output_base_filename + ".h\"\n\n")
 
-        # Include all the platform headers.
-        source_files = self.depfinder.gather_source_files()
-        platform_deps = self.depfinder.gather_platform_deps(source_files)
-        if len(platform_deps) > 0:
-            output_file.write("// Standard and platform headers.\n")
-            for dep in platform_deps:
-                output_file.write("#include <" + dep + ">\n")
-            output_file.write("\n")
+        # Include all required platform headers.
+        self.__write_platform_headers(output_file, self.depfinder.gather_platform_deps(source_files))
 
         # Concatenate the content of all source files.
         self.__write_files(output_file, source_files)
 
         output_file.close()
 
+    def __write_platform_headers(self, output_file, headers):
+        if len(headers) > 0:
+            output_file.write("// Standard and platform headers.\n")
+            for header in headers:
+                output_file.write("#include <" + header + ">\n")
+
     def __write_files(self, output_file, input_file_paths):
         for input_file_path in input_file_paths:
             text = load_file(input_file_path)
             text = self.__strip_dependencies(text)
-            text = self.__strip_custom_literals(text)
-            text = self.__strip_custom_regexes(text)
+            text = self.manifest.strip_custom_literals(text)
+            text = self.manifest.strip_custom_regexes(text)
+            text = text.strip("\n")
+                
+            if len(text) == 0:
+                info("file {0} is empty after stripping and will be omitted from the output.".format(input_file_path))
+                continue
+
+            text = text + "\n"
+
+            relative_path = make_relative_path(input_file_path, self.manifest.input_root_path)
+            relative_path = back_to_forward_slashes(relative_path)
+
+            Width = 80
+            output_file.write("\n\n")
+            output_file.write("// {0}\n".format("*" * (Width - 3)))
+            output_file.write("// ***  {0}{1}  ***\n".format(relative_path, " " * (Width - len(relative_path) - 13)))
+            output_file.write("// {0}\n".format("*" * (Width - 3)))
+            output_file.write("\n")
+            
             output_file.write(text)
 
     def __strip_dependencies(self, text):
         return re.sub(r"^#include .*" + NEWLINE_REGEX, "", text, 0, re.MULTILINE)
-
-    def __strip_custom_literals(self, text):
-        for literal in self.manifest.literals_to_strip:
-            text = text.replace(literal, "")
-        return text
-
-    def __strip_custom_regexes(self, text):
-        for regex in self.manifest.regexes_to_strip:
-            text = re.sub(regex, "", text, 0, re.MULTILINE)
-        return text
 
 
 #
@@ -301,7 +339,7 @@ class Tester:
         self.manifest = manifest
 
     def compile(self):
-        print "Test-compiling...\n"
+        print("Test-compiling...")
         for command_line in self.manifest.test_command_lines:
             command_line = command_line.replace("$OutputBasePath", self.manifest.output_base_path)
             command_line = command_line.replace("$OutputHeaderFilePath", self.manifest.get_output_header_file_path())
@@ -314,10 +352,10 @@ class Tester:
 #
 
 def main():
-    print "appleseed.gather version " + VersionString
+    print("appleseed.gather version " + VersionString)
     
     if len(sys.argv) < 2:
-        print "Usage: " + sys.argv[0] + " manifest.xml"
+        print("Usage: " + sys.argv[0] + " manifest.xml")
         sys.exit(1)
 
     manifest = Manifest(sys.argv[1])
