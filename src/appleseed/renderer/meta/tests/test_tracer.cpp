appleseedhq/appleseed

commit 6cf378b93f370a55f446cd1886b5f2082c8e2bf6

properly handle alpha masking in direct lighting computations.

src/appleseed/renderer/kernel/lighting/directlighting.cpp
@@ -30,9 +30,8 @@
 #include "directlighting.h"
 
 // appleseed.renderer headers.
-#include "renderer/kernel/intersection/intersector.h"
 #include "renderer/kernel/lighting/lightsampler.h"
-#include "renderer/kernel/lighting/transmission.h"
+#include "renderer/kernel/lighting/tracer.h"
 #include "renderer/kernel/shading/shadingcontext.h"
 #include "renderer/kernel/shading/shadingpoint.h"
 #include "renderer/modeling/bsdf/bsdf.h"
@@ -104,15 +103,20 @@ void compute_direct_lighting_bsdf_sampling(
             continue;
 
         // Trace a ray in the direction of the reflection.
-        const ShadingRay light_ray(point, incoming, 0.0f, ~0);
-        ShadingPoint light_shading_point;
-        shading_context.get_intersector().trace(
-            light_ray,
-            light_shading_point,
-            parent_shading_point);
+        Tracer tracer(
+            shading_context.get_intersector(),
+            shading_context.get_texture_cache(),
+            sampling_context);
+        const ShadingPoint& light_shading_point =
+            tracer.trace(point, incoming, parent_shading_point);
 
         // todo: wouldn't it be more efficient to look the environment up at this point?
         if (!light_shading_point.hit())
+            continue;
+
+        // Discard occluded samples.
+        double weight = tracer.get_transmission();
+        if (weight == 0.0)
             continue;
 
         // Retrieve the material at the intersection point.
@@ -124,9 +128,6 @@ void compute_direct_lighting_bsdf_sampling(
         const EDF* edf = material->get_edf();
         if (edf == 0)
             continue;
-
-        // todo: handle alpha masking.
-        double weight = 1.0;
 
         // Evaluate the input values of the EDF.
         InputEvaluator edf_input_evaluator(shading_context.get_texture_cache());
@@ -234,13 +235,12 @@ void compute_direct_lighting_light_sampling(
         cos_on *= rcp_sample_distance;
 
         // Compute the transmission factor between the light sample and the shading point.
-        const double transmission =
-            compute_transmission_between(
-                sampling_context,
-                shading_context,
-                point,
-                sample.m_input_params.m_point,
-                parent_shading_point);
+        Tracer tracer(
+            shading_context.get_intersector(),
+            shading_context.get_texture_cache(),
+            sampling_context);
+        tracer.trace_between(point, sample.m_input_params.m_point, parent_shading_point);
+        const double transmission = tracer.get_transmission();
 
         // Discard occluded samples.
         if (transmission == 0.0)
@@ -357,15 +357,20 @@ void compute_direct_lighting_single_sample(
             return;
 
         // Trace a ray in the direction of the reflection.
-        const ShadingRay light_ray(point, incoming, 0.0f, ~0);
-        ShadingPoint light_shading_point;
-        shading_context.get_intersector().trace(
-            light_ray,
-            light_shading_point,
-            parent_shading_point);
+        Tracer tracer(
+            shading_context.get_intersector(),
+            shading_context.get_texture_cache(),
+            sampling_context);
+        const ShadingPoint& light_shading_point =
+            tracer.trace(point, incoming, parent_shading_point);
 
         // todo: wouldn't it be more efficient to look the environment up at this point?
         if (!light_shading_point.hit())
+            return;
+
+        // Discard occluded samples.
+        double weight = tracer.get_transmission();
+        if (weight == 0.0)
             return;
 
         // Retrieve the material at the intersection point.
@@ -377,9 +382,6 @@ void compute_direct_lighting_single_sample(
         const EDF* edf = material->get_edf();
         if (edf == 0)
             return;
-
-        // todo: handle alpha masking.
-        double weight = 1.0;
 
         // Evaluate the input values of the EDF.
         InputEvaluator edf_input_evaluator(shading_context.get_texture_cache());
@@ -455,13 +457,12 @@ void compute_direct_lighting_single_sample(
         cos_on *= rcp_sample_distance;
 
         // Compute the transmission factor between the light sample and the shading point.
-        const double transmission =
-            compute_transmission_between(
-                sampling_context,
-                shading_context,
-                point,
-                sample.m_input_params.m_point,
-                parent_shading_point);
+        Tracer tracer(
+            shading_context.get_intersector(),
+            shading_context.get_texture_cache(),
+            sampling_context);
+        tracer.trace_between(point, sample.m_input_params.m_point, parent_shading_point);
+        const double transmission = tracer.get_transmission();
 
         // Discard occluded samples.
         if (transmission == 0.0)

src/appleseed/renderer/kernel/lighting/imagebasedlighting.cpp
@@ -30,8 +30,7 @@
 #include "imagebasedlighting.h"
 
 // appleseed.renderer headers.
-#include "renderer/kernel/intersection/intersector.h"
-#include "renderer/kernel/lighting/transmission.h"
+#include "renderer/kernel/lighting/tracer.h"
 #include "renderer/kernel/shading/shadingcontext.h"
 #include "renderer/kernel/shading/shadingpoint.h"
 #include "renderer/modeling/bsdf/bsdf.h"
@@ -107,13 +106,12 @@ namespace
                 continue;
 
             // Compute the transmission factor toward the incoming direction.
-            const double transmission =
-                compute_transmission(
-                    sampling_context,
-                    shading_context,
-                    point,
-                    incoming,
-                    parent_shading_point);
+            Tracer tracer(
+                shading_context.get_intersector(),
+                shading_context.get_texture_cache(),
+                sampling_context);
+            tracer.trace(point, incoming, parent_shading_point);
+            const double transmission = tracer.get_transmission();
 
             // Discard occluded samples.
             if (transmission == 0.0)
@@ -195,13 +193,12 @@ namespace
                 continue;
 
             // Compute the transmission factor toward the incoming direction.
-            const double transmission =
-                compute_transmission(
-                    sampling_context,
-                    shading_context,
-                    point,
-                    incoming,
-                    parent_shading_point);
+            Tracer tracer(
+                shading_context.get_intersector(),
+                shading_context.get_texture_cache(),
+                sampling_context);
+            tracer.trace(point, incoming, parent_shading_point);
+            const double transmission = tracer.get_transmission();
 
             // Discard occluded samples.
             if (transmission == 0.0)

commit c64b4b14bb816bda24bdeb879daf0dbd03915e90

fixed regression introduced in commit 6cf378b93f370a55f446cd1886b5f2082c8e2bf6: don't take into account the transmission of the fully opaque occluder (which is obviously 0).
also fixed the unit tests for renderer::Tracer.

src/appleseed/renderer/kernel/lighting/directlighting.cpp
@@ -107,16 +107,16 @@ void compute_direct_lighting_bsdf_sampling(
             shading_context.get_intersector(),
             shading_context.get_texture_cache(),
             sampling_context);
+        double weight;
         const ShadingPoint& light_shading_point =
-            tracer.trace(point, incoming, parent_shading_point);
+            tracer.trace(
+                point,
+                incoming,
+                weight,
+                parent_shading_point);
 
         // todo: wouldn't it be more efficient to look the environment up at this point?
         if (!light_shading_point.hit())
-            continue;
-
-        // Discard occluded samples.
-        double weight = tracer.get_transmission();
-        if (weight == 0.0)
             continue;
 
         // Retrieve the material at the intersection point.
@@ -239,11 +239,16 @@ void compute_direct_lighting_light_sampling(
             shading_context.get_intersector(),
             shading_context.get_texture_cache(),
             sampling_context);
-        tracer.trace_between(point, sample.m_input_params.m_point, parent_shading_point);
-        const double transmission = tracer.get_transmission();
+        double transmission;
+        const ShadingPoint& shading_point =
+            tracer.trace_between(
+                point,
+                sample.m_input_params.m_point,
+                transmission,
+                parent_shading_point);
 
         // Discard occluded samples.
-        if (transmission == 0.0)
+        if (shading_point.hit())
             continue;
 
         // Evaluate the BSDF.
@@ -361,16 +366,16 @@ void compute_direct_lighting_single_sample(
             shading_context.get_intersector(),
             shading_context.get_texture_cache(),
             sampling_context);
+        double weight;
         const ShadingPoint& light_shading_point =
-            tracer.trace(point, incoming, parent_shading_point);
+            tracer.trace(
+                point,
+                incoming,
+                weight,
+                parent_shading_point);
 
         // todo: wouldn't it be more efficient to look the environment up at this point?
         if (!light_shading_point.hit())
-            return;
-
-        // Discard occluded samples.
-        double weight = tracer.get_transmission();
-        if (weight == 0.0)
             return;
 
         // Retrieve the material at the intersection point.
@@ -461,11 +466,16 @@ void compute_direct_lighting_single_sample(
             shading_context.get_intersector(),
             shading_context.get_texture_cache(),
             sampling_context);
-        tracer.trace_between(point, sample.m_input_params.m_point, parent_shading_point);
-        const double transmission = tracer.get_transmission();
+        double transmission;
+        const ShadingPoint& shading_point =
+            tracer.trace_between(
+                point,
+                sample.m_input_params.m_point,
+                transmission,
+                parent_shading_point);
 
         // Discard occluded samples.
-        if (transmission == 0.0)
+        if (shading_point.hit())
             return;
 
         // Evaluate the BSDF.

src/appleseed/renderer/kernel/lighting/imagebasedlighting.cpp
@@ -110,11 +110,16 @@ namespace
                 shading_context.get_intersector(),
                 shading_context.get_texture_cache(),
                 sampling_context);
-            tracer.trace(point, incoming, parent_shading_point);
-            const double transmission = tracer.get_transmission();
+            double transmission;
+            const ShadingPoint& shading_point =
+                tracer.trace(
+                    point,
+                    incoming,
+                    transmission,
+                    parent_shading_point);
 
             // Discard occluded samples.
-            if (transmission == 0.0)
+            if (shading_point.hit())
                 continue;
 
             // Evaluate the environment's EDF.
@@ -197,11 +202,16 @@ namespace
                 shading_context.get_intersector(),
                 shading_context.get_texture_cache(),
                 sampling_context);
-            tracer.trace(point, incoming, parent_shading_point);
-            const double transmission = tracer.get_transmission();
+            double transmission;
+            const ShadingPoint& shading_point =
+                tracer.trace(
+                    point,
+                    incoming,
+                    transmission,
+                    parent_shading_point);
 
             // Discard occluded samples.
-            if (transmission == 0.0)
+            if (shading_point.hit())
                 continue;
 
             // Evaluate the BSDF.

src/appleseed/renderer/kernel/lighting/tracer.cpp
@@ -47,20 +47,23 @@ namespace renderer
 const ShadingPoint& Tracer::trace(
     const Vector3d&         origin,
     const Vector3d&         direction,
+    double&                 transmission,
     const ShadingPoint*     parent_shading_point)
 {
+    transmission = 1.0;
+
     const ShadingPoint* shading_point_ptr = parent_shading_point;
     size_t shading_point_index = 0;
     Vector3d point = origin;
 
-    do
+    while (true)
     {
         // Construct the visibility ray.
         const ShadingRay ray(
             point,
             direction,
-            0.0f,                   // ray time
-            ~0);                    // ray flags
+            0.0f,           // ray time
+            ~0);            // ray flags
 
         // Trace the ray.
         m_shading_points[shading_point_index].clear();
@@ -80,12 +83,9 @@ const ShadingPoint& Tracer::trace(
         // Retrieve the material at the shading point.
         const Material* material = shading_point_ptr->get_material();
 
-        // Return full occlusion if the surface has no material.
+        // Stop if the surface has no material.
         if (material == 0)
-        {
-            m_transmission = 0.0;
             break;
-        }
 
         // Retrieve the surface shader.
         const SurfaceShader& surface_shader = material->get_surface_shader();
@@ -98,13 +98,16 @@ const ShadingPoint& Tracer::trace(
             *shading_point_ptr,
             alpha_mask);
 
+        // Stop as soon as we reach a fully opaque occluder.
+        if (alpha_mask[0] == 1.0f)
+            break;
+
         // Update the transmission factor.
-        m_transmission *= 1.0 - static_cast<double>(alpha_mask[0]);
+        transmission *= 1.0 - static_cast<double>(alpha_mask[0]);
 
         // Move past this partial occluder.
         point = shading_point_ptr->get_point();
     }
-    while (m_transmission > 0.0);
 
     return *shading_point_ptr;
 }
@@ -112,17 +115,20 @@ const ShadingPoint& Tracer::trace(
 const ShadingPoint& Tracer::trace_between(
     const Vector3d&         origin,
     const Vector3d&         target,
+    double&                 transmission,
     const ShadingPoint*     parent_shading_point)
 {
     // todo: get rid of this epsilon.
     const double Eps = 1.0e-6;
     const double SafeMaxDistance = 1.0 - Eps;
 
+    transmission = 1.0;
+
     const ShadingPoint* shading_point_ptr = parent_shading_point;
     size_t shading_point_index = 0;
     Vector3d point = origin;
 
-    do
+    while (true)
     {
         // Construct the visibility ray.
         const ShadingRay ray(
@@ -151,12 +157,9 @@ const ShadingPoint& Tracer::trace_between(
         // Retrieve the material at the shading point.
         const Material* material = shading_point_ptr->get_material();
 
-        // Return full occlusion if the surface has no material.
+        // Stop if the surface has no material.
         if (material == 0)
-        {
-            m_transmission = 0.0;
             break;
-        }
 
         // Retrieve the surface shader.
         const SurfaceShader& surface_shader = material->get_surface_shader();
@@ -169,13 +172,16 @@ const ShadingPoint& Tracer::trace_between(
             *shading_point_ptr,
             alpha_mask);
 
+        // Stop as soon as we reach a fully opaque occluder.
+        if (alpha_mask[0] == 1.0f)
+            break;
+
         // Update the transmission factor.
-        m_transmission *= 1.0 - static_cast<double>(alpha_mask[0]);
+        transmission *= 1.0 - static_cast<double>(alpha_mask[0]);
 
         // Move past this partial occluder.
         point = shading_point_ptr->get_point();
     }
-    while (m_transmission > 0.0);
 
     return *shading_point_ptr;
 }

src/appleseed/renderer/kernel/lighting/tracer.h
@@ -54,27 +54,31 @@ class Tracer
         TextureCache&                   texture_cache,
         SamplingContext&                sampling_context);
 
-    // Compute occlusion in a given direction.
+    // Compute the transmission in a given direction. Returns the intersection
+    // with the closest fully opaque occluder and the transmission factor up
+    // to (but excluding) this occluder, or a miss if there is no fully opaque
+    // occluder in this direction.
     const ShadingPoint& trace(
         const foundation::Vector3d&     origin,
         const foundation::Vector3d&     direction,
+        double&                         transmission,
         const ShadingPoint*             parent_shading_point = 0);
 
-    // Compute occlusion between two points.
+    // Compute the transmission between two points. Returns the intersection
+    // with the closest fully opaque occluder and the transmission factor up
+    // to (but excluding) this occluder, or a miss if there is no fully opaque
+    // occluder in the segment [origin, target).
     const ShadingPoint& trace_between(
         const foundation::Vector3d&     origin,
         const foundation::Vector3d&     target,
+        double&                         transmission,
         const ShadingPoint*             parent_shading_point = 0);
-
-    // Return the transmission factor between the origin and the closest opaque occluder.
-    double get_transmission() const;
 
   private:
     const Intersector&                  m_intersector;
     TextureCache&                       m_texture_cache;
     SamplingContext&                    m_sampling_context;
     ShadingPoint                        m_shading_points[2];
-    double                              m_transmission;
 };
 
 
@@ -89,13 +93,7 @@ inline Tracer::Tracer(
   : m_intersector(intersector)
   , m_texture_cache(texture_cache)
   , m_sampling_context(sampling_context)
-  , m_transmission(1.0)
 {
-}
-
-inline double Tracer::get_transmission() const
-{
-    return m_transmission;
 }
 
 }       // namespace renderer

src/appleseed/renderer/kernel/rendering/lighttracing/lighttracingsamplegenerator.cpp
@@ -373,11 +373,14 @@ namespace
                     m_shading_context.get_intersector(),
                     m_shading_context.get_texture_cache(),
                     sampling_context);
-                tracer.trace_between(m_camera_position, vertex_position_world);
-                transmission = tracer.get_transmission();
+                const ShadingPoint& shading_point =
+                    tracer.trace_between(
+                        m_camera_position,
+                        vertex_position_world,
+                        transmission);
 
                 // Reject vertices not directly visible from the camera.
-                if (transmission == 0.0)
+                if (shading_point.hit())
                     return false;
 
                 // Compute the vertex-to-camera direction vector.

src/appleseed/renderer/meta/tests/test_tracer.cpp
@@ -210,20 +210,28 @@ TEST_SUITE(Renderer_Kernel_Lighting_Tracer)
 
     TEST_CASE_F(Trace_GivenOriginAndDirection_NoOccluder, Fixture<EmptyScene>)
     {
+        double transmission;
         const ShadingPoint& shading_point =
-            m_tracer.trace(Vector3d(0.0), Vector3d(1.0, 0.0, 0.0));
+            m_tracer.trace(
+                Vector3d(0.0),
+                Vector3d(1.0, 0.0, 0.0),
+                transmission);
 
         EXPECT_FALSE(shading_point.hit());
-        EXPECT_EQ(1.0, m_tracer.get_transmission());
+        EXPECT_EQ(1.0, transmission);
     }
 
     TEST_CASE_F(TraceBetween_GivenOriginAndTarget_NoOccluder, Fixture<EmptyScene>)
     {
+        double transmission;
         const ShadingPoint& shading_point =
-            m_tracer.trace_between(Vector3d(0.0, 0.0, 0.0), Vector3d(5.0, 0.0, 0.0));
+            m_tracer.trace_between(
+                Vector3d(0.0, 0.0, 0.0),
+                Vector3d(5.0, 0.0, 0.0),
+                transmission);
 
         EXPECT_FALSE(shading_point.hit());
-        EXPECT_EQ(1.0, m_tracer.get_transmission());
+        EXPECT_EQ(1.0, transmission);
     }
 
     struct SceneWithSingleOpaqueOccluder
@@ -237,22 +245,30 @@ TEST_SUITE(Renderer_Kernel_Lighting_Tracer)
 
     TEST_CASE_F(Trace_GivenOriginAndDirection_SingleOpaqueOccluder, Fixture<SceneWithSingleOpaqueOccluder>)
     {
+        double transmission;
         const ShadingPoint& shading_point =
-            m_tracer.trace(Vector3d(0.0), Vector3d(1.0, 0.0, 0.0));
+            m_tracer.trace(
+                Vector3d(0.0, 0.0, 0.0),
+                Vector3d(1.0, 0.0, 0.0),
+                transmission);
 
         ASSERT_TRUE(shading_point.hit());
         EXPECT_FEQ(Vector3d(2.0, 0.0, 0.0), shading_point.get_point());
-        EXPECT_EQ(0.0, m_tracer.get_transmission());
+        EXPECT_EQ(1.0, transmission);
     }
 
     TEST_CASE_F(TraceBetween_GivenOriginAndTarget_SingleOpaqueOccluder, Fixture<SceneWithSingleOpaqueOccluder>)
     {
+        double transmission;
         const ShadingPoint& shading_point =
-            m_tracer.trace_between(Vector3d(0.0, 0.0, 0.0), Vector3d(5.0, 0.0, 0.0));
+            m_tracer.trace_between(
+                Vector3d(0.0, 0.0, 0.0),
+                Vector3d(5.0, 0.0, 0.0),
+                transmission);
 
         ASSERT_TRUE(shading_point.hit());
         EXPECT_FEQ(Vector3d(2.0, 0.0, 0.0), shading_point.get_point());
-        EXPECT_EQ(0.0, m_tracer.get_transmission());
+        EXPECT_EQ(1.0, transmission);
     }
 
     struct SceneWithSingleTransparentOccluder
@@ -266,20 +282,28 @@ TEST_SUITE(Renderer_Kernel_Lighting_Tracer)
 
     TEST_CASE_F(Trace_GivenOriginAndDirection_SingleTransparentOccluder, Fixture<SceneWithSingleTransparentOccluder>)
     {
+        double transmission;
         const ShadingPoint& shading_point =
-            m_tracer.trace(Vector3d(0.0), Vector3d(1.0, 0.0, 0.0));
+            m_tracer.trace(
+                Vector3d(0.0, 0.0, 0.0),
+                Vector3d(1.0, 0.0, 0.0),
+                transmission);
 
         ASSERT_FALSE(shading_point.hit());
-        EXPECT_FEQ(0.5, m_tracer.get_transmission());
+        EXPECT_FEQ(0.5, transmission);
     }
 
     TEST_CASE_F(TraceBetween_GivenOriginAndTarget_SingleTransparentOccluder, Fixture<SceneWithSingleTransparentOccluder>)
     {
+        double transmission;
         const ShadingPoint& shading_point =
-            m_tracer.trace_between(Vector3d(0.0, 0.0, 0.0), Vector3d(5.0, 0.0, 0.0));
+            m_tracer.trace_between(
+                Vector3d(0.0, 0.0, 0.0),
+                Vector3d(5.0, 0.0, 0.0),
+                transmission);
 
         ASSERT_FALSE(shading_point.hit());
-        EXPECT_FEQ(0.5, m_tracer.get_transmission());
+        EXPECT_FEQ(0.5, transmission);
     }
 
     struct SceneWithTransparentThenOpaqueOccluders
@@ -294,30 +318,42 @@ TEST_SUITE(Renderer_Kernel_Lighting_Tracer)
 
     TEST_CASE_F(Trace_GivenOriginAndDirection_TransparentThenOpaqueOccluders, Fixture<SceneWithTransparentThenOpaqueOccluders>)
     {
+        double transmission;
         const ShadingPoint& shading_point =
-            m_tracer.trace(Vector3d(0.0), Vector3d(1.0, 0.0, 0.0));
+            m_tracer.trace(
+                Vector3d(0.0, 0.0, 0.0),
+                Vector3d(1.0, 0.0, 0.0),
+                transmission);
 
         ASSERT_TRUE(shading_point.hit());
         EXPECT_FEQ(Vector3d(4.0, 0.0, 0.0), shading_point.get_point());
-        EXPECT_FEQ(0.0, m_tracer.get_transmission());
+        EXPECT_FEQ(0.5, transmission);
     }
 
     TEST_CASE_F(TraceBetween_GivenOriginAndTarget_TransparentThenOpaqueOccluders_TargetPastOpaqueOccluder, Fixture<SceneWithTransparentThenOpaqueOccluders>)
     {
+        double transmission;
         const ShadingPoint& shading_point =
-            m_tracer.trace_between(Vector3d(0.0, 0.0, 0.0), Vector3d(5.0, 0.0, 0.0));
+            m_tracer.trace_between(
+                Vector3d(0.0, 0.0, 0.0),
+                Vector3d(5.0, 0.0, 0.0),
+                transmission);
 
         ASSERT_TRUE(shading_point.hit());
         EXPECT_FEQ(Vector3d(4.0, 0.0, 0.0), shading_point.get_point());
-        EXPECT_FEQ(0.0, m_tracer.get_transmission());
+        EXPECT_FEQ(0.5, transmission);
     }
 
     TEST_CASE_F(TraceBetween_GivenOriginAndTarget_TransparentThenOpaqueOccluders_TargetOnOpaqueOccluder, Fixture<SceneWithTransparentThenOpaqueOccluders>)
     {
+        double transmission;
         const ShadingPoint& shading_point =
-            m_tracer.trace_between(Vector3d(0.0, 0.0, 0.0), Vector3d(4.0, 0.0, 0.0));
+            m_tracer.trace_between(
+                Vector3d(0.0, 0.0, 0.0),
+                Vector3d(4.0, 0.0, 0.0),
+                transmission);
 
         ASSERT_FALSE(shading_point.hit());
-        EXPECT_FEQ(0.5, m_tracer.get_transmission());
+        EXPECT_FEQ(0.5, transmission);
     }
 }