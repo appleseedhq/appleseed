e7e12a60a93b9811c5de8d9f60ad79d78541c164
Francois Beaune <beaune@aist.enst.fr>
2011-08-20 12:49:14 +0200

    small code flow improvement.

--- src/appleseed/renderer/kernel/rendering/masterrenderer.cpp
+++ src/appleseed/renderer/kernel/rendering/masterrenderer.cpp
@@ -332,12 +332,11 @@ IRendererController::Status MasterRenderer::render_frame(IFrameRenderer* frame_r
     {
         const IRendererController::Status status = m_renderer_controller->on_progress();
 
-        if (status == IRendererController::ContinueRendering)
-            continue;
-
-        frame_renderer->stop_rendering();
-
-        return status;
+        if (status != IRendererController::ContinueRendering)
+        {
+            frame_renderer->stop_rendering();
+            return status;
+        }
     }
 
     return IRendererController::TerminateRendering;
