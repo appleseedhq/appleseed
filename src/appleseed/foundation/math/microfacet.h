fef058eedef138d45b01842788ad47c59436ad8a
Francois Beaune <beaune@aist.enst.fr>
2011-11-08 00:32:52 +0100

    new component foundation/math/microfacet.h.

---
+++ src/appleseed/foundation/math/microfacet.h
@@ -0,0 +1,362 @@
+
+//
+// This source file is part of appleseed.
+// Visit http://appleseedhq.net/ for additional information and resources.
+//
+// This software is released under the MIT license.
+//
+// Copyright (c) 2010-2011 Francois Beaune
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+#ifndef APPLESEED_FOUNDATION_MATH_MICROFACET_H
+#define APPLESEED_FOUNDATION_MATH_MICROFACET_H
+
+// appleseed.foundation headers.
+#include "foundation/math/scalar.h"
+#include "foundation/math/vector.h"
+
+// Standard headers.
+#include <cassert>
+#include <cmath>
+
+namespace foundation
+{
+
+//
+// Blinn-Phong Microfacet Distribution Function.
+//
+// References:
+//
+//   [1] Physically Based Rendering, first edition, pp. 444-447 and 681-684
+//
+//   [2] Microfacet Models for Refraction through Rough Surfaces
+//       http://www.cs.cornell.edu/~srm/publications/EGSR07-btdf.pdf
+//
+
+template <typename T>
+class BlinnMDF
+{
+  public:
+    // e is the cosine exponent.
+    explicit BlinnMDF(const T e);
+
+    // Sample the product of the MDF and |H.n|.
+    Vector<T, 3> sample(const Vector<T, 2>& s) const;
+
+    T evaluate(const T cos_alpha) const;
+
+    T evaluate_pdf(const T cos_alpha) const;
+
+  private:
+    const T m_e;
+};
+
+
+//
+// Beckmann Microfacet Distribution Function.
+//
+// References:
+//
+//   [1] http://en.wikipedia.org/wiki/Specular_highlight#Beckmann_distribution
+//
+//   [2] A Microfacet Based Coupled Specular-Matte BRDF Model with Importance Sampling
+//       http://sirkan.iit.bme.hu/~szirmay/scook.pdf
+//
+
+template <typename T>
+class BeckmannMDF
+{
+  public:
+    // m is the RMS slope of the surface microfacets (the roughness of the material).
+    explicit BeckmannMDF(const T m);
+
+    // Sample the product of the MDF and |H.n|.
+    Vector<T, 3> sample(const Vector<T, 2>& s) const;
+
+    T evaluate(const T cos_alpha) const;
+
+    T evaluate_pdf(const T cos_alpha) const;
+
+  private:
+    const T m_m;
+};
+
+
+//
+// Ward Microfacet Distribution Function.
+//
+// Reference:
+//
+//   A Microfacet Based Coupled Specular-Matte BRDF Model with Importance Sampling
+//   http://sirkan.iit.bme.hu/~szirmay/scook.pdf
+//
+// Note: not a true MDF as it integrates to values less than 1!
+//
+
+template <typename T>
+class WardMDF
+{
+  public:
+    // m is the RMS slope of the surface microfacets (the roughness of the material).
+    explicit WardMDF(const T m);
+
+    // Sample the product of the MDF and |H.n|.
+    Vector<T, 3> sample(const Vector<T, 2>& s) const;
+
+    T evaluate(const T cos_alpha) const;
+
+    T evaluate_pdf(const T cos_alpha) const;
+
+  private:
+    const T m_m;
+};
+
+
+//
+// GGX Microfacet Distribution Function.
+//
+// Reference:
+//
+//   Microfacet Models for Refraction through Rough Surfaces
+//   http://www.cs.cornell.edu/~srm/publications/EGSR07-btdf.pdf
+//
+
+template <typename T>
+class GGXMDF
+{
+  public:
+    // alpha_g is the peak width.
+    explicit GGXMDF(const T alpha_g);
+
+    // Sample the product of the MDF and |H.n|.
+    Vector<T, 3> sample(const Vector<T, 2>& s) const;
+
+    T evaluate(const T cos_alpha) const;
+
+    T evaluate_pdf(const T cos_alpha) const;
+
+  private:
+    const T m_alpha_g;
+};
+
+
+//
+// BlinnMDF class implementation.
+//
+
+template <typename T>
+inline BlinnMDF<T>::BlinnMDF(const T e)
+  : m_e(e)
+{
+}
+
+template <typename T>
+inline Vector<T, 3> BlinnMDF<T>::sample(const Vector<T, 2>& s) const
+{
+    assert(s[0] >= 0.0 && s[0] < 1.0);
+    assert(s[1] >= 0.0 && s[1] < 1.0);
+
+    const T alpha = std::acos(std::pow(T(1.0) - s[0], T(1.0) / (m_e + T(2.0))));
+    const T phi = TwoPi * s[1];
+
+    return Vector<T, 3>::unit_vector(alpha, phi);
+}
+
+template <typename T>
+inline T BlinnMDF<T>::evaluate(const T cos_alpha) const
+{
+    assert(cos_alpha >= T(0.0));
+
+    return (m_e + T(2.0)) * RcpTwoPi * std::pow(cos_alpha, m_e);
+}
+
+template <typename T>
+inline T BlinnMDF<T>::evaluate_pdf(const T cos_alpha) const
+{
+    assert(cos_alpha >= T(0.0));
+
+    return (m_e + T(2.0)) * RcpTwoPi * std::pow(cos_alpha, m_e + T(1.0));
+}
+
+
+//
+// BeckmannMDF class implementation.
+//
+
+template <typename T>
+inline BeckmannMDF<T>::BeckmannMDF(const T m)
+  : m_m(m)
+{
+}
+
+template <typename T>
+inline Vector<T, 3> BeckmannMDF<T>::sample(const Vector<T, 2>& s) const
+{
+    // Same sampling procedure as for the Ward distribution.
+
+    assert(s[0] >= 0.0 && s[0] < 1.0);
+    assert(s[1] >= 0.0 && s[1] < 1.0);
+
+    const double alpha = std::atan(m_m * std::sqrt(-std::log(1.0 - s[0])));
+    const double phi = TwoPi * s[1];
+
+    return Vector<T, 3>::unit_vector(alpha, phi);
+}
+
+template <typename T>
+inline T BeckmannMDF<T>::evaluate(const T cos_alpha) const
+{
+    assert(cos_alpha >= T(0.0));
+
+    if (cos_alpha == T(0.0))
+        return T(0.0);
+
+    const T cos_alpha_2 = cos_alpha * cos_alpha;
+    const T cos_alpha_4 = cos_alpha_2 * cos_alpha_2;
+    const T tan_alpha_2 = (T(1.0) - cos_alpha_2) / cos_alpha_2;
+    const T m_2 = m_m * m_m;
+
+    // Note: in [2] there's a missing Pi factor in the denominator.
+    return std::exp(-tan_alpha_2 / m_2) / (m_2 * T(Pi) * cos_alpha_4);
+}
+
+template <typename T>
+inline T BeckmannMDF<T>::evaluate_pdf(const T cos_alpha) const
+{
+    // Same PDF as for the Ward distribution.
+
+    assert(cos_alpha >= T(0.0));
+
+    if (cos_alpha == T(0.0))
+        return T(0.0);
+
+    const T cos_alpha_2 = cos_alpha * cos_alpha;
+    const T cos_alpha_3 = cos_alpha * cos_alpha_2;
+    const T tan_alpha_2 = (T(1.0) - cos_alpha_2) / cos_alpha_2;
+    const T m_2 = m_m * m_m;
+
+    return std::exp(-tan_alpha_2 / m_2) / (m_2 * T(Pi) * cos_alpha_3);
+}
+
+
+//
+// WardMDF class implementation.
+//
+
+template <typename T>
+inline WardMDF<T>::WardMDF(const T m)
+  : m_m(m)
+{
+}
+
+template <typename T>
+inline Vector<T, 3> WardMDF<T>::sample(const Vector<T, 2>& s) const
+{
+    assert(s[0] >= 0.0 && s[0] < 1.0);
+    assert(s[1] >= 0.0 && s[1] < 1.0);
+
+    const double alpha = std::atan(m_m * std::sqrt(-std::log(1.0 - s[0])));
+    const double phi = TwoPi * s[1];
+
+    return Vector<T, 3>::unit_vector(alpha, phi);
+}
+
+template <typename T>
+inline T WardMDF<T>::evaluate(const T cos_alpha) const
+{
+    assert(cos_alpha >= T(0.0));
+
+    if (cos_alpha == T(0.0))
+        return T(0.0);
+
+    const T cos_alpha_2 = cos_alpha * cos_alpha;
+    const T cos_alpha_3 = cos_alpha * cos_alpha_2;
+    const T tan_alpha_2 = (T(1.0) - cos_alpha_2) / cos_alpha_2;
+    const T m_2 = m_m * m_m;
+
+    return std::exp(-tan_alpha_2 / m_2) / (m_2 * T(Pi) * cos_alpha_3);
+}
+
+template <typename T>
+inline T WardMDF<T>::evaluate_pdf(const T cos_alpha) const
+{
+    return evaluate(cos_alpha);
+}
+
+
+//
+// GGXMDF class implementation.
+//
+
+template <typename T>
+inline GGXMDF<T>::GGXMDF(const T alpha_g)
+  : m_alpha_g(alpha_g)
+{
+}
+
+template <typename T>
+inline Vector<T, 3> GGXMDF<T>::sample(const Vector<T, 2>& s) const
+{
+    assert(s[0] >= 0.0 && s[0] < 1.0);
+    assert(s[1] >= 0.0 && s[1] < 1.0);
+
+    const T alpha = std::atan(m_alpha_g * std::sqrt(s[0] / (T(1.0) - s[0])));
+    const T phi = TwoPi * s[1];
+
+    return Vector<T, 3>::unit_vector(alpha, phi);
+}
+
+template <typename T>
+inline T GGXMDF<T>::evaluate(const T cos_alpha) const
+{
+    assert(cos_alpha >= T(0.0));
+
+    const T alpha_g_2 = m_alpha_g * m_alpha_g;
+
+    if (cos_alpha == T(0.0))
+        return alpha_g_2 * RcpPi;
+
+    const T cos_alpha_2 = cos_alpha * cos_alpha;
+    const T cos_alpha_4 = cos_alpha_2 * cos_alpha_2;
+    const T tan_alpha_2 = (T(1.0) - cos_alpha_2) / cos_alpha_2;
+
+    return alpha_g_2 / (Pi * cos_alpha_4 * square(alpha_g_2 + tan_alpha_2));
+}
+
+template <typename T>
+inline T GGXMDF<T>::evaluate_pdf(const T cos_alpha) const
+{
+    assert(cos_alpha >= T(0.0));
+
+    if (cos_alpha == T(0.0))
+        return T(0.0);
+
+    const T cos_alpha_2 = cos_alpha * cos_alpha;
+    const T cos_alpha_3 = cos_alpha * cos_alpha_2;
+    const T tan_alpha_2 = (T(1.0) - cos_alpha_2) / cos_alpha_2;
+    const T alpha_g_2 = m_alpha_g * m_alpha_g;
+
+    return alpha_g_2 / (Pi * cos_alpha_3 * square(alpha_g_2 + tan_alpha_2));
+}
+
+}       // namespace foundation
+
+#endif  // !APPLESEED_FOUNDATION_MATH_MICROFACET_H
---
+++ src/appleseed/foundation/meta/tests/test_microfacet.cpp
@@ -0,0 +1,490 @@
+
+//
+// This source file is part of appleseed.
+// Visit http://appleseedhq.net/ for additional information and resources.
+//
+// This software is released under the MIT license.
+//
+// Copyright (c) 2010-2011 Francois Beaune
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+// appleseed.foundation headers.
+#include "foundation/math/sampling/mappings.h"
+#include "foundation/math/microfacet.h"
+#include "foundation/math/qmc.h"
+#include "foundation/math/scalar.h"
+#include "foundation/math/vector.h"
+#include "foundation/utility/makevector.h"
+#include "foundation/utility/maplefile.h"
+#include "foundation/utility/test.h"
+
+// Standard headers.
+#include <cmath>
+#include <cstddef>
+#include <string>
+
+using namespace foundation;
+using namespace std;
+
+TEST_SUITE(Foundation_Math_Microfacet)
+{
+    template <typename MDF>
+    bool is_positive(
+        const MDF&      mdf,
+        const size_t    sample_count)
+    {
+        for (size_t i = 0; i < sample_count; ++i)
+        {
+            const double theta = radical_inverse_base2<double>(i) * HalfPi;
+            const double cos_theta = cos(theta);
+
+            const double value = mdf.evaluate(cos_theta);
+
+            if (value < 0.0)
+                return false;
+        }
+
+        return true;
+    }
+
+    template <typename MDF>
+    double integrate_spherical(
+        const MDF&      mdf,
+        const size_t    sample_count)
+    {
+        double integral = 0.0;
+
+        for (size_t i = 0; i < sample_count; ++i)
+        {
+            const double theta = radical_inverse_base2<double>(i) * HalfPi;
+            const double cos_theta = cos(theta);
+
+            const double value = mdf.evaluate(cos_theta);
+            const double sample = value * HalfPi;
+
+            integral += sample * cos_theta * sin(theta);
+        }
+
+        integral *= TwoPi;
+        integral /= static_cast<double>(sample_count);
+
+        return integral;
+    }
+
+    template <typename T>
+    struct UniformHemisphereSampler
+    {
+        static Vector<T, 3> sample(const Vector<T, 2>& s)
+        {
+            return sample_hemisphere_uniform(s);
+        }
+
+        static T pdf(const Vector<T, 3>& v)
+        {
+            return RcpTwoPi;
+        }
+    };
+
+    template <typename T>
+    struct CosineHemisphereSampler
+    {
+        static Vector<T, 3> sample(const Vector<T, 2>& s)
+        {
+            return sample_hemisphere_cosine(s);
+        }
+
+        static T pdf(const Vector<T, 3>& v)
+        {
+            return v.y * RcpPi;
+        }
+    };
+
+    template <typename MDF, typename Sampler>
+    double integrate_random(
+        const MDF&      mdf,
+        const Sampler&  sampler,
+        const size_t    sample_count)
+    {
+        double integral = 0.0;
+
+        for (size_t i = 0; i < sample_count; ++i)
+        {
+            static const size_t Bases[] = { 2 };
+            const Vector2d s = hammersley_sequence<double, 2>(Bases, i, sample_count);
+
+            const Vector3d w = sampler.sample(s);
+            const double pdf = sampler.pdf(w);
+            const double cos_theta = w.y;
+
+            const double value = mdf.evaluate(cos_theta);
+            const double sample = value / pdf;
+
+            integral += sample * cos_theta;
+        }
+
+        integral /= static_cast<double>(sample_count);
+
+        return integral;
+    }
+
+    template <typename MDF>
+    double integrate_importance(
+        const MDF&      mdf,
+        const size_t    sample_count)
+    {
+        double integral = 0.0;
+
+        for (size_t i = 0; i < sample_count; ++i)
+        {
+            static const size_t Bases[] = { 2 };
+            const Vector2d s = hammersley_sequence<double, 2>(Bases, i, sample_count);
+
+            const Vector3d w = mdf.sample(s);
+            const double cos_theta = w.y;
+            const double pdf = mdf.evaluate_pdf(cos_theta);
+
+            const double value = mdf.evaluate(cos_theta);
+            const double sample = value / pdf;
+
+            integral += sample * cos_theta;
+        }
+
+        integral /= static_cast<double>(sample_count);
+
+        return integral;
+    }
+
+    template <typename MDF>
+    void plot(
+        MapleFile&      file,
+        const string&   name,
+        const MDF&      mdf,
+        const size_t    point_count,
+        const size_t    sample_count)
+    {
+        vector<double> angles(point_count);
+        vector<double> densities(point_count);
+
+        for (size_t i = 0; i < point_count; ++i)
+        {
+            const double angle = fit<double>(i, 0, point_count - 1, -HalfPi, +HalfPi);
+            const double cos_angle = cos(angle);
+
+            angles[i] = rad_to_deg(angle);
+            densities[i] = mdf.evaluate(cos_angle) * cos_angle;
+        }
+
+        vector<double> angle_samples(sample_count);
+        vector<double> density_samples(sample_count);
+
+        for (size_t i = 0; i < sample_count; ++i)
+        {
+            static const size_t Bases[] = { 2 };
+            const Vector2d s = hammersley_sequence<double, 2>(Bases, i, sample_count);
+            const Vector3d w = mdf.sample(s);
+            const double cos_angle = w.y;
+            const double angle = acos(cos_angle) * (w.x < 0.0 ? -1.0 : 1.0);
+
+            angle_samples[i] = rad_to_deg(angle);
+            density_samples[i] = mdf.evaluate(cos_angle) * cos_angle;
+        }
+
+        file.define(name, angles, densities);
+        file.define(name + "_samples", angle_samples, density_samples);
+
+        file.plot(
+            make_vector(
+                MaplePlotDef(name)
+                    .set_legend("Microfacet Distribution Function (" + name + ")"),
+                MaplePlotDef(name + "_samples")
+                    .set_legend("Integration Samples")
+                    .set_style("point")
+                    .set_color("red")));
+    }
+
+    //
+    // Test settings.
+    //
+
+    const size_t PositivityTestSampleCount = 256;
+    const size_t IntegrationSampleCount = 8192;
+    const size_t FunctionPlotSampleCount = 256;
+    const size_t FunctionSamplingSampleCount = 64;
+    const double IntegrationEps = 1.0e-3;
+
+    //
+    // Blinn-Phong MDF.
+    //
+
+    TEST_CASE(BlinnMDF_Evaluate_ReturnsNonNegativeValues)
+    {
+        BlinnMDF<double> mdf(10.0);
+
+        EXPECT_TRUE(is_positive(mdf, PositivityTestSampleCount));
+    }
+
+    TEST_CASE(BlinnMDF_Evaluate_GivenCosThetaIsZero_ReturnsZero)
+    {
+        BlinnMDF<double> mdf(10.0);
+
+        const double limit = mdf.evaluate(0.0);
+
+        EXPECT_FEQ(0.0, limit);
+    }
+
+    TEST_CASE(BlinnMDF_EvaluatePDF_GivenCosThetaIsZero_ReturnsZero)
+    {
+        BlinnMDF<double> mdf(10.0);
+
+        const double limit = mdf.evaluate_pdf(0.0);
+
+        EXPECT_FEQ(0.0, limit);
+    }
+
+    TEST_CASE(BlinnMDF_IntegratedOverSphericalCoordinates_EqualsOne)
+    {
+        BlinnMDF<double> mdf(10.0);
+
+        const double integral = integrate_spherical(mdf, IntegrationSampleCount);
+
+        EXPECT_FEQ_EPS(1.0, integral, IntegrationEps);
+    }
+
+    TEST_CASE(BlinnMDF_IntegratedOverSolidAngle_UsingUniformSampling_EqualsOne)
+    {
+        BlinnMDF<double> mdf(10.0);
+
+        const double integral = integrate_random(mdf, UniformHemisphereSampler<double>(), IntegrationSampleCount);
+
+        EXPECT_FEQ_EPS(1.0, integral, IntegrationEps);
+    }
+
+    TEST_CASE(BlinnMDF_IntegratedOverSolidAngle_UsingCosineWeightedSampling_EqualsOne)
+    {
+        BlinnMDF<double> mdf(10.0);
+
+        const double integral = integrate_random(mdf, CosineHemisphereSampler<double>(), IntegrationSampleCount);
+
+        EXPECT_FEQ_EPS(1.0, integral, IntegrationEps);
+    }
+
+    TEST_CASE(BlinnMDF_IntegratedOverSolidAngle_UsingImportanceSampling_EqualsOne)
+    {
+        BlinnMDF<double> mdf(10.0);
+
+        const double integral = integrate_importance(mdf, IntegrationSampleCount);
+
+        EXPECT_FEQ_EPS(1.0, integral, IntegrationEps);
+    }
+
+    TEST_CASE(BlinnMDF_GeneratePlotFiles)
+    {
+        MapleFile file("unit tests/outputs/test_microfacet_blinn.mpl");
+        plot(file, "blinn_1",  BlinnMDF<double>( 1.0), FunctionPlotSampleCount, FunctionSamplingSampleCount);
+        plot(file, "blinn_10", BlinnMDF<double>(10.0), FunctionPlotSampleCount, FunctionSamplingSampleCount);
+        plot(file, "blinn_50", BlinnMDF<double>(50.0), FunctionPlotSampleCount, FunctionSamplingSampleCount);
+    }
+
+    //
+    // Beckmann MDF.
+    //
+
+    TEST_CASE(BeckmannMDF_Evaluate_ReturnsNonNegativeValues)
+    {
+        BeckmannMDF<double> mdf(0.5);
+
+        EXPECT_TRUE(is_positive(mdf, PositivityTestSampleCount));
+    }
+
+    TEST_CASE(BeckmannMDF_Evaluate_GivenCosThetaIsZero_ReturnsZero)
+    {
+        BeckmannMDF<double> mdf(0.5);
+
+        const double limit = mdf.evaluate(0.0);
+
+        EXPECT_FEQ(0.0, limit);
+    }
+
+    TEST_CASE(BeckmannMDF_EvaluatePDF_GivenCosThetaIsZero_ReturnsZero)
+    {
+        BeckmannMDF<double> mdf(0.5);
+
+        const double limit = mdf.evaluate_pdf(0.0);
+
+        EXPECT_FEQ(0.0, limit);
+    }
+
+    TEST_CASE(BeckmannMDF_IntegratedOverSphericalCoordinates_EqualsOne)
+    {
+        BeckmannMDF<double> mdf(0.5);
+
+        const double integral = integrate_spherical(mdf, IntegrationSampleCount);
+
+        EXPECT_FEQ_EPS(1.0, integral, IntegrationEps);
+    }
+
+    TEST_CASE(BeckmannMDF_IntegratedOverSolidAngle_UsingUniformSampling_EqualsOne)
+    {
+        BeckmannMDF<double> mdf(0.5);
+
+        const double integral = integrate_random(mdf, UniformHemisphereSampler<double>(), IntegrationSampleCount);
+
+        EXPECT_FEQ_EPS(1.0, integral, IntegrationEps);
+    }
+
+    TEST_CASE(BeckmannMDF_IntegratedOverSolidAngle_UsingCosineWeightedSampling_EqualsOne)
+    {
+        BeckmannMDF<double> mdf(0.5);
+
+        const double integral = integrate_random(mdf, CosineHemisphereSampler<double>(), IntegrationSampleCount);
+
+        EXPECT_FEQ_EPS(1.0, integral, IntegrationEps);
+    }
+
+    TEST_CASE(BeckmannMDF_IntegratedOverSolidAngle_UsingImportanceSampling_EqualsOne)
+    {
+        BeckmannMDF<double> mdf(0.5);
+
+        const double integral = integrate_importance(mdf, IntegrationSampleCount);
+
+        EXPECT_FEQ_EPS(1.0, integral, IntegrationEps);
+    }
+
+    TEST_CASE(BeckmannMDF_GeneratePlotFiles)
+    {
+        MapleFile file("unit tests/outputs/test_microfacet_beckmann.mpl");
+        plot(file, "beckmann_0_1", BeckmannMDF<double>(0.1), FunctionPlotSampleCount, FunctionSamplingSampleCount);
+        plot(file, "beckmann_0_5", BeckmannMDF<double>(0.5), FunctionPlotSampleCount, FunctionSamplingSampleCount);
+        plot(file, "beckmann_1_0", BeckmannMDF<double>(1.0), FunctionPlotSampleCount, FunctionSamplingSampleCount);
+    }
+
+    //
+    // Ward MDF.
+    //
+
+    TEST_CASE(WardMDF_Evaluate_ReturnsNonNegativeValues)
+    {
+        WardMDF<double> mdf(0.5);
+
+        EXPECT_TRUE(is_positive(mdf, PositivityTestSampleCount));
+    }
+
+    TEST_CASE(WardMDF_Evaluate_GivenCosThetaIsZero_ReturnsZero)
+    {
+        WardMDF<double> mdf(0.5);
+
+        const double limit = mdf.evaluate(0.0);
+
+        EXPECT_FEQ(0.0, limit);
+    }
+
+    TEST_CASE(WardMDF_EvaluatePDF_GivenCosThetaIsZero_ReturnsZero)
+    {
+        WardMDF<double> mdf(0.5);
+
+        const double limit = mdf.evaluate_pdf(0.0);
+
+        EXPECT_FEQ(0.0, limit);
+    }
+
+    TEST_CASE(WardMDF_GeneratePlotFiles)
+    {
+        MapleFile file("unit tests/outputs/test_microfacet_ward.mpl");
+        plot(file, "ward_0_1", WardMDF<double>(0.1), FunctionPlotSampleCount, FunctionSamplingSampleCount);
+        plot(file, "ward_0_5", WardMDF<double>(0.5), FunctionPlotSampleCount, FunctionSamplingSampleCount);
+        plot(file, "ward_1_0", WardMDF<double>(1.0), FunctionPlotSampleCount, FunctionSamplingSampleCount);
+    }
+
+    //
+    // GGX MDF.
+    //
+
+    TEST_CASE(GGXMDF_Evaluate_ReturnsNonNegativeValues)
+    {
+        GGXMDF<double> mdf(0.5);
+
+        EXPECT_TRUE(is_positive(mdf, PositivityTestSampleCount));
+    }
+
+    TEST_CASE(GGXMDF_Evaluate_GivenCosThetaIsZero_ReturnsLimitValue)
+    {
+        const double AlphaG = 0.5;
+        GGXMDF<double> mdf(AlphaG);
+
+        const double Expected = AlphaG * AlphaG * RcpPi;
+        const double limit = mdf.evaluate(0.0);
+
+        EXPECT_FEQ(Expected, limit);
+    }
+
+    TEST_CASE(GGXMDF_EvaluatePDF_GivenCosThetaIsZero_ReturnsZero)
+    {
+        GGXMDF<double> mdf(0.5);
+
+        const double limit = mdf.evaluate_pdf(0.0);
+
+        EXPECT_FEQ(0.0, limit);
+    }
+
+    TEST_CASE(GGXMDF_IntegratedOverSphericalCoordinates_EqualsOne)
+    {
+        GGXMDF<double> mdf(0.5);
+
+        const double integral = integrate_spherical(mdf, IntegrationSampleCount);
+
+        EXPECT_FEQ_EPS(1.0, integral, IntegrationEps);
+    }
+
+    TEST_CASE(GGXMDF_IntegratedOverSolidAngle_UsingUniformSampling_EqualsOne)
+    {
+        GGXMDF<double> mdf(0.5);
+
+        const double integral = integrate_random(mdf, UniformHemisphereSampler<double>(), IntegrationSampleCount);
+
+        EXPECT_FEQ_EPS(1.0, integral, IntegrationEps);
+    }
+
+    TEST_CASE(GGXMDF_IntegratedOverSolidAngle_UsingCosineWeightedSampling_EqualsOne)
+    {
+        GGXMDF<double> mdf(0.5);
+
+        const double integral = integrate_random(mdf, CosineHemisphereSampler<double>(), IntegrationSampleCount);
+
+        EXPECT_FEQ_EPS(1.0, integral, IntegrationEps);
+    }
+
+    TEST_CASE(GGXMDF_IntegratedOverSolidAngle_UsingImportanceSampling_EqualsOne)
+    {
+        GGXMDF<double> mdf(0.5);
+
+        const double integral = integrate_importance(mdf, IntegrationSampleCount);
+
+        EXPECT_FEQ_EPS(1.0, integral, IntegrationEps);
+    }
+
+    TEST_CASE(GGXMDF_GeneratePlotFiles)
+    {
+        MapleFile file("unit tests/outputs/test_microfacet_ggx.mpl");
+        plot(file, "ggx_0_1", GGXMDF<double>(0.1), FunctionPlotSampleCount, FunctionSamplingSampleCount);
+        plot(file, "ggx_0_5", GGXMDF<double>(0.5), FunctionPlotSampleCount, FunctionSamplingSampleCount);
+        plot(file, "ggx_2_0", GGXMDF<double>(2.0), FunctionPlotSampleCount, FunctionSamplingSampleCount);
+    }
+}
