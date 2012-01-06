b4599e98ee7e7201ebdd087dff2354875a39920a
Francois Beaune <beaune@aist.enst.fr>
2012-01-06 16:55:29 +0100

    fixed compilation on gcc 4.2.

--- src/appleseed.studio/mainwindow/project/objectinstancecollectionitem.cpp
+++ src/appleseed.studio/mainwindow/project/objectinstancecollectionitem.cpp
@@ -53,7 +53,7 @@ ObjectInstanceCollectionItem::ObjectInstanceCollectionItem(
     Assembly&                   assembly,
     ObjectInstanceContainer&    object_instances,
     ProjectBuilder&             project_builder)
-  : CollectionItemBase(g_class_uid, "Object Instances")
+  : CollectionItemBase<ObjectInstance>(g_class_uid, "Object Instances")
   , m_assembly(assembly)
   , m_project_builder(project_builder)
 {
