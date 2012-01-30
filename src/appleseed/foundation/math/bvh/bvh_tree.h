f995453a8db2e99c65efcf741fee8a0e4cec6bf3
Francois Beaune <beaune@aist.enst.fr>
2012-01-30 11:34:23 +0100

    made foundation::bvh::Tree::NodeType public.

--- src/appleseed/foundation/math/bvh/bvh_tree.h
+++ src/appleseed/foundation/math/bvh/bvh_tree.h
@@ -58,6 +58,7 @@ class Tree
     // Item, AABB, node and tree types.
     typedef Item ItemType;
     typedef AABB<T, N> AABBType;
+    typedef Node<T, N> NodeType;
     typedef Tree<T, N, Item> TreeType;
 
     // Constructor.
@@ -99,8 +100,6 @@ class Tree
 
     template <typename Tree, typename Builder>
     friend class TreeStatistics;
-
-    typedef Node<T, N> NodeType;
 
     typedef std::vector<NodeType> NodeVector;
     typedef std::vector<ItemType> ItemVector;
