e7c43c51699b9e382f390b9ed6649ac3985dd22b
Francois Beaune <beaune@aist.enst.fr>
2012-02-10 00:42:03 +0100

    inlined BVH nodes intersection code.

--- src/appleseed/foundation/math/bvh/bvh_intersector.h
+++ src/appleseed/foundation/math/bvh/bvh_intersector.h
@@ -36,7 +36,9 @@
 #include "foundation/math/intersection.h"
 #include "foundation/math/ray.h"
 #ifdef APPLESEED_FOUNDATION_USE_SSE
+#include "foundation/math/fp.h"
 #include "foundation/platform/compiler.h"
+#include "foundation/platform/sse.h"
 #endif
 
 // Standard headers.
@@ -118,20 +120,134 @@ class Intersector
 // Intersector class implementation.
 //
 
-#ifdef APPLESEED_FOUNDATION_USE_SSE
-
 namespace impl
 {
-    int intersect_bvh_nodes(
+
+#ifdef APPLESEED_FOUNDATION_USE_SSE
+
+    FORCE_INLINE int intersect_bvh_nodes(
         const Ray3d&        ray,
         const RayInfo3d&    ray_info,
         const AABB3f&       left_bbox,
         const AABB3f&       right_bbox,
         double              tmin[2],
-        double              tmax[2]);
-}
+        double              tmax[2])
+    {
+        const sse2d mposinf = set1pd(FP<double>::pos_inf());
+        const sse2d mneginf = set1pd(FP<double>::neg_inf());
 
-#endif
+        // X slabs.
+
+        ALIGN_SSE_VARIABLE const float bbx[4] =
+        {
+            left_bbox.min.x,
+            right_bbox.min.x,
+            left_bbox.max.x,
+            right_bbox.max.x
+        };
+
+        const sse4f mbbx4f = loadps(bbx);
+        const sse2d mbbminx2d = _mm_cvtps_pd(mbbx4f);
+        const sse2d mbbmaxx2d = _mm_cvtps_pd(shuffleps(mbbx4f, mbbx4f, _MM_SHUFFLE(0, 0, 3, 2)));
+
+        const sse2d mrox = set1pd(ray.m_org.x);
+        const sse2d mrrcpdx = set1pd(ray_info.m_rcp_dir.x);
+        const sse2d mx1 = mulpd(mrrcpdx, subpd(mbbminx2d, mrox));
+        const sse2d mx2 = mulpd(mrrcpdx, subpd(mbbmaxx2d, mrox));
+
+        sse2d mtmax = maxpd(minpd(mx1, mposinf), minpd(mx2, mposinf));
+        sse2d mtmin = minpd(maxpd(mx1, mneginf), maxpd(mx2, mneginf));
+
+        // Y slabs.
+
+        ALIGN_SSE_VARIABLE const float bby[4] =
+        {
+            left_bbox.min.y,
+            right_bbox.min.y,
+            left_bbox.max.y,
+            right_bbox.max.y
+        };
+
+        const sse4f mbby4f = loadps(bby);
+        const sse2d mbbminy2d = _mm_cvtps_pd(mbby4f);
+        const sse2d mbbmaxy2d = _mm_cvtps_pd(shuffleps(mbby4f, mbby4f, _MM_SHUFFLE(0, 0, 3, 2)));
+
+        const sse2d mroy = set1pd(ray.m_org.y);
+        const sse2d mrrcpdy = set1pd(ray_info.m_rcp_dir.y);
+        const sse2d my1 = mulpd(mrrcpdy, subpd(mbbminy2d, mroy));
+        const sse2d my2 = mulpd(mrrcpdy, subpd(mbbmaxy2d, mroy));
+
+        mtmax = minpd(mtmax, maxpd(minpd(my1, mposinf), minpd(my2, mposinf)));
+        mtmin = maxpd(mtmin, minpd(maxpd(my1, mneginf), maxpd(my2, mneginf)));
+
+        // Z slabs.
+
+        ALIGN_SSE_VARIABLE const float bbz[4] =
+        {
+            left_bbox.min.z,
+            right_bbox.min.z,
+            left_bbox.max.z,
+            right_bbox.max.z
+        };
+
+        const sse4f mbbz4f = loadps(bbz);
+        const sse2d mbbminz2d = _mm_cvtps_pd(mbbz4f);
+        const sse2d mbbmaxz2d = _mm_cvtps_pd(shuffleps(mbbz4f, mbbz4f, _MM_SHUFFLE(0, 0, 3, 2)));
+
+        const sse2d mroz = set1pd(ray.m_org.z);
+        const sse2d mrrcpdz = set1pd(ray_info.m_rcp_dir.z);
+        const sse2d mz1 = mulpd(mrrcpdz, subpd(mbbminz2d, mroz));
+        const sse2d mz2 = mulpd(mrrcpdz, subpd(mbbmaxz2d, mroz));
+
+        mtmax = minpd(mtmax, maxpd(minpd(mz1, mposinf), minpd(mz2, mposinf)));
+        mtmin = maxpd(mtmin, minpd(maxpd(mz1, mneginf), maxpd(mz2, mneginf)));
+
+        // Conclusion.
+
+        const sse2d mraytmin = set1pd(ray.m_tmin);
+        const sse2d mraytmax = set1pd(ray.m_tmax);
+
+        const int hits =
+            movemaskpd(
+                orpd(
+                    cmpgtpd(mtmin, mtmax),
+                    orpd(
+                        cmpltpd(mtmax, mraytmin),
+                        cmpgepd(mtmin, mraytmax)))) ^ 3;
+
+        if (hits)
+        {
+            storepd(tmin, maxpd(mraytmin, mtmin));
+            storepd(tmax, minpd(mraytmax, mtmax));
+        }
+
+        return hits;
+    }
+
+#else
+
+    FORCE_INLINE int intersect_bvh_nodes(
+        const Ray3d&        ray,
+        const RayInfo3d&    ray_info,
+        const AABB3f&       left_bbox,
+        const AABB3f&       right_bbox,
+        double              tmin[2],
+        double              tmax[2])
+    {
+        int result = 0;
+
+        if (intersect(ray, ray_info, AABB3d(left_bbox), tmin[0], tmax[0]))
+            result |= 1;
+
+        if (intersect(ray, ray_info, AABB3d(right_bbox), tmin[1], tmax[1]))
+            result |= 2;
+
+        return result;
+    }
+
+#endif  // APPLESEED_FOUNDATION_USE_SSE
+
+}       // namespace impl
 
 #ifdef FOUNDATION_BVH_ENABLE_TRAVERSAL_STATS
 #define FOUNDATION_BVH_TRAVERSAL_STATS(x) x
