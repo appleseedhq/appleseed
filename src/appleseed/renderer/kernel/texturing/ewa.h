1ac7f336a8859907382663caf615dbecef9552c5
Francois Beaune <beaune@aist.enst.fr>
2012-03-18 13:15:39 +0100

    switched the reconstruction filter in the EWA filter from bilinear to bicubic.

--- src/appleseed/renderer/kernel/texturing/ewa.h
+++ src/appleseed/renderer/kernel/texturing/ewa.h
@@ -36,7 +36,7 @@
 
 //---------------------------------------------------------------------------------------------
 // If enabled, EWAFilterAK::filter() will return a color representative of the weights of the
-// EWA and bilinear filters (red = EWA, blue = bilinear).
+// EWA and reconstruction filters (red = EWA, blue = reconstruction).
 //---------------------------------------------------------------------------------------------
 
 #undef EWA_DEBUG_DISPLAY_WEIGHTS
@@ -151,16 +151,16 @@ class EWAFilterAK
         const float Pi = 3.14159265f;
         const float area = Pi * r1 * r2;
 
-        // Compute the EWA filter and bilinear filter weights.
+        // Compute the EWA filter and reconstruction filter weights.
         const float AreaThreshold = 4.0f;
         const float ewa_weight = (area - Pi) / (AreaThreshold - Pi);
-        const float bilinear_weight = 1.0f - ewa_weight;
+        const float rec_weight = 1.0f - ewa_weight;
 
 #ifdef EWA_DEBUG_DISPLAY_WEIGHTS
 
         result[0] = ewa_weight;
         result[1] = 0.0f;
-        result[2] = bilinear_weight;
+        result[2] = rec_weight;
 
         for (int i = 3; i < NumChannels; ++i)
             result[i] = 1.0f;
@@ -168,7 +168,11 @@ class EWAFilterAK
         return;
 
 #endif
+
+        //
         // EWA filtering.
+        //
+
         if (ewa_weight > 0.0f)
         {
             // Compute the bounding box of the ellipse.
@@ -219,47 +223,69 @@ class EWAFilterAK
             }
         }
 
-        // Bilinear filtering.
-        if (bilinear_weight > 0.0f)
+        //
+        // Reconstruction.
+        //
+
+        if (rec_weight > 0.0f)
         {
             const int texture_width = texture.width();
             const int texture_height = texture.height();
-            const float px = center_x * (texture_width - 1) / texture_width;
-            const float py = center_y * (texture_height - 1) / texture_height;
-            const int ix0 = truncate<int>(px);
-            const int iy0 = truncate<int>(py);
-            const int ix1 = ix0 + 1;
-            const int iy1 = iy0 + 1;
 
-            const float wx1 = px - ix0;
-            const float wy1 = py - iy0;
-            const float wx0 = 1.0f - wx1;
-            const float wy0 = 1.0f - wy1;
-            const float w00 = wx0 * wy0;
-            const float w10 = wx1 * wy0;
-            const float w01 = wx0 * wy1;
-            const float w11 = wx1 * wy1;
+            const float px = center_x / texture_width * (texture_width - 1);
+            const float py = center_y / texture_height * (texture_height - 1);
+            const int ix = truncate<int>(px);
+            const int iy = truncate<int>(py);
+
+            const float wx = px - ix;
+            const float wy = py - iy;
 
             SSE_ALIGN float texel00[NumChannels];
             SSE_ALIGN float texel10[NumChannels];
+            SSE_ALIGN float texel20[NumChannels];
+            SSE_ALIGN float texel30[NumChannels];
             SSE_ALIGN float texel01[NumChannels];
             SSE_ALIGN float texel11[NumChannels];
+            SSE_ALIGN float texel21[NumChannels];
+            SSE_ALIGN float texel31[NumChannels];
+            SSE_ALIGN float texel02[NumChannels];
+            SSE_ALIGN float texel12[NumChannels];
+            SSE_ALIGN float texel22[NumChannels];
+            SSE_ALIGN float texel32[NumChannels];
+            SSE_ALIGN float texel03[NumChannels];
+            SSE_ALIGN float texel13[NumChannels];
+            SSE_ALIGN float texel23[NumChannels];
+            SSE_ALIGN float texel33[NumChannels];
 
-            texture.get(ix0, iy0, texel00);
-            texture.get(ix1, iy0, texel10);
-            texture.get(ix0, iy1, texel01);
-            texture.get(ix1, iy1, texel11);
+            texture.get(ix - 1, iy - 1, texel00);
+            texture.get(ix + 0, iy - 1, texel10);
+            texture.get(ix + 1, iy - 1, texel20);
+            texture.get(ix + 2, iy - 1, texel30);
+            texture.get(ix - 1, iy + 0, texel01);
+            texture.get(ix + 0, iy + 0, texel11);
+            texture.get(ix + 1, iy + 0, texel21);
+            texture.get(ix + 2, iy + 0, texel31);
+            texture.get(ix - 1, iy + 1, texel02);
+            texture.get(ix + 0, iy + 1, texel12);
+            texture.get(ix + 1, iy + 1, texel22);
+            texture.get(ix + 2, iy + 1, texel32);
+            texture.get(ix - 1, iy + 2, texel03);
+            texture.get(ix + 0, iy + 2, texel13);
+            texture.get(ix + 1, iy + 2, texel23);
+            texture.get(ix + 2, iy + 2, texel33);
 
             for (int i = 0; i < NumChannels; ++i)
             {
-                const float bilinear_result =
-                    w00 * texel00[i] +
-                    w10 * texel10[i] +
-                    w01 * texel01[i] +
-                    w11 * texel11[i];
+                const float rec_result =
+                    cubic_interpolation(
+                        cubic_interpolation(texel00[i], texel10[i], texel20[i], texel30[i], wx),
+                        cubic_interpolation(texel01[i], texel11[i], texel21[i], texel31[i], wx),
+                        cubic_interpolation(texel02[i], texel12[i], texel22[i], texel32[i], wx),
+                        cubic_interpolation(texel03[i], texel13[i], texel23[i], texel33[i], wx),
+                        wy);
 
                 result[i] *= ewa_weight;
-                result[i] += bilinear_weight * bilinear_result;
+                result[i] += rec_weight * rec_result;
             }
         }
     }
@@ -273,6 +299,16 @@ class EWAFilterAK
     static Int truncate(const float x)
     {
         return static_cast<Int>(_mm_cvttss_si32(_mm_load_ss(&x)));
+    }
+
+    static float cubic_interpolation(
+        const float     v0,
+        const float     v1,
+        const float     v2,
+        const float     v3,
+        const float     x)
+    {
+        return v1 + 0.5f * x * (v2 - v0 + x * (2.0f * v0 - 5.0f * v1 + 4.0f * v2 - v3 + x * (3.0f * (v1 - v2) + v3 - v0)));
     }
 };
 
--- src/appleseed/renderer/meta/tests/test_ewa.cpp
+++ src/appleseed/renderer/meta/tests/test_ewa.cpp
@@ -385,12 +385,15 @@ TEST_SUITE(EWAFilteringExploration)
             const float Pi = 3.14159265f;
             const float area = Pi * r1 * r2;
 
-            // Compute the EWA filter and bilinear filter weights.
+            // Compute the EWA filter and reconstruction filter weights.
             const float AreaThreshold = 4.0f;
             const float ewa_weight = (area - Pi) / (AreaThreshold - Pi);
-            const float bilinear_weight = 1.0f - ewa_weight;
+            const float rec_weight = 1.0f - ewa_weight;
 
+            //
             // EWA filtering.
+            //
+
             if (ewa_weight > 0.0f)
             {
                 // Compute the bounding box of the ellipse.
@@ -415,7 +418,7 @@ TEST_SUITE(EWAFilteringExploration)
                     {
                         if (q < F)
                         {
-                            float texel[NumChannels];
+                            SSE_ALIGN float texel[NumChannels];
                             texture.get(x, y, texel);
 
                             const float w = m_weights[q <= 0.0f ? 0 : truncate<size_t>(q)];
@@ -476,47 +479,69 @@ TEST_SUITE(EWAFilteringExploration)
                     Color3f(1.0f, 1.0f, 0.0f));
             }
 
-            // Bilinear filtering.
-            if (bilinear_weight > 0.0f)
+            //
+            // Reconstruction.
+            //
+
+            if (rec_weight > 0.0f)
             {
                 const int texture_width = texture.width();
                 const int texture_height = texture.height();
-                const float px = center_x * (texture_width - 1) / texture_width;
-                const float py = center_y * (texture_height - 1) / texture_height;
-                const int ix0 = truncate<int>(px);
-                const int iy0 = truncate<int>(py);
-                const int ix1 = ix0 + 1;
-                const int iy1 = iy0 + 1;
 
-                const float wx1 = px - ix0;
-                const float wy1 = py - iy0;
-                const float wx0 = 1.0f - wx1;
-                const float wy0 = 1.0f - wy1;
-                const float w00 = wx0 * wy0;
-                const float w10 = wx1 * wy0;
-                const float w01 = wx0 * wy1;
-                const float w11 = wx1 * wy1;
+                const float px = center_x / texture_width * (texture_width - 1);
+                const float py = center_y / texture_height * (texture_height - 1);
+                const int ix = truncate<int>(px);
+                const int iy = truncate<int>(py);
 
-                float texel00[NumChannels];
-                float texel10[NumChannels];
-                float texel01[NumChannels];
-                float texel11[NumChannels];
+                const float wx = px - ix;
+                const float wy = py - iy;
 
-                texture.get(ix0, iy0, texel00);
-                texture.get(ix1, iy0, texel10);
-                texture.get(ix0, iy1, texel01);
-                texture.get(ix1, iy1, texel11);
+                SSE_ALIGN float texel00[NumChannels];
+                SSE_ALIGN float texel10[NumChannels];
+                SSE_ALIGN float texel20[NumChannels];
+                SSE_ALIGN float texel30[NumChannels];
+                SSE_ALIGN float texel01[NumChannels];
+                SSE_ALIGN float texel11[NumChannels];
+                SSE_ALIGN float texel21[NumChannels];
+                SSE_ALIGN float texel31[NumChannels];
+                SSE_ALIGN float texel02[NumChannels];
+                SSE_ALIGN float texel12[NumChannels];
+                SSE_ALIGN float texel22[NumChannels];
+                SSE_ALIGN float texel32[NumChannels];
+                SSE_ALIGN float texel03[NumChannels];
+                SSE_ALIGN float texel13[NumChannels];
+                SSE_ALIGN float texel23[NumChannels];
+                SSE_ALIGN float texel33[NumChannels];
+
+                texture.get(ix - 1, iy - 1, texel00);
+                texture.get(ix + 0, iy - 1, texel10);
+                texture.get(ix + 1, iy - 1, texel20);
+                texture.get(ix + 2, iy - 1, texel30);
+                texture.get(ix - 1, iy + 0, texel01);
+                texture.get(ix + 0, iy + 0, texel11);
+                texture.get(ix + 1, iy + 0, texel21);
+                texture.get(ix + 2, iy + 0, texel31);
+                texture.get(ix - 1, iy + 1, texel02);
+                texture.get(ix + 0, iy + 1, texel12);
+                texture.get(ix + 1, iy + 1, texel22);
+                texture.get(ix + 2, iy + 1, texel32);
+                texture.get(ix - 1, iy + 2, texel03);
+                texture.get(ix + 0, iy + 2, texel13);
+                texture.get(ix + 1, iy + 2, texel23);
+                texture.get(ix + 2, iy + 2, texel33);
 
                 for (int i = 0; i < NumChannels; ++i)
                 {
-                    const float bilinear_result =
-                        w00 * texel00[i] +
-                        w10 * texel10[i] +
-                        w01 * texel01[i] +
-                        w11 * texel11[i];
+                    const float rec_result =
+                        cubic_interpolation(
+                            cubic_interpolation(texel00[i], texel10[i], texel20[i], texel30[i], wx),
+                            cubic_interpolation(texel01[i], texel11[i], texel21[i], texel31[i], wx),
+                            cubic_interpolation(texel02[i], texel12[i], texel22[i], texel32[i], wx),
+                            cubic_interpolation(texel03[i], texel13[i], texel23[i], texel33[i], wx),
+                            wy);
 
                     result[i] *= ewa_weight;
-                    result[i] += bilinear_weight * bilinear_result;
+                    result[i] += rec_weight * rec_result;
                 }
             }
         }
@@ -528,6 +553,16 @@ TEST_SUITE(EWAFilteringExploration)
 
         enum { WeightCount = 256 };
         float m_weights[WeightCount];
+
+        static float cubic_interpolation(
+            const float         v0,
+            const float         v1,
+            const float         v2,
+            const float         v3,
+            const float         x)
+        {
+            return v1 + 0.5f * x * (v2 - v0 + x * (2.0f * v0 - 5.0f * v1 + 4.0f * v2 - v3 + x * (3.0f * (v1 - v2) + v3 - v0)));
+        }
     };
 
     //---------------------------------------------------------------------------------------------
