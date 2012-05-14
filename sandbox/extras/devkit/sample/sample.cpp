f97f94f5736e9f730888f95f9baee71f31aaba18
Francois Beaune <beaune@aist.enst.fr>
2012-05-14 11:52:08 +0200

    added first bits of the appleseed devkit.

---
+++ sandbox/extras/devkit/sample/sample.cpp
@@ -0,0 +1,256 @@
+
+//
+// This source file is part of appleseed.
+// Visit http://appleseedhq.net/ for additional information and resources.
+//
+// This software is released under the MIT license.
+//
+// Copyright (c) 2010-2012 Francois Beaune, Jupiter Jazz
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+// appleseed.renderer headers. Only include header files from renderer/api/.
+#include "renderer/api/bsdf.h"
+#include "renderer/api/camera.h"
+#include "renderer/api/color.h"
+#include "renderer/api/environment.h"
+#include "renderer/api/frame.h"
+#include "renderer/api/light.h"
+#include "renderer/api/log.h"
+#include "renderer/api/material.h"
+#include "renderer/api/object.h"
+#include "renderer/api/project.h"
+#include "renderer/api/rendering.h"
+#include "renderer/api/scene.h"
+#include "renderer/api/surfaceshader.h"
+#include "renderer/api/utility.h"
+
+// appleseed.foundation headers.
+#include "foundation/math/matrix.h"
+#include "foundation/math/scalar.h"
+#include "foundation/math/transform.h"
+#include "foundation/utility/containers/specializedarrays.h"
+#include "foundation/utility/log/consolelogtarget.h"
+#include "foundation/utility/autoreleaseptr.h"
+#include "foundation/utility/searchpaths.h"
+
+// Standard headers.
+#include <cstddef>
+#include <cstdio>
+#include <memory>
+#include <string>
+
+// Define shorter namespaces for convenience.
+namespace asf = foundation;
+namespace asr = renderer;
+
+// Set the root path of the sample program. If the working directory is set to bin\Debug
+// or bin\Release (i.e. where the appleseed DLL is located for the current configuration),
+// you don't need to change anything. Otherwise you will need to set this variable to the
+// root path of the sample program (the path to the sample.cpp file).
+static const std::string root_path = "../../sample";
+
+asf::auto_release_ptr<asr::Project> build_project()
+{
+    // Create an empty project.
+    asf::auto_release_ptr<asr::Project> project(asr::ProjectFactory::create("test project"));
+    project->set_path((root_path + "/output/test.appleseed").c_str());
+
+    // Add default configurations to the project.
+    project->add_default_configurations();
+
+    // Set the number of samples. This is basically the quality parameter: the higher the number
+    // of samples, the smoother the image but the longer the rendering time.
+    project->configurations()
+        .get_by_name("final")->get_parameters()
+            .insert_path("generic_tile_renderer.min_samples", "25")
+            .insert_path("generic_tile_renderer.max_samples", "25");
+
+    // Create a scene.
+    asf::auto_release_ptr<asr::Scene> scene(asr::SceneFactory::create());
+
+    // Create an assembly.
+    asf::auto_release_ptr<asr::Assembly> assembly(
+        asr::AssemblyFactory::create(
+            "assembly",
+            asr::ParamArray()));
+
+    // Create a color called "gray" and insert it into the assembly.
+    static const float GrayReflectance[] = { 0.5f, 0.5f, 0.5f };
+    assembly->colors().insert(
+        asr::ColorEntityFactory::create(
+            "gray",
+            asr::ParamArray()
+                .insert("color_space", "srgb"),
+            asr::ColorValueArray(3, GrayReflectance)));
+
+    // Create a BRDF called "diffuse_gray_brdf" and insert it into the assembly.
+    assembly->bsdfs().insert(
+        asr::LambertianBRDFFactory().create(
+            "diffuse_gray_brdf",
+            asr::ParamArray()
+                .insert("reflectance", "gray")));
+
+    // Create a physical surface shader and insert it into the assembly.
+    assembly->surface_shaders().insert(
+        asr::PhysicalSurfaceShaderFactory().create(
+            "physical_surface_shader",
+            asr::ParamArray()));
+
+    // Create a material called "gray_material" and insert it into the assembly.
+    assembly->materials().insert(
+        asr::MaterialFactory::create(
+            "gray_material",
+            asr::ParamArray()
+                .insert("surface_shader", "physical_surface_shader")
+                .insert("bsdf", "diffuse_gray_brdf")));
+
+    // Load the scene geometry from disk.
+    asf::SearchPaths search_paths;
+    search_paths.push_back(root_path + "/data");
+    asr::MeshObjectArray objects =
+        asr::MeshObjectReader::read(
+            search_paths,
+            "cube",
+            asr::ParamArray()
+                .insert("filename", "scene.obj"));
+
+    // Insert all the objects into the assembly.
+    for (size_t i = 0; i < objects.size(); ++i)
+    {
+        // Insert this object into the scene.
+        asr::MeshObject* object = objects[i];
+        assembly->objects().insert(asf::auto_release_ptr<asr::Object>(object));
+
+        // Create the array of material names.
+        asf::StringArray material_names;
+        material_names.push_back("gray_material");
+
+        // Create an instance of this object and insert it into the assembly.
+        const std::string instance_name = std::string(object->get_name()) + "_inst";
+        assembly->object_instances().insert(
+            asr::ObjectInstanceFactory::create(
+                instance_name.c_str(),
+                asr::ParamArray(),
+                *object,
+                asf::Transformd(asf::Matrix4d::identity()),
+                material_names));
+    }
+
+    // Create a color called "light_exitance" and insert it into the assembly.
+    static const float LightExitance[] = { 1.0f, 1.0f, 1.0f };
+    assembly->colors().insert(
+        asr::ColorEntityFactory::create(
+            "light_exitance",
+            asr::ParamArray()
+                .insert("color_space", "srgb")
+                .insert("multiplier", "30.0"),
+            asr::ColorValueArray(3, LightExitance)));
+
+    // Create a point light called "light" and insert it into the assembly.
+    asf::auto_release_ptr<asr::Light> light(
+        asr::PointLightFactory().create(
+            "light",
+            asr::ParamArray()
+                .insert("exitance", "light_exitance")));
+    light->set_transform(asf::Transformd(
+        asf::Matrix4d::translation(asf::Vector3d(0.6, 2.0, 1.0))));
+    assembly->lights().insert(light);
+
+    // Create an instance of the assembly and insert it into the scene.
+    scene->assembly_instances().insert(
+        asr::AssemblyInstanceFactory::create(
+            "assembly_inst",
+            asr::ParamArray(),
+            *assembly,
+            asf::Transformd(asf::Matrix4d::identity())));
+
+    // Insert the assembly into the scene.
+    scene->assemblies().insert(assembly);
+
+    // Create a pinhole camera with film dimensions 0.980 x 0.735 in (24.892 x 18.669 mm).
+    asf::auto_release_ptr<asr::Camera> camera(
+        asr::PinholeCameraFactory().create(
+            "camera",
+            asr::ParamArray()
+                .insert("film_dimensions", "0.024892 0.018669")
+                .insert("focal_length", "0.035")));
+
+    // Place and orient the camera. By default cameras are located in (0.0, 0.0, 0.0)
+    // and are looking toward Z- (0.0, 0.0, -1.0).
+    camera->transform_sequence().set_transform(
+        0.0,
+        asf::Transformd(
+            asf::Matrix4d::rotation(asf::Vector3d(1.0, 0.0, 0.0), asf::deg_to_rad(-20.0)) *
+            asf::Matrix4d::translation(asf::Vector3d(0.0, 0.8, 11.0))));
+
+    // Bind the camera to the scene.
+    scene->set_camera(camera);
+
+    // Create a frame and bind it to the project.
+    project->set_frame(
+        asr::FrameFactory::create(
+            "beauty",
+            asr::ParamArray()
+                .insert("camera", scene->get_camera()->get_name())
+                .insert("resolution", "640 480")
+                .insert("color_space", "srgb")));
+
+    // Create an environment and bind it to the scene.
+    scene->set_environment(
+        asr::EnvironmentFactory::create(
+            "environment",
+            asr::ParamArray()));
+
+    // Bind the scene to the project.
+    project->set_scene(scene);
+
+    return project;
+}
+
+int main()
+{
+    // Create a log target that outputs to stderr, and binds it to the renderer's global logger.
+    // Eventually you will probably want to redirect log messages to your own target. For this
+    // you will need to subclass foundation::LogTargetBase (foundation/utility/log/logtargetbase.h).
+    std::auto_ptr<asf::LogTargetBase> log_target(asf::create_console_log_target(stderr));
+    asr::global_logger().add_target(log_target.get());
+
+    // Build the project.
+    asf::auto_release_ptr<asr::Project> project(build_project());
+
+    // Create the master renderer.
+    asr::DefaultRendererController renderer_controller;
+    asr::MasterRenderer renderer(
+        project.ref(),
+        project->configurations().get_by_name("final")->get_inherited_parameters(),
+        &renderer_controller);
+
+    // Render the frame.
+    renderer.render();
+
+    // Save the frame to disk.
+    project->get_frame()->write((root_path + "/output/test.png").c_str());
+
+    // Save the project to disk.
+    asr::ProjectFileWriter::write(project.ref());
+
+    return 0;
+}
