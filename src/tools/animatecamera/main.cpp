appleseedhq/appleseed

commit 4c74d071e066d118f88681f129a78730315416f3

ported code to boost.filesystem API version 3 and fixed display of paths on Windows (always use \ instead of / on Windows).

src/tools/animatecamera/main.cpp
@@ -202,7 +202,7 @@ namespace
             auto_release_ptr<Project> project(
                 reader.read(
                     g_cl.m_filenames.values()[0].c_str(),
-                    schema_path.file_string().c_str()));
+                    schema_path.string().c_str()));
 
             // Bail out if the master project file couldn't be read.
             if (project.get() == 0)
@@ -526,7 +526,7 @@ int main(int argc, const char* argv[])
     global_logger().add_target(&logger.get_log_target());
 
     const string base_output_filename =
-        filesystem::path(g_cl.m_filenames.values()[1]).stem();
+        filesystem::path(g_cl.m_filenames.values()[1]).stem().string();
 
     auto_ptr<AnimationGenerator> generator;
 

src/appleseed.cli/main.cpp
@@ -85,8 +85,8 @@ namespace
 
         SettingsFileReader reader(logger);
         reader.read(
-            settings_file_path.file_string().c_str(),
-            schema_file_path.file_string().c_str(),
+            settings_file_path.string().c_str(),
+            schema_file_path.string().c_str(),
             g_settings);
     }
 
@@ -194,7 +194,7 @@ namespace
         {
             RENDERER_LOG_WARNING(
                 "automatic benchmark results archiving to %s failed: i/o error.",
-                xmlfile_path.file_string().c_str());
+                xmlfile_path.string().c_str());
         }
 
         const filesystem::path old_current_path =
@@ -348,7 +348,7 @@ namespace
             return
                 reader.read(
                     project_filename.c_str(),
-                    schema_path.file_string().c_str());
+                    schema_path.string().c_str());
         }
     }
 
@@ -427,7 +427,7 @@ namespace
         RENDERER_LOG_INFO("archiving frame to disk...");
         char* archive_path;
         project->get_frame()->archive(
-            autosave_path.directory_string().c_str(),
+            autosave_path.string().c_str(),
             &archive_path);
 
         // Write the frame to disk.

src/appleseed.studio/main/main.cpp
@@ -72,10 +72,11 @@ namespace
 
         // Construct the error message.
         const string informative_text =
-            "Specifically, it was expected that " + executable_path.filename() + " would "
-            "reside in a bin/ subdirectory inside the main directory of the application, "
-            "but it appears not to be the case (" + executable_path.filename() +
-            " seems to be located in " + executable_path.parent_path().directory_string() + ").";
+            "Specifically, it was expected that " + executable_path.filename().string() + " would "
+            "reside in a " + filesystem::path("bin/").make_preferred().string() + " subdirectory "
+            "inside the main directory of the application, but it appears not to be the case "
+            "(" + executable_path.filename().string() +
+            " seems to be located in " + executable_path.parent_path().string() + ").";
 
         // Display a message box.
         QMessageBox msgbox;
@@ -192,7 +193,7 @@ namespace
 
             // Load and apply the stylesheet.
             string stylesheet;
-            if (load_stylesheet(stylesheet_path.file_string(), stylesheet))
+            if (load_stylesheet(stylesheet_path.string(), stylesheet))
                 application.setStyleSheet(QString::fromStdString(stylesheet));
         }
     }

src/tools/normalizeprojectfile/main.cpp
@@ -74,7 +74,7 @@ int main(int argc, const char* argv[])
     auto_release_ptr<Project> project(
         reader.read(
             cl.m_filename.values()[0].c_str(),
-            schema_path.file_string().c_str()));
+            schema_path.string().c_str()));
 
     // Bail out if the project couldn't be loaded.
     if (project.get() == 0)

src/CMakeLists.txt
@@ -121,7 +121,6 @@ include_directories (
 # All configurations.
 set (preprocessor_definitions_common
     ${preprocessor_definitions_common}
-    BOOST_FILESYSTEM_VERSION=2              # TODO: port code to Boost.Filesystem version 3 (Boost >= 1.44)
     APPLESEED_VERSION_STRING="${git_tag}"
 )
 if (CMAKE_SIZEOF_VOID_P MATCHES 4)

src/appleseed.shared/application/application.cpp
@@ -47,14 +47,12 @@ namespace shared {
 // Application class implementation.
 //
 
-// Return true if the application is correctly installed, false otherwise.
 bool Application::is_correctly_installed()
 {
     const char* root_path = get_root_path();
     return strlen(root_path) > 0;
 }
 
-// Check if the application is correctly installed.
 void Application::check_installation(Logger& logger)
 {
     if (!is_correctly_installed())
@@ -66,13 +64,14 @@ void Application::check_installation(Logger& logger)
         LOG_FATAL(
             logger,
             "The application failed to start because it is not properly installed. "
-            "Please reinstall the application.\n\n"
-            "Specifically, it was expected that %s would reside in a bin/ subdirectory "
+            "Please reinstall the application.\n"
+            "Specifically, it was expected that %s would reside in a %s subdirectory "
             "inside the main directory of the application, but it appears not to be "
             "the case (%s seems to be located in %s).",
-            executable_path.filename().c_str(),
-            executable_path.filename().c_str(),
-            executable_path.parent_path().directory_string().c_str());
+            executable_path.filename().string().c_str(),
+            filesystem::path("bin/").make_preferred().string().c_str(),
+            executable_path.filename().string().c_str(),
+            executable_path.parent_path().string().c_str());
     }
 }
 
@@ -104,7 +103,7 @@ namespace
 
     void copy_directory_path_to_buffer(const filesystem::path& path, char* output)
     {
-        const string path_string = path.directory_string();
+        const string path_string = path.string();
 
         assert(path_string.size() <= FOUNDATION_MAX_PATH_LENGTH);
 
@@ -113,7 +112,6 @@ namespace
     }
 }
 
-// Return the root path of the application.
 const char* Application::get_root_path()
 {
     static char root_path_buffer[FOUNDATION_MAX_PATH_LENGTH + 1];
@@ -138,7 +136,6 @@ const char* Application::get_root_path()
     return root_path_buffer;
 }
 
-// Return the root path of the application's tests.
 const char* Application::get_tests_root_path()
 {
     static char tests_root_path_buffer[FOUNDATION_MAX_PATH_LENGTH + 1];
@@ -150,7 +147,7 @@ const char* Application::get_tests_root_path()
 
         if (compute_root_path(root_path))
         {
-            root_path = root_path / filesystem::path("tests/");
+            root_path = root_path / filesystem::path("tests");
             copy_directory_path_to_buffer(root_path, tests_root_path_buffer);
         }
         else

src/appleseed.shared/application/commandlinehandlerbase.cpp
@@ -135,7 +135,7 @@ void CommandLineHandlerBase::parse(
 
     if (impl->m_help.is_set())
     {
-        const string program_name = filesystem::path(argv[0]).filename();
+        const string program_name = filesystem::path(argv[0]).filename().string();
         print_program_usage(program_name.c_str(), logger);
         exit(0);
     }

src/appleseed.studio/mainwindow/mainwindow.cpp
@@ -62,6 +62,7 @@
 #include <QAction>
 #include <QActionGroup>
 #include <QApplication>
+#include <QDir>
 #include <QFileDialog>
 #include <QGridLayout>
 #include <QIcon>
@@ -706,15 +707,15 @@ void MainWindow::slot_open_project()
             &selected_filter,
             options);
 
-    const filesystem::path path(filepath.toStdString());
-
-    filepath = QString::fromStdString(path.file_string());
+    filepath = QDir::toNativeSeparators(filepath);
 
     if (!filepath.isEmpty())
     {
+        const filesystem::path path(filepath.toStdString());
+
         m_settings.insert_path(
             LAST_DIRECTORY_SETTINGS_KEY,
-            path.parent_path().external_directory_string());
+            path.parent_path().string());
 
         const bool successful =
             m_project_manager.load_project(filepath.toAscii().constData());
@@ -800,15 +801,15 @@ void MainWindow::slot_save_project_as()
             &selected_filter,
             options);
 
-    const filesystem::path path(filepath.toStdString());
-
-    filepath = QString::fromStdString(path.file_string());
+    filepath = QDir::toNativeSeparators(filepath);
 
     if (!filepath.isEmpty())
     {
+        const filesystem::path path(filepath.toStdString());
+
         m_settings.insert_path(
             LAST_DIRECTORY_SETTINGS_KEY,
-            path.parent_path().external_directory_string());
+            path.parent_path().string());
 
         m_project_manager.save_project_as(filepath.toAscii().constData());
     }
@@ -889,8 +890,8 @@ void MainWindow::slot_load_settings()
 
     const bool success =
         reader.read(
-            settings_file_path.file_string().c_str(),
-            schema_file_path.file_string().c_str(),
+            settings_file_path.string().c_str(),
+            schema_file_path.string().c_str(),
             settings);
 
     if (success)
@@ -905,7 +906,7 @@ void MainWindow::slot_save_settings()
     SettingsFileWriter writer;
 
     writer.write(
-        settings_file_path.file_string().c_str(),
+        settings_file_path.string().c_str(),
         m_settings);
 }
 
@@ -975,9 +976,7 @@ void MainWindow::slot_save_frame()
             &selected_filter,
             options);
 
-    const filesystem::path path(filepath.toStdString());
-
-    filepath = QString::fromStdString(path.file_string());
+    filepath = QDir::toNativeSeparators(filepath);
 
     if (!filepath.isEmpty())
     {

commit ea08888796006387c800c23b63a8ca7bb3c9b675

fixed regression introduced in 4c74d071e066d118f88681f129a78730315416f3 ("ported code to boost.filesystem API version 3").

src/tools/animatecamera/main.cpp
@@ -182,10 +182,10 @@ namespace
             const filesystem::path path(filename);
 
             stringstream sstr;
-            sstr << path.stem();
+            sstr << path.stem().string();
             sstr << '.';
             sstr << setw(digits) << setfill('0') << number;
-            sstr << path.extension();
+            sstr << path.extension().string();
 
             return sstr.str();
         }