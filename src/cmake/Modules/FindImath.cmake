6e5c21eae9b9df8bde2cb288399736e586947309
Esteban Tovagliari <ramenhdr@gmail.com>
2012-06-24 13:37:49 +0200

    Added CMake FindPackage modules.

---
+++ src/cmake/Modules/FindImath.cmake
@@ -0,0 +1,41 @@
+# - Find Imath
+# Find Imath headers and libraries.
+#
+#  IMATH_INCLUDE_DIRS - where to find IMATH uncludes.
+#  IMATH_LIBRARIES    - List of libraries when using IMATH.
+#  IMATH_FOUND        - True if IMATH found.
+
+# Look for the header file.
+find_path( IMATH_INCLUDE_DIR NAMES OpenEXR/ImathVec.h)
+
+# Look for the libraries.
+find_library( IMATH_HALF_LIBRARY NAMES Half)
+find_library( IMATH_IEX_LIBRARY NAMES Iex)
+find_library( IMATH_MATH_LIBRARY NAMES Imath)
+
+# handle the QUIETLY and REQUIRED arguments and set IMATH_FOUND to TRUE if
+# all listed variables are TRUE
+include( FindPackageHandleStandardArgs)
+find_package_handle_standard_args( IMATH DEFAULT_MSG	IMATH_HALF_LIBRARY
+														IMATH_IEX_LIBRARY
+														IMATH_MATH_LIBRARY
+														IMATH_INCLUDE_DIR
+														)
+# Copy the results to the output variables.
+if(IMATH_FOUND)
+	set( IMATH_LIBRARIES ${IMATH_HALF_LIBRARY}
+							${IMATH_IEX_LIBRARY}
+							${IMATH_MATH_LIBRARY}
+							)
+
+	set( IMATH_INCLUDE_DIRS ${IMATH_INCLUDE_DIR})
+else(IMATH_FOUND)
+	set(IMATH_LIBRARIES)
+	set(IMATH_INCLUDE_DIRS)
+endif(IMATH_FOUND)
+
+mark_as_advanced(	IMATH_HALF_LIBRARY
+					IMATH_IEX_LIBRARY
+					IMATH_MATH_LIBRARY
+					IMATH_INCLUDE_DIR
+					)
---
+++ src/cmake/Modules/FindOpenEXR.cmake
@@ -0,0 +1,36 @@
+# - Find OpenEXR
+# Find OPENEXR headers and libraries.
+#
+#  OPENEXR_INCLUDE_DIRS - where to find OPENEXR uncludes.
+#  OPENEXR_LIBRARIES    - List of libraries when using OPENEXR.
+#  OPENEXR_FOUND        - True if OPENEXR found.
+
+# Look for the header file.
+find_path( OPENEXR_INCLUDE_DIR NAMES OpenEXR/ImfHeader.h)
+
+# Look for the libraries.
+find_library( OPENEXR_IMF_LIBRARY NAMES IlmImf)
+find_library( OPENEXR_THREADS_LIBRARY NAMES IlmThread)
+
+# handle the QUIETLY and REQUIRED arguments and set OPENEXR_FOUND to TRUE if
+# all listed variables are TRUE
+include( FindPackageHandleStandardArgs)
+find_package_handle_standard_args( OPENEXR DEFAULT_MSG  OPENEXR_INCLUDE_DIR
+														OPENEXR_IMF_LIBRARY
+														OPENEXR_THREADS_LIBRARY
+														)
+
+# Copy the results to the output variables.
+if(OPENEXR_FOUND)
+	set( OPENEXR_LIBRARIES  ${OPENEXR_IMF_LIBRARY} ${OPENEXR_THREADS_LIBRARY})
+
+	set( OPENEXR_INCLUDE_DIRS ${OPENEXR_INCLUDE_DIR})
+else(OPENEXR_FOUND)
+	set(OPENEXR_LIBRARIES)
+	set(OPENEXR_INCLUDE_DIRS)
+endif(OPENEXR_FOUND)
+
+mark_as_advanced( OPENEXR_IMF_LIBRARY
+				  OPENEXR_INCLUDE_DIR
+				  OPENEXR_THREADS_LIBRARY
+				  )
