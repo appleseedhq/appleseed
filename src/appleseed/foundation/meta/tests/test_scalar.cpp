b6f86737ee69458c739b33205310323cf9972db1
Francois Beaune <beaune@aist.enst.fr>
2012-07-13 08:53:38 +0200

    homogenized formatting.

--- src/appleseed/foundation/meta/tests/test_scalar.cpp
+++ src/appleseed/foundation/meta/tests/test_scalar.cpp
@@ -152,11 +152,11 @@ TEST_SUITE(Foundation_Math_Scalar)
 
     TEST_CASE(TestFactorial)
     {
-        EXPECT_EQ(1, factorial(0));
-        EXPECT_EQ(1, factorial(1));
-        EXPECT_EQ(2, factorial(2));
-        EXPECT_EQ(6, factorial(3));
-        EXPECT_EQ(24, factorial(4));
+        EXPECT_EQ(1,   factorial(0));
+        EXPECT_EQ(1,   factorial(1));
+        EXPECT_EQ(2,   factorial(2));
+        EXPECT_EQ(6,   factorial(3));
+        EXPECT_EQ(24,  factorial(4));
         EXPECT_EQ(120, factorial(5));
     }
 
