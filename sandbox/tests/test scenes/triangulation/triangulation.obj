appleseedhq/appleseed

commit d25e4d5e224ae2bdc7712025d0696963e53e1993

the triangulator now has an option to keep degenerate triangles in the triangulation.

src/appleseed/foundation/math/triangulator.h
@@ -76,6 +76,15 @@ class Triangulator
     // Index array.
     typedef std::vector<size_t> IndexArray;
 
+    enum Options
+    {
+        Default                 = 0,        // none of the flags below
+        KeepDegenerateTriangles = 1 << 0    // insert degenerate triangles into triangulation
+    };
+
+    // Constructor.
+    explicit Triangulator(const int options = Default);
+
     // Triangulate a polygon.
     // Returns true if triangulation was successful, false otherwise.
     bool triangulate(
@@ -88,9 +97,9 @@ class Triangulator
 
     enum Orientation
     {
-        Degenerate = 0,     // degenerate polygon, no clear orientation
-        CCW,                // counterclockwise
-        CW                  // clockwise
+        Degenerate = 0,                     // degenerate polygon, no clear orientation
+        CCW,                                // counterclockwise orientation
+        CW                                  // clockwise orientation
     };
 
     struct Link
@@ -99,8 +108,9 @@ class Triangulator
         size_t  m_next;
     };
 
-    std::vector<Link>   m_links;
-    Polygon2            m_polygon2;
+    const int               m_options;
+    std::vector<Link>       m_links;
+    Polygon2                m_polygon2;
 
     // Compute the approximate normal of a 3D, nearly planar polygon.
     // The returned normal is not unit-length. It may also be a null
@@ -143,6 +153,12 @@ class Triangulator
 //
 // Triangulator class implementation.
 //
+
+template <typename T>
+Triangulator<T>::Triangulator(const int options)
+  : m_options(options)
+{
+}
 
 template <typename T>
 bool Triangulator<T>::triangulate(
@@ -220,45 +236,43 @@ bool Triangulator<T>::triangulate(
         const Vector2Type e1 = v2 - v0;
         const ValueType det = e0[0] * e1[1] - e1[0] * e0[1];
 
-        // Skip degenerate triangles.
-        if (det == ValueType(0.0))
+        // Only consider convex vertices.
+        if (det < ValueType(0.0))
         {
-            // Remove the current vertex from the list.
-            m_links[prev].m_next = next;
-            m_links[next].m_prev = prev;
-            --remaining_vertices;
+            // Update the failed iteration counter and detect infinite loop.
+            if (++failed_iterations >= remaining_vertices)
+                return false;
 
-            // Continue with the vertex curr + 2 to avoid creating high valence triangles.
-            curr = m_links[next].m_next;
-            failed_iterations = 0;
+            // Consider the next vertex.
+            curr = next;
             continue;
         }
 
-        // Test for convexity and earity.
-        if (det > ValueType(0.0) && is_ear(prev, curr, next))
+        // Insert the triangle into the triangulation.
+        if (det == ValueType(0.0))
         {
-            // Create a new triangle.
+            if (m_options & KeepDegenerateTriangles)
+            {
+                triangles.push_back(prev);
+                triangles.push_back(curr);
+                triangles.push_back(next);
+            }
+        }
+        else if (det > ValueType(0.0) && is_ear(prev, curr, next))
+        {
             triangles.push_back(prev);
             triangles.push_back(curr);
             triangles.push_back(next);
-
-            // Remove the current vertex from the list.
-            m_links[prev].m_next = next;
-            m_links[next].m_prev = prev;
-            --remaining_vertices;
-
-            // Continue with the vertex curr + 2 to avoid creating high valence triangles.
-            curr = m_links[next].m_next;
-            failed_iterations = 0;
-            continue;
         }
 
-        // Update the failed iteration counter and detect infinite loop.
-        if (++failed_iterations >= remaining_vertices)
-            return false;
+        // Remove the current vertex from the list.
+        m_links[prev].m_next = next;
+        m_links[next].m_prev = prev;
+        --remaining_vertices;
 
-        // Consider the next vertex.
-        curr = next;
+        // Continue with the vertex curr + 2 to avoid creating high valence triangles.
+        curr = m_links[next].m_next;
+        failed_iterations = 0;
     }
 
     // Triangulation succeeded.

src/appleseed/foundation/meta/tests/test_triangulator.cpp
@@ -73,6 +73,40 @@ TEST_SUITE(Foundation_Math_Triangulator)
         EXPECT_EQ(TriangulatorType::CW, orientation);
     }
 
+    TEST_CASE_F(Triangulate_GivenQuadWithCoincidentVertices_KeepDegenerateTrianglesIsFalse_ReturnsOneTriangle, Fixture)
+    {
+        TriangulatorType::Polygon3 polygon;
+        polygon.push_back(Vector3Type(0.0, 0.0, 0.0));
+        polygon.push_back(Vector3Type(0.0, 1.0, 0.0));
+        polygon.push_back(Vector3Type(1.0, 1.0, 0.0));
+        polygon.push_back(Vector3Type(0.0, 0.0, 0.0));
+
+        TriangulatorType triangulator;
+        TriangulatorType::IndexArray triangles;
+
+        const bool success = triangulator.triangulate(polygon, triangles);
+
+        ASSERT_TRUE(success);
+        EXPECT_EQ(1 * 3, triangles.size());
+    }
+
+    TEST_CASE_F(Triangulate_GivenQuadWithCoincidentVertices_KeepDegenerateTrianglesIsTrue_ReturnsTwoTriangles, Fixture)
+    {
+        TriangulatorType::Polygon3 polygon;
+        polygon.push_back(Vector3Type(0.0, 0.0, 0.0));
+        polygon.push_back(Vector3Type(0.0, 1.0, 0.0));
+        polygon.push_back(Vector3Type(1.0, 1.0, 0.0));
+        polygon.push_back(Vector3Type(0.0, 0.0, 0.0));
+
+        TriangulatorType triangulator(TriangulatorType::KeepDegenerateTriangles);
+        TriangulatorType::IndexArray triangles;
+
+        const bool success = triangulator.triangulate(polygon, triangles);
+
+        ASSERT_TRUE(success);
+        EXPECT_EQ(2 * 3, triangles.size());
+    }
+
     TEST_CASE_F(Triangulate_GivenSelfCrossingTriangle_ReturnsFalse, Fixture)
     {
         TriangulatorType::Polygon3 polygon;

commit ed3bfd5a1dd69c1f9a47c60586ee3beea06896cb

fixed major regression in foundation::Triangulator introduced in d25e4d5e224ae2bdc7712025d0696963e53e1993 ("the triangulator now has an option to keep degenerate triangles in the triangulation.").

src/appleseed/foundation/math/triangulator.h
@@ -34,6 +34,7 @@
 #include "foundation/math/vector.h"
 #include "foundation/utility/memory.h"
 #include "foundation/utility/test.h"
+#include "foundation/utility/vpythonfile.h"
 
 // Standard headers.
 #include <cassert>
@@ -42,6 +43,8 @@
 
 DECLARE_TEST_CASE(Foundation_Math_Triangulator, ComputePolygonOrientation_GivenLowestLeftmostTriangleIsValid_ReturnsCorrectOrientation);
 DECLARE_TEST_CASE(Foundation_Math_Triangulator, ComputePolygonOrientation_GivenLowestLeftmostTriangleIsDegenerate_ReturnsCorrectOrientation);
+
+#undef FOUNDATION_TRIANGULATOR_DEBUG
 
 namespace foundation
 {
@@ -147,6 +150,13 @@ class Triangulator
         const size_t        prev,
         const size_t        curr,
         const size_t        next) const;
+
+    void debug(
+        const size_t        remaining_vertices,
+        const size_t        failed_iterations,
+        const size_t        prev,
+        const size_t        curr,
+        const size_t        next) const;
 };
 
 
@@ -226,6 +236,10 @@ bool Triangulator<T>::triangulate(
         const size_t prev = m_links[curr].m_prev;
         const size_t next = m_links[curr].m_next;
 
+#ifdef FOUNDATION_TRIANGULATOR_DEBUG
+        debug(remaining_vertices, failed_iterations, prev, curr, next);
+#endif
+
         // Fetch the vertices of the triangle (prev, curr, next).
         const Vector2Type& v0 = m_polygon2[prev];
         const Vector2Type& v1 = m_polygon2[curr];
@@ -236,21 +250,9 @@ bool Triangulator<T>::triangulate(
         const Vector2Type e1 = v2 - v0;
         const ValueType det = e0[0] * e1[1] - e1[0] * e0[1];
 
-        // Only consider convex vertices.
-        if (det < ValueType(0.0))
-        {
-            // Update the failed iteration counter and detect infinite loop.
-            if (++failed_iterations >= remaining_vertices)
-                return false;
-
-            // Consider the next vertex.
-            curr = next;
-            continue;
-        }
-
-        // Insert the triangle into the triangulation.
         if (det == ValueType(0.0))
         {
+            // Degenerate triangle.
             if (m_options & KeepDegenerateTriangles)
             {
                 triangles.push_back(prev);
@@ -260,9 +262,20 @@ bool Triangulator<T>::triangulate(
         }
         else if (det > ValueType(0.0) && is_ear(prev, curr, next))
         {
+            // Ear.
             triangles.push_back(prev);
             triangles.push_back(curr);
             triangles.push_back(next);
+        }
+        else
+        {
+            // Not an ear: update the failed iteration counter and detect infinite loop.
+            if (++failed_iterations >= remaining_vertices)
+                return false;
+
+            // Consider the next vertex.
+            curr = next;
+            continue;
         }
 
         // Remove the current vertex from the list.
@@ -457,16 +470,57 @@ bool Triangulator<T>::is_ear(
     // of the triangle (prev, curr, next) then this is not an ear. In practice,
     // it is equivalent and more efficient to check only the reflex vertices.
     // todo: check only the reflex vertices.
-    for (size_t i = m_links[curr].m_next; i != curr; i = m_links[i].m_next)
+    for (size_t i = m_links[next].m_next; i != prev; i = m_links[i].m_next)
     {
-        if (i == prev || i == next)
-            continue;
-
         if (point_in_triangle(m_polygon2[i], v0, v1, v2))
             return false;
     }
 
     return true;
+}
+
+template <typename T>
+void Triangulator<T>::debug(
+    const size_t            remaining_vertices,
+    const size_t            failed_iterations,
+    const size_t            prev,
+    const size_t            curr,
+    const size_t            next) const
+{
+    // Open a VPython file.
+    char filename[100];
+    std::sprintf(filename, "poly-remaining=%d-attempt=%d.py", remaining_vertices, failed_iterations + 1);
+    VPythonFile file(filename);
+
+    // Draw the remaining polygon.
+    std::vector<Vector3d> poly;
+    for (size_t i = 0, c = curr; i < remaining_vertices + 1; ++i, c = m_links[c].m_next)
+        poly.push_back(Vector3d(m_polygon2[c].x, m_polygon2[c].y, 0.0));
+    file.draw_polyline(poly.size(), &poly[0], "white", 0.0025);
+
+    // Fetch the vertices of the candidate ear.
+    const Vector2Type& v0 = m_polygon2[prev];
+    const Vector2Type& v1 = m_polygon2[curr];
+    const Vector2Type& v2 = m_polygon2[next];
+
+    // Compute the orientation of the candidate ear.
+    const Vector2Type e0 = v1 - v0;
+    const Vector2Type e1 = v2 - v0;
+    const ValueType det = e0[0] * e1[1] - e1[0] * e0[1];
+
+    // Determine if this triangle is effectively an ear.
+    const bool ear = det > ValueType(0.0) && is_ear(prev, curr, next);
+
+    // Draw the candidate ear.
+    poly.clear();
+    poly.push_back(Vector3d(v0.x, v0.y, 0.0));
+    poly.push_back(Vector3d(v1.x, v1.y, 0.0));
+    poly.push_back(Vector3d(v2.x, v2.y, 0.0));
+    poly.push_back(Vector3d(v0.x, v0.y, 0.0));
+    file.draw_polyline(poly.size(), &poly[0], ear ? "green" : "red", 0.0025);
+
+    // Highlight the tip of the candidate ear.
+    file.draw_point(Vector3d(v1.x, v1.y, 0.0), "yellow", 10);
 }
 
 }       // namespace foundation

sandbox/tests/test scenes/triangulation/triangulation.appleseed
@@ -1,0 +1,56 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!-- File generated by appleseed.dll version 1.1.0-alpha-13-54-g6ddfcec. -->
+<project format_revision="2">
+    <scene>
+        <camera name="camera" model="pinhole_camera">
+            <parameter name="controller_target" value="0.159616 -0.0322777 0.179968" />
+            <parameter name="film_dimensions" value="0.024892 0.018669" />
+            <parameter name="focal_length" value="0.035" />
+            <transform time="0">
+                <matrix>
+                    0.326335200485926 0.027462687767263 0.944855088203693 3.894091330925785
+                    0.000000000000000 0.999577865733700 -0.029053232785040 -0.147108622195326
+                    -0.945254112354879 0.009481092545670 0.326197443215501 1.469241159353234
+                    0.000000000000000 0.000000000000000 0.000000000000000 1.000000000000000
+                </matrix>
+            </transform>
+        </camera>
+        <environment name="environment" model="generic_environment" />
+        <assembly name="assembly">
+            <object name="triangulation" model="mesh_object">
+                <parameter name="filename" value="triangulation.obj" />
+            </object>
+            <object_instance name="triangulation.plug_inst" object="triangulation.plug">
+                <transform>
+                    <matrix>
+                        1.000000000000000 0.000000000000000 0.000000000000000 0.000000000000000
+                        0.000000000000000 1.000000000000000 0.000000000000000 0.000000000000000
+                        0.000000000000000 0.000000000000000 1.000000000000000 0.000000000000000
+                        0.000000000000000 0.000000000000000 0.000000000000000 1.000000000000000
+                    </matrix>
+                </transform>
+            </object_instance>
+        </assembly>
+        <assembly_instance name="assembly_inst" assembly="assembly">
+            <transform>
+                <matrix>
+                    1.000000000000000 0.000000000000000 0.000000000000000 0.000000000000000
+                    0.000000000000000 1.000000000000000 0.000000000000000 0.000000000000000
+                    0.000000000000000 0.000000000000000 1.000000000000000 0.000000000000000
+                    0.000000000000000 0.000000000000000 0.000000000000000 1.000000000000000
+                </matrix>
+            </transform>
+        </assembly_instance>
+    </scene>
+    <output>
+        <frame name="beauty">
+            <parameter name="camera" value="camera" />
+            <parameter name="color_space" value="srgb" />
+            <parameter name="resolution" value="640 480" />
+        </frame>
+    </output>
+    <configurations>
+        <configuration name="final" base="base_final" />
+        <configuration name="interactive" base="base_interactive" />
+    </configurations>
+</project>

sandbox/tests/test scenes/triangulation/triangulation.obj
@@ -1,0 +1,124 @@
+mtllib triangulation.mtl
+o triangulation.obj
+v 0.049557 0.021480 1.156520
+v 0.049704 0.022175 1.156409
+v 0.049704 0.022175 1.325500
+v 0.022276 -0.111231 1.311679
+v 0.022278 -0.111231 1.082112
+v 0.023243 -0.106559 1.091280
+v 0.030437 -0.071545 1.126294
+v 0.039505 -0.027427 1.148774
+v 0.037052 0.149520 1.091280
+v 0.031912 0.172000 1.047161
+v 0.030140 0.179745 0.998254
+v 0.030142 0.179745 -0.975371
+v 0.031914 0.172000 -1.024278
+v 0.037054 0.149520 -1.068396
+v 0.040007 0.136612 -1.081305
+v 0.040007 0.136612 -1.304469
+v 0.013990 0.250376 -1.311604
+v 0.013990 0.250376 1.309558
+v 0.040003 0.136612 1.318777
+v 0.040003 0.136612 1.104188
+v 0.041878 0.114506 1.126294
+v 0.045616 0.070388 1.148774
+v 0.049704 0.022175 -1.133527
+v 0.049704 0.022175 -1.299541
+v 0.041880 0.114506 -1.103411
+v 0.045618 0.070388 -1.125891
+v 0.022295 -0.111231 -1.059228
+v 0.022297 -0.111231 -1.309291
+v 0.049566 0.021480 -1.133636
+v 0.039515 -0.027427 -1.125891
+v 0.030447 -0.071545 -1.103411
+v 0.023251 -0.106559 -1.068396
+v 0.013069 -0.129039 1.047161
+v -0.049702 -0.250376 1.291198
+v -0.049702 -0.250376 -1.325500
+v 0.013077 -0.129039 -1.024278
+v 0.009069 -0.136785 -0.975371
+v 0.009062 -0.136785 0.998254
+vn 0.979176 -0.203013 -0.000159
+vn 0.998205 -0.059887 -0.000080
+vn 0.998205 -0.059887 -0.000080
+vn 0.942442 -0.334370 -0.000077
+vn 0.942442 -0.334370 -0.000077
+vn 0.979176 -0.203013 -0.000159
+vn 0.979176 -0.203013 -0.000159
+vn 0.979176 -0.203013 -0.000159
+vn 0.974838 0.222916 0.000002
+vn 0.974838 0.222916 0.000002
+vn 0.974838 0.222916 0.000002
+vn 0.974838 0.222916 0.000002
+vn 0.974838 0.222916 0.000002
+vn 0.974838 0.222916 0.000002
+vn 0.988063 0.154052 0.000001
+vn 0.988063 0.154052 0.000001
+vn 0.974838 0.222916 0.000002
+vn 0.974838 0.222916 0.000002
+vn 0.988058 0.154080 0.000001
+vn 0.988058 0.154080 0.000001
+vn 0.996425 0.084487 0.000000
+vn 0.996425 0.084487 0.000000
+vn 0.998319 -0.057955 -0.000113
+vn 0.998319 -0.057955 -0.000113
+vn 0.996429 0.084431 0.000000
+vn 0.996429 0.084431 0.000000
+vn 0.943097 -0.332519 -0.000110
+vn 0.943097 -0.332519 -0.000110
+vn 0.979966 -0.199166 -0.000223
+vn 0.979966 -0.199166 -0.000223
+vn 0.979966 -0.199166 -0.000223
+vn 0.979966 -0.199166 -0.000223
+vn 0.888174 -0.459507 0.000005
+vn 0.888174 -0.459507 0.000005
+vn 0.888174 -0.459507 0.000005
+vn 0.888174 -0.459507 0.000005
+vn 0.888174 -0.459507 0.000005
+vn 0.888174 -0.459507 0.000005
+vt 0.210302 0.719440
+vt 0.210305 0.719885
+vt 0.205693 0.719885
+vt 0.206070 0.634388
+vt 0.212331 0.634388
+vt 0.212081 0.637382
+vt 0.211126 0.659822
+vt 0.210513 0.688096
+vt 0.212081 0.801498
+vt 0.213284 0.815905
+vt 0.214618 0.820869
+vt 0.268443 0.820869
+vt 0.269777 0.815905
+vt 0.270980 0.801498
+vt 0.271332 0.793225
+vt 0.277418 0.793225
+vt 0.277613 0.866134
+vt 0.206128 0.866134
+vt 0.205877 0.793225
+vt 0.211729 0.793225
+vt 0.211126 0.779058
+vt 0.210513 0.750783
+vt 0.272756 0.719885
+vt 0.277284 0.719885
+vt 0.271935 0.779058
+vt 0.272548 0.750783
+vt 0.270730 0.634388
+vt 0.277550 0.634388
+vt 0.272759 0.719440
+vt 0.272548 0.688096
+vt 0.271935 0.659822
+vt 0.270980 0.637382
+vt 0.213284 0.622975
+vt 0.206629 0.545212
+vt 0.277992 0.545212
+vt 0.269777 0.622975
+vt 0.268443 0.618010
+vt 0.214618 0.618010
+g plug
+usemtl Default
+f 1/1/1 2/2/2 3/3/3 4/4/4 5/5/5 6/6/6 7/7/7 8/8/8 
+f 9/9/9 10/10/10 11/11/11 12/12/12 13/13/13 14/14/14 15/15/15 16/16/16 17/17/17 18/18/18 19/19/19 20/20/20 
+f 21/21/21 20/20/20 19/19/19 3/3/3 2/2/2 22/22/22 
+f 23/23/23 24/24/24 16/16/16 15/15/15 25/25/25 26/26/26 
+f 27/27/27 28/28/28 24/24/24 23/23/23 29/29/29 30/30/30 31/31/31 32/32/32 
+f 33/33/33 5/5/5 4/4/4 34/34/34 35/35/35 28/28/28 27/27/27 36/36/36 37/37/37 38/38/38 