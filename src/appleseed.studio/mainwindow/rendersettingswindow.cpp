appleseedhq/appleseed

commit 7a2ce241ef241096bfb4fcd365ac911d4c78d2fd

present configurations in alphabetical order in the Render Settings dialog.

src/appleseed.studio/mainwindow/rendersettingswindow.cpp
@@ -125,15 +125,22 @@ RenderSettingsWindow::~RenderSettingsWindow()
 
 void RenderSettingsWindow::reload()
 {
-    m_current_configuration_name.clear();
-
-    m_ui->combobox_configurations->clear();
+    vector<QString> configs;
 
     for (const_each<ConfigurationContainer> i = m_project_manager.get_project()->configurations(); i; ++i)
     {
         if (!BaseConfigurationFactory::is_base_configuration(i->get_name()))
-            m_ui->combobox_configurations->addItem(i->get_name());
+            configs.push_back(i->get_name());
     }
+
+    sort(configs.begin(), configs.end());
+
+    m_ui->combobox_configurations->clear();
+
+    for (size_t i = 0; i < configs.size(); ++i)
+        m_ui->combobox_configurations->addItem(configs[i]);
+
+    m_current_configuration_name.clear();
 }
 
 void RenderSettingsWindow::create_panels()

commit fdcc7dd25667d29347ad9a7d9414b6676d671c01

fixed regression introduced in 7a2ce241ef241096bfb4fcd365ac911d4c78d2fd ("present configurations in alphabetical order in the Render Settings dialog").

src/appleseed.studio/mainwindow/rendersettingswindow.cpp
@@ -135,12 +135,11 @@ void RenderSettingsWindow::reload()
 
     sort(configs.begin(), configs.end());
 
+    m_current_configuration_name.clear();
     m_ui->combobox_configurations->clear();
 
     for (size_t i = 0; i < configs.size(); ++i)
         m_ui->combobox_configurations->addItem(configs[i]);
-
-    m_current_configuration_name.clear();
 }
 
 void RenderSettingsWindow::create_panels()