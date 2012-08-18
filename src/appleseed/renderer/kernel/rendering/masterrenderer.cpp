appleseedhq/appleseed

commit 482aa7d20fc6bd9ed7a8a5ef2cb6cf969750dbd9

abort rendering if any of the entities' on_frame_begin() method fails.

src/appleseed/renderer/kernel/rendering/masterrenderer.cpp
@@ -389,8 +389,11 @@ IRendererController::Status MasterRenderer::render_frame_sequence(IFrameRenderer
     while (true) 
     {
         assert(!frame_renderer->is_rendering());
+
+        if (!m_project.get_scene()->on_frame_begin(m_project))
+            return IRendererController::AbortRendering;
+
         m_renderer_controller->on_frame_begin();
-        m_project.get_scene()->on_frame_begin(m_project);
 
         frame_renderer->start_rendering();
 
@@ -412,7 +415,9 @@ IRendererController::Status MasterRenderer::render_frame_sequence(IFrameRenderer
         }
 
         assert(!frame_renderer->is_rendering());
+
         m_project.get_scene()->on_frame_end(m_project);
+
         m_renderer_controller->on_frame_end();
 
         if (status != IRendererController::RestartRendering)

src/appleseed/renderer/kernel/rendering/progressive/progressiveframerenderer.cpp
@@ -147,7 +147,8 @@ namespace
             m_abort_switch.abort();
 
             // Wait until the statistics printing thread is terminated.
-            m_statistics_thread->join();
+            if (m_statistics_thread.get())
+                m_statistics_thread->join();
 
             // Delete tile callbacks.
             for (const_each<TileCallbackVector> i = m_tile_callbacks; i; ++i)

src/appleseed/renderer/modeling/bsdf/ashikhminbrdf.cpp
@@ -105,11 +105,12 @@ namespace
             return Model;
         }
 
-        virtual void on_frame_begin(
+        virtual bool on_frame_begin(
             const Project&      project,
             const Assembly&     assembly) override
         {
-            BSDF::on_frame_begin(project, assembly);
+            if (!BSDF::on_frame_begin(project, assembly))
+                return false;
 
             m_uniform_reflectance =
                 m_inputs.source("diffuse_reflectance")->is_uniform() &&
@@ -130,6 +131,8 @@ namespace
 
             if (m_uniform_shininess)
                 compute_sval(m_uniform_sval, values->m_nu, values->m_nv);
+
+            return true;
         }
 
         FORCE_INLINE virtual Mode sample(

src/appleseed/renderer/modeling/bsdf/bsdf.cpp
@@ -61,10 +61,11 @@ BSDF::BSDF(
     set_name(name);
 }
 
-void BSDF::on_frame_begin(
+bool BSDF::on_frame_begin(
     const Project&      project,
     const Assembly&     assembly)
 {
+    return true;
 }
 
 void BSDF::on_frame_end(

src/appleseed/renderer/modeling/bsdf/bsdf.h
@@ -104,7 +104,8 @@ class DLLSYMBOL BSDF
     Type get_type() const;
 
     // This method is called once before rendering each frame.
-    virtual void on_frame_begin(
+    // Returns true on success, false otherwise.
+    virtual bool on_frame_begin(
         const Project&              project,
         const Assembly&             assembly);
 

src/appleseed/renderer/modeling/bsdf/bsdfmix.cpp
@@ -86,17 +86,20 @@ namespace
             return Model;
         }
 
-        virtual void on_frame_begin(
+        virtual bool on_frame_begin(
             const Project&      project,
             const Assembly&     assembly) override
         {
-            BSDF::on_frame_begin(project, assembly);
+            if (!BSDF::on_frame_begin(project, assembly))
+                return false;
 
             m_bsdf[0] = retrieve_bsdf(assembly, "bsdf0");
             m_bsdf[1] = retrieve_bsdf(assembly, "bsdf1");
 
             m_bsdf_data_offset[0] = get_inputs().compute_data_size();
             m_bsdf_data_offset[1] = m_bsdf_data_offset[0] + m_bsdf[0]->compute_input_data_size(assembly);
+
+            return true;
         }
 
         virtual size_t compute_input_data_size(

src/appleseed/renderer/modeling/bsdf/kelemenbrdf.cpp
@@ -108,11 +108,12 @@ namespace
             return Model;
         }
 
-        virtual void on_frame_begin(
+        virtual bool on_frame_begin(
             const Project&      project,
             const Assembly&     assembly) override
         {
-            BSDF::on_frame_begin(project, assembly);
+            if (!BSDF::on_frame_begin(project, assembly))
+                return false;
 
             // todo: implement proper error handling.
             assert(m_inputs.source("specular_reflectance")->is_uniform());
@@ -143,6 +144,8 @@ namespace
             m_s /= s_denom;
 
             // plot_specular_albedo_curves();
+
+            return true;
         }
 
         virtual void on_frame_end(

src/appleseed/renderer/modeling/bsdf/lambertianbrdf.cpp
@@ -86,11 +86,12 @@ namespace
             return Model;
         }
 
-        virtual void on_frame_begin(
+        virtual bool on_frame_begin(
             const Project&      project,
             const Assembly&     assembly) override
         {
-            BSDF::on_frame_begin(project, assembly);
+            if (!BSDF::on_frame_begin(project, assembly))
+                return false;
 
             if (m_inputs.source("reflectance")->is_uniform() &&
                 m_inputs.source("reflectance_multiplier")->is_uniform())
@@ -104,6 +105,8 @@ namespace
                 m_brdf_value = values->m_reflectance;
                 m_brdf_value *= static_cast<float>(values->m_reflectance_multiplier * RcpPi);
             }
+
+            return true;
         }
 
         FORCE_INLINE virtual Mode sample(

commit 740084e4c79499228352f32a8459399a298096e1

fixed a regression introduced in 482aa7d20fc6bd9ed7a8a5ef2cb6cf969750dbd9 ("abort rendering if any of the entities' on_frame_begin() method fails"): the on_frame_begin() method of the renderer controller (which might modify the transform of the active camera) needs to be called *before* the on_frame_begin() method of the scene which assumes the scene is up-to-date and ready to be rendered.

src/appleseed/renderer/kernel/rendering/masterrenderer.cpp
@@ -390,10 +390,16 @@ IRendererController::Status MasterRenderer::render_frame_sequence(IFrameRenderer
     {
         assert(!frame_renderer->is_rendering());
 
-        if (!m_project.get_scene()->on_frame_begin(m_project))
-            return IRendererController::AbortRendering;
-
+        // The renderer controller might alter the scene (like the transform of the camera).
+        // It needs to be called before renderer::Scene::on_frame_begin() which assumes the
+        // scene is up-to-date and ready to be rendered.
         m_renderer_controller->on_frame_begin();
+
+        if (!m_project.get_scene()->on_frame_begin(m_project))
+        {
+            m_renderer_controller->on_frame_end();
+            return IRendererController::AbortRendering;
+        }
 
         frame_renderer->start_rendering();
 