7728b2bfdb775ea54dcadf5b94be9f4acca6da7e
Esteban Tovagliari <ramenhdr@gmail.com>
2012-08-18 15:30:17 +0200

    Improved image related classes py bindings.

--- src/appleseed.py/bind_image.cpp
+++ src/appleseed.py/bind_image.cpp
@@ -28,6 +28,8 @@
 // Has to be first, to avoid redifinition warnings.
 #include "bind_auto_release_ptr.h"
 
+#include <algorithm>
+
 #include "foundation/image/image.h"
 #include "renderer/kernel/aov/tilestack.h"
 #include "renderer/kernel/aov/imagestack.h"
@@ -39,19 +41,87 @@ using namespace renderer;
 namespace detail
 {
 
+std::string image_stack_get_name( const ImageStack *img, const size_t index)
+{
+    return img->get_name( index);
+}
+
+void tile_data_to_py_array( const Tile& tile, bpy::object& buffer)
+{
+    void *array = 0;
+    Py_ssize_t len;
+
+    int success = PyObject_AsWriteBuffer( buffer.ptr(), &array, &len);
+
+    if(success != 0)
+        bpy::throw_error_already_set();
+
+    if (len < tile.get_size())
+    {
+        PyErr_SetString(PyExc_IndexError, "Buffer size is smaller than data size");
+        bpy::throw_error_already_set();
+    }
+
+    std::copy( tile.get_storage(), tile.get_storage() + tile.get_size(), reinterpret_cast<uint8*>( array));
+}
+
 } // detail
 
 void bind_image()
 {
-    bpy::class_<Tile, boost::noncopyable>( "Tile", bpy::no_init)
+
+    bpy::enum_<PixelFormat>( "PixelFormat")
+        .value( "PixelFormatUInt8"  , PixelFormatUInt8)
+        .value( "PixelFormatUInt16" , PixelFormatUInt16)
+        .value( "PixelFormatUInt32" , PixelFormatUInt32)
+        .value( "PixelFormatHalf"   , PixelFormatHalf)
+        .value( "PixelFormatFloat"  , PixelFormatFloat)
+        .value( "PixelFormatDouble" , PixelFormatDouble)
         ;
+
+    bpy::class_<Tile, boost::noncopyable>( "Tile", bpy::no_init)
+        .def( "get_pixel_format", &Tile::get_pixel_format)
+        .def( "get_width", &Tile::get_width)
+        .def( "get_height", &Tile::get_height)
+        .def( "get_channel_count", &Tile::get_channel_count)
+        .def( "get_pixel_count", &Tile::get_pixel_count)
+        .def( "get_size", &Tile::get_size)
+        .def( "copy_data_to", detail::tile_data_to_py_array) // TODO: maybe this needs a better name.
+        ;
+
+    bpy::class_<CanvasProperties, boost::noncopyable>( "CanvasProperties", bpy::no_init)
+        .def_readonly( "canvas_width", &CanvasProperties::m_canvas_width)
+        .def_readonly( "canvas_height", &CanvasProperties::m_canvas_height)
+        .def_readonly( "tile_width", &CanvasProperties::m_tile_width)
+        .def_readonly( "tile_height", &CanvasProperties::m_tile_height)
+        .def_readonly( "channel_count", &CanvasProperties::m_channel_count)
+        .def_readonly( "pixel_format", &CanvasProperties::m_pixel_format)
+
+        .def_readonly( "rcp_canvas_width", &CanvasProperties::m_rcp_canvas_width)
+        .def_readonly( "rcp_canvas_height", &CanvasProperties::m_rcp_canvas_height)
+        .def_readonly( "rcp_tile_width", &CanvasProperties::m_rcp_tile_width)
+        .def_readonly( "rcp_tile_height", &CanvasProperties::m_rcp_tile_height)
+        .def_readonly( "tile_count_x", &CanvasProperties::m_tile_count_x)
+        .def_readonly( "tile_count_y", &CanvasProperties::m_tile_count_y)
+        .def_readonly( "tile_count", &CanvasProperties::m_tile_count)
+        .def_readonly( "pixel_count", &CanvasProperties::m_pixel_count)
+        .def_readonly( "pixel_size", &CanvasProperties::m_pixel_size)
+
+        .def( "get_tile_width", &CanvasProperties::get_tile_width)
+        .def( "get_tile_height", &CanvasProperties::get_tile_height)
+        ;
+
+    const Tile& (Image::* image_get_tile)( const size_t, const size_t) const = &Image::tile;
 
     bpy::class_<Image, boost::noncopyable>( "Image", bpy::no_init)
-        ;
-
-    bpy::class_<TileStack, boost::noncopyable>( "TileStack", bpy::no_init)
+        .def( "properties", &Image::properties, bpy::return_value_policy<bpy::reference_existing_object>())
+        .def( "tile", image_get_tile, bpy::return_value_policy<bpy::reference_existing_object>())
         ;
 
     bpy::class_<ImageStack, boost::noncopyable>( "ImageStack", bpy::no_init)
+        .def( "empty", &ImageStack::empty)
+        .def( "size", &ImageStack::size)
+        .def( "get_name", detail::image_stack_get_name)
+        .def( "get_image", &ImageStack::get_image, bpy::return_value_policy<bpy::reference_existing_object>())
         ;
 }
