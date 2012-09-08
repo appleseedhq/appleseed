81fa376ad08b9645e4014bd11af711a43f161a95
Francois Beaune <beaune@aist.enst.fr>
2012-09-08 18:43:13 +0200

    exposed bpy_dict_to_dictionary() and dictionary_to_bpy_dict(), plus minor formatting tweaks.

--- src/appleseed.python/dict2dict.cpp
+++ src/appleseed.python/dict2dict.cpp
@@ -28,191 +28,192 @@
 
 #include "dict2dict.h"
 
+#include "renderer/utility/paramarray.h"
+
 #include "foundation/math/vector.h"
-#include "foundation/utility/string.h"
+#include "foundation/utility/containers/dictionary.h"
 #include "foundation/utility/foreach.h"
 #include "foundation/utility/iostreamop.h"
+#include "foundation/utility/string.h"
 
 namespace bpy = boost::python;
 using namespace foundation;
 using namespace renderer;
 
-ParamArray& push_dict(ParamArray& dict, const char* name)
+namespace
 {
-    return dict.push(name);
-}
-
-Dictionary& push_dict(Dictionary& dict, const char* name)
-{
-    if (!dict.dictionaries().exist(name))
-        dict.dictionaries().insert(name, Dictionary());
-
-    return dict.dictionaries().get(name);
-}
-
-template<class Dict>
-Dict convert_from_bpy_dict(const bpy::dict& d)
-{
-    Dict result;
-
-    bpy::list values = d.values();
-    bpy::list keys = d.keys();
-
-    for (int i = 0, e = bpy::len(d); i < e; ++i)
+    ParamArray& push_dict(ParamArray& dict, const char* name)
     {
-        bpy::object key(keys[i] );
-        bpy::object value(values[i] );
+        return dict.push(name);
+    }
 
-        // keys
-        bpy::extract<const char*> key_extractor(key );
-        if (!key_extractor.check() )
+    Dictionary& push_dict(Dictionary& dict, const char* name)
+    {
+        if (!dict.dictionaries().exist(name))
+            dict.dictionaries().insert(name, Dictionary());
+
+        return dict.dictionaries().get(name);
+    }
+
+    template <class Dict>
+    Dict convert_from_bpy_dict(const bpy::dict& d)
+    {
+        Dict result;
+
+        bpy::list values = d.values();
+        bpy::list keys = d.keys();
+
+        for (int i = 0, e = bpy::len(d); i < e; ++i)
         {
-            PyErr_SetString(PyExc_TypeError, "Incompatible key type. Only strings accepted." );
+            bpy::object key(keys[i] );
+            bpy::object value(values[i] );
+
+            // keys
+            bpy::extract<const char*> key_extractor(key );
+            if (!key_extractor.check() )
+            {
+                PyErr_SetString(PyExc_TypeError, "Incompatible key type. Only strings accepted." );
+                bpy::throw_error_already_set();
+            }
+
+            // string
+            {
+                bpy::extract<const char*> extractor(value );
+                if (extractor.check())
+                {
+                    result.insert(key_extractor(), extractor());
+                    continue;
+                }
+            }
+
+            // int
+            {
+                bpy::extract<int> extractor(value );
+                if (extractor.check())
+                {
+                    result.insert(key_extractor(), extractor());
+                    continue;
+                }
+            }
+
+            // double
+            {
+                bpy::extract<double> extractor(value );
+                if (extractor.check())
+                {
+                    result.insert(key_extractor(), extractor());
+                    continue;
+                }
+            }
+
+            // Vector2i
+            {
+                bpy::extract<Vector2i> extractor(value );
+                if (extractor.check())
+                {
+                    result.insert(key_extractor(), extractor());
+                    continue;
+                }
+            }
+
+            // Vector2f
+            {
+                bpy::extract<Vector2f> extractor(value );
+                if (extractor.check())
+                {
+                    result.insert(key_extractor(), extractor());
+                    continue;
+                }
+            }
+
+            // Vector2d
+            {
+                bpy::extract<Vector2d> extractor(value );
+                if (extractor.check())
+                {
+                    result.insert(key_extractor(), extractor());
+                    continue;
+                }
+            }
+
+            // TODO: add conversions from bpy::tuple to Vector<T,N>.
+            // ...
+
+            // TODO: check more types here if needed... (est.)
+
+            // dict
+            {
+                bpy::extract<bpy::dict> extractor(value );
+                if (extractor.check())
+                {
+                    // recurse
+                    push_dict(result, key_extractor()) = convert_from_bpy_dict<Dict>(extractor());
+                    continue;
+                }
+            }
+
+            PyErr_SetString(PyExc_TypeError, "Incompatible value type - must be string, int, double or dictionary." );
             bpy::throw_error_already_set();
         }
 
-        // string
-        {
-            bpy::extract<const char*> extractor(value );
-            if (extractor.check())
-            {
-                result.insert(key_extractor(), extractor());
-                continue;
-            }
-        }
+        return result;
+    }
 
-        // int
+    bpy::object obj_from_string(const std::string& str)
+    {
+        // guess the type of the value represented by str.
+        try
         {
-            bpy::extract<int> extractor(value );
-            if (extractor.check())
-            {
-                result.insert(key_extractor(), extractor());
-                continue;
-            }
+            // bool
+            bool b = from_string<bool>(str);
+            return bpy::object(b);
         }
+        catch (ExceptionStringConversionError&) {}
 
-        // double
+        try
         {
-            bpy::extract<double> extractor(value );
-            if (extractor.check())
-            {
-                result.insert(key_extractor(), extractor());
-                continue;
-            }
+            // int / double
+            double d = from_string<double>(str);
+            return bpy::object(d);
         }
+        catch (ExceptionStringConversionError&) {}
 
-        // Vector2i
+        try
         {
-            bpy::extract<Vector2i> extractor(value );
-            if (extractor.check())
-            {
-                result.insert(key_extractor(), extractor());
-                continue;
-            }
+            // Vector2i
+            Vector2i v = from_string<Vector2i>(str);
+            return bpy::object(v);
         }
+        catch (ExceptionStringConversionError&) {}
 
-        // Vector2f
+        try
         {
-            bpy::extract<Vector2f> extractor(value );
-            if (extractor.check())
-            {
-                result.insert(key_extractor(), extractor());
-                continue;
-            }
+            // Vector2f
+            Vector2f v = from_string<Vector2f>(str);
+            return bpy::object(v);
         }
+        catch (ExceptionStringConversionError&) {}
 
-        // Vector2d
+        try
         {
-            bpy::extract<Vector2d> extractor(value );
-            if (extractor.check())
-            {
-                result.insert(key_extractor(), extractor());
-                continue;
-            }
+            // Vector2d
+            Vector2d v = from_string<Vector2d>(str);
+            return bpy::object(v);
         }
+        catch (ExceptionStringConversionError&) {}
 
         // TODO: add conversions from bpy::tuple to Vector<T,N>.
-        // ...
 
         // TODO: check more types here if needed... (est.)
 
-        // dict
-        {
-            bpy::extract<bpy::dict> extractor(value );
-            if (extractor.check())
-            {
-                // recurse
-                push_dict(result, key_extractor()) = convert_from_bpy_dict<Dict>(extractor());
-                continue;
-            }
-        }
-
-        PyErr_SetString(PyExc_TypeError, "Incompatible value type - must be string, int, double or dictionary." );
-        bpy::throw_error_already_set();
+        // as a fallback, return a string
+        return bpy::object(str);
     }
-
-    return result;
 }
 
-Dictionary bpy_dict_to_dict(const bpy::dict& d)
+Dictionary bpy_dict_to_dictionary(const bpy::dict& d)
 {
     return convert_from_bpy_dict<Dictionary>(d);
-}
-
-ParamArray bpy_dict_to_param_array(const bpy::dict& d)
-{
-    return convert_from_bpy_dict<ParamArray>(d);
-}
-
-bpy::object obj_from_string(const std::string& str)
-{
-    // guess the type of the value represented by str.
-    try
-    {
-        // bool
-        bool b = from_string<bool>(str);
-        return bpy::object(b);
-    }
-    catch(ExceptionStringConversionError&) {}
-
-    try
-    {
-        // int / double
-        double d = from_string<double>(str);
-        return bpy::object(d);
-    }
-    catch(ExceptionStringConversionError&) {}
-
-    try
-    {
-        // Vector2i
-        Vector2i v = from_string<Vector2i>(str);
-        return bpy::object(v);
-    }
-    catch(ExceptionStringConversionError&) {}
-
-    try
-    {
-        // Vector2f
-        Vector2f v = from_string<Vector2f>(str);
-        return bpy::object(v);
-    }
-    catch(ExceptionStringConversionError&) {}
-
-    try
-    {
-        // Vector2d
-        Vector2d v = from_string<Vector2d>(str);
-        return bpy::object(v);
-    }
-    catch(ExceptionStringConversionError&) {}
-
-    // TODO: add conversions from bpy::tuple to Vector<T,N>.
-
-    // TODO: check more types here if needed... (est.)
-
-    // as a fallback, return a string
-    return bpy::object(str);
 }
 
 bpy::dict dictionary_to_bpy_dict(const Dictionary& dict)
@@ -231,7 +232,11 @@ bpy::dict dictionary_to_bpy_dict(const Dictionary& dict)
     return result;
 }
 
-// Keep this for a bit, in case ParamArray needs special handling.
+ParamArray bpy_dict_to_param_array(const bpy::dict& d)
+{
+    return convert_from_bpy_dict<ParamArray>(d);
+}
+
 bpy::dict param_array_to_bpy_dict(const ParamArray& array)
 {
     return dictionary_to_bpy_dict(array);
