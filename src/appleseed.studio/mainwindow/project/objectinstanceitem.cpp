appleseedhq/appleseed

commit 95b045bf06c731cd331d68f6c2515f1acefc1b42

adding support for nested assemblies, work in progress.

src/appleseed.studio/mainwindow/project/objectinstanceitem.cpp
@@ -62,7 +62,7 @@ namespace
 {
     enum Side
     {
-        FrontSide = 0,
+        FrontSide,
         BackSide,
         FrontAndBackSides
     };
@@ -77,7 +77,7 @@ namespace
         }
 
         explicit MaterialAssignmentData(const Side side)
-            : m_side(side)
+          : m_side(side)
         {
         }
 
@@ -98,11 +98,12 @@ namespace studio {
 
 ObjectInstanceItem::ObjectInstanceItem(
     ObjectInstance*     object_instance,
-    Assembly&           assembly,
+    Assembly&           parent,
+    AssemblyItem*       parent_item,
     ProjectBuilder&     project_builder)
   : EntityItemBase<ObjectInstance>(object_instance)
-  , m_object_instance(object_instance)
-  , m_assembly(assembly)
+  , m_parent(parent)
+  , m_parent_item(parent_item)
   , m_project_builder(project_builder)
 {
     set_allow_edition(false);
@@ -110,30 +111,44 @@ ObjectInstanceItem::ObjectInstanceItem(
     update_style();
 }
 
+const Assembly& ObjectInstanceItem::get_assembly() const
+{
+    return m_parent;
+}
+
+namespace
+{
+    void add_material_assignment_menu_actions(QMenu* menu, const ObjectInstanceItem* item)
+    {
+        menu->addSeparator();
+
+        menu->addAction("Assign Material To Front Side...", item, SLOT(slot_assign_material()))
+            ->setData(QVariant::fromValue(MaterialAssignmentData(FrontSide)));
+
+        menu->addAction("Assign Material To Back Side...", item, SLOT(slot_assign_material()))
+            ->setData(QVariant::fromValue(MaterialAssignmentData(BackSide)));
+
+        menu->addAction("Assign Material To Both Sides...", item, SLOT(slot_assign_material()))
+            ->setData(QVariant::fromValue(MaterialAssignmentData(FrontAndBackSides)));
+
+        menu->addSeparator();
+
+        menu->addAction("Unassign Front Side Material", item, SLOT(slot_unassign_material()))
+            ->setData(QVariant::fromValue(MaterialAssignmentData(FrontSide)));
+
+        menu->addAction("Unassign Back Side Material", item, SLOT(slot_unassign_material()))
+            ->setData(QVariant::fromValue(MaterialAssignmentData(BackSide)));
+
+        menu->addAction("Unassign Both Sides Materials", item, SLOT(slot_unassign_material()))
+            ->setData(QVariant::fromValue(MaterialAssignmentData(FrontAndBackSides)));
+    }
+}
+
 QMenu* ObjectInstanceItem::get_single_item_context_menu() const
 {
     QMenu* menu = ItemBase::get_single_item_context_menu();
-    menu->addSeparator();
 
-    menu->addAction("Assign Material To Front Side...", this, SLOT(slot_assign_material()))
-        ->setData(QVariant::fromValue(MaterialAssignmentData(FrontSide)));
-
-    menu->addAction("Assign Material To Back Side...", this, SLOT(slot_assign_material()))
-        ->setData(QVariant::fromValue(MaterialAssignmentData(BackSide)));
-
-    menu->addAction("Assign Material To Both Sides...", this, SLOT(slot_assign_material()))
-        ->setData(QVariant::fromValue(MaterialAssignmentData(FrontAndBackSides)));
-
-    menu->addSeparator();
-
-    menu->addAction("Unassign Front Side Material", this, SLOT(slot_unassign_material()))
-        ->setData(QVariant::fromValue(MaterialAssignmentData(FrontSide)));
-
-    menu->addAction("Unassign Back Side Material", this, SLOT(slot_unassign_material()))
-        ->setData(QVariant::fromValue(MaterialAssignmentData(BackSide)));
-
-    menu->addAction("Unassign Both Sides Materials", this, SLOT(slot_unassign_material()))
-        ->setData(QVariant::fromValue(MaterialAssignmentData(FrontAndBackSides)));
+    add_material_assignment_menu_actions(menu, this);
 
     return menu;
 }
@@ -166,38 +181,14 @@ namespace
 
 QMenu* ObjectInstanceItem::get_multiple_items_context_menu(const QList<ItemBase*>& items) const
 {
-    if (!are_in_assembly(items_to_object_instance_items(items), m_assembly.get_uid()))
+    if (!are_in_assembly(items_to_object_instance_items(items), m_parent.get_uid()))
         return 0;
 
     QMenu* menu = ItemBase::get_multiple_items_context_menu(items);
-    menu->addSeparator();
 
-    menu->addAction("Assign Material To Front Side...", this, SLOT(slot_assign_material()))
-        ->setData(QVariant::fromValue(MaterialAssignmentData(FrontSide, items)));
-
-    menu->addAction("Assign Material To Back Side...", this, SLOT(slot_assign_material()))
-        ->setData(QVariant::fromValue(MaterialAssignmentData(BackSide, items)));
-
-    menu->addAction("Assign Material To Both Sides...", this, SLOT(slot_assign_material()))
-        ->setData(QVariant::fromValue(MaterialAssignmentData(FrontAndBackSides, items)));
-
-    menu->addSeparator();
-
-    menu->addAction("Unassign Front Side Material", this, SLOT(slot_unassign_material()))
-        ->setData(QVariant::fromValue(MaterialAssignmentData(FrontSide, items)));
-
-    menu->addAction("Unassign Back Side Material", this, SLOT(slot_unassign_material()))
-        ->setData(QVariant::fromValue(MaterialAssignmentData(BackSide, items)));
-
-    menu->addAction("Unassign Both Sides Materials", this, SLOT(slot_unassign_material()))
-        ->setData(QVariant::fromValue(MaterialAssignmentData(FrontAndBackSides, items)));
+    add_material_assignment_menu_actions(menu, this);
 
     return menu;
-}
-
-const Assembly& ObjectInstanceItem::get_assembly() const
-{
-    return m_assembly;
 }
 
 namespace
@@ -236,7 +227,7 @@ void ObjectInstanceItem::slot_assign_material()
 
     const QString window_title =
         data.m_items.empty()
-            ? QString("Assign Material to %1").arg(m_object_instance->get_name())
+            ? QString("Assign Material to %1").arg(m_entity->get_name())
             : QString("Assign Material to Multiple Object Instances");
 
     EntityBrowserWindow* browser_window =
@@ -244,7 +235,7 @@ void ObjectInstanceItem::slot_assign_material()
             treeWidget(),
             window_title.toStdString());
 
-    EntityBrowser<Assembly> entity_browser(m_assembly);
+    EntityBrowser<Assembly> entity_browser(m_parent);
 
     browser_window->add_items_page(
         "material",
@@ -317,7 +308,10 @@ void ObjectInstanceItem::slot_delete()
     if (!allows_deletion())
         return;
 
-    m_project_builder.remove_object_instance(m_assembly, m_object_instance->get_uid());
+    m_project_builder.remove_object_instance(
+        m_parent,
+        m_parent_item,
+        m_entity->get_uid());
 
     // 'this' no longer exists at this point.
 }
@@ -325,10 +319,10 @@ void ObjectInstanceItem::slot_delete()
 void ObjectInstanceItem::assign_material(const bool front_side, const bool back_side, const char* material_name)
 {
     if (front_side)
-        m_object_instance->assign_material(0, ObjectInstance::FrontSide, material_name);
+        m_entity->assign_material(0, ObjectInstance::FrontSide, material_name);
 
     if (back_side)
-        m_object_instance->assign_material(0, ObjectInstance::BackSide, material_name);
+        m_entity->assign_material(0, ObjectInstance::BackSide, material_name);
 
     m_project_builder.notify_project_modification();
 
@@ -338,10 +332,10 @@ void ObjectInstanceItem::assign_material(const bool front_side, const bool back_
 void ObjectInstanceItem::unassign_material(const bool front_side, const bool back_side)
 {
     if (front_side)
-        m_object_instance->clear_front_materials();
+        m_entity->clear_front_materials();
 
     if (back_side)
-        m_object_instance->clear_back_materials();
+        m_entity->clear_back_materials();
 
     m_project_builder.notify_project_modification();
 
@@ -350,8 +344,8 @@ void ObjectInstanceItem::unassign_material(const bool front_side, const bool bac
 
 void ObjectInstanceItem::update_style()
 {
-    if (m_object_instance->get_front_material_names().empty() &&
-        m_object_instance->get_back_material_names().empty())
+    if (m_entity->get_front_material_names().empty() &&
+        m_entity->get_back_material_names().empty())
     {
         setTextColor(0, QColor(255, 0, 255, 255));
     }

src/appleseed.studio/CMakeLists.txt
@@ -112,7 +112,8 @@ set (mainwindow_project_sources
     mainwindow/project/assemblyinstanceitem.h
     mainwindow/project/assemblyitem.cpp
     mainwindow/project/assemblyitem.h
-    mainwindow/project/cameraitem.h
+    mainwindow/project/basegroupitem.cpp
+    mainwindow/project/basegroupitem.h
     mainwindow/project/collectionitem.h
     mainwindow/project/collectionitembase.h
     mainwindow/project/entitybrowser.cpp
@@ -130,11 +131,9 @@ set (mainwindow_project_sources
     mainwindow/project/entityitem.h
     mainwindow/project/entityitembase.cpp
     mainwindow/project/entityitembase.h
-    mainwindow/project/environmentitem.h
     mainwindow/project/exceptioninvalidentityname.h
     mainwindow/project/itembase.cpp
     mainwindow/project/itembase.h
-    mainwindow/project/itemtypemap.h
     mainwindow/project/multimodelcollectionitem.h
     mainwindow/project/multimodelentityeditorformfactory.h
     mainwindow/project/multimodelentityitem.h
@@ -152,14 +151,15 @@ set (mainwindow_project_sources
     mainwindow/project/projectexplorer.h
     mainwindow/project/projectmanager.cpp
     mainwindow/project/projectmanager.h
-    mainwindow/project/projecttree.cpp
-    mainwindow/project/projecttree.h
+    mainwindow/project/sceneitem.cpp
+    mainwindow/project/sceneitem.h
     mainwindow/project/singlemodelcollectionitem.h
     mainwindow/project/singlemodelentityeditorformfactory.cpp
     mainwindow/project/singlemodelentityeditorformfactory.h
     mainwindow/project/singlemodelentityitem.h
     mainwindow/project/texturecollectionitem.cpp
     mainwindow/project/texturecollectionitem.h
+    mainwindow/project/textureitem.cpp
     mainwindow/project/textureitem.h
     mainwindow/project/tools.cpp
     mainwindow/project/tools.h

src/appleseed.studio/mainwindow/project/assemblycollectionitem.cpp
@@ -57,12 +57,14 @@ namespace
 }
 
 AssemblyCollectionItem::AssemblyCollectionItem(
-    Scene&              scene,
     AssemblyContainer&  assemblies,
+    BaseGroup&          parent,
+    BaseGroupItem*      parent_item,
     ProjectBuilder&     project_builder,
     ParamArray&         settings)
   : CollectionItemBase<Assembly>(g_class_uid, "Assemblies")
-  , m_scene(scene)
+  , m_parent(parent)
+  , m_parent_item(parent_item)
   , m_project_builder(project_builder)
   , m_settings(settings)
 {
@@ -72,25 +74,16 @@ AssemblyCollectionItem::AssemblyCollectionItem(
 QMenu* AssemblyCollectionItem::get_single_item_context_menu() const
 {
     QMenu* menu = CollectionItemBase<Assembly>::get_single_item_context_menu();
-    menu->addSeparator();
 
-    menu->addAction("Create Assembly...", this, SLOT(slot_create_assembly()));
+    menu->addSeparator();
+    menu->addAction("Create Assembly...", this, SLOT(slot_create()));
 
     return menu;
 }
 
-AssemblyItem& AssemblyCollectionItem::get_item(const Assembly& assembly) const
+void AssemblyCollectionItem::slot_create()
 {
-    const ItemMap::const_iterator i = m_items.find(assembly.get_uid());
-    assert(i != m_items.end());
-
-    return *static_cast<AssemblyItem*>(i->second);
-}
-
-void AssemblyCollectionItem::slot_create_assembly()
-{
-    const string assembly_name_suggestion =
-        get_name_suggestion("assembly", m_scene.assemblies());
+    const string assembly_name_suggestion = get_name_suggestion("assembly", m_parent.assemblies());
 
     const string assembly_name =
         get_entity_name_dialog(
@@ -100,7 +93,7 @@ void AssemblyCollectionItem::slot_create_assembly()
             assembly_name_suggestion);
 
     if (!assembly_name.empty())
-        m_project_builder.insert_assembly(assembly_name);
+        m_project_builder.insert_assembly(m_parent, m_parent_item, assembly_name);
 }
 
 ItemBase* AssemblyCollectionItem::create_item(Assembly* assembly) const
@@ -109,8 +102,9 @@ ItemBase* AssemblyCollectionItem::create_item(Assembly* assembly) const
 
     return
         new AssemblyItem(
-            assembly,
-            m_scene,
+            *assembly,
+            m_parent,
+            m_parent_item,
             m_project_builder,
             m_settings);
 }

src/appleseed.studio/mainwindow/project/assemblycollectionitem.h
@@ -43,6 +43,7 @@
 
 // Forward declarations.
 namespace appleseed { namespace studio { class AssemblyItem; } }
+namespace appleseed { namespace studio { class BaseGroupItem; } }
 namespace appleseed { namespace studio { class ItemBase; } }
 namespace appleseed { namespace studio { class ProjectBuilder; } }
 namespace renderer  { class ParamArray; }
@@ -58,20 +59,20 @@ class AssemblyCollectionItem
 
   public:
     AssemblyCollectionItem(
-        renderer::Scene&                scene,
         renderer::AssemblyContainer&    assemblies,
+        renderer::BaseGroup&            parent,
+        BaseGroupItem*                  parent_item,
         ProjectBuilder&                 project_builder,
         renderer::ParamArray&           settings);
 
     virtual QMenu* get_single_item_context_menu() const override;
 
-    AssemblyItem& get_item(const renderer::Assembly& assembly) const;
-
   public slots:
-    void slot_create_assembly();
+    void slot_create();
 
   private:
-    renderer::Scene&        m_scene;
+    renderer::BaseGroup&    m_parent;
+    BaseGroupItem*          m_parent_item;
     ProjectBuilder&         m_project_builder;
     renderer::ParamArray&   m_settings;
 

src/appleseed.studio/mainwindow/project/assemblyinstancecollectionitem.cpp
@@ -50,11 +50,13 @@ namespace
 }
 
 AssemblyInstanceCollectionItem::AssemblyInstanceCollectionItem(
-    Scene&                      scene,
     AssemblyInstanceContainer&  assembly_instances,
+    BaseGroup&                  parent,
+    BaseGroupItem*              parent_item,
     ProjectBuilder&             project_builder)
   : CollectionItemBase<AssemblyInstance>(g_class_uid, "Assembly Instances")
-  , m_scene(scene)
+  , m_parent(parent)
+  , m_parent_item(parent_item)
   , m_project_builder(project_builder)
 {
     add_items(assembly_instances);
@@ -64,7 +66,12 @@ ItemBase* AssemblyInstanceCollectionItem::create_item(AssemblyInstance* assembly
 {
     assert(assembly_instance);
 
-    return new AssemblyInstanceItem(assembly_instance, m_project_builder);
+    return
+        new AssemblyInstanceItem(
+            assembly_instance,
+            m_parent,
+            m_parent_item,
+            m_project_builder);
 }
 
 }   // namespace studio

src/appleseed.studio/mainwindow/project/assemblyinstancecollectionitem.h
@@ -38,10 +38,8 @@
 // appleseed.foundation headers.
 #include "foundation/platform/compiler.h"
 
-// Qt headers.
-#include <QObject>
-
 // Forward declarations.
+namespace appleseed { namespace studio { class BaseGroupItem; } }
 namespace appleseed { namespace studio { class ItemBase; } }
 namespace appleseed { namespace studio { class ProjectBuilder; } }
 
@@ -51,17 +49,17 @@ namespace studio {
 class AssemblyInstanceCollectionItem
   : public CollectionItemBase<renderer::AssemblyInstance>
 {
-    Q_OBJECT
-
   public:
     AssemblyInstanceCollectionItem(
-        renderer::Scene&                        scene,
         renderer::AssemblyInstanceContainer&    assembly_instances,
+        renderer::BaseGroup&                    parent,
+        BaseGroupItem*                          parent_item,
         ProjectBuilder&                         project_builder);
 
   private:
-    renderer::Scene&    m_scene;
-    ProjectBuilder&     m_project_builder;
+    renderer::BaseGroup&    m_parent;
+    BaseGroupItem*          m_parent_item;
+    ProjectBuilder&         m_project_builder;
 
     virtual ItemBase* create_item(renderer::AssemblyInstance* assembly_instance) const override;
 };

src/appleseed.studio/mainwindow/project/assemblyinstanceitem.h
@@ -35,12 +35,11 @@
 // appleseed.foundation headers.
 #include "foundation/platform/compiler.h"
 
-// Qt headers.
-#include <QObject>
-
 // Forward declarations.
+namespace appleseed { namespace studio { class BaseGroupItem; } }
 namespace appleseed { namespace studio { class ProjectBuilder; } }
 namespace renderer  { class AssemblyInstance; }
+namespace renderer  { class BaseGroup; }
 
 namespace appleseed {
 namespace studio {
@@ -48,15 +47,16 @@ namespace studio {
 class AssemblyInstanceItem
   : public EntityItemBase<renderer::AssemblyInstance>
 {
-    Q_OBJECT
-
   public:
     AssemblyInstanceItem(
         renderer::AssemblyInstance* assembly_instance,
+        renderer::BaseGroup&        parent,
+        BaseGroupItem*              parent_item,
         ProjectBuilder&             project_builder);
 
   private:
-    renderer::AssemblyInstance*     m_assembly_instance;
+    renderer::BaseGroup&            m_parent;
+    BaseGroupItem*                  m_parent_item;
     ProjectBuilder&                 m_project_builder;
 
     virtual void slot_delete() override;

commit 5c1173f3e65379fdba08abc2f0b24ce431d0a666

fixed regression introduced in 95b045bf06c731cd331d68f6c2515f1acefc1b42 ("adding support for nested assemblies, work in progress"): could no longer mass-assign/unassigned materials to object instances.

src/appleseed.studio/mainwindow/project/objectinstanceitem.cpp
@@ -76,11 +76,6 @@ namespace
         {
         }
 
-        explicit MaterialAssignmentData(const Side side)
-          : m_side(side)
-        {
-        }
-
         MaterialAssignmentData(
             const Side              side,
             const QList<ItemBase*>& items)
@@ -114,29 +109,32 @@ const Assembly& ObjectInstanceItem::get_assembly() const
 
 namespace
 {
-    void add_material_assignment_menu_actions(QMenu* menu, const ObjectInstanceItem* item)
+    void add_material_assignment_menu_actions(
+        QMenu*                      menu,
+        const ObjectInstanceItem*   item,
+        const QList<ItemBase*>&     items = QList<ItemBase*>())
     {
         menu->addSeparator();
 
         menu->addAction("Assign Material To Front Side...", item, SLOT(slot_assign_material()))
-            ->setData(QVariant::fromValue(MaterialAssignmentData(FrontSide)));
+            ->setData(QVariant::fromValue(MaterialAssignmentData(FrontSide, items)));
 
         menu->addAction("Assign Material To Back Side...", item, SLOT(slot_assign_material()))
-            ->setData(QVariant::fromValue(MaterialAssignmentData(BackSide)));
+            ->setData(QVariant::fromValue(MaterialAssignmentData(BackSide, items)));
 
         menu->addAction("Assign Material To Both Sides...", item, SLOT(slot_assign_material()))
-            ->setData(QVariant::fromValue(MaterialAssignmentData(FrontAndBackSides)));
+            ->setData(QVariant::fromValue(MaterialAssignmentData(FrontAndBackSides, items)));
 
         menu->addSeparator();
 
         menu->addAction("Unassign Front Side Material", item, SLOT(slot_unassign_material()))
-            ->setData(QVariant::fromValue(MaterialAssignmentData(FrontSide)));
+            ->setData(QVariant::fromValue(MaterialAssignmentData(FrontSide, items)));
 
         menu->addAction("Unassign Back Side Material", item, SLOT(slot_unassign_material()))
-            ->setData(QVariant::fromValue(MaterialAssignmentData(BackSide)));
+            ->setData(QVariant::fromValue(MaterialAssignmentData(BackSide, items)));
 
         menu->addAction("Unassign Both Sides Materials", item, SLOT(slot_unassign_material()))
-            ->setData(QVariant::fromValue(MaterialAssignmentData(FrontAndBackSides)));
+            ->setData(QVariant::fromValue(MaterialAssignmentData(FrontAndBackSides, items)));
     }
 }
 
@@ -182,7 +180,7 @@ QMenu* ObjectInstanceItem::get_multiple_items_context_menu(const QList<ItemBase*
 
     QMenu* menu = ItemBase::get_multiple_items_context_menu(items);
 
-    add_material_assignment_menu_actions(menu, this);
+    add_material_assignment_menu_actions(menu, this, items);
 
     return menu;
 }