appleseedhq/appleseed

commit f75b987efb8301af5277476f9164227db9d00f61

simplified the code by making the const version of renderer::TransformSequence::earliest_transform() return an identity transform if the sequence is empty.  some code

src/appleseed.studio/mainwindow/rendering/cameracontroller.cpp
@@ -104,9 +104,7 @@ void CameraController::configure_controller(const Scene* scene)
 {
     // Set the controller orientation and position based on the scene camera.
     m_controller.set_transform(
-        m_camera->transform_sequence().empty()
-            ? Matrix4d::identity()
-            : m_camera->transform_sequence().earliest_transform().get_local_to_parent());
+        m_camera->transform_sequence().earliest_transform().get_local_to_parent());
 
     if (m_camera->get_parameters().strings().exist("controller_target"))
     {

src/appleseed/renderer/kernel/lighting/lightsampler.cpp
@@ -172,10 +172,8 @@ void LightSampler::collect_emitting_triangles(
         // Compute the object space to world space transformation.
         // todo: add support for moving light-emitters.
         const Transformd& object_instance_transform = object_instance->get_transform();
-        const Transformd assembly_instance_transform =
-            assembly_instance.transform_sequence().empty()
-                ? Transformd::identity()
-                : assembly_instance.transform_sequence().earliest_transform();
+        const Transformd& assembly_instance_transform =
+            assembly_instance.transform_sequence().earliest_transform();
         const Transformd global_transform = assembly_instance_transform * object_instance_transform;
 
         // Retrieve the object.

src/appleseed/renderer/kernel/rendering/lighttracing/lighttracingsamplegenerator.cpp
@@ -599,9 +599,7 @@ namespace
                 parent_shading_point,
                 ShadingRay(light_sample.m_point, emission_direction, 0.0, 0.0, 0.0f, ~0),
                 light_sample.m_triangle->m_assembly_instance,
-                light_sample.m_triangle->m_assembly_instance->transform_sequence().empty()
-                    ? Transformd::identity()
-                    : light_sample.m_triangle->m_assembly_instance->transform_sequence().earliest_transform(),
+                light_sample.m_triangle->m_assembly_instance->transform_sequence().earliest_transform(),
                 light_sample.m_triangle->m_object_instance_index,
                 light_sample.m_triangle->m_region_index,
                 light_sample.m_triangle->m_triangle_index,

src/appleseed/renderer/utility/transformsequence.cpp
@@ -57,49 +57,18 @@ TransformSequence::TransformSequence()
 }
 
 TransformSequence::TransformSequence(const TransformSequence& rhs)
-  : m_capacity(rhs.m_size)      // shrink to size
-  , m_size(rhs.m_size)
 {
-    if (rhs.m_keys)
-    {
-        m_keys = new TransformKey[m_size];
-        memcpy(m_keys, rhs.m_keys, m_size * sizeof(TransformKey));
-    }
-    else m_keys = 0;
-
-    if (rhs.m_interpolators)
-    {
-        m_interpolators = new TransformInterpolatord[m_size - 1];
-        memcpy(m_interpolators, rhs.m_interpolators, (m_size - 1) * sizeof(TransformInterpolatord));
-    }
-    else m_interpolators = 0;
+    copy_from(rhs);
 }
 
 TransformSequence::~TransformSequence()
 {
-    delete [] m_keys;
-    delete [] m_interpolators;
+    clear();
 }
 
 TransformSequence& TransformSequence::operator=(const TransformSequence& rhs)
 {
-    m_capacity = rhs.m_size;    // shrink to size
-    m_size = rhs.m_size;
-
-    if (rhs.m_keys)
-    {
-        m_keys = new TransformKey[m_size];
-        memcpy(m_keys, rhs.m_keys, m_size * sizeof(TransformKey));
-    }
-    else m_keys = 0;
-
-    if (rhs.m_interpolators)
-    {
-        m_interpolators = new TransformInterpolatord[m_size - 1];
-        memcpy(m_interpolators, rhs.m_interpolators, (m_size - 1) * sizeof(TransformInterpolatord));
-    }
-    else m_interpolators = 0;
-
+    copy_from(rhs);
     return *this;
 }
 
@@ -181,9 +150,17 @@ Transformd& TransformSequence::earliest_transform()
     return m_keys[earliest_index].m_transform;
 }
 
+namespace
+{
+    const Transformd Identity = Transformd::identity();
+}
+
 const Transformd& TransformSequence::earliest_transform() const
 {
-    return const_cast<TransformSequence*>(this)->earliest_transform();
+    return
+        m_size == 0
+            ? Identity
+            : const_cast<TransformSequence*>(this)->earliest_transform();
 }
 
 bool TransformSequence::prepare()
@@ -258,10 +235,32 @@ TransformSequence TransformSequence::operator*(const TransformSequence& rhs) con
     return result;
 }
 
+void TransformSequence::copy_from(const TransformSequence& rhs)
+{
+    m_capacity = rhs.m_size;    // shrink to size on copy
+    m_size = rhs.m_size;
+
+    if (rhs.m_keys)
+    {
+        m_keys = new TransformKey[m_size];
+        memcpy(m_keys, rhs.m_keys, m_size * sizeof(TransformKey));
+    }
+    else m_keys = 0;
+
+    if (rhs.m_interpolators)
+    {
+        m_interpolators = new TransformInterpolatord[m_size - 1];
+        memcpy(m_interpolators, rhs.m_interpolators, (m_size - 1) * sizeof(TransformInterpolatord));
+    }
+    else m_interpolators = 0;
+}
+
 void TransformSequence::interpolate(
     const double        time,
     Transformd&         result) const
 {
+    assert(m_size > 0);
+
     size_t begin = 0;
     size_t end = m_size;
 

src/appleseed/renderer/utility/transformsequence.h
@@ -124,6 +124,8 @@ class DLLSYMBOL TransformSequence
     TransformKey*                       m_keys;
     foundation::TransformInterpolatord* m_interpolators;
 
+    void copy_from(const TransformSequence& rhs);
+
     void interpolate(
         const double                    time,
         foundation::Transformd&         result) const;

src/appleseed/renderer/meta/tests/test_transformsequence.cpp
@@ -86,7 +86,14 @@ TEST_SUITE(Renderer_Utility_TransformSequence)
         EXPECT_EQ(NewTransform, transform);
     }
 
-    TEST_CASE(GetEarliestTransform_EarliestTransformIsFirst)
+    TEST_CASE(EarliestTransform_EmptySequence_ReturnsIdentity)
+    {
+        const TransformSequence sequence;   // const is mandatory here
+
+        EXPECT_EQ(Transformd::identity(), sequence.earliest_transform());
+    }
+
+    TEST_CASE(EarliestTransform_EarliestTransformIsFirst)
     {
         const Transformd Transform1(Matrix4d::translation(Vector3d(1.0, 2.0, 3.0)));
         const Transformd Transform2(Matrix4d::translation(Vector3d(4.0, 5.0, 6.0)));
@@ -98,7 +105,7 @@ TEST_SUITE(Renderer_Utility_TransformSequence)
         EXPECT_EQ(Transform1, sequence.earliest_transform());
     }
 
-    TEST_CASE(GetEarliestTransform_EarliestTransformIsLast)
+    TEST_CASE(EarliestTransform_EarliestTransformIsLast)
     {
         const Transformd Transform1(Matrix4d::translation(Vector3d(1.0, 2.0, 3.0)));
         const Transformd Transform2(Matrix4d::translation(Vector3d(4.0, 5.0, 6.0)));
@@ -156,6 +163,15 @@ TEST_SUITE(Renderer_Utility_TransformSequence)
         sequence.set_transform(1.0, Transformd::identity());
 
         EXPECT_EQ(1, sequence.size());
+    }
+
+    TEST_CASE(Prepare_GivenEmptySequence_ReturnsTrue)
+    {
+        TransformSequence sequence;
+
+        const bool success = sequence.prepare();
+
+        EXPECT_TRUE(success);
     }
 
     TEST_CASE(Evaluate_GivenNoTransform_ReturnsIdentityTransformRegardlessOfTime)

commit fc2206c62d9b9cb85ac58cc57402d4b0b7382fb7

fixed bug introduced in f75b987efb8301af5277476f9164227db9d00f61 ("simplified the code by making the const version of renderer::TransformSequence::earliest_transform() return an identity transform if the sequence is empty") and simplified the renderer::TransformSequence API.

src/appleseed.python/bind_transform.cpp
@@ -62,7 +62,7 @@ namespace detail
 
     UnalignedTransformd44 transform_seq_get_earliest(const TransformSequence* seq)
     {
-        Transformd xform(seq->earliest_transform());
+        Transformd xform(seq->get_earliest_transform());
         return UnalignedTransformd44(xform);
     }
 }
@@ -127,7 +127,7 @@ void bind_transform()
     bpy::class_<TransformSequence, boost::noncopyable>("TransformSequence", bpy::no_init)
         .def("set_transform", &detail::transform_seq_set_transform)
         .def("get_transform", &detail::transform_seq_get_transform)
-        .def("earliest_transform", &detail::transform_seq_get_earliest)
+        .def("get_earliest_transform", &detail::transform_seq_get_earliest)
 
         .def("empty", &TransformSequence::empty)
         .def("size", &TransformSequence::size)

src/appleseed.studio/mainwindow/rendering/cameracontroller.cpp
@@ -104,7 +104,7 @@ void CameraController::configure_controller(const Scene* scene)
 {
     // Set the controller orientation and position based on the scene camera.
     m_controller.set_transform(
-        m_camera->transform_sequence().earliest_transform().get_local_to_parent());
+        m_camera->transform_sequence().get_earliest_transform().get_local_to_parent());
 
     if (m_camera->get_parameters().strings().exist("controller_target"))
     {

src/appleseed/renderer/kernel/lighting/lightsampler.cpp
@@ -174,7 +174,7 @@ void LightSampler::collect_emitting_triangles(
         // todo: add support for moving light-emitters.
         const Transformd& object_instance_transform = object_instance->get_transform();
         const Transformd& assembly_instance_transform =
-            assembly_instance.transform_sequence().earliest_transform();
+            assembly_instance.transform_sequence().get_earliest_transform();
         const Transformd global_transform = assembly_instance_transform * object_instance_transform;
 
         // Retrieve the object.

src/appleseed/renderer/kernel/rendering/lighttracing/lighttracingsamplegenerator.cpp
@@ -599,7 +599,7 @@ namespace
                 parent_shading_point,
                 ShadingRay(light_sample.m_point, emission_direction, 0.0, 0.0, 0.0f, ~0),
                 light_sample.m_triangle->m_assembly_instance,
-                light_sample.m_triangle->m_assembly_instance->transform_sequence().earliest_transform(),
+                light_sample.m_triangle->m_assembly_instance->transform_sequence().get_earliest_transform(),
                 light_sample.m_triangle->m_object_instance_index,
                 light_sample.m_triangle->m_region_index,
                 light_sample.m_triangle->m_triangle_index,

src/appleseed/renderer/modeling/project/projectfilereader.cpp
@@ -868,7 +868,7 @@ namespace
         {
             TransformSequence sequence;
             copy_transform_sequence_to(sequence);
-            return sequence.earliest_transform();
+            return sequence.get_earliest_transform();
         }
 
       private:

src/appleseed/renderer/utility/transformsequence.cpp
@@ -129,9 +129,15 @@ void TransformSequence::get_transform(
     transform = m_keys[index].m_transform;
 }
 
-Transformd& TransformSequence::earliest_transform()
+namespace
 {
-    assert(m_size > 0);
+    const Transformd Identity = Transformd::identity();
+}
+
+const Transformd& TransformSequence::get_earliest_transform() const
+{
+    if (m_size == 0)
+        return Identity;
 
     double earliest_time = m_keys[0].m_time;
     size_t earliest_index = 0;
@@ -148,19 +154,6 @@ Transformd& TransformSequence::earliest_transform()
     }
 
     return m_keys[earliest_index].m_transform;
-}
-
-namespace
-{
-    const Transformd Identity = Transformd::identity();
-}
-
-const Transformd& TransformSequence::earliest_transform() const
-{
-    return
-        m_size == 0
-            ? Identity
-            : const_cast<TransformSequence*>(this)->earliest_transform();
 }
 
 bool TransformSequence::prepare()

src/appleseed/renderer/utility/transformsequence.h
@@ -81,9 +81,9 @@ class DLLSYMBOL TransformSequence
         double&                         time,
         foundation::Transformd&         transform) const;
 
-    // Access the transform at the lowest time value.
-    foundation::Transformd& earliest_transform();
-    const foundation::Transformd& earliest_transform() const;
+    // Return the transform with the lowest time value.
+    // Return the identity transform if the sequence is empty.
+    const foundation::Transformd& get_earliest_transform() const;
 
     // Prepare the sequence for quick evaluation.
     // This method must be called after new transforms have been set and

src/appleseed/renderer/meta/tests/test_transformsequence.cpp
@@ -86,14 +86,14 @@ TEST_SUITE(Renderer_Utility_TransformSequence)
         EXPECT_EQ(NewTransform, transform);
     }
 
-    TEST_CASE(EarliestTransform_EmptySequence_ReturnsIdentity)
+    TEST_CASE(GetEarliestTransform_EmptySequence_ReturnsIdentity)
     {
-        const TransformSequence sequence;   // const is mandatory here
+        const TransformSequence sequence;
 
-        EXPECT_EQ(Transformd::identity(), sequence.earliest_transform());
+        EXPECT_EQ(Transformd::identity(), sequence.get_earliest_transform());
     }
 
-    TEST_CASE(EarliestTransform_EarliestTransformIsFirst)
+    TEST_CASE(GetEarliestTransform_EarliestTransformIsFirst)
     {
         const Transformd Transform1(Matrix4d::translation(Vector3d(1.0, 2.0, 3.0)));
         const Transformd Transform2(Matrix4d::translation(Vector3d(4.0, 5.0, 6.0)));
@@ -102,10 +102,10 @@ TEST_SUITE(Renderer_Utility_TransformSequence)
         sequence.set_transform(1.0, Transform1);
         sequence.set_transform(2.0, Transform2);
 
-        EXPECT_EQ(Transform1, sequence.earliest_transform());
+        EXPECT_EQ(Transform1, sequence.get_earliest_transform());
     }
 
-    TEST_CASE(EarliestTransform_EarliestTransformIsLast)
+    TEST_CASE(GetEarliestTransform_EarliestTransformIsLast)
     {
         const Transformd Transform1(Matrix4d::translation(Vector3d(1.0, 2.0, 3.0)));
         const Transformd Transform2(Matrix4d::translation(Vector3d(4.0, 5.0, 6.0)));
@@ -114,7 +114,7 @@ TEST_SUITE(Renderer_Utility_TransformSequence)
         sequence.set_transform(2.0, Transform2);
         sequence.set_transform(1.0, Transform1);
 
-        EXPECT_EQ(Transform1, sequence.earliest_transform());
+        EXPECT_EQ(Transform1, sequence.get_earliest_transform());
     }
 
     TEST_CASE(Empty_GivenDefaultConstructedSequence_ReturnsTrue)