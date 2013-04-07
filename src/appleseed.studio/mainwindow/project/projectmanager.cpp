appleseedhq/appleseed

commit eb2f0503a2a36f0c20c45b9b98f42f7d34a694c8

automatically update project files on loading in appleseed.studio.

src/appleseed.studio/mainwindow/project/projectmanager.cpp
@@ -67,7 +67,15 @@ bool ProjectManager::load_project(const string& filepath)
     auto_release_ptr<Project> loaded_project(
         reader.read(filepath.c_str(), schema_filepath.c_str()));
 
-    return try_set_project(loaded_project);
+    ProjectFileUpdater updater;
+    const bool project_modified = updater.update(loaded_project.ref());
+
+    const bool project_set = try_set_project(loaded_project);
+
+    if (project_set && project_modified)
+        set_project_dirty_flag();
+
+    return project_set;
 }
 
 bool ProjectManager::load_builtin_project(const string& name)

src/appleseed/renderer/modeling/project/projectfileupdater.cpp
@@ -355,14 +355,18 @@ namespace
     };
 }
 
-void ProjectFileUpdater::update(Project& project)
+bool ProjectFileUpdater::update(Project& project)
 {
+    bool modified = false;
+
     const size_t format_revision = project.get_format_revision();
 
     switch (format_revision)
     {
-      case 2: { Updater_2_to_3 updater(project); updater.update(); }
-      case 3: { Updater_3_to_4 updater(project); updater.update(); }
+      case 0: ; // Nothing to do
+      case 1: ; // Nothing to do
+      case 2: { Updater_2_to_3 updater(project); updater.update(); modified = true; }
+      case 3: { Updater_3_to_4 updater(project); updater.update(); modified = true; }
 
       case 4:
         // Project is up-to-date.
@@ -372,6 +376,8 @@ void ProjectFileUpdater::update(Project& project)
         RENDERER_LOG_ERROR("unsupported project format revision: " FMT_SIZE_T, format_revision);
         break;
     }
+
+    return modified;
 }
 
 }   // namespace renderer

src/appleseed/renderer/modeling/project/projectfileupdater.h
@@ -45,7 +45,7 @@ namespace renderer
 class DLLSYMBOL ProjectFileUpdater
 {
   public:
-    void update(Project& project);
+    bool update(Project& project);
 };
 
 }       // namespace renderer

commit 62c603170cde56acfc072fcb85f4dbf0a74d3abc

fixed regression.
fixed regression introduced in eb2f0503a2a36f0c20c45b9b98f42f7d34a694c8 ("automatically update project files on loading in appleseed.studio"): loading an invalid project file would crash appleseed.studio.

src/appleseed.studio/mainwindow/project/projectmanager.cpp
@@ -61,29 +61,33 @@ void ProjectManager::create_project()
 
 bool ProjectManager::load_project(const string& filepath)
 {
-    const string schema_filepath = get_project_schema_filepath();
+    {
+        const string schema_filepath = get_project_schema_filepath();
 
-    ProjectFileReader reader;
-    auto_release_ptr<Project> loaded_project(
-        reader.read(filepath.c_str(), schema_filepath.c_str()));
+        ProjectFileReader reader;
+        auto_release_ptr<Project> loaded_project(
+            reader.read(filepath.c_str(), schema_filepath.c_str()));
+
+        if (loaded_project.get() == 0)
+            return false;
+
+        m_project = loaded_project;
+    }
 
     ProjectFileUpdater updater;
-    const bool project_modified = updater.update(loaded_project.ref());
+    m_dirty_flag = updater.update(m_project.ref());
 
-    const bool project_set = try_set_project(loaded_project);
-
-    if (project_set && project_modified)
-        set_project_dirty_flag();
-
-    return project_set;
+    return true;
 }
 
 bool ProjectManager::load_builtin_project(const string& name)
 {
     ProjectFileReader reader;
-    auto_release_ptr<Project> loaded_project = reader.load_builtin(name.c_str());
+    m_project = reader.load_builtin(name.c_str());
 
-    return try_set_project(loaded_project);
+    m_dirty_flag = false;
+
+    return true;
 }
 
 bool ProjectManager::reload_project()
@@ -171,17 +175,6 @@ string ProjectManager::get_project_schema_filepath()
         / "project.xsd";
 
     return schema_path.string();
-}
-
-bool ProjectManager::try_set_project(auto_release_ptr<Project> project)
-{
-    if (project.get() == 0)
-        return false;
-
-    m_project = project;
-    m_dirty_flag = false;
-
-    return true;
 }
 
 }   // namespace studio

src/appleseed.studio/mainwindow/project/projectmanager.h
@@ -105,14 +105,10 @@ class ProjectManager
     bool is_project_dirty() const;
 
   private:
-    typedef foundation::auto_release_ptr<renderer::Project> ProjectAutoReleasePtr;
-
-    ProjectAutoReleasePtr   m_project;
-    bool                    m_dirty_flag;
+    foundation::auto_release_ptr<renderer::Project> m_project;
+    bool                                            m_dirty_flag;
 
     static std::string get_project_schema_filepath();
-
-    bool try_set_project(ProjectAutoReleasePtr project);
 };
 
 }       // namespace studio