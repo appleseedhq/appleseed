appleseedhq/appleseed

commit 9df2ed1d33e3d57247ca2029b043c7f2cc9ec5ad

projects are now transparently updated to the latest format revision by renderer::ProjectFileReader.

src/appleseed.studio/mainwindow/project/projectmanager.cpp
@@ -171,21 +171,16 @@ string ProjectManager::get_project_schema_filepath()
 
 bool ProjectManager::do_load_project(const string& filepath)
 {
-    {
-        const string schema_filepath = get_project_schema_filepath();
+    const string schema_filepath = get_project_schema_filepath();
 
-        ProjectFileReader reader;
-        auto_release_ptr<Project> loaded_project(
-            reader.read(filepath.c_str(), schema_filepath.c_str()));
+    ProjectFileReader reader;
+    auto_release_ptr<Project> loaded_project(
+        reader.read(filepath.c_str(), schema_filepath.c_str()));
 
-        if (loaded_project.get() == 0)
-            return false;
+    if (loaded_project.get() == 0)
+        return false;
 
-        m_project = loaded_project;
-    }
-
-    ProjectFileUpdater updater;
-    m_dirty_flag = updater.update(m_project.ref());
+    m_project = loaded_project;
 
     return true;
 }

src/appleseed.cli/main.cpp
@@ -479,19 +479,10 @@ namespace
 
             // Load the project from disk.
             ProjectFileReader reader;
-            auto_release_ptr<Project> project =
+            return
                 reader.read(
                     project_filename.c_str(),
                     schema_path.string().c_str());
-
-            if (project.get())
-            {
-                // Update the project.
-                ProjectFileUpdater updater;
-                updater.update(project.ref());
-            }
-
-            return project;
         }
     }
 

src/appleseed/renderer/modeling/project/project.cpp
@@ -75,6 +75,12 @@ namespace renderer
 // Project class implementation.
 //
 
+namespace
+{
+    // Revision number of the project format.
+    const size_t ProjectFormatRevision = 5;
+}
+
 struct Project::Impl
 {
     size_t                      m_format_revision;
@@ -86,7 +92,7 @@ struct Project::Impl
     auto_ptr<TraceContext>      m_trace_context;
 
     Impl()
-      : m_format_revision(0)
+      : m_format_revision(ProjectFormatRevision)
     {
     }
 };

src/appleseed/renderer/modeling/project/projectfilereader.cpp
@@ -58,6 +58,7 @@
 #include "renderer/modeling/project/configuration.h"
 #include "renderer/modeling/project/configurationcontainer.h"
 #include "renderer/modeling/project/project.h"
+#include "renderer/modeling/project/projectfileupdater.h"
 #include "renderer/modeling/project-builtin/cornellboxproject.h"
 #include "renderer/modeling/project-builtin/defaultproject.h"
 #include "renderer/modeling/scene/assembly.h"
@@ -2686,7 +2687,7 @@ auto_release_ptr<Project> ProjectFileReader::construct_builtin_project(
 }
 
 void ProjectFileReader::postprocess_project(
-    const Project&          project,
+    Project&                project,
     EventCounters&          event_counters) const
 {
     if (!event_counters.has_errors())
@@ -2694,6 +2695,9 @@ void ProjectFileReader::postprocess_project(
 
     if (!event_counters.has_errors())
         complete_project(project, event_counters);
+
+    if (!event_counters.has_errors())
+        upgrade_project(project, event_counters);
 }
 
 void ProjectFileReader::validate_project(
@@ -2737,7 +2741,7 @@ void ProjectFileReader::validate_project(
 }
 
 void ProjectFileReader::complete_project(
-    const Project&          project,
+    Project&                project,
     EventCounters&          event_counters) const
 {
     // Add a default environment if the project doesn't define any.
@@ -2747,6 +2751,14 @@ void ProjectFileReader::complete_project(
             EnvironmentFactory::create("environment", ParamArray()));
         project.get_scene()->set_environment(environment);
     }
+}
+
+void ProjectFileReader::upgrade_project(
+    Project&                project,
+    EventCounters&          event_counters) const
+{
+    ProjectFileUpdater updater;
+    updater.update(project);
 }
 
 void ProjectFileReader::print_loading_results(

src/appleseed/renderer/modeling/project/projectfilereader.h
@@ -80,7 +80,7 @@ class DLLSYMBOL ProjectFileReader
 
     // Finish loading a project.
     void postprocess_project(
-        const Project&          project,
+        Project&                project,
         EventCounters&          event_counters) const;
 
     // Check the validity of a project.
@@ -90,7 +90,12 @@ class DLLSYMBOL ProjectFileReader
 
     // Add missing entities to a valid project.
     void complete_project(
-        const Project&          project,
+        Project&                project,
+        EventCounters&          event_counters) const;
+
+    // Update a project to the latest project format revision.
+    void upgrade_project(
+        Project&                project,
         EventCounters&          event_counters) const;
 
     void print_loading_results(

src/appleseed/renderer/modeling/project/projectfileupdater.cpp
@@ -164,6 +164,46 @@ namespace
 
 
     //
+    // Update from revision 0 to revision 1.
+    //
+
+    class Updater_0_to_1
+      : public Updater
+    {
+      public:
+        explicit Updater_0_to_1(Project& project)
+          : Updater(project, 0)
+        {
+        }
+
+        virtual void update() OVERRIDE
+        {
+            // Nothing to do.
+        }
+    };
+
+
+    //
+    // Update from revision 1 to revision 2.
+    //
+
+    class Updater_1_to_2
+      : public Updater
+    {
+      public:
+        explicit Updater_1_to_2(Project& project)
+          : Updater(project, 1)
+        {
+        }
+
+        virtual void update() OVERRIDE
+        {
+            // Nothing to do.
+        }
+    };
+
+
+    //
     // Update from revision 2 to revision 3.
     //
 
@@ -381,8 +421,8 @@ bool ProjectFileUpdater::update(Project& project)
 
     switch (format_revision)
     {
-      case 0: ; // Nothing to do
-      case 1: ; // Nothing to do
+      case 0: { Updater_0_to_1 updater(project); updater.update(); modified = true; }
+      case 1: { Updater_1_to_2 updater(project); updater.update(); modified = true; }
       case 2: { Updater_2_to_3 updater(project); updater.update(); modified = true; }
       case 3: { Updater_3_to_4 updater(project); updater.update(); modified = true; }
       case 4: { Updater_4_to_5 updater(project); updater.update(); modified = true; }

src/appleseed/renderer/modeling/project/projectfileupdater.h
@@ -39,7 +39,7 @@ namespace renderer
 {
 
 //
-// Project file reader.
+// Project file updater.
 //
 
 class DLLSYMBOL ProjectFileUpdater

src/appleseed/renderer/modeling/project/projectfilewriter.cpp
@@ -90,9 +90,6 @@ namespace renderer
 
 namespace
 {
-    // Revision number of the project format.
-    const size_t ProjectFormatRevision = 5;
-
     // Floating-point formatting settings.
     const char* VectorFormat     = "%.15f";
     const char* MatrixFormat     = "%.15f";
@@ -744,7 +741,7 @@ namespace
         void write_project(const Project& project)
         {
             XMLElement element("project", m_file, m_indenter);
-            element.add_attribute("format_revision", ProjectFormatRevision);
+            element.add_attribute("format_revision", project.get_format_revision());
             element.write(true);
 
             if (project.get_scene())

commit 09bc87bb52314ff7e951e81c6928471a388d7e7e

fixed regression: the dirty flag was not cleared after a project was (successfully) loaded.
the regression introduced in 9df2ed1d33e3d57247ca2029b043c7f2cc9ec5ad ("projects are now transparently updated to the latest format revision by renderer::ProjectFileReader").

src/appleseed.studio/mainwindow/project/projectmanager.cpp
@@ -181,6 +181,7 @@ bool ProjectManager::do_load_project(const string& filepath)
         return false;
 
     m_project = loaded_project;
+    m_dirty_flag = false;
 
     return true;
 }