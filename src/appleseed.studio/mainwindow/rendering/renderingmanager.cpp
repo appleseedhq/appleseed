appleseedhq/appleseed

commit fedd9029aaa40cdf5c13cdf118f0c15cdf2e6e31

bug fix: the rendering time displayed in appleseed.studio's status bar was sometimes incorrect when rendering failed.

src/appleseed.studio/mainwindow/rendering/renderingmanager.cpp
@@ -287,7 +287,6 @@ void RenderingManager::slot_rendering_begin()
     }
 
     const int IdleUpdateRate = 15;  // hertz
-
     m_render_widget_update_timer.start(1000 / IdleUpdateRate, this);
 }
 
@@ -322,6 +321,7 @@ void RenderingManager::slot_frame_begin()
 
 void RenderingManager::slot_frame_end()
 {
+    m_rendering_timer.stop();
     m_status_bar.stop_rendering_time_display();
 
     m_render_widget->update();

src/appleseed/foundation/utility/stopwatch.h
@@ -60,6 +60,9 @@ class Stopwatch
     // Start or restart the stopwatch.
     void start();
 
+    // Stop the stopwatch.
+    void stop();
+
     // Measure the time elapsed since the last call to start().
     // The stopwatch keeps running.
     Stopwatch& measure();
@@ -124,6 +127,12 @@ inline void Stopwatch<Timer>::start()
 {
     m_is_running = true;
     m_start = m_timer.read();
+}
+
+template <typename Timer>
+inline void Stopwatch<Timer>::stop()
+{
+    m_is_running = false;
 }
 
 template <typename Timer>

commit 54108b758079bb089c8cb029548fea3c7535041c

fixed regression: total rendering time was no longer written to the log panel in appleseed.studio.
regression introduced in fedd9029aaa40cdf5c13cdf118f0c15cdf2e6e31 ("bug fix: the rendering time displayed in appleseed.studio's status bar was sometimes incorrect when rendering failed.").

also cleaned up the foundation::Stopwatch API.

src/appleseed.studio/mainwindow/rendering/renderingmanager.cpp
@@ -282,17 +282,12 @@ void RenderingManager::timerEvent(QTimerEvent* event)
 
 void RenderingManager::print_final_rendering_time()
 {
-    if (m_rendering_timer.is_running())
-    {
-        m_rendering_timer.measure();
+    const double rendering_time = m_rendering_timer.get_seconds();
+    const string rendering_time_string = pretty_time(rendering_time, 3);
 
-        const double rendering_time = m_rendering_timer.get_seconds();
-        const string rendering_time_string = pretty_time(rendering_time, 3);
+    RENDERER_LOG_INFO("rendering finished in %s.", rendering_time_string.c_str());
 
-        RENDERER_LOG_INFO("rendering finished in %s.", rendering_time_string.c_str());
-
-        m_status_bar.set_text("Rendering finished in " + rendering_time_string);
-    }
+    m_status_bar.set_text("Rendering finished in " + rendering_time_string);
 }
 
 void RenderingManager::print_average_luminance()
@@ -346,6 +341,8 @@ void RenderingManager::slot_rendering_begin()
     apply_permanent_states();
     consume_delayed_actions();
 
+    m_rendering_timer.clear();
+
     const int IdleUpdateRate = 15;  // hertz
     m_render_widget_update_timer.start(1000 / IdleUpdateRate, this);
 }
@@ -376,13 +373,13 @@ void RenderingManager::slot_frame_begin()
         m_camera_changed = false;
     }
 
-    m_rendering_timer.start();
     m_status_bar.start_rendering_time_display(&m_rendering_timer);
+    m_rendering_timer.start();
 }
 
 void RenderingManager::slot_frame_end()
 {
-    m_rendering_timer.stop();
+    m_rendering_timer.measure();
     m_status_bar.stop_rendering_time_display();
 
     m_render_widget->update();

src/appleseed/foundation/utility/stopwatch.h
@@ -56,19 +56,17 @@ class Stopwatch
 
     // Return the internal timer.
     Timer& get_timer();
+    const Timer& get_timer() const;
+
+    // Set the elapsed time to 0.
+    void clear();
 
     // Start or restart the stopwatch.
     void start();
 
-    // Stop the stopwatch.
-    void stop();
-
     // Measure the time elapsed since the last call to start().
     // The stopwatch keeps running.
     Stopwatch& measure();
-
-    // Return true if the stopwatch is running, i.e. if start() has been called at least once.
-    bool is_running() const;
 
     // Read the number of timer ticks elapsed since the last call to start().
     // measure() must have been called prior to calling this method.
@@ -82,7 +80,6 @@ class Stopwatch
     Timer   m_timer;        // internal timer
     uint64  m_timer_freq;   // frequency of the internal timer
     uint64  m_overhead;     // measured overhead of calling start() + measure()
-    bool    m_is_running;   // true if start() was called at least once
     uint64  m_start;        // timer value when start() is called
     uint64  m_elapsed;      // elapsed time when measure() is called, adjusted for overhead
 
@@ -111,7 +108,6 @@ Stopwatch<Timer>::Stopwatch(const size_t overhead_measures)
         m_overhead = measure_overhead(overhead_measures);
     }
 
-    m_is_running = false;
     m_start = 0;
     m_elapsed = 0;
 }
@@ -123,24 +119,27 @@ inline Timer& Stopwatch<Timer>::get_timer()
 }
 
 template <typename Timer>
-inline void Stopwatch<Timer>::start()
+inline const Timer& Stopwatch<Timer>::get_timer() const
 {
-    m_is_running = true;
-    m_start = m_timer.read();
+    return m_timer;
 }
 
 template <typename Timer>
-inline void Stopwatch<Timer>::stop()
+inline void Stopwatch<Timer>::clear()
 {
-    m_is_running = false;
+    m_elapsed = 0;
+}
+
+template <typename Timer>
+inline void Stopwatch<Timer>::start()
+{
+    m_start = m_timer.read();
 }
 
 template <typename Timer>
 inline Stopwatch<Timer>& Stopwatch<Timer>::measure()
 {
-    assert(m_is_running);
-
-    // Compute elapsed time.
+    // Compute and store elapsed time.
     const uint64 end = m_timer.read();
     m_elapsed = end >= m_start ? end - m_start : 0;
 
@@ -150,12 +149,6 @@ inline Stopwatch<Timer>& Stopwatch<Timer>::measure()
     else m_elapsed = 0;
 
     return *this;
-}
-
-template <typename Timer>
-bool Stopwatch<Timer>::is_running() const
-{
-    return m_is_running;
 }
 
 template <typename Timer>