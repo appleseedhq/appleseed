appleseedhq/appleseed

commit 9e24e22a61b7678ca54d79f1c31eecd9a34982c9

implemented automatic binding of environment inputs.

src/appleseed/renderer/modeling/environment/environment.cpp
@@ -29,9 +29,15 @@
 // Interface header.
 #include "environment.h"
 
+// appleseed.renderer headers.
+#include "renderer/modeling/environmentedf/environmentedf.h"
+#include "renderer/modeling/environmentshader/environmentshader.h"
+#include "renderer/modeling/input/inputarray.h"
+
 // appleseed.foundation headers.
 #include "foundation/utility/containers/dictionary.h"
 #include "foundation/utility/containers/specializedarrays.h"
+#include "foundation/utility/uid.h"
 
 using namespace foundation;
 using namespace std;
@@ -49,13 +55,16 @@ namespace
 }
 
 Environment::Environment(
-    const char*                         name,
-    const ParamArray&                   params)
-  : Entity(g_class_uid, params)
+    const char*         name,
+    const ParamArray&   params)
+  : ConnectableEntity(g_class_uid, params)
   , m_environment_edf(0)
   , m_environment_shader(0)
 {
     set_name(name);
+
+    m_inputs.declare("environment_edf", InputEntity, "");
+    m_inputs.declare("environment_shader", InputEntity, "");
 }
 
 void Environment::release()
@@ -63,15 +72,18 @@ void Environment::release()
     delete this;
 }
 
-void Environment::bind_entities(
-    const EnvironmentEDFContainer&      environment_edfs,
-    const EnvironmentShaderContainer&   environment_shaders)
+bool Environment::on_frame_begin(const Project& project)
 {
-    m_environment_edf =
-        get_optional_entity<EnvironmentEDF>(environment_edfs, m_params, "environment_edf");
+    m_environment_edf = static_cast<EnvironmentEDF*>(m_inputs.get_entity("environment_edf"));
+    m_environment_shader = static_cast<EnvironmentShader*>(m_inputs.get_entity("environment_shader"));
 
-    m_environment_shader =
-        get_optional_entity<EnvironmentShader>(environment_shaders, m_params, "environment_shader");
+    return true;
+}
+
+void Environment::on_frame_end(const Project& project)
+{
+    m_environment_edf = 0;
+    m_environment_shader = 0;
 }
 
 const char* Environment::get_model() const
@@ -98,7 +110,8 @@ DictionaryArray EnvironmentFactory::get_widget_definitions()
             .insert("name", "environment_edf")
             .insert("label", "Environment EDF")
             .insert("widget", "entity_picker")
-            .insert("entity_types", Dictionary().insert("environment_edf", "Environment EDFs"))
+            .insert("entity_types",
+                Dictionary().insert("environment_edf", "Environment EDFs"))
             .insert("use", "optional"));
 
     definitions.push_back(
@@ -106,15 +119,16 @@ DictionaryArray EnvironmentFactory::get_widget_definitions()
             .insert("name", "environment_shader")
             .insert("label", "Environment Shader")
             .insert("widget", "entity_picker")
-            .insert("entity_types", Dictionary().insert("environment_shader", "Environment Shaders"))
+            .insert("entity_types",
+                Dictionary().insert("environment_shader", "Environment Shaders"))
             .insert("use", "optional"));
 
     return definitions;
 }
 
 auto_release_ptr<Environment> EnvironmentFactory::create(
-    const char*                         name,
-    const ParamArray&                   params)
+    const char*         name,
+    const ParamArray&   params)
 {
     return auto_release_ptr<Environment>(new Environment(name, params));
 }

src/appleseed/renderer/modeling/environment/environment.h
@@ -30,14 +30,21 @@
 #define APPLESEED_RENDERER_MODELING_ENVIRONMENT_ENVIRONMENT_H
 
 // appleseed.renderer headers.
-#include "renderer/global/global.h"
-#include "renderer/modeling/entity/entity.h"
-#include "renderer/modeling/scene/containers.h"
+#include "renderer/modeling/entity/connectableentity.h"
+
+// appleseed.foundation headers.
+#include "foundation/platform/compiler.h"
+#include "foundation/utility/autoreleaseptr.h"
+
+// appleseed.main headers.
+#include "main/dllsymbol.h"
 
 // Forward declarations.
 namespace foundation    { class DictionaryArray; }
 namespace renderer      { class EnvironmentEDF; }
 namespace renderer      { class EnvironmentShader; }
+namespace renderer      { class ParamArray; }
+namespace renderer      { class Project; }
 
 namespace renderer
 {
@@ -46,20 +53,22 @@ namespace renderer
 // Environment.
 //
 
-class RENDERERDLL Environment
-  : public Entity
+class DLLSYMBOL Environment
+  : public ConnectableEntity
 {
   public:
     // Delete this instance.
-    virtual void release();
+    virtual void release() override;
 
     // Return a string identifying the model of this environment.
     const char* get_model() const;
 
-    // Perform entity binding.
-    void bind_entities(
-        const EnvironmentEDFContainer&      environment_edfs,
-        const EnvironmentShaderContainer&   environment_shaders);
+    // This method is called once before rendering each frame.
+    // Returns true on success, false otherwise.
+    bool on_frame_begin(const Project& project);
+
+    // This method is called once after rendering each frame.
+    void on_frame_end(const Project& project);
 
     // Return the EDF of this environment, or 0 if the environment doesn't have one.
     EnvironmentEDF* get_environment_edf() const;
@@ -70,13 +79,13 @@ class RENDERERDLL Environment
   private:
     friend class EnvironmentFactory;
 
-    EnvironmentEDF*     m_environment_edf;
-    EnvironmentShader*  m_environment_shader;
+    EnvironmentEDF*         m_environment_edf;
+    EnvironmentShader*      m_environment_shader;
 
     // Constructor.
     Environment(
-        const char*                         name,
-        const ParamArray&                   params);
+        const char*         name,
+        const ParamArray&   params);
 };
 
 
@@ -84,7 +93,7 @@ class RENDERERDLL Environment
 // Environment factory.
 //
 
-class RENDERERDLL EnvironmentFactory
+class DLLSYMBOL EnvironmentFactory
 {
   public:
     // Return a string identifying this environment model.
@@ -95,8 +104,8 @@ class RENDERERDLL EnvironmentFactory
 
     // Create a new environment.
     static foundation::auto_release_ptr<Environment> create(
-        const char*                         name,
-        const ParamArray&                   params);
+        const char*         name,
+        const ParamArray&   params);
 };
 
 

src/appleseed/renderer/modeling/input/inputbinder.cpp
@@ -198,9 +198,14 @@ void InputBinder::bind_scene_entities_inputs(
     // Bind environment inputs.
     if (scene.get_environment())
     {
-        scene.get_environment()->bind_entities(
-            scene.environment_edfs(),
-            scene.environment_shaders());
+        Environment& environment = *scene.get_environment();
+        bind_scene_entity_inputs(
+            scene,
+            scene_symbols,
+            SymbolTable::symbol_name(SymbolTable::SymbolEnvironment),
+            environment.get_name(),
+            environment.get_parameters(),
+            environment.get_inputs());
     }
 }
 
@@ -456,6 +461,14 @@ bool InputBinder::try_bind_scene_entity_to_input(
             entity_name,
             param_value,
             input);
+        return true;
+
+      case SymbolTable::SymbolEnvironmentEDF:
+        input.bind(scene.environment_edfs().get_by_name(param_value));
+        return true;
+
+      case SymbolTable::SymbolEnvironmentShader:
+        input.bind(scene.environment_shaders().get_by_name(param_value));
         return true;
 
       default:

src/appleseed/renderer/modeling/scene/scene.cpp
@@ -183,6 +183,10 @@ bool Scene::on_frame_begin(const Project& project)
 
     success = success && invoke_on_frame_begin(project, environment_edfs());
     success = success && invoke_on_frame_begin(project, environment_shaders());
+
+    if (impl->m_environment.get())
+        success = success && impl->m_environment->on_frame_begin(project);
+
     success = success && invoke_on_frame_begin(project, assemblies());
     success = success && invoke_on_frame_begin(project, assembly_instances());
 
@@ -193,6 +197,10 @@ void Scene::on_frame_end(const Project& project)
 {
     invoke_on_frame_end(project, assembly_instances());
     invoke_on_frame_end(project, assemblies());
+
+    if (impl->m_environment.get())
+        impl->m_environment->on_frame_end(project);
+
     invoke_on_frame_end(project, environment_shaders());
     invoke_on_frame_end(project, environment_edfs());
 

commit d8539db4bfc5fbe048b66101671ac711ad555315

fixed regression: assigning an environment EDF to a render layer had no effect anymore.
regression likely introduced in 9e24e22a61b7678ca54d79f1c31eecd9a34982c9 ("implemented automatic binding of environment inputs").

src/appleseed/renderer/modeling/environment/environment.cpp
@@ -72,10 +72,15 @@ void Environment::release()
     delete this;
 }
 
+const char* Environment::get_model() const
+{
+    return EnvironmentFactory::get_model();
+}
+
 bool Environment::on_frame_begin(const Project& project)
 {
-    m_environment_edf = static_cast<EnvironmentEDF*>(m_inputs.get_entity("environment_edf"));
-    m_environment_shader = static_cast<EnvironmentShader*>(m_inputs.get_entity("environment_shader"));
+    m_environment_edf = get_uncached_environment_edf();
+    m_environment_shader = get_uncached_environment_shader();
 
     return true;
 }
@@ -86,9 +91,14 @@ void Environment::on_frame_end(const Project& project)
     m_environment_shader = 0;
 }
 
-const char* Environment::get_model() const
+EnvironmentEDF* Environment::get_uncached_environment_edf() const
 {
-    return EnvironmentFactory::get_model();
+    return static_cast<EnvironmentEDF*>(m_inputs.get_entity("environment_edf"));
+}
+
+EnvironmentShader* Environment::get_uncached_environment_shader() const
+{
+    return static_cast<EnvironmentShader*>(m_inputs.get_entity("environment_shader"));
 }
 
 

src/appleseed/renderer/modeling/environment/environment.h
@@ -70,11 +70,19 @@ class DLLSYMBOL Environment
     // This method is called once after rendering each frame.
     void on_frame_end(const Project& project);
 
+    //
+    // The get_*() methods below retrieve entities that were cached by on_frame_begin().
+    // To retrieve the entities before on_frame_begin() or after on_frame_end() is called,
+    // use the get_uncached_*() variants.
+    //
+
     // Return the EDF of this environment, or 0 if the environment doesn't have one.
     EnvironmentEDF* get_environment_edf() const;
+    EnvironmentEDF* get_uncached_environment_edf() const;
 
     // Return the shader of this environment, or 0 if the environment doesn't have one.
     EnvironmentShader* get_environment_shader() const;
+    EnvironmentShader* get_uncached_environment_shader() const;
 
   private:
     friend class EnvironmentFactory;

src/appleseed/renderer/modeling/project/project.cpp
@@ -275,7 +275,7 @@ namespace
             format,
             scene);
 
-        EnvironmentEDF* env_edf = scene.get_environment()->get_environment_edf();
+        EnvironmentEDF* env_edf = scene.get_environment()->get_uncached_environment_edf();
 
         if (env_edf)
             assign_entity_to_render_layer(aov_images, mapping, format, *env_edf);