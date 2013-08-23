appleseedhq/appleseed

commit 45c2bfc7a9cde6bae6283b51be58884559c0b8a0

collapse meshes with deformation motion blur to static meshes if all the poses are identical.

src/appleseed/renderer/modeling/object/meshobject.cpp
@@ -146,7 +146,7 @@ size_t MeshObject::get_vertex_count() const
     return impl->m_tess.m_vertices.size();
 }
 
-GVector3 MeshObject::get_vertex(const size_t index) const
+const GVector3& MeshObject::get_vertex(const size_t index) const
 {
     return impl->m_tess.m_vertices[index];
 }
@@ -168,7 +168,7 @@ size_t MeshObject::get_vertex_normal_count() const
     return impl->m_tess.m_vertex_normals.size();
 }
 
-GVector3 MeshObject::get_vertex_normal(const size_t index) const
+const GVector3& MeshObject::get_vertex_normal(const size_t index) const
 {
     return impl->m_tess.m_vertex_normals[index];
 }
@@ -183,7 +183,7 @@ size_t MeshObject::get_tex_coords_count() const
     return impl->m_tess.get_uv_vertex_count();
 }
 
-GVector2 MeshObject::get_tex_coords(const size_t index) const
+const GVector2& MeshObject::get_tex_coords(const size_t index) const
 {
     return impl->m_tess.get_uv_vertex(index);
 }
@@ -205,7 +205,7 @@ size_t MeshObject::get_triangle_count() const
     return impl->m_tess.m_primitives.size();
 }
 
-Triangle MeshObject::get_triangle(const size_t index) const
+const Triangle& MeshObject::get_triangle(const size_t index) const
 {
     return impl->m_tess.m_primitives[index];
 }
@@ -233,6 +233,11 @@ GVector3 MeshObject::get_vertex_pose(
     const size_t        motion_segment_index) const
 {
     return impl->m_tess.get_vertex_pose(vertex_index, motion_segment_index);
+}
+
+void MeshObject::clear_vertex_poses()
+{
+    impl->m_tess.clear_vertex_poses();
 }
 
 void MeshObject::reserve_material_slots(const size_t count)

src/appleseed/renderer/modeling/object/meshobject.h
@@ -78,24 +78,24 @@ class DLLSYMBOL MeshObject
     void reserve_vertices(const size_t count);
     size_t push_vertex(const GVector3& vertex);
     size_t get_vertex_count() const;
-    GVector3 get_vertex(const size_t index) const;
+    const GVector3& get_vertex(const size_t index) const;
 
     // Insert and access vertex normals.
     void reserve_vertex_normals(const size_t count);
     size_t push_vertex_normal(const GVector3& normal);
     size_t get_vertex_normal_count() const;
-    GVector3 get_vertex_normal(const size_t index) const;
+    const GVector3& get_vertex_normal(const size_t index) const;
 
     // Insert and access texture coordinates.
     size_t push_tex_coords(const GVector2& tex_coords);
     size_t get_tex_coords_count() const;
-    GVector2 get_tex_coords(const size_t index) const;
+    const GVector2& get_tex_coords(const size_t index) const;
 
     // Insert and access triangles.
     void reserve_triangles(const size_t count);
     size_t push_triangle(const Triangle& triangle);
     size_t get_triangle_count() const;
-    Triangle get_triangle(const size_t index) const;
+    const Triangle& get_triangle(const size_t index) const;
 
     // Set/get the number of motion segments (the number of motion vectors per vertex).
     void set_motion_segment_count(const size_t count);
@@ -113,6 +113,9 @@ class DLLSYMBOL MeshObject
     GVector3 get_vertex_pose(
         const size_t    vertex_index,
         const size_t    motion_segment_index) const;
+
+    // Remove all vertex poses.
+    void clear_vertex_poses();
 
     // Insert and access material slots.
     void reserve_material_slots(const size_t count);

src/appleseed.python/bind_mesh_object.cpp
@@ -123,8 +123,7 @@ void bind_mesh_object()
         .def_readwrite("a0", &Triangle::m_a0)
         .def_readwrite("a1", &Triangle::m_a1)
         .def_readwrite("a2", &Triangle::m_a2)
-        .def_readwrite("pa", &Triangle::m_pa)
-        ;
+        .def_readwrite("pa", &Triangle::m_pa);
 
     bpy::class_<MeshObject, auto_release_ptr<MeshObject>, bpy::bases<Object>, boost::noncopyable>("MeshObject", bpy::no_init)
         .def("__init__", bpy::make_constructor(detail::create_mesh_obj))
@@ -132,36 +131,34 @@ void bind_mesh_object()
         .def("reserve_vertices", &MeshObject::reserve_vertices)
         .def("push_vertex", &MeshObject::push_vertex)
         .def("get_vertex_count", &MeshObject::get_vertex_count)
-        .def("get_vertex", &MeshObject::get_vertex)
+        .def("get_vertex", &MeshObject::get_vertex, bpy::return_value_policy<bpy::reference_existing_object>())
 
         .def("reserve_vertex_normals", &MeshObject::reserve_vertex_normals)
         .def("push_vertex_normal", &MeshObject::push_vertex_normal)
         .def("get_vertex_normal_count", &MeshObject::get_vertex_normal_count)
-        .def("get_vertex_normal", &MeshObject::get_vertex_normal)
+        .def("get_vertex_normal", &MeshObject::get_vertex_normal, bpy::return_value_policy<bpy::reference_existing_object>())
 
         .def("push_tex_coords", &MeshObject::push_tex_coords)
         .def("get_tex_coords_count", &MeshObject::get_tex_coords_count)
-        .def("get_tex_coords", &MeshObject::get_tex_coords)
+        .def("get_tex_coords", &MeshObject::get_tex_coords, bpy::return_value_policy<bpy::reference_existing_object>())
 
         .def("reserve_triangles", &MeshObject::reserve_triangles)
         .def("push_triangle", &MeshObject::push_triangle)
         .def("get_triangle_count", &MeshObject::get_triangle_count)
-        .def("get_triangle", &MeshObject::get_triangle)
+        .def("get_triangle", &MeshObject::get_triangle, bpy::return_value_policy<bpy::reference_existing_object>())
 
         .def("set_motion_segment_count", &MeshObject::set_motion_segment_count)
         .def("get_motion_segment_count", &MeshObject::get_motion_segment_count)
 
         .def("set_vertex_pose", &MeshObject::set_vertex_pose)
         .def("get_vertex_pose", &MeshObject::get_vertex_pose)
-        ;
+        .def("clear_vertex_poses", &MeshObject::clear_vertex_poses);
 
     boost::python::implicitly_convertible<auto_release_ptr<MeshObject>, auto_release_ptr<Object> >();
 
     bpy::class_<MeshObjectReader>("MeshObjectReader", bpy::no_init)
-        .def("read", detail::read_mesh_objects).staticmethod("read")
-        ;
+        .def("read", detail::read_mesh_objects).staticmethod("read");
 
     bpy::class_<MeshObjectWriter>("MeshObjectWriter", bpy::no_init)
-        .def("write", detail::write_mesh_object).staticmethod("write")
-        ;
+        .def("write", detail::write_mesh_object).staticmethod("write");
 }

src/appleseed/foundation/utility/attributeset.cpp
@@ -64,6 +64,15 @@ AttributeSet::ChannelID AttributeSet::create_channel(
     return m_channels.size() - 1;
 }
 
+void AttributeSet::delete_channel(const ChannelID channel_id)
+{
+    assert(channel_id < m_channels.size());
+
+    delete m_channels[channel_id];
+
+    m_channels.erase(m_channels.begin() + channel_id);
+}
+
 AttributeSet::ChannelID AttributeSet::find_channel(const char* name) const
 {
     assert(name);

src/appleseed/foundation/utility/attributeset.h
@@ -64,6 +64,9 @@ class AttributeSet
         const NumericTypeID type,
         const size_t        dimension);
 
+    // Delete an existing channel.
+    void delete_channel(const ChannelID channel_id);
+
     // Find a given attribute channel. Return InvalidChannelID if
     // the requested channel does not exist. Since this method is
     // typically called with a literal value in argument ("uv"),
@@ -72,8 +75,7 @@ class AttributeSet
     ChannelID find_channel(const char* name) const;
 
     // Return the number of attributes in a given attribute channel.
-    size_t get_attribute_count(
-        const ChannelID     channel_id) const;
+    size_t get_attribute_count(const ChannelID channel_id) const;
 
     // Insert a new attribute at the end of a given attribute channel.
     // Return the index of the attribute in the attribute channel.
@@ -114,8 +116,7 @@ class AttributeSet
 // AttributeSet class implementation.
 //
 
-inline size_t AttributeSet::get_attribute_count(
-    const ChannelID         channel_id) const
+inline size_t AttributeSet::get_attribute_count(const ChannelID channel_id) const
 {
     // Get the channel descriptor.
     assert(channel_id < m_channels.size());

src/appleseed/renderer/kernel/tessellation/statictessellation.h
@@ -106,6 +106,9 @@ class StaticTessellation
         const size_t    vertex_index,
         const size_t    motion_segment_index) const;
 
+    // Remove all vertex poses.
+    void clear_vertex_poses();
+
     // Compute the local space bounding box of the tessellation over the shutter interval.
     GAABB3 compute_local_bbox() const;
 
@@ -243,6 +246,16 @@ inline GVector3 StaticTessellation<Primitive>::get_vertex_pose(
         &v);
 
     return v;
+}
+
+template <typename Primitive>
+void StaticTessellation<Primitive>::clear_vertex_poses()
+{
+    if (m_vp_cid != foundation::AttributeSet::InvalidChannelID)
+    {
+        m_vertex_attributes.delete_channel(m_vp_cid);
+        m_vp_cid = foundation::AttributeSet::InvalidChannelID;
+    }
 }
 
 template <typename Primitive>

src/appleseed/renderer/modeling/object/meshobjectreader.cpp
@@ -458,11 +458,11 @@ namespace
             "loaded mesh file %s (%s %s, %s %s, %s %s) in %s.",
             filename,
             pretty_int(builder.get_objects().size()).c_str(),
-            plural(builder.get_objects().size(), "object").c_str(),
+            builder.get_objects().size() > 1 ? "objects" : "object",
             pretty_int(builder.get_total_vertex_count()).c_str(),
-            plural(builder.get_total_vertex_count(), "vertex", "vertices").c_str(),
+            builder.get_total_vertex_count() > 1 ? "vertices" : "vertex",
             pretty_int(builder.get_total_triangle_count()).c_str(),
-            plural(builder.get_total_triangle_count(), "triangle").c_str(),
+            builder.get_total_triangle_count() > 1 ? "triangles" : "triangle",
             pretty_time(stopwatch.get_seconds()).c_str());
 
         objects = array_vector<MeshObjectArray>(builder.get_objects());
@@ -471,13 +471,13 @@ namespace
     }
 
     bool set_vertex_poses(
-        const MeshObjectArray&  objects,
-        const MeshObjectArray&  objects_next,
+        MeshObjectArray&        objects,
+        const MeshObjectArray&  poses,
         const size_t            motion_segment_index,
         const char*             filename,
         const char*             base_object_name)
     {
-        if (objects.size() != objects_next.size())
+        if (objects.size() != poses.size())
         {
             RENDERER_LOG_ERROR(
                 "while reading key frame for object \"%s\" from mesh file %s: "
@@ -486,7 +486,7 @@ namespace
                 filename,
                 objects.size(),
                 objects.size() > 1 ? "s" : "",
-                objects_next.size());
+                poses.size());
 
             return false;
         }
@@ -494,9 +494,9 @@ namespace
         for (size_t i = 0; i < objects.size(); ++i)
         {
             MeshObject* object = objects[i];
-            const MeshObject* object_next = objects_next[i];
+            const MeshObject* pose = poses[i];
 
-            if (object->get_vertex_count() != object_next->get_vertex_count())
+            if (object->get_vertex_count() != pose->get_vertex_count())
             {
                 RENDERER_LOG_ERROR(
                     "while reading key frame for object \"%s\" from mesh file %s: "
@@ -505,12 +505,12 @@ namespace
                     filename,
                     object->get_vertex_count(),
                     object->get_vertex_count() > 1 ? "vertices" : "vertex",
-                    object_next->get_vertex_count());
+                    pose->get_vertex_count());
 
                 return false;
             }
 
-            if (object->get_triangle_count() != object_next->get_triangle_count())
+            if (object->get_triangle_count() != pose->get_triangle_count())
             {
                 RENDERER_LOG_WARNING(
                     "while reading key frame for object \"%s\" from mesh file %s: "
@@ -519,13 +519,13 @@ namespace
                     filename,
                     object->get_triangle_count(),
                     object->get_triangle_count() > 1 ? "triangles" : "triangle",
-                    object_next->get_triangle_count());
+                    pose->get_triangle_count());
             }
 
-            const size_t vertex_count = object_next->get_vertex_count();
+            const size_t vertex_count = pose->get_vertex_count();
 
             for (size_t j = 0; j < vertex_count; ++j)
-                object->set_vertex_pose(j, motion_segment_index, object_next->get_vertex(j));
+                object->set_vertex_pose(j, motion_segment_index, pose->get_vertex(j));
         }
 
         return true;
@@ -550,6 +550,48 @@ namespace
         }
     };
 
+    bool has_actual_motion(const MeshObject& object)
+    {
+        const size_t motion_segment_count = object.get_motion_segment_count();
+        const size_t vertex_count = object.get_vertex_count();
+
+        for (size_t i = 0; i < vertex_count; ++i)
+        {
+            if (object.get_vertex_pose(i, 0) != object.get_vertex(i))
+                return true;
+
+            for (size_t j = 1; j < motion_segment_count; ++j)
+            {
+                if (object.get_vertex_pose(i, j) != object.get_vertex_pose(i, j - 1))
+                    return true;
+            }
+        }
+
+        return false;
+    }
+
+    void collapse_to_static_object(MeshObject& object)
+    {
+        if (object.get_motion_segment_count() == 0)
+            return;
+
+        if (has_actual_motion(object))
+            return;
+
+        RENDERER_LOG_INFO(
+            "collapsing mesh object \"%s\" (%s %s, %s %s, %s %s) to static mesh.",
+            object.get_path().c_str(),
+            pretty_int(object.get_vertex_count()).c_str(),
+            object.get_vertex_count() > 1 ? "vertices" : "vertex",
+            pretty_int(object.get_triangle_count()).c_str(),
+            object.get_triangle_count() > 1 ? "triangles" : "triangle",
+            pretty_int(object.get_motion_segment_count()).c_str(),
+            object.get_motion_segment_count() > 1 ? "motion segments" : "motion segment");
+
+        object.clear_vertex_poses();
+        object.set_motion_segment_count(0);
+    }
+
     bool read_key_framed_mesh_object(
         const SearchPaths&      search_paths,
         const StringDictionary& filenames,
@@ -558,6 +600,7 @@ namespace
         MeshObjectArray&        objects)
     {
         vector<MeshObjectKeyFrame> key_frames;
+        key_frames.reserve(filenames.size());
 
         for (const_each<StringDictionary> i = filenames; i; ++i)
             key_frames.push_back(MeshObjectKeyFrame(from_string<double>(i->name()), i->value<string>()));
@@ -578,18 +621,26 @@ namespace
         {
             const string& filename = key_frames[i].m_filename;
 
-            MeshObjectArray key_frame;
+            MeshObjectArray poses;
 
             if (!read_mesh_object(
                     search_paths.qualify(filename).c_str(),
                     base_object_name,
                     params,
-                    key_frame))
+                    poses))
                 return false;
 
-            if (!set_vertex_poses(objects, key_frame, i - 1, filename.c_str(), base_object_name))
+            if (!set_vertex_poses(
+                    objects,
+                    poses,
+                    i - 1,
+                    filename.c_str(),
+                    base_object_name))
                 return false;
         }
+
+        for (size_t i = 0; i < objects.size(); ++i)
+            collapse_to_static_object(*objects[i]);
 
         return true;
     }

commit 4c0dd0f1f2de03359788dfae3c4e8d090242e184

fixed potential crash (returning reference to temporary) introduced in 45c2bfc7a9cde6bae6283b51be58884559c0b8a0.

src/appleseed/renderer/modeling/object/meshobject.cpp
@@ -183,7 +183,7 @@ size_t MeshObject::get_tex_coords_count() const
     return impl->m_tess.get_uv_vertex_count();
 }
 
-const GVector2& MeshObject::get_tex_coords(const size_t index) const
+GVector2 MeshObject::get_tex_coords(const size_t index) const
 {
     return impl->m_tess.get_uv_vertex(index);
 }

src/appleseed/renderer/modeling/object/meshobject.h
@@ -89,7 +89,7 @@ class DLLSYMBOL MeshObject
     // Insert and access texture coordinates.
     size_t push_tex_coords(const GVector2& tex_coords);
     size_t get_tex_coords_count() const;
-    const GVector2& get_tex_coords(const size_t index) const;
+    GVector2 get_tex_coords(const size_t index) const;
 
     // Insert and access triangles.
     void reserve_triangles(const size_t count);