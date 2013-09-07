appleseedhq/appleseed

commit 8e9b1d1d7b3eee5bb99cefef0b9b8f782c28233c

the foundation/utility/job component now uses condition variables instead of yielding.

src/appleseed/foundation/utility/job/jobqueue.cpp
@@ -31,9 +31,13 @@
 
 // appleseed.foundation headers.
 #include "foundation/platform/thread.h"
+#include "foundation/utility/job/abortswitch.h"
 #include "foundation/utility/job/ijob.h"
 #include "foundation/utility/foreach.h"
 #include "foundation/utility/iterators.h"
+
+// boost headers.
+#include "boost/thread/condition_variable.hpp"
 
 // Standard headers.
 #include <cassert>
@@ -50,9 +54,10 @@ namespace foundation
 
 struct JobQueue::Impl
 {
-    mutable Spinlock    m_spinlock;
-    JobList             m_scheduled_jobs;
-    JobList             m_running_jobs;
+    mutable mutex           m_mutex;
+    condition_variable_any  m_event;
+    JobList                 m_scheduled_jobs;
+    JobList                 m_running_jobs;
 
     static void delete_jobs(JobList& list)
     {
@@ -86,49 +91,52 @@ JobQueue::~JobQueue()
 
 void JobQueue::clear_scheduled_jobs()
 {
-    Spinlock::ScopedLock lock(impl->m_spinlock);
+    mutex::scoped_lock lock(impl->m_mutex);
 
     impl->delete_jobs(impl->m_scheduled_jobs);
+
+    // Notify worker threads that all scheduled jobs are gone.
+    impl->m_event.notify_all();
 }
 
 bool JobQueue::has_scheduled_jobs() const
 {
-    Spinlock::ScopedLock lock(impl->m_spinlock);
+    mutex::scoped_lock lock(impl->m_mutex);
 
     return !impl->m_scheduled_jobs.empty();
 }
 
 bool JobQueue::has_running_jobs() const
 {
-    Spinlock::ScopedLock lock(impl->m_spinlock);
+    mutex::scoped_lock lock(impl->m_mutex);
 
     return !impl->m_running_jobs.empty();
 }
 
 bool JobQueue::has_scheduled_or_running_jobs() const
 {
-    Spinlock::ScopedLock lock(impl->m_spinlock);
+    mutex::scoped_lock lock(impl->m_mutex);
 
     return !impl->m_scheduled_jobs.empty() || !impl->m_running_jobs.empty();
 }
 
 size_t JobQueue::get_scheduled_job_count() const
 {
-    Spinlock::ScopedLock lock(impl->m_spinlock);
+    mutex::scoped_lock lock(impl->m_mutex);
 
     return impl->m_scheduled_jobs.size();
 }
 
 size_t JobQueue::get_running_job_count() const
 {
-    Spinlock::ScopedLock lock(impl->m_spinlock);
+    mutex::scoped_lock lock(impl->m_mutex);
 
     return impl->m_running_jobs.size();
 }
 
 size_t JobQueue::get_total_job_count() const
 {
-    Spinlock::ScopedLock lock(impl->m_spinlock);
+    mutex::scoped_lock lock(impl->m_mutex);
 
     return impl->m_scheduled_jobs.size() + impl->m_running_jobs.size();
 }
@@ -137,34 +145,43 @@ void JobQueue::schedule(IJob* job, const bool transfer_ownership)
 {
     assert(job);
 
-    Spinlock::ScopedLock lock(impl->m_spinlock);
+    mutex::scoped_lock lock(impl->m_mutex);
 
     impl->m_scheduled_jobs.push_back(JobInfo(job, transfer_ownership));
+
+    // Notify worker threads that a new scheduled job is available.
+    impl->m_event.notify_all();
 }
 
 void JobQueue::wait_until_completion()
 {
-    while (true)
-    {
-        if (impl->m_spinlock.try_lock())
-        {
-            if (impl->m_scheduled_jobs.empty() && impl->m_running_jobs.empty())
-            {
-                impl->m_spinlock.unlock();
-                return;
-            }
+    mutex::scoped_lock lock(impl->m_mutex);
 
-            impl->m_spinlock.unlock();
-        }
-
-        yield();
-    }
+    // Wait until for all scheduled and running jobs.
+    while (!impl->m_scheduled_jobs.empty() || !impl->m_running_jobs.empty())
+        impl->m_event.wait(lock);
 }
 
 JobQueue::RunningJobInfo JobQueue::acquire_scheduled_job()
 {
-    Spinlock::ScopedLock lock(impl->m_spinlock);
+    mutex::scoped_lock lock(impl->m_mutex);
 
+    return acquire_scheduled_job_unlocked();
+}
+
+JobQueue::RunningJobInfo JobQueue::wait_for_scheduled_job(AbortSwitch& abort_switch)
+{
+    mutex::scoped_lock lock(impl->m_mutex);
+
+    // Wait for a scheduled job to be available.
+    while (impl->m_scheduled_jobs.empty() && !abort_switch.is_aborted())
+        impl->m_event.wait(lock);
+
+    return acquire_scheduled_job_unlocked();
+}
+
+JobQueue::RunningJobInfo JobQueue::acquire_scheduled_job_unlocked()
+{
     // Bail out if there is no scheduled job.
     if (impl->m_scheduled_jobs.empty())
         return RunningJobInfo(JobInfo(0, false), impl->m_running_jobs.end());
@@ -174,19 +191,30 @@ JobQueue::RunningJobInfo JobQueue::acquire_scheduled_job()
     impl->m_scheduled_jobs.pop_front();
     impl->m_running_jobs.push_back(job_info);
 
+    // Notify worker threads that a job has moved from the scheduled to the running state.
+    impl->m_event.notify_all();
+
     return RunningJobInfo(job_info, pred(impl->m_running_jobs.end()));
 }
 
 void JobQueue::retire_running_job(const RunningJobInfo& running_job_info)
 {
+    mutex::scoped_lock lock(impl->m_mutex);
+
     // Remove the job from the running list.
-    impl->m_spinlock.lock();
     impl->m_running_jobs.erase(running_job_info.second);
-    impl->m_spinlock.unlock();
 
     // Delete the job.
     if (running_job_info.first.m_owned)
         delete running_job_info.first.m_job;
+
+    // Notify worker threads that a scheduled job was retired.
+    impl->m_event.notify_all();
+}
+
+void JobQueue::signal_event()
+{
+    impl->m_event.notify_all();
 }
 
 }   // namespace foundation

src/appleseed/foundation/utility/job/jobqueue.h
@@ -42,6 +42,7 @@
 #include <list>
 
 // Forward declarations.
+namespace foundation    { class AbortSwitch; }
 namespace foundation    { class IJob; }
 
 // Unit test case declarations.
@@ -134,8 +135,17 @@ class DLLSYMBOL JobQueue
     // Acquire a scheduled job and change its state from 'scheduled' to 'running'.
     RunningJobInfo acquire_scheduled_job();
 
+    // Wait for a scheduled job to be available.
+    RunningJobInfo wait_for_scheduled_job(AbortSwitch& abort_switch);
+
+    // Acquire a scheduled job without any locking.
+    RunningJobInfo acquire_scheduled_job_unlocked();
+
     // Retire a running job. The job is deleted if it is owned by the queue.
     void retire_running_job(const RunningJobInfo& running_job_info);
+
+    // Signal a queue event.
+    void signal_event();
 };
 
 }       // namespace foundation

src/appleseed/foundation/utility/job/workerthread.cpp
@@ -76,6 +76,8 @@ void WorkerThread::start()
         return;
 
     m_abort_switch.clear();
+
+    // Start the thread.
     m_thread = new thread(m_thread_func);
 }
 
@@ -85,9 +87,12 @@ void WorkerThread::stop()
     if (!m_thread)
         return;
 
+    // Ask for the thread to stop, and wait until it has.
     m_abort_switch.abort();
+    m_job_queue.signal_event();
     m_thread->join();
 
+    // Delete the thread object.
     delete m_thread;
     m_thread = 0;
 }
@@ -98,17 +103,14 @@ void WorkerThread::run()
     {
         // Acquire a job.
         const JobQueue::RunningJobInfo running_job_info =
-            m_job_queue.acquire_scheduled_job();
+            m_job_queue.wait_for_scheduled_job(m_abort_switch);
 
         // Handle the case where the job queue is empty.
         if (running_job_info.first.m_job == 0)
         {
             if (m_flags & JobManager::KeepRunningOnEmptyQueue)
             {
-                // Give up time slice.
-                yield();
-
-                // Keep the thread running and checking for new jobs.
+                // Keep the thread running and waiting for new jobs.
                 continue;
             }
             else

commit 13f88d1d8a8fb981436a7b7c40dd0e32a3ff7808

fixed race condition introduced in 8e9b1d1d7b3eee5bb99cefef0b9b8f782c28233c: foundation::JobQueue::signal_event() must acquire the mutex.

src/appleseed/foundation/utility/job/jobqueue.cpp
@@ -214,6 +214,8 @@ void JobQueue::retire_running_job(const RunningJobInfo& running_job_info)
 
 void JobQueue::signal_event()
 {
+    mutex::scoped_lock lock(impl->m_mutex);
+
     impl->m_event.notify_all();
 }
 