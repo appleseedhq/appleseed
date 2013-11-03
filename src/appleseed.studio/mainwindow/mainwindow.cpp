appleseedhq/appleseed

commit 2bd657a6bbacdf8fd1ed7496d90fa7802aa05798

it is now possible to reload the current project during interactive rendering.

src/appleseed.studio/mainwindow/mainwindow.cpp
@@ -109,6 +109,7 @@ MainWindow::MainWindow(QWidget* parent)
   , m_ui(new Ui::MainWindow())
   , m_rendering_manager(m_status_bar)
   , m_project_explorer(0)
+  , m_was_rendering_before_open(false)
 {
     m_ui->setupUi(this);
 
@@ -139,6 +140,14 @@ MainWindow::~MainWindow()
 
 void MainWindow::open_project(const QString& filepath)
 {
+    m_was_rendering_before_open = m_rendering_manager.is_rendering();
+
+    if (m_was_rendering_before_open)
+    {
+        m_rendering_manager.abort_rendering();
+        m_rendering_manager.wait_until_rendering_end();
+    }
+
     set_project_widgets_enabled(false);
     set_rendering_widgets_enabled(false, false);
 
@@ -776,7 +785,11 @@ void MainWindow::start_rendering(const bool interactive)
 {
     assert(m_project_manager.is_project_open());
 
+    // Enable/disable widgets appropriately. File -> Reload is enabled during interactive rendering.
+    set_project_widgets_enabled(false);
     set_rendering_widgets_enabled(true, true);
+    if (interactive)
+        m_ui->action_file_reload_project->setEnabled(true);
 
     // Internally, clear the main image to transparent black and delete all AOV images.
     Project* project = m_project_manager.get_project();
@@ -922,7 +935,12 @@ void MainWindow::slot_reload_project()
 void MainWindow::slot_open_project_complete(const QString& filepath, const bool successful)
 {
     if (successful)
+    {
         on_project_change();
+
+        if (m_was_rendering_before_open)
+            start_rendering(true);
+    }
     else
     {
         show_project_file_loading_failed_message_box(this, filepath);

src/appleseed.studio/mainwindow/mainwindow.h
@@ -112,6 +112,7 @@ class MainWindow
     ProjectManager                      m_project_manager;
     ProjectExplorer*                    m_project_explorer;
     RenderingManager                    m_rendering_manager;
+    bool                                m_was_rendering_before_open;
 
     typedef std::map<std::string, RenderTab*> RenderTabCollection;
 

commit 5b82ff55b251726f461ad86e078f1f3cef4b26d2

fixed regression: the project explorer must remain enabled during interactive rendering.
regression introduced in 2bd657a6bbacdf8fd1ed7496d90fa7802aa05798 ("it is now possible to reload the current project during interactive rendering").

src/appleseed.studio/mainwindow/mainwindow.cpp
@@ -148,7 +148,8 @@ void MainWindow::open_project(const QString& filepath)
         m_rendering_manager.wait_until_rendering_end();
     }
 
-    set_project_widgets_enabled(false);
+    set_file_widgets_enabled(false);
+    set_project_explorer_enabled(false);
     set_rendering_widgets_enabled(false, false);
 
     const filesystem::path path(filepath.toStdString());
@@ -629,7 +630,8 @@ void MainWindow::on_project_change()
 void MainWindow::update_workspace()
 {
     update_window_title();
-    set_project_widgets_enabled(true);
+    set_file_widgets_enabled(true);
+    set_project_explorer_enabled(true);
     set_rendering_widgets_enabled(true, false);
 }
 
@@ -676,9 +678,9 @@ void MainWindow::update_window_title()
     setWindowTitle(title);
 }
 
-void MainWindow::set_project_widgets_enabled(const bool is_enabled)
+void MainWindow::set_file_widgets_enabled(const bool is_enabled)
 {
-    const bool is_project_open = is_enabled && m_project_manager.is_project_open();
+    const bool is_project_open = m_project_manager.is_project_open();
 
     // File -> New Project.
     m_ui->action_file_new_project->setEnabled(is_enabled);
@@ -696,19 +698,24 @@ void MainWindow::set_project_widgets_enabled(const bool is_enabled)
 
     // File -> Reload Project.
     m_ui->action_file_reload_project->setEnabled(
+        is_enabled &&
         is_project_open &&
         m_project_manager.get_project()->has_path());
 
     // File -> Save Project and Save Project As.
-    m_ui->action_file_save_project->setEnabled(is_project_open);
-    m_action_save_project->setEnabled(is_project_open);
-    m_ui->action_file_save_project_as->setEnabled(is_project_open);
+    m_ui->action_file_save_project->setEnabled(is_enabled && is_project_open);
+    m_action_save_project->setEnabled(is_enabled && is_project_open);
+    m_ui->action_file_save_project_as->setEnabled(is_enabled && is_project_open);
+}
 
-    // Project Explorer.
-    m_ui->label_filter->setEnabled(is_project_open);
-    m_ui->lineedit_filter->setEnabled(is_project_open);
-    m_ui->pushbutton_clear_filter->setEnabled(is_project_open);
-    m_ui->treewidget_project_explorer_scene->setEnabled(is_project_open);
+void MainWindow::set_project_explorer_enabled(const bool is_enabled)
+{
+    const bool is_project_open = m_project_manager.is_project_open();
+
+    m_ui->label_filter->setEnabled(is_enabled && is_project_open);
+    m_ui->lineedit_filter->setEnabled(is_enabled && is_project_open);
+    m_ui->pushbutton_clear_filter->setEnabled(is_enabled && is_project_open);
+    m_ui->treewidget_project_explorer_scene->setEnabled(is_enabled && is_project_open);
 }
 
 void MainWindow::set_rendering_widgets_enabled(const bool is_enabled, const bool is_rendering)
@@ -786,10 +793,11 @@ void MainWindow::start_rendering(const bool interactive)
     assert(m_project_manager.is_project_open());
 
     // Enable/disable widgets appropriately. File -> Reload is enabled during interactive rendering.
-    set_project_widgets_enabled(false);
-    set_rendering_widgets_enabled(true, true);
+    set_file_widgets_enabled(false);
     if (interactive)
         m_ui->action_file_reload_project->setEnabled(true);
+    set_project_explorer_enabled(true);
+    set_rendering_widgets_enabled(true, true);
 
     // Internally, clear the main image to transparent black and delete all AOV images.
     Project* project = m_project_manager.get_project();

src/appleseed.studio/mainwindow/mainwindow.h
@@ -146,7 +146,8 @@ class MainWindow
     void update_project_explorer();
     void update_window_title();
 
-    void set_project_widgets_enabled(const bool is_enabled);
+    void set_file_widgets_enabled(const bool is_enabled);
+    void set_project_explorer_enabled(const bool is_enabled);
     void set_rendering_widgets_enabled(const bool is_enabled, const bool is_rendering);
 
     void recreate_render_widgets();