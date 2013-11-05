appleseedhq/appleseed

commit d8903fd15f44ba22b7673beba1a8e7efda7ba26f

preserve the state of the render widgets (zoom level, scrollbars position, etc.) across project reloads.

src/appleseed.studio/mainwindow/mainwindow.cpp
@@ -35,7 +35,6 @@
 // appleseed.studio headers.
 #include "help/about/aboutwindow.h"
 #include "mainwindow/project/projectexplorer.h"
-#include "mainwindow/rendering/rendertab.h"
 #include "mainwindow/logwidget.h"
 #include "utility/interop.h"
 #include "utility/settingskeys.h"
@@ -109,7 +108,6 @@ MainWindow::MainWindow(QWidget* parent)
   , m_ui(new Ui::MainWindow())
   , m_rendering_manager(m_status_bar)
   , m_project_explorer(0)
-  , m_was_rendering_before_open(false)
 {
     m_ui->setupUi(this);
 
@@ -140,9 +138,9 @@ MainWindow::~MainWindow()
 
 void MainWindow::open_project(const QString& filepath)
 {
-    m_was_rendering_before_open = m_rendering_manager.is_rendering();
+    save_state_before_project_open();
 
-    if (m_was_rendering_before_open)
+    if (m_rendering_manager.is_rendering())
     {
         m_rendering_manager.abort_rendering();
         m_rendering_manager.wait_until_rendering_end();
@@ -584,6 +582,33 @@ namespace
     }
 }
 
+void MainWindow::save_state_before_project_open()
+{
+    m_state_before_project_open.reset(new StateBeforeProjectOpen());
+
+    m_state_before_project_open->m_is_rendering = m_rendering_manager.is_rendering();
+
+    for (const_each<RenderTabCollection> i = m_render_tabs; i; ++i)
+        m_state_before_project_open->m_render_tab_states[i->first] = i->second->save_state();
+}
+
+void MainWindow::restore_state_after_project_open()
+{
+    assert(m_state_before_project_open.get());
+
+    for (const_each<RenderTabCollection> i = m_render_tabs; i; ++i)
+    {
+        const RenderTabStateCollection& tab_states = m_state_before_project_open->m_render_tab_states;
+        const RenderTabStateCollection::const_iterator tab_state_it = tab_states.find(i->first);
+
+        if (tab_state_it != tab_states.end())
+            i->second->load_state(tab_state_it->second);
+    }
+
+    if (m_state_before_project_open->m_is_rendering)
+        start_rendering(true);
+}
+
 bool MainWindow::can_close_project()
 {
     // No project open: no problem.
@@ -625,6 +650,8 @@ void MainWindow::on_project_change()
     m_status_bar.clear();
 
     update_workspace();
+
+    restore_state_after_project_open();
 }
 
 void MainWindow::update_workspace()
@@ -945,9 +972,6 @@ void MainWindow::slot_open_project_complete(const QString& filepath, const bool 
     if (successful)
     {
         on_project_change();
-
-        if (m_was_rendering_before_open)
-            start_rendering(true);
     }
     else
     {

src/appleseed.studio/mainwindow/mainwindow.h
@@ -34,6 +34,7 @@
 #include "debug/tests/testwindow.h"
 #include "mainwindow/project/projectmanager.h"
 #include "mainwindow/rendering/renderingmanager.h"
+#include "mainwindow/rendering/rendertab.h"
 #include "mainwindow/qtlogtarget.h"
 #include "mainwindow/rendersettingswindow.h"
 #include "mainwindow/statusbar.h"
@@ -54,7 +55,6 @@
 // Forward declarations.
 namespace appleseed { namespace studio { class LogWidget; } }
 namespace appleseed { namespace studio { class ProjectExplorer; } }
-namespace appleseed { namespace studio { class RenderTab; } }
 namespace Ui        { class MainWindow; }
 class QAction;
 class QCloseEvent;
@@ -88,35 +88,43 @@ class MainWindow
 
   private:
     // Not wrapped in std::auto_ptr<> to avoid pulling in the UI definition code.
-    Ui::MainWindow*                     m_ui;
+    Ui::MainWindow*                         m_ui;
 
-    QAction*                            m_action_new_project;
-    QAction*                            m_action_open_project;
-    QAction*                            m_clear_open_recent_menu;
-    QAction*                            m_action_save_project;
-    QAction*                            m_action_start_interactive_rendering;
-    QAction*                            m_action_start_final_rendering;
-    QAction*                            m_action_stop_rendering;
+    QAction*                                m_action_new_project;
+    QAction*                                m_action_open_project;
+    QAction*                                m_clear_open_recent_menu;
+    QAction*                                m_action_save_project;
+    QAction*                                m_action_start_interactive_rendering;
+    QAction*                                m_action_start_final_rendering;
+    QAction*                                m_action_stop_rendering;
 
-    std::vector<QAction*>               m_recently_opened;
+    std::vector<QAction*>                   m_recently_opened;
 
-    StatusBar                           m_status_bar;
-    std::auto_ptr<QtLogTarget>          m_log_target;
+    StatusBar                               m_status_bar;
+    std::auto_ptr<QtLogTarget>              m_log_target;
 
-    renderer::ParamArray                m_settings;
+    renderer::ParamArray                    m_settings;
 
-    std::auto_ptr<RenderSettingsWindow> m_render_settings_window;
-    std::auto_ptr<TestWindow>           m_test_window;
-    std::auto_ptr<BenchmarkWindow>      m_benchmark_window;
+    std::auto_ptr<RenderSettingsWindow>     m_render_settings_window;
+    std::auto_ptr<TestWindow>               m_test_window;
+    std::auto_ptr<BenchmarkWindow>          m_benchmark_window;
 
-    ProjectManager                      m_project_manager;
-    ProjectExplorer*                    m_project_explorer;
-    RenderingManager                    m_rendering_manager;
-    bool                                m_was_rendering_before_open;
+    ProjectManager                          m_project_manager;
+    ProjectExplorer*                        m_project_explorer;
+    RenderingManager                        m_rendering_manager;
 
     typedef std::map<std::string, RenderTab*> RenderTabCollection;
+    typedef std::map<std::string, RenderTab::State> RenderTabStateCollection;
 
-    RenderTabCollection                 m_render_tabs;
+    RenderTabCollection                     m_render_tabs;
+
+    struct StateBeforeProjectOpen
+    {
+        bool                                m_is_rendering;
+        RenderTabStateCollection            m_render_tab_states;
+    };
+
+    std::auto_ptr<StateBeforeProjectOpen>   m_state_before_project_open;
 
     void build_menus();
     void build_override_shading_menu_item();
@@ -138,6 +146,9 @@ class MainWindow
     void print_startup_information();
 
     renderer::ParamArray get_project_params(const char* configuration_name) const;
+
+    void save_state_before_project_open();
+    void restore_state_after_project_open();
 
     bool can_close_project();
     void on_project_change();

src/appleseed.studio/mainwindow/rendering/rendertab.cpp
@@ -112,6 +112,21 @@ RenderWidget* RenderTab::get_render_widget() const
     return m_render_widget;
 }
 
+RenderTab::State RenderTab::save_state() const
+{
+    State state;
+    state.m_zoom_handler_state = m_zoom_handler->save_state();
+    state.m_pan_handler_state = m_pan_handler->save_state();
+    return state;
+}
+
+void RenderTab::load_state(const State& state)
+{
+    // The order matters here.
+    m_zoom_handler->load_state(state.m_zoom_handler_state);
+    m_pan_handler->load_state(state.m_pan_handler_state);
+}
+
 void RenderTab::slot_render_widget_context_menu(const QPoint& point)
 {
     emit signal_render_widget_context_menu(m_render_widget->mapToGlobal(point));

src/appleseed.studio/mainwindow/rendering/rendertab.h
@@ -79,6 +79,15 @@ class RenderTab
 
     RenderWidget* get_render_widget() const;
 
+    struct State
+    {
+        WidgetZoomHandler::State            m_zoom_handler_state;
+        ScrollAreaPanHandler::State         m_pan_handler_state;
+    };
+
+    State save_state() const;
+    void load_state(const State& state);
+
   signals:
     void signal_render_widget_context_menu(const QPoint& point);
     void signal_set_render_region(const QRect& rect);

src/appleseed.studio/utility/scrollareapanhandler.cpp
@@ -56,6 +56,22 @@ ScrollAreaPanHandler::~ScrollAreaPanHandler()
     m_scroll_area->removeEventFilter(this);
 }
 
+ScrollAreaPanHandler::State ScrollAreaPanHandler::save_state() const
+{
+    State state;
+    state.m_hbar_value = m_scroll_area->horizontalScrollBar()->value();
+    state.m_vbar_value = m_scroll_area->verticalScrollBar()->value();
+    return state;
+}
+
+void ScrollAreaPanHandler::load_state(const State& state)
+{
+    m_state = state;
+
+    m_scroll_area->horizontalScrollBar()->setValue(m_state.m_hbar_value);
+    m_scroll_area->verticalScrollBar()->setValue(m_state.m_vbar_value);
+}
+
 bool ScrollAreaPanHandler::eventFilter(QObject* object, QEvent* event)
 {
     assert(object == m_scroll_area);

src/appleseed.studio/utility/scrollareapanhandler.h
@@ -51,12 +51,22 @@ class ScrollAreaPanHandler
 
     ~ScrollAreaPanHandler();
 
+    struct State
+    {
+        int         m_hbar_value;
+        int         m_vbar_value;
+    };
+
+    State save_state() const;
+    void load_state(const State& state);
+
   private:
     QScrollArea*    m_scroll_area;
     bool            m_is_dragging;
     QPoint          m_initial_mouse_position;
     int             m_initial_hbar_value;
     int             m_initial_vbar_value;
+    State           m_state;
 
     virtual bool eventFilter(QObject* object, QEvent* event);
 

src/appleseed.studio/utility/widgetzoomhandler.cpp
@@ -68,9 +68,10 @@ WidgetZoomHandler::WidgetZoomHandler(
   , m_content_widget(content_widget)
   , m_content_width(content_widget->width())
   , m_content_height(content_widget->height())
-  , m_scale_factor(1.0)
 {
     compute_min_max_scale_factors();
+
+    m_state.m_scale_factor = 1.0;
 
     m_scroll_area->installEventFilter(this);
     m_scroll_area->viewport()->installEventFilter(this);
@@ -80,6 +81,18 @@ WidgetZoomHandler::~WidgetZoomHandler()
 {
     m_scroll_area->viewport()->removeEventFilter(this);
     m_scroll_area->removeEventFilter(this);
+}
+
+WidgetZoomHandler::State WidgetZoomHandler::save_state() const
+{
+    return m_state;
+}
+
+void WidgetZoomHandler::load_state(const State& state)
+{
+    m_state = state;
+
+    apply_scale_factor();
 }
 
 bool WidgetZoomHandler::eventFilter(QObject* object, QEvent* event)
@@ -156,7 +169,7 @@ void WidgetZoomHandler::zoom_out()
 
 void WidgetZoomHandler::reset_zoom()
 {
-    m_scale_factor = 1.0;
+    m_state.m_scale_factor = 1.0;
     apply_scale_factor();
 }
 
@@ -179,9 +192,9 @@ void WidgetZoomHandler::compute_min_max_scale_factors()
 
 void WidgetZoomHandler::multiply_scale_factor(const double multiplier)
 {
-    m_scale_factor =
+    m_state.m_scale_factor =
         clamp(
-            m_scale_factor * multiplier,
+            m_state.m_scale_factor * multiplier,
             m_min_scale_factor,
             m_max_scale_factor);
 
@@ -210,8 +223,8 @@ void WidgetZoomHandler::apply_scale_factor()
     const int old_widget_height = m_scroll_area->widget()->height();
 
     m_content_widget->setFixedSize(
-        static_cast<int>(m_content_width * m_scale_factor),
-        static_cast<int>(m_content_height * m_scale_factor));
+        static_cast<int>(m_content_width * m_state.m_scale_factor),
+        static_cast<int>(m_content_height * m_state.m_scale_factor));
 
     // Make sure the size of the scrollarea widget is up-to-date.
     if (m_scroll_area->widget()->layout())

src/appleseed.studio/utility/widgetzoomhandler.h
@@ -55,6 +55,14 @@ class WidgetZoomHandler
 
     ~WidgetZoomHandler();
 
+    struct State
+    {
+        double          m_scale_factor;
+    };
+
+    State save_state() const;
+    void load_state(const State& state);
+
   private:
     QScrollArea*        m_scroll_area;
     QWidget*            m_content_widget;
@@ -62,7 +70,7 @@ class WidgetZoomHandler
     const int           m_content_height;
     double              m_min_scale_factor;
     double              m_max_scale_factor;
-    double              m_scale_factor;
+    State               m_state;
 
     virtual bool eventFilter(QObject* object, QEvent* event);
 

commit 031de55d68c28829b0c22f6727b65028dc961cd2

fixed regression: appleseed.studio would crash when loading a built-in scene.
regression introduced in d8903fd15f44ba22b7673beba1a8e7efda7ba26f ("preserve the state of the render widgets (zoom level, scrollbars position, etc.) across project reloads").

src/appleseed.studio/mainwindow/mainwindow.cpp
@@ -594,19 +594,20 @@ void MainWindow::save_state_before_project_open()
 
 void MainWindow::restore_state_after_project_open()
 {
-    assert(m_state_before_project_open.get());
-
-    for (const_each<RenderTabCollection> i = m_render_tabs; i; ++i)
+    if (m_state_before_project_open.get())
     {
-        const RenderTabStateCollection& tab_states = m_state_before_project_open->m_render_tab_states;
-        const RenderTabStateCollection::const_iterator tab_state_it = tab_states.find(i->first);
+        for (const_each<RenderTabCollection> i = m_render_tabs; i; ++i)
+        {
+            const RenderTabStateCollection& tab_states = m_state_before_project_open->m_render_tab_states;
+            const RenderTabStateCollection::const_iterator tab_state_it = tab_states.find(i->first);
 
-        if (tab_state_it != tab_states.end())
-            i->second->load_state(tab_state_it->second);
+            if (tab_state_it != tab_states.end())
+                i->second->load_state(tab_state_it->second);
+        }
+
+        if (m_state_before_project_open->m_is_rendering)
+            start_rendering(true);
     }
-
-    if (m_state_before_project_open->m_is_rendering)
-        start_rendering(true);
 }
 
 bool MainWindow::can_close_project()