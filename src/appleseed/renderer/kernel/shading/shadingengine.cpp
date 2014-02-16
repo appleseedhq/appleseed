appleseedhq/appleseed

commit ae4c935f0b4c6f052ee7b60ac82275e10c086036

aovs now have an alpha channel.

src/appleseed/renderer/kernel/shading/shadingengine.cpp
@@ -31,7 +31,9 @@
 #include "shadingengine.h"
 
 // appleseed.renderer headers.
+#include "renderer/kernel/aov/shadingfragmentstack.h"
 #include "renderer/kernel/shading/shadingcontext.h"
+#include "renderer/kernel/shading/shadingfragment.h"
 #include "renderer/kernel/shading/shadingray.h"
 #include "renderer/kernel/shading/shadingresult.h"
 #include "renderer/modeling/environment/environment.h"
@@ -87,22 +89,22 @@ void ShadingEngine::shade_hit_point(
     // Retrieve the material of the intersected surface.
     const Material* material = shading_point.get_material();
 
-    // Compute the alpha channel.
+    // Compute the alpha channel of the main output.
     if (material && material->get_alpha_map())
     {
         // There is an alpha map: evaluate it.
         material->get_alpha_map()->evaluate(
             shading_context.get_texture_cache(),
             shading_point.get_uv(0),
-            shading_result.m_alpha);
+            shading_result.m_main.m_alpha);
     }
     else
     {
         // No alpha map: solid sample.
-        shading_result.m_alpha = Alpha(1.0);
+        shading_result.m_main.m_alpha = Alpha(1.0);
     }
 
-    if (shading_result.m_alpha[0] > 0.0f)
+    if (shading_result.m_main.m_alpha[0] > 0.0f)
     {
         // Use the diagnostic surface shader if there is one.
         const SurfaceShader* surface_shader = m_diagnostic_surface_shader.get();
@@ -112,7 +114,7 @@ void ShadingEngine::shade_hit_point(
             if (material == 0)
             {
                 // The intersected surface has no material: return solid pink.
-                shading_result.set_to_solid_pink();
+                shading_result.set_to_solid_pink_linear_rgb();
                 return;
             }
 
@@ -122,7 +124,7 @@ void ShadingEngine::shade_hit_point(
             if (surface_shader == 0)
             {
                 // The intersected surface has no surface shader: return solid pink.
-                shading_result.set_to_solid_pink();
+                shading_result.set_to_solid_pink_linear_rgb();
                 return;
             }
         }
@@ -136,21 +138,21 @@ void ShadingEngine::shade_hit_point(
             shading_result);
 
         // Set the surface shader AOV.
-        shading_result.m_aovs.set(surface_shader->get_render_layer_index(), shading_result.m_color);
+        shading_result.set_aov_for_entity(*surface_shader);
     }
     else
     {
         // Alpha is zero: shade as transparent black.
-        shading_result.set_to_transparent_black();
+        shading_result.set_to_transparent_black_linear_rgb();
     }
 
     // Set AOVs.
-    shading_result.m_aovs.set(shading_point.get_assembly().get_render_layer_index(), shading_result.m_color);
-    shading_result.m_aovs.set(shading_point.get_assembly_instance().get_render_layer_index(), shading_result.m_color);
-    shading_result.m_aovs.set(shading_point.get_object().get_render_layer_index(), shading_result.m_color);
-    shading_result.m_aovs.set(shading_point.get_object_instance().get_render_layer_index(), shading_result.m_color);
+    shading_result.set_aov_for_entity(shading_point.get_assembly());
+    shading_result.set_aov_for_entity(shading_point.get_assembly_instance());
+    shading_result.set_aov_for_entity(shading_point.get_object());
+    shading_result.set_aov_for_entity(shading_point.get_object_instance());
     if (material)
-        shading_result.m_aovs.set(material->get_render_layer_index(), shading_result.m_color);
+        shading_result.set_aov_for_entity(*material);
 }
 
 void ShadingEngine::shade_environment(
@@ -175,12 +177,12 @@ void ShadingEngine::shade_environment(
             shading_result);
 
         // Set environment shader AOV.
-        shading_result.m_aovs.set(environment_shader->get_render_layer_index(), shading_result.m_color);
+        shading_result.set_aov_for_entity(*environment_shader);
     }
     else
     {
         // No environment shader: return transparent black.
-        shading_result.set_to_transparent_black();
+        shading_result.set_to_transparent_black_linear_rgb();
     }
 }
 

src/appleseed/renderer/kernel/shading/shadingresult.h
@@ -32,7 +32,9 @@
 
 // appleseed.renderer headers.
 #include "renderer/global/globaltypes.h"
-#include "renderer/kernel/aov/spectrumstack.h"
+#include "renderer/kernel/aov/shadingfragmentstack.h"
+#include "renderer/kernel/shading/shadingfragment.h"
+#include "renderer/modeling/entity/entity.h"
 
 // appleseed.foundation headers.
 #include "foundation/core/concepts/noncopyable.h"
@@ -52,40 +54,46 @@ class ShadingResult
   public:
     // Public members.
     foundation::ColorSpace  m_color_space;
-    Spectrum                m_color;
-    Alpha                   m_alpha;
-    SpectrumStack           m_aovs;
+    ShadingFragment         m_main;
+    ShadingFragmentStack    m_aovs;
+
+    // Constructor.
+    explicit ShadingResult(const size_t aov_count = 0);
 
     // Return true if this shading result contains valid linear RGB values;
     // false if the color, alpha or any AOV contains NaN or negative values.
     bool is_valid_linear_rgb() const;
 
-    // Set the main color and all AOVs to transparent black in linear RGBA.
-    void set_to_transparent_black();
+    // Set the main color and all AOVs to transparent black in linear RGB.
+    void set_to_transparent_black_linear_rgb();
 
-    // Set the main color to solid pink in linear RGBA (used for debugging).
-    // All AOVs are set to black.
-    void set_to_solid_pink();
+    // Set the main color to solid pink in linear RGB (used for debugging).
+    // All AOVs are set to transparent black.
+    void set_to_solid_pink_linear_rgb();
 
     // Set the main color to a given fully opaque linear RGB value.
-    // All AOVs are set to black.
+    // All AOVs are set to transparent black.
     void set_to_linear_rgb(const foundation::Color3f& linear_rgb);
 
     // Set the main color to a given linear RGBA value.
-    // All AOVs are set to black.
+    // All AOVs are set to transparent black.
     void set_to_linear_rgba(const foundation::Color4f& linear_rgba);
 
+    // Copy the main output to the AOV of a given entity.
+    void set_aov_for_entity(const Entity& entity);
+
     // Transform the shading result to the linear RGB color space.
-    void transform_to_linear_rgb(
-        const foundation::LightingConditions&   lighting);
+    void transform_to_linear_rgb(const foundation::LightingConditions& lighting);
 
     // Transform the shading result to the spectral color space.
-    void transform_to_spectrum(
-        const foundation::LightingConditions&   lighting);
+    void transform_to_spectrum(const foundation::LightingConditions& lighting);
 
     // Composite this shading result over 'background'.
     // Both shading results must be expressed in linear RGB.
     void composite_over_linear_rgb(const ShadingResult& background);
+
+    // Multiply main and AOV colors by their respective alpha channels.
+    void apply_alpha_premult_linear_rgb();
 };
 
 
@@ -93,12 +101,17 @@ class ShadingResult
 // ShadingResult class implementation.
 //
 
-inline void ShadingResult::set_to_transparent_black()
+inline ShadingResult::ShadingResult(const size_t aov_count)
+  : m_aovs(aov_count)
+{
+}
+
+inline void ShadingResult::set_to_transparent_black_linear_rgb()
 {
     set_to_linear_rgba(foundation::Color4f(0.0f));
 }
 
-inline void ShadingResult::set_to_solid_pink()
+inline void ShadingResult::set_to_solid_pink_linear_rgb()
 {
     set_to_linear_rgba(foundation::Color4f(1.0f, 0.0f, 1.0f, 1.0f));
 }
@@ -108,23 +121,9 @@ inline void ShadingResult::set_to_linear_rgb(const foundation::Color3f& linear_r
     set_to_linear_rgba(foundation::Color4f(linear_rgb[0], linear_rgb[1], linear_rgb[2], 1.0f));
 }
 
-inline void ShadingResult::set_to_linear_rgba(const foundation::Color4f& linear_rgba)
+inline void ShadingResult::set_aov_for_entity(const Entity& entity)
 {
-    m_color_space = foundation::ColorSpaceLinearRGB;
-
-    m_color[0] = linear_rgba[0];
-    m_color[1] = linear_rgba[1];
-    m_color[2] = linear_rgba[2];
-
-    m_alpha.set(linear_rgba[3]);
-
-    const size_t aov_count = m_aovs.size();
-
-    for (size_t i = 0; i < aov_count; ++i)
-    {
-        Spectrum& aov = m_aovs[i];
-        aov[0] = aov[1] = aov[2] = 0.0f;
-    }
+    m_aovs.set(entity.get_render_layer_index(), m_main);
 }
 
 }       // namespace renderer

src/appleseed/renderer/modeling/surfaceshader/surfaceshadercollection.cpp
@@ -32,6 +32,8 @@
 
 // appleseed.renderer headers.
 #include "renderer/global/globaltypes.h"
+#include "renderer/kernel/aov/shadingfragmentstack.h"
+#include "renderer/kernel/shading/shadingfragment.h"
 #include "renderer/kernel/shading/shadingresult.h"
 #include "renderer/modeling/input/inputarray.h"
 #include "renderer/modeling/input/inputformat.h"
@@ -143,16 +145,14 @@ namespace
                 shading_point,
                 shading_result);
 
-            shading_result.m_aovs.set(
-                m_surface_shaders[0]->get_render_layer_index(),
-                shading_result.m_color);
+            shading_result.set_aov_for_entity(*m_surface_shaders[0]);
 
             for (size_t i = 1; i < MaxShaderCount; ++i)
             {
                 if (m_surface_shaders[i])
                 {
                     ShadingResult local_result;
-                    local_result.m_alpha = Alpha(1.0);
+                    local_result.m_main.m_alpha = Alpha(1.0);
 
                     m_surface_shaders[i]->evaluate(
                         sampling_context,
@@ -161,9 +161,7 @@ namespace
                         shading_point,
                         local_result);
 
-                    shading_result.m_aovs.set(
-                        m_surface_shaders[i]->get_render_layer_index(),
-                        local_result.m_color);
+                    shading_result.set_aov_for_entity(*m_surface_shaders[i]);
                 }
             }
         }

src/appleseed/CMakeLists.txt
@@ -786,8 +786,10 @@ source_group ("renderer\\global" FILES
 )
 
 set (renderer_kernel_aov_sources
+    renderer/kernel/aov/aovsettings.h
     renderer/kernel/aov/imagestack.cpp
     renderer/kernel/aov/imagestack.h
+    renderer/kernel/aov/shadingfragmentstack.h
     renderer/kernel/aov/spectrumstack.h
     renderer/kernel/aov/tilestack.h
 )
@@ -1050,6 +1052,7 @@ set (renderer_kernel_shading_sources
     renderer/kernel/shading/shadingcontext.h
     renderer/kernel/shading/shadingengine.cpp
     renderer/kernel/shading/shadingengine.h
+    renderer/kernel/shading/shadingfragment.h
     renderer/kernel/shading/shadingpoint.cpp
     renderer/kernel/shading/shadingpoint.h
     renderer/kernel/shading/shadingpointbuilder.cpp

src/appleseed/renderer/kernel/aov/aovsettings.h
@@ -27,69 +27,18 @@
 // THE SOFTWARE.
 //
 
-// Interface header.
-#include "blanksamplerenderer.h"
+#ifndef APPLESEED_RENDERER_KERNEL_AOV_AOVSETTINGS_H
+#define APPLESEED_RENDERER_KERNEL_AOV_AOVSETTINGS_H
 
-// appleseed.renderer headers.
-#include "renderer/global/globaltypes.h"
-#include "renderer/kernel/shading/shadingresult.h"
-
-// appleseed.foundation headers.
-#include "foundation/math/vector.h"
-#include "foundation/utility/statistics.h"
-
-// Forward declarations.
-namespace renderer  { class PixelContext; }
-
-using namespace foundation;
+// Standard headers.
+#include <cstddef>
 
 namespace renderer
 {
 
-namespace
-{
-    //
-    // Blank sample renderer.
-    //
+// The maximum number of AOVs that can be handled at once.
+const size_t MaxAOVCount = 16;
 
-    class BlankSampleRenderer
-      : public ISampleRenderer
-    {
-      public:
-        virtual void release() OVERRIDE
-        {
-            delete this;
-        }
+}       // namespace renderer
 
-        virtual void render_sample(
-            SamplingContext&    sampling_context,
-            const PixelContext& pixel_context,
-            const Vector2d&     image_point,
-            ShadingResult&      shading_result) OVERRIDE
-        {
-            shading_result.set_to_transparent_black_linear_rgb();
-        }
-
-        virtual StatisticsVector get_statistics() const OVERRIDE
-        {
-            return StatisticsVector();
-        }
-    };
-}
-
-
-//
-// BlankSampleRendererFactory class implementation.
-//
-
-void BlankSampleRendererFactory::release()
-{
-    delete this;
-}
-
-ISampleRenderer* BlankSampleRendererFactory::create(const bool primary)
-{
-    return new BlankSampleRenderer();
-}
-
-}   // namespace renderer
+#endif  // !APPLESEED_RENDERER_KERNEL_AOV_AOVSETTINGS_H

src/appleseed/renderer/kernel/aov/imagestack.cpp
@@ -58,6 +58,7 @@ struct ImageStack::Impl
     struct NamedImage
     {
         string              m_name;
+        ImageStack::Type    m_type;
         Image*              m_image;
     };
 
@@ -110,6 +111,12 @@ const char* ImageStack::get_name(const size_t index) const
     return impl->m_images[index].m_name.c_str();
 }
 
+ImageStack::Type ImageStack::get_type(const size_t index) const
+{
+    assert(index < impl->m_images.size());
+    return impl->m_images[index].m_type;
+}
+
 const Image& ImageStack::get_image(const size_t index) const
 {
     assert(index < impl->m_images.size());
@@ -131,11 +138,13 @@ size_t ImageStack::get(const char* name) const
 
 size_t ImageStack::append(
     const char*             name,
-    const PixelFormat       format)
+    const Type              type,
+    const PixelFormat       pixel_format)
 {
     Impl::NamedImage named_image;
 
     named_image.m_name = name;
+    named_image.m_type = type;
     named_image.m_image =
         new Image(
             impl->m_canvas_width,
@@ -143,7 +152,7 @@ size_t ImageStack::append(
             impl->m_tile_width,
             impl->m_tile_height,
             4,
-            format);
+            pixel_format);
 
     const size_t aov_index = impl->m_images.size();
 

src/appleseed/renderer/kernel/aov/imagestack.h
@@ -48,13 +48,19 @@ namespace renderer
 {
 
 //
-// A stack of named images.
+// An array of named images.
 //
 
 class DLLSYMBOL ImageStack
   : public foundation::NonCopyable
 {
   public:
+    enum Type
+    {
+        ContributionType,
+        IdentificationType
+    };
+
     ImageStack(
         const size_t                    canvas_width,
         const size_t                    canvas_height,
@@ -71,6 +77,8 @@ class DLLSYMBOL ImageStack
 
     const char* get_name(const size_t index) const;
 
+    Type get_type(const size_t index) const;
+
     const foundation::Image& get_image(const size_t index) const;
 
     // Returns ~0 if the requested image cannot be found.
@@ -78,7 +86,8 @@ class DLLSYMBOL ImageStack
 
     size_t append(
         const char*                     name,
-        const foundation::PixelFormat   format);
+        const Type                      type,
+        const foundation::PixelFormat   pixel_format);
 
     TileStack tiles(
         const size_t                    tile_x,

src/appleseed/renderer/kernel/aov/shadingfragmentstack.h
@@ -1,0 +1,210 @@
+
+//
+// This source file is part of appleseed.
+// Visit http://appleseedhq.net/ for additional information and resources.
+//
+// This software is released under the MIT license.
+//
+// Copyright (c) 2010-2013 Francois Beaune, Jupiter Jazz Limited
+// Copyright (c) 2014 Francois Beaune, The appleseedhq Organization
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+#ifndef APPLESEED_RENDERER_KERNEL_AOV_SHADINGFRAGMENTSTACK_H
+#define APPLESEED_RENDERER_KERNEL_AOV_SHADINGFRAGMENTSTACK_H
+
+// appleseed.renderer headers.
+#include "renderer/kernel/aov/aovsettings.h"
+#include "renderer/kernel/aov/spectrumstack.h"
+#include "renderer/kernel/shading/shadingfragment.h"
+
+// appleseed.foundation headers.
+#include "foundation/core/concepts/noncopyable.h"
+
+// Standard headers.
+#include <cassert>
+#include <cstddef>
+
+namespace renderer
+{
+
+//
+// A small array of shading fragments.
+//
+
+class ShadingFragmentStack
+  : public foundation::NonCopyable
+{
+  public:
+    explicit ShadingFragmentStack(const size_t size);
+
+    size_t size() const;
+
+    ShadingFragment& operator[](const size_t index);
+    const ShadingFragment& operator[](const size_t index) const;
+
+    ShadingFragmentStack& operator+=(const ShadingFragmentStack& rhs);
+    ShadingFragmentStack& operator*=(const float rhs);
+
+    void set(const size_t index, const ShadingFragment& rhs);
+    void add(const size_t index, const ShadingFragment& rhs);
+
+    struct ColorProxy
+    {
+        ShadingFragmentStack& m_parent;
+
+        explicit ColorProxy(ShadingFragmentStack& parent);
+
+        void set(const float val);
+
+        ColorProxy& operator=(const SpectrumStack& rhs);
+        ColorProxy& operator*=(const float rhs);
+    };
+
+    struct AlphaProxy
+    {
+        ShadingFragmentStack& m_parent;
+
+        explicit AlphaProxy(ShadingFragmentStack& parent);
+
+        void set(const float val);
+
+        AlphaProxy& operator*=(const float rhs);
+    };
+
+    ColorProxy      m_color;
+    AlphaProxy      m_alpha;
+
+  private:
+    ShadingFragment m_fragments[MaxAOVCount];
+    size_t          m_size;
+};
+
+
+//
+// ShadingFragmentStack class implementation.
+//
+
+inline ShadingFragmentStack::ShadingFragmentStack(const size_t size)
+  : m_color(*this)
+  , m_alpha(*this)
+  , m_size(size)
+{
+    assert(size <= MaxAOVCount);
+}
+
+inline size_t ShadingFragmentStack::size() const
+{
+    return m_size;
+}
+
+inline ShadingFragment& ShadingFragmentStack::operator[](const size_t index)
+{
+    assert(index < m_size);
+    return m_fragments[index];
+}
+
+inline const ShadingFragment& ShadingFragmentStack::operator[](const size_t index) const
+{
+    assert(index < m_size);
+    return m_fragments[index];
+}
+
+inline ShadingFragmentStack& ShadingFragmentStack::operator+=(const ShadingFragmentStack& rhs)
+{
+    assert(m_size == rhs.m_size);
+
+    for (size_t i = 0; i < m_size; ++i)
+        m_fragments[i] += rhs.m_fragments[i];
+
+    return *this;
+}
+
+inline ShadingFragmentStack& ShadingFragmentStack::operator*=(const float rhs)
+{
+    for (size_t i = 0; i < m_size; ++i)
+        m_fragments[i] *= rhs;
+
+    return *this;
+}
+
+inline void ShadingFragmentStack::set(const size_t index, const ShadingFragment& rhs)
+{
+    if (index < m_size)
+        m_fragments[index] = rhs;
+}
+
+inline void ShadingFragmentStack::add(const size_t index, const ShadingFragment& rhs)
+{
+    if (index < m_size)
+        m_fragments[index] += rhs;
+}
+
+inline ShadingFragmentStack::ColorProxy::ColorProxy(ShadingFragmentStack& parent)
+  : m_parent(parent)
+{
+}
+
+inline void ShadingFragmentStack::ColorProxy::set(const float val)
+{
+    for (size_t i = 0; i < m_parent.m_size; ++i)
+        m_parent.m_fragments[i].m_color.set(val);
+}
+
+inline ShadingFragmentStack::ColorProxy& ShadingFragmentStack::ColorProxy::operator=(const SpectrumStack& rhs)
+{
+    assert(m_parent.m_size == rhs.size());
+
+    for (size_t i = 0; i < m_parent.m_size; ++i)
+        m_parent.m_fragments[i].m_color = rhs[i];
+
+    return *this;
+}
+
+inline ShadingFragmentStack::ColorProxy& ShadingFragmentStack::ColorProxy::operator*=(const float rhs)
+{
+    for (size_t i = 0; i < m_parent.m_size; ++i)
+        m_parent.m_fragments[i].m_color *= rhs;
+
+    return *this;
+}
+
+inline ShadingFragmentStack::AlphaProxy::AlphaProxy(ShadingFragmentStack& parent)
+  : m_parent(parent)
+{
+}
+
+inline void ShadingFragmentStack::AlphaProxy::set(const float val)
+{
+    for (size_t i = 0; i < m_parent.m_size; ++i)
+        m_parent.m_fragments[i].m_alpha.set(val);
+}
+
+inline ShadingFragmentStack::AlphaProxy& ShadingFragmentStack::AlphaProxy::operator*=(const float rhs)
+{
+    for (size_t i = 0; i < m_parent.m_size; ++i)
+        m_parent.m_fragments[i].m_alpha *= rhs;
+
+    return *this;
+}
+
+}       // namespace renderer
+
+#endif  // !APPLESEED_RENDERER_KERNEL_AOV_SHADINGFRAGMENTSTACK_H

commit d8b44d5d3136df5e29f02f51d0d60426881db6c6

fixed regression in "collection" surface shader.
all AOVs were identical to the main output.

regression introduced in ae4c935f0b4c6f052ee7b60ac82275e10c086036 ("aovs now have an alpha channel").

src/appleseed/renderer/kernel/shading/shadingengine.cpp
@@ -138,7 +138,7 @@ void ShadingEngine::shade_hit_point(
             shading_result);
 
         // Set the surface shader AOV.
-        shading_result.set_aov_for_entity(*surface_shader);
+        shading_result.set_entity_aov(*surface_shader);
     }
     else
     {
@@ -147,12 +147,12 @@ void ShadingEngine::shade_hit_point(
     }
 
     // Set AOVs.
-    shading_result.set_aov_for_entity(shading_point.get_assembly());
-    shading_result.set_aov_for_entity(shading_point.get_assembly_instance());
-    shading_result.set_aov_for_entity(shading_point.get_object());
-    shading_result.set_aov_for_entity(shading_point.get_object_instance());
+    shading_result.set_entity_aov(shading_point.get_assembly());
+    shading_result.set_entity_aov(shading_point.get_assembly_instance());
+    shading_result.set_entity_aov(shading_point.get_object());
+    shading_result.set_entity_aov(shading_point.get_object_instance());
     if (material)
-        shading_result.set_aov_for_entity(*material);
+        shading_result.set_entity_aov(*material);
 }
 
 void ShadingEngine::shade_environment(
@@ -177,7 +177,7 @@ void ShadingEngine::shade_environment(
             shading_result);
 
         // Set environment shader AOV.
-        shading_result.set_aov_for_entity(*environment_shader);
+        shading_result.set_entity_aov(*environment_shader);
     }
     else
     {

src/appleseed/renderer/kernel/shading/shadingresult.h
@@ -53,9 +53,9 @@ class ShadingResult
 {
   public:
     // Public members.
-    foundation::ColorSpace  m_color_space;
-    ShadingFragment         m_main;
-    ShadingFragmentStack    m_aovs;
+    foundation::ColorSpace      m_color_space;
+    ShadingFragment             m_main;
+    ShadingFragmentStack        m_aovs;
 
     // Constructor.
     explicit ShadingResult(const size_t aov_count = 0);
@@ -80,7 +80,12 @@ class ShadingResult
     void set_to_linear_rgba(const foundation::Color4f& linear_rgba);
 
     // Copy the main output to the AOV of a given entity.
-    void set_aov_for_entity(const Entity& entity);
+    void set_entity_aov(const Entity& entity);
+
+    // Store a shading fragment to the AOV of a given entity.
+    void set_entity_aov(
+        const Entity&           entity,
+        const ShadingFragment&  fragment);
 
     // Transform the shading result to the linear RGB color space.
     void transform_to_linear_rgb(const foundation::LightingConditions& lighting);
@@ -121,9 +126,16 @@ inline void ShadingResult::set_to_linear_rgb(const foundation::Color3f& linear_r
     set_to_linear_rgba(foundation::Color4f(linear_rgb[0], linear_rgb[1], linear_rgb[2], 1.0f));
 }
 
-inline void ShadingResult::set_aov_for_entity(const Entity& entity)
+inline void ShadingResult::set_entity_aov(const Entity& entity)
 {
     m_aovs.set(entity.get_render_layer_index(), m_main);
+}
+
+inline void ShadingResult::set_entity_aov(
+    const Entity&               entity,
+    const ShadingFragment&      fragment)
+{
+    m_aovs.set(entity.get_render_layer_index(), fragment);
 }
 
 }       // namespace renderer

src/appleseed/renderer/modeling/surfaceshader/surfaceshadercollection.cpp
@@ -145,7 +145,7 @@ namespace
                 shading_point,
                 shading_result);
 
-            shading_result.set_aov_for_entity(*m_surface_shaders[0]);
+            shading_result.set_entity_aov(*m_surface_shaders[0]);
 
             for (size_t i = 1; i < MaxShaderCount; ++i)
             {
@@ -161,7 +161,7 @@ namespace
                         shading_point,
                         local_result);
 
-                    shading_result.set_aov_for_entity(*m_surface_shaders[i]);
+                    shading_result.set_entity_aov(*m_surface_shaders[i], local_result.m_main);
                 }
             }
         }