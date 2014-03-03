a2ac6285756bdc3073ddea410f9fb4e274c56d06
Francois Beaune <beaune@aist.enst.fr>
2014-03-03 08:49:46 +0100

    delete widgets immediately in appleseed::studio::clear_layout().

--- src/appleseed.studio/utility/miscellaneous.cpp
+++ src/appleseed.studio/utility/miscellaneous.cpp
@@ -86,7 +86,7 @@ void clear_layout(QLayout* layout)
 
         if (item->layout())
             clear_layout(item->layout());
-        else item->widget()->deleteLater();
+        else delete item->widget();
 
         delete item;
     }
