appleseedhq/appleseed

commit b310276f19f4be0f808120d8ee88c7c8d567943b

fixed Qt runtime warning.

src/appleseed.studio/utility/miscellaneous.cpp
@@ -80,11 +80,14 @@ QShortcut* create_window_local_shortcut(QWidget* parent, const Qt::Key key)
 
 void clear_layout(QLayout* layout)
 {
-    while (QLayoutItem* item = layout->takeAt(0))
+    while (!layout->isEmpty())
     {
+        QLayoutItem* item = layout->takeAt(0);
+
         if (item->layout())
             clear_layout(item->layout());
         else item->widget()->deleteLater();
+
         delete item;
     }
 }

commit 3e19ae3f57aae8aee226bdf99b8d520abbaf8f32

fixed regression introduced in b310276f19f4be0f808120d8ee88c7c8d567943b ("fixed Qt runtime warning").
QLayout::isEmpty() returns true even if the layout contains items:
https://qt.gitorious.org/qt/qt/source/0315971ee951e9abe7f288564ddf2e81aeed1fd8:src/gui/kernel/qlayout.cpp#L609

src/appleseed.studio/utility/miscellaneous.cpp
@@ -80,7 +80,7 @@ QShortcut* create_window_local_shortcut(QWidget* parent, const Qt::Key key)
 
 void clear_layout(QLayout* layout)
 {
-    while (!layout->isEmpty())
+    for (int i = layout->count(); i > 0; --i)
     {
         QLayoutItem* item = layout->takeAt(0);
 