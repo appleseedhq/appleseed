5c4872e6a311e9fe4edfcd107e7249013a530215
Francois Beaune <beaune@aist.enst.fr>
2014-03-04 21:19:13 +0100

    formatting fixes and minor code cleanups.

--- src/appleseed/renderer/kernel/rendering/rendererservices.cpp
+++ src/appleseed/renderer/kernel/rendering/rendererservices.cpp
@@ -32,78 +32,37 @@
 
 // appleseed.renderer headers.
 #include "renderer/global/globallogger.h"
-#include "renderer/kernel/shading/shadingpoint.h"
-#include "renderer/modeling/project/project.h"
-#include "renderer/modeling/scene/scene.h"
-#include "renderer/utility/transformsequence.h"
 
 namespace renderer
 {
 
 //
-// RenderServices class implementation.
+// RendererServices class implementation.
 //
 
-RendererServices::RendererServices(const Project& project,
-                                   OIIO::TextureSystem& texture_sys)
+RendererServices::RendererServices(
+    const Project&          project,
+    OIIO::TextureSystem&    texture_sys)
   : OSL::RendererServices()
   , m_project(project)
   , m_texture_sys(texture_sys)
 {
 }
 
-bool RendererServices::texture(OSL::ustring filename, 
-                               OSL::TextureOpt& options,
-                               OSL::ShaderGlobals* sg,
-                               float s, 
-                               float t, 
-                               float dsdx, 
-                               float dtdx,
-                               float dsdy, 
-                               float dtdy, 
-                               float *result)
+bool RendererServices::texture(
+    OSL::ustring            filename,
+    OSL::TextureOpt&        options,
+    OSL::ShaderGlobals*     sg,
+    float                   s,
+    float                   t,
+    float                   dsdx,
+    float                   dtdx,
+    float                   dsdy,
+    float                   dtdy,
+    float*                  result)
 {
-    const bool status =  m_texture_sys.texture(filename, 
-                                               options, 
-                                               s, 
-                                               t,
-                                               dsdx, 
-                                               dtdx, 
-                                               dsdy, 
-                                               dtdy, 
-                                               result);
-    if (!status)
-        log_error(m_texture_sys.geterror());
-
-    return status;
-}
-
-bool RendererServices::texture3d(OSL::ustring filename, 
-                                 OSL::TextureOpt& options,
-                                 OSL::ShaderGlobals* sg, 
-                                 const OSL::Vec3& P,
-                                 const OSL::Vec3& dPdx, 
-                                 const OSL::Vec3& dPdy,
-                                 const OSL::Vec3& dPdz, 
-                                 float* result)
-{
-    const bool status = m_texture_sys.texture3d(filename, options, P, dPdx, dPdy, dPdz, result);
-
-    if (!status)
-        log_error(m_texture_sys.geterror());
-
-    return status;    
-}
-
-bool RendererServices::environment(OSL::ustring filename, 
-                                   OSL::TextureOpt& options,
-                                   OSL::ShaderGlobals* sg, 
-                                   const OSL::Vec3& R,
-                                   const OSL::Vec3& dRdx, 
-                                   const OSL::Vec3& dRdy, 
-                                   float* result)
-{
-    const bool status = m_texture_sys.environment(filename, options, R, dRdx, dRdy, result);
+    const bool status =
+        m_texture_sys.texture(filename, options, s, t, dsdx, dtdx, dsdy, dtdy, result);
 
     if (!status)
         log_error(m_texture_sys.geterror());
@@ -111,13 +70,18 @@ bool RendererServices::environment(OSL::ustring filename,
     return status;
 }
 
-bool RendererServices::get_texture_info(OSL::ustring filename,
-                                        int subimage,
-                                        OSL::ustring dataname,
-                                        OSL::TypeDesc datatype,
-                                        void* data)
+bool RendererServices::texture3d(
+    OSL::ustring            filename,
+    OSL::TextureOpt&        options,
+    OSL::ShaderGlobals*     sg,
+    const OSL::Vec3&        P,
+    const OSL::Vec3&        dPdx,
+    const OSL::Vec3&        dPdy,
+    const OSL::Vec3&        dPdz,
+    float*                  result)
 {
-    const bool status = m_texture_sys.get_texture_info(filename, subimage, dataname, datatype, data);
+    const bool status =
+        m_texture_sys.texture3d(filename, options, P, dPdx, dPdy, dPdz, result);
 
     if (!status)
         log_error(m_texture_sys.geterror());
@@ -125,65 +89,107 @@ bool RendererServices::get_texture_info(OSL::ustring filename,
     return status;
 }
 
-bool RendererServices::get_matrix(OSL::Matrix44& result,
-                                  OSL::TransformationPtr xform,
-                                  float time)
+bool RendererServices::environment(
+    OSL::ustring            filename,
+    OSL::TextureOpt&        options,
+    OSL::ShaderGlobals*     sg,
+    const OSL::Vec3&        R,
+    const OSL::Vec3&        dRdx,
+    const OSL::Vec3&        dRdy,
+    float*                  result)
+{
+    const bool status =
+        m_texture_sys.environment(filename, options, R, dRdx, dRdy, result);
+
+    if (!status)
+        log_error(m_texture_sys.geterror());
+
+    return status;
+}
+
+bool RendererServices::get_texture_info(
+    OSL::ustring            filename,
+    int                     subimage,
+    OSL::ustring            dataname,
+    OSL::TypeDesc           datatype,
+    void*                   data)
+{
+    const bool status =
+        m_texture_sys.get_texture_info(filename, subimage, dataname, datatype, data);
+
+    if (!status)
+        log_error(m_texture_sys.geterror());
+
+    return status;
+}
+
+bool RendererServices::get_matrix(
+    OSL::Matrix44&          result,
+    OSL::TransformationPtr  xform,
+    float                   time)
 {
     return false;
 }
 
-bool RendererServices::get_matrix(OSL::Matrix44& result,
-                                  OSL::TransformationPtr xform)
+bool RendererServices::get_matrix(
+    OSL::Matrix44&          result,
+    OSL::TransformationPtr  xform)
 {
     return false;
 }
 
-bool RendererServices::get_matrix(OSL::Matrix44& result,
-                                  OIIO::ustring from,
-                                  float time)
+bool RendererServices::get_matrix(
+    OSL::Matrix44&          result,
+    OIIO::ustring           from,
+    float                   time)
 {
     return false;
 }
 
-bool RendererServices::get_matrix(OSL::Matrix44& result,
-                                  OIIO::ustring from)
+bool RendererServices::get_matrix(
+    OSL::Matrix44&          result,
+    OIIO::ustring           from)
 {
     return false;
 }
 
-bool RendererServices::get_attribute(void* renderstate,
-                                     bool derivatives,
-                                     OIIO::ustring object,
-                                     OIIO::TypeDesc type,
-                                     OIIO::ustring name,
-                                     void* val)
+bool RendererServices::get_attribute(
+    void*                   renderstate,
+    bool                    derivatives,
+    OIIO::ustring           object,
+    OIIO::TypeDesc          type,
+    OIIO::ustring           name,
+    void*                   val)
 {
     return false;
 }
 
-bool RendererServices::get_array_attribute(void* renderstate,
-                                           bool derivatives,
-                                           OIIO::ustring object,
-                                           OIIO::TypeDesc type,
-                                           OIIO::ustring name,
-                                           int index,
-                                           void* val)
+bool RendererServices::get_array_attribute(
+    void*                   renderstate,
+    bool                    derivatives,
+    OIIO::ustring           object,
+    OIIO::TypeDesc          type,
+    OIIO::ustring           name,
+    int                     index,
+    void*                   val)
 {
     return false;
 }
 
-bool RendererServices::get_userdata(bool derivatives,
-                                    OIIO::ustring name,
-                                    OIIO::TypeDesc type,
-                                    void* renderstate,
-                                    void* val)
+bool RendererServices::get_userdata(
+    bool                    derivatives,
+    OIIO::ustring           name,
+    OIIO::TypeDesc          type,
+    void*                   renderstate,
+    void*                   val)
 {
     return false;
 }
 
-bool RendererServices::has_userdata(OIIO::ustring name,
-                                    OIIO::TypeDesc type,
-                                    void* renderstate)
+bool RendererServices::has_userdata(
+    OIIO::ustring           name,
+    OIIO::TypeDesc          type,
+    void*                   renderstate)
 {
     return false;
 }
