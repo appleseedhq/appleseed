043deb96eaf412298ea06b315219447bc43f68c5
nullas <sean.null@gmail.com>
2014-03-19 15:30:45 -0400

    fix #322. Rearrange the order of header files

--- src/appleseed/foundation/meta/tests/test_memory.cpp
+++ src/appleseed/foundation/meta/tests/test_memory.cpp
@@ -34,9 +34,9 @@
 
 // Standard headers.
 #include <cstddef>
-#include <vector>
-#include <ctime>
 #include <cstdlib>
+#include <ctime>
+#include <vector>
 
 using namespace foundation;
 using namespace std;
@@ -139,11 +139,11 @@ TEST_SUITE(Foundation_Utility_Memory)
         srand((unsigned)time(0));
         bool aligned = true;
         void* pointers[test_times];
-        for (int i=0; i < test_times; ++i)
+        for (int i = 0; i < test_times; ++i)
         {
             size_t alloc_size = rand() % 1001;
             pointers[i] = malloc(alloc_size);
-            if (!is_aligned(pointers[i],16))
+            if (!is_aligned(pointers[i], 16))
                 aligned = false;
         }
         for (int i = 0; i < test_times; ++i)
