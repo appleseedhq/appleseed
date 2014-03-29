12afcabfe71fc61df273a120b19ee505fcda131e
Luis Barrancos <luis@jupiter-jazz.com>
2014-03-29 12:12:00 +0000

    Fixed potential negative pdf values.
            - Fixed potential negative pdf values when evaluating the BRDF
              response, one of the cos(phi_diff) factors was not absolute.

--- src/appleseed/renderer/modeling/bsdf/orennayarbrdf.cpp
+++ src/appleseed/renderer/modeling/bsdf/orennayarbrdf.cpp
@@ -102,7 +102,7 @@ namespace
         {
             // No reflection below the shading surface.
             const Vector3d& n = shading_basis.get_normal();
-            const double cos_on = min(dot(outgoing, n), 1.0);
+            const double cos_on = dot(outgoing, n);
             if (cos_on < 0.0)
                 return Absorption;
 
@@ -211,6 +211,8 @@ namespace
             const double sigma2 = square(roughness);
 
             const double C1 = 1.0 - 0.5 * (sigma2 / (sigma2 + 0.33));
+            assert( C1 >= 0.0 );
+
             double C2 = 0.45 * sigma2 / (sigma2 + 0.09);
 
             const Vector3d V_perp_N = normalize(outgoing - n * dot(outgoing, n));
@@ -224,13 +226,17 @@ namespace
                 const double temp = 2.0 * beta * RcpPi;
                 C2 *= sin(alpha) - square(temp) * temp;
             }
+            assert( C2 >= 0.0);
 
             const double C3 = 0.125 * (sigma2 / (sigma2 + 0.09) * square(4.0 * alpha * beta * RcpPiSq)) * tan((alpha + beta) * 0.5);
+            assert( C3 >= 0.0);
 
             value = reflectance ;
-            value *= static_cast<float>(C1 + (cos_phi_diff * C2 * tan(beta)) + (1 - abs(cos_phi_diff)) * C3);
+            value *= static_cast<float>(C1 + (abs(cos_phi_diff) * C2 * tan(beta)) + (1 - abs(cos_phi_diff)) * C3);
             value += square(reflectance) * static_cast<float>(0.17 * cos_in * (sigma2 / (sigma2 + 0.13)) *
                                                              (1 - cos_phi_diff * square(2 * beta * RcpPi)));
+
+            assert(min_value(value) >= 0.0 );
         }
 
       private:
