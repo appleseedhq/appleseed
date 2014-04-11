69d092e989fab796529c663e7e900c315843a5ef
Francois Beaune <beaune@aist.enst.fr>
2014-04-11 10:23:49 +0200

    small code tweaks and cleanups.

--- src/appleseed.studio/mainwindow/rendering/pixelinspectorhandler.cpp
+++ src/appleseed.studio/mainwindow/rendering/pixelinspectorhandler.cpp
@@ -5,7 +5,7 @@
 //
 // This software is released under the MIT license.
 //
-// Copyright (c) 2014 , The appleseedhq Organization
+// Copyright (c) 2014 Freddy Chaleur, The appleseedhq Organization
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -25,8 +25,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 //
-// Interface header.
 
+// Interface header.
 #include "pixelinspectorhandler.h"
 
 // appleseed.studio headers.
@@ -36,23 +36,25 @@
 
 // appleseed.renderer headers.
 #include "renderer/api/frame.h"
-#include "renderer/api/log.h"
 #include "renderer/api/project.h"
 #include "renderer/api/scene.h"
 
 // appleseed.foundation headers.
+#include "foundation/image/color.h"
 #include "foundation/image/image.h"
+#include "foundation/math/scalar.h"
+#include "foundation/math/vector.h"
 
 // Qt headers.
 #include <QEvent>
 #include <QMouseEvent>
+#include <QPoint>
 #include <QString>
-#include <Qt>
 #include <QToolTip>
 #include <QWidget>
 
 // Standard headers.
-#include <cassert>
+#include <cstddef>
 #include <sstream>
 
 using namespace foundation;
@@ -96,7 +98,7 @@ bool PixelInspectorHandler::eventFilter(QObject* object, QEvent* event)
     switch (event->type())
     {
        case QEvent::MouseMove:
-         show_tooltip(mouse_event->pos(),mouse_event->globalPos());
+         show_tooltip(mouse_event->pos(), mouse_event->globalPos());
          return false;
     }
 
@@ -105,50 +107,39 @@ bool PixelInspectorHandler::eventFilter(QObject* object, QEvent* event)
 
 void PixelInspectorHandler::show_tooltip(const QPoint& point, const QPoint& global_point)
 {
-    Color4f linear_rgba;
     const Vector2d pix = m_mouse_tracker.widget_to_pixel(point);
     const Vector2d ndc = m_mouse_tracker.widget_to_ndc(point);
 
-    stringstream sstr;
+    Color4f linear_rgba;
+    m_project.get_frame()->image().get_pixel(
+        truncate<size_t>(pix.x),
+        truncate<size_t>(pix.y),
+        linear_rgba);
 
-    sstr << "picking details tooltip:" << endl;
-
-    sstr << "  ndc coords       " << pix.x << ", " << pix.y << endl;
-
-    RENDERER_LOG_INFO("%s", sstr.str().c_str());
-
-    m_project.get_frame()->image().get_pixel(static_cast<size_t>(pix.x), static_cast<size_t>(pix.y) , linear_rgba);
-    QToolTip::showText
-     (
-         global_point,
-        (
-            QString
-            (
-                "Pixel Inspector \n\n"
-                "Pixel: \n"
-                "X: %1\n"
-                "Y: %2\n\n"
-                "NDC:  \n"
-                "X: %3\n"
-                "Y: %4\n\n"
-                "Color: \n"
-                "R: %5\n"
-                "G: %6\n"
-                "B: %7\n"
-                "A: %8"
-            ).arg
-            (
-                QString::number( pix.x ),
-                QString::number( pix.y ),
-                QString::number( ndc.x,'f',3 ),
-                QString::number( ndc.y,'f',3 ),
-                QString::number( linear_rgba.r,'f',4 ),
-                QString::number( linear_rgba.g,'f',4 ),
-                QString::number( linear_rgba.b,'f',4 ),
-                QString::number( linear_rgba.a,'f',4 )
-            )
-        )
-     );
+    QToolTip::showText(
+        global_point,
+        QString(
+            "Pixel Inspector\n\n"
+            "Pixel:\n"
+            "X: %1\n"
+            "Y: %2\n\n"
+            "NDC:\n"
+            "X: %3\n"
+            "Y: %4\n\n"
+            "Color:\n"
+            "R: %5\n"
+            "G: %6\n"
+            "B: %7\n"
+            "A: %8")
+        .arg(
+            QString::number(pix.x),
+            QString::number(pix.y),
+            QString::number(ndc.x, 'f', 5),
+            QString::number(ndc.y, 'f', 5),
+            QString::number(linear_rgba.r, 'f', 3),
+            QString::number(linear_rgba.g, 'f', 3),
+            QString::number(linear_rgba.b, 'f', 3),
+            QString::number(linear_rgba.a, 'f', 3)));
 }
 
 }   // namespace studio
--- src/appleseed.studio/mainwindow/rendering/pixelinspectorhandler.h
+++ src/appleseed.studio/mainwindow/rendering/pixelinspectorhandler.h
@@ -5,7 +5,7 @@
 //
 // This software is released under the MIT license.
 //
-// Copyright (c) 2014 , The appleseedhq Organization
+// Copyright (c) 2014 Freddy Chaleur, The appleseedhq Organization
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -50,10 +50,10 @@ class PixelInspectorHandler
 
   public:
     PixelInspectorHandler(
-           QWidget*                            widget,
-           const MouseCoordinatesTracker&      mouse_tracker,
-           const ProjectExplorer&              project_explorer,
-           const renderer::Project&            project);
+        QWidget*                            widget,
+        const MouseCoordinatesTracker&      mouse_tracker,
+        const ProjectExplorer&              project_explorer,
+        const renderer::Project&            project);
 
     ~PixelInspectorHandler();
 
