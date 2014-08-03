68e12e0c2bc45dfd624b856e0b31a75ff3628347
Francois Beaune <beaune@aist.enst.fr>
2014-08-03 11:37:04 +0200

    added Disney BRDF model to API header file.

--- src/appleseed/renderer/api/bsdf.h
+++ src/appleseed/renderer/api/bsdf.h
@@ -38,6 +38,7 @@
 #include "renderer/modeling/bsdf/bsdfmix.h"
 #include "renderer/modeling/bsdf/bsdftraits.h"
 #include "renderer/modeling/bsdf/diffusebtdf.h"
+#include "renderer/modeling/bsdf/disneybrdf.h"
 #include "renderer/modeling/bsdf/ibsdffactory.h"
 #include "renderer/modeling/bsdf/kelemenbrdf.h"
 #include "renderer/modeling/bsdf/lambertianbrdf.h"
