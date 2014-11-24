6e078b498914234c20348802a283f42d0b245119
Francois Beaune <beaune@aist.enst.fr>
2014-11-24 18:02:32 +0100

    added a unit test.

--- src/appleseed/foundation/meta/tests/test_matrix.cpp
+++ src/appleseed/foundation/meta/tests/test_matrix.cpp
@@ -665,6 +665,18 @@ TEST_SUITE(Foundation_Math_Matrix33)
 
         EXPECT_TRUE(is_normalized(q));
     }
+
+    TEST_CASE(ConstructRotationMatrixGivenQuaternion)
+    {
+        const Vector3d from = normalize(Vector3d(1.0, 1.0, 0.0));
+        const Vector3d to = normalize(Vector3d(1.0, 0.0, 1.0));
+
+        const Quaterniond q = Quaterniond::rotation(from, to);
+        const Matrix3d m = Matrix3d::rotation(q);
+        const Vector3d result = m * from;
+
+        EXPECT_FEQ(to, result);
+    }
 }
 
 TEST_SUITE(Foundation_Math_Matrix44)
