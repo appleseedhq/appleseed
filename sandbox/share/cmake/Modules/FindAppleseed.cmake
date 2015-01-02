d571f8856c10ed4bb16c9b11f26b3b05f835e235
Francois Beaune <beaune@aist.enst.fr>
2015-01-02 20:17:00 +0100

    fixed typos and formatting.

--- sandbox/share/cmake/Modules/FindAppleseed.cmake
+++ sandbox/share/cmake/Modules/FindAppleseed.cmake
@@ -1,3 +1,4 @@
+
 #
 # This source file is part of appleseed.
 # Visit http://appleseedhq.net/ for additional information and resources.
@@ -26,32 +27,33 @@
 #
 
 
-# Find Appleseed's headers and libraries.
 #
-#  This module defines
-# APPLESEED_INCLUDE_DIRS - where to find APPLESEED uncludes.
-# APPLESEED_LIBRARIES    - List of libraries when using APPLESEED.
-# APPLESEED_FOUND        - True if APPLESEED found.
+# Find appleseed's headers and libraries.
+#
+# This module defines:
+#
+#   APPLESEED_INCLUDE_DIRS - Where to find appleseed header files
+#   APPLESEED_LIBRARIES    - List of libraries when using appleseed
+#   APPLESEED_FOUND        - True if appleseed was found
+#
 
-# Look for the header file.
-FIND_PATH( APPLESEED_INCLUDE_DIR NAMES renderer/api/project.h)
+# Look for a core header file.
+FIND_PATH (APPLESEED_INCLUDE_DIR NAMES renderer/api/project.h)
 
-# Look for the libraries.
-FIND_LIBRARY( APPLESEED_LIBRARY NAMES appleseed)
+# Look for the library.
+FIND_LIBRARY (APPLESEED_LIBRARY NAMES appleseed)
 
-# handle the QUIETLY and REQUIRED arguments and set APPLESEED_FOUND to TRUE if
-# all listed variables are TRUE
-INCLUDE( FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS( APPLESEED DEFAULT_MSG APPLESEED_LIBRARY
-                                                         APPLESEED_INCLUDE_DIR)
+# Handle the QUIETLY and REQUIRED arguments and set APPLESEED_FOUND to TRUE if all listed variables are TRUE.
+INCLUDE (FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS (APPLESEED DEFAULT_MSG APPLESEED_LIBRARY APPLESEED_INCLUDE_DIR)
+
 # Copy the results to the output variables.
-IF(APPLESEED_FOUND)
-    SET( APPLESEED_LIBRARIES ${APPLESEED_LIBRARY})
-    SET( APPLESEED_INCLUDE_DIRS ${APPLESEED_INCLUDE_DIR})
-ELSE()
-    SET( APPLESEED_LIBRARIES)
-    SET( APPLESEED_INCLUDE_DIRS)
-ENDIF()
+IF (APPLESEED_FOUND)
+    SET (APPLESEED_LIBRARIES ${APPLESEED_LIBRARY})
+    SET (APPLESEED_INCLUDE_DIRS ${APPLESEED_INCLUDE_DIR})
+ELSE ()
+    SET (APPLESEED_LIBRARIES)
+    SET (APPLESEED_INCLUDE_DIRS)
+ENDIF ()
 
-MARK_AS_ADVANCED( APPLESEED_LIBRARY
-                  APPLESEED_INCLUDE_DIR)
+MARK_AS_ADVANCED (APPLESEED_LIBRARY APPLESEED_INCLUDE_DIR)
