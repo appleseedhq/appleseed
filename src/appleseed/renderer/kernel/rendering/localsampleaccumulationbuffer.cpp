appleseedhq/appleseed

commit 17906c2f60f068488f1a8a1cbb2c085ab2619623

optimized renderer::LocalSampleAccumulationBuffer::develop_to_frame().

src/appleseed/renderer/kernel/rendering/localsampleaccumulationbuffer.cpp
@@ -197,37 +197,57 @@ namespace
         const AABB2u&           crop_window,
         const bool              undo_premultiplied_alpha)
     {
-        const size_t tile_width = color_tile.get_width();
-        const size_t tile_height = color_tile.get_height();
-        const size_t level_width = level.get_width();
-        const size_t level_height = level.get_height();
+        const AABB2u r =
+            AABB2u::intersect(
+                AABB2u(
+                    Vector2u(origin_x, origin_y),
+                    Vector2u(origin_x + color_tile.get_width(), origin_y + color_tile.get_height())),
+                crop_window);
 
-        for (size_t y = 0; y < tile_height; ++y)
+        if (undo_premultiplied_alpha)
         {
-            for (size_t x = 0; x < tile_width; ++x)
+            for (size_t iy = r.min.y; iy < r.max.y; ++iy)
             {
-                Color<float, 5> values;
+                const size_t level_width = level.get_width();
+                const size_t src_y = (iy * level.get_height() / image_height) * level_width;
+                const size_t dest_y = (iy - origin_y) * color_tile.get_width() - origin_x;
 
-                const size_t ix = origin_x + x;
-                const size_t iy = origin_y + y;
-
-                if (crop_window.contains(Vector2u(ix, iy)))
+                for (size_t ix = r.min.x; ix < r.max.x; ++ix)
                 {
+                    Color<float, 5> values;
+
                     level.get_pixel(
-                        ix * level_width / image_width,
-                        iy * level_height / image_height,
+                        src_y + ix * level_width / image_width,
                         &values[0]);
 
-                    if (undo_premultiplied_alpha)
-                    {
-                        const float rcp_alpha = values[3] == 0.0f ? 0.0f : 1.0f / values[3];
-                        values[0] *= rcp_alpha;
-                        values[1] *= rcp_alpha;
-                        values[2] *= rcp_alpha;
-                    }
+                    const float rcp_alpha = values[3] == 0.0f ? 0.0f : 1.0f / values[3];
+                    values[0] *= rcp_alpha;
+                    values[1] *= rcp_alpha;
+                    values[2] *= rcp_alpha;
 
-                    color_tile.set_pixel(x, y, Color4f(&values[0]));
-                    depth_tile.set_component(x, y, 0, values[4]);
+                    color_tile.set_pixel<float>(dest_y + ix, &values[0]);
+                    depth_tile.set_component(dest_y + ix, 0, values[4]);
+                }
+            }
+        }
+        else
+        {
+            for (size_t iy = r.min.y; iy < r.max.y; ++iy)
+            {
+                const size_t level_width = level.get_width();
+                const size_t src_y = (iy * level.get_height() / image_height) * level_width;
+                const size_t dest_y = (iy - origin_y) * color_tile.get_width() - origin_x;
+
+                for (size_t ix = r.min.x; ix < r.max.x; ++ix)
+                {
+                    Color<float, 5> values;
+
+                    level.get_pixel(
+                        src_y + ix * level_width / image_width,
+                        &values[0]);
+
+                    color_tile.set_pixel<float>(dest_y + ix, &values[0]);
+                    depth_tile.set_component(dest_y + ix, 0, values[4]);
                 }
             }
         }

src/appleseed/CMakeLists.txt
@@ -1168,6 +1168,7 @@ source_group ("renderer\\kernel\\volume" FILES
 
 set (renderer_meta_benchmarks_sources
     renderer/meta/benchmarks/benchmark_frame.cpp
+    renderer/meta/benchmarks/benchmark_localsampleaccumulationbuffer.cpp
     renderer/meta/benchmarks/benchmark_transformsequence.cpp
 )
 list (APPEND appleseed_sources

src/appleseed/renderer/meta/benchmarks/benchmark_localsampleaccumulationbuffer.cpp
@@ -1,0 +1,88 @@
+
+//
+// This source file is part of appleseed.
+// Visit http://appleseedhq.net/ for additional information and resources.
+//
+// This software is released under the MIT license.
+//
+// Copyright (c) 2015 Francois Beaune, The appleseedhq Organization
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+// appleseed.renderer headers.
+#include "renderer/kernel/aov/imagestack.h"
+#include "renderer/kernel/rendering/localsampleaccumulationbuffer.h"
+#include "renderer/modeling/frame/frame.h"
+#include "renderer/utility/paramarray.h"
+
+// appleseed.foundation headers.
+#include "foundation/image/canvasproperties.h"
+#include "foundation/image/image.h"
+#include "foundation/image/pixel.h"
+#include "foundation/utility/autoreleaseptr.h"
+#include "foundation/utility/benchmark.h"
+
+using namespace foundation;
+using namespace renderer;
+
+BENCHMARK_SUITE(Renderer_Kernel_Rendering_LocalSampleAccumulationBuffer)
+{
+    template <bool PremultipliedAlpha>
+    struct Fixture
+    {
+        auto_release_ptr<Frame>         m_frame;
+        LocalSampleAccumulationBuffer   m_buffer;
+
+        Fixture()
+          : m_frame(
+                FrameFactory::create(
+                    "frame",
+                    ParamArray()
+                        .insert("resolution", "1920 1080")
+                        .insert("tile_size", "64 64")
+                        .insert("pixel_format", "float")
+                        .insert("filter", "box")
+                        .insert("filter_size", "2.0")
+                        .insert("color_space", "linear_rgb")
+                        .insert("premultiplied_alpha", PremultipliedAlpha)))
+          , m_buffer(
+                m_frame->image().properties().m_canvas_width,
+                m_frame->image().properties().m_canvas_height,
+                m_frame->get_filter())
+        {
+            m_frame->aov_images().append("depth", ImageStack::ContributionType, 4, PixelFormatFloat);
+
+            m_buffer.clear();
+
+            // Make sure tiles are allocated in the frame.
+            m_buffer.develop_to_frame(m_frame.ref());
+        }
+    };
+
+    BENCHMARK_CASE_F(DevelopToFrame_PremultipledAlpha, Fixture<true>)
+    {
+        m_buffer.develop_to_frame(m_frame.ref());
+    }
+
+    BENCHMARK_CASE_F(DevelopToFrame_StraightAlpha, Fixture<false>)
+    {
+        m_buffer.develop_to_frame(m_frame.ref());
+    }
+}

commit b6ac01d89a74f3dc5e1ce09d6c6b9768990b9489

fixed regression introduced in 17906c2f60f068488f1a8a1cbb2c085ab2619623 ("optimized renderer::LocalSampleAccumulationBuffer::develop_to_frame()").

src/appleseed/renderer/kernel/rendering/localsampleaccumulationbuffer.cpp
@@ -201,18 +201,18 @@ namespace
             AABB2u::intersect(
                 AABB2u(
                     Vector2u(origin_x, origin_y),
-                    Vector2u(origin_x + color_tile.get_width(), origin_y + color_tile.get_height())),
+                    Vector2u(origin_x + color_tile.get_width() - 1, origin_y + color_tile.get_height() - 1)),
                 crop_window);
 
         if (undo_premultiplied_alpha)
         {
-            for (size_t iy = r.min.y; iy < r.max.y; ++iy)
+            for (size_t iy = r.min.y; iy <= r.max.y; ++iy)
             {
                 const size_t level_width = level.get_width();
                 const size_t src_y = (iy * level.get_height() / image_height) * level_width;
                 const size_t dest_y = (iy - origin_y) * color_tile.get_width() - origin_x;
 
-                for (size_t ix = r.min.x; ix < r.max.x; ++ix)
+                for (size_t ix = r.min.x; ix <= r.max.x; ++ix)
                 {
                     Color<float, 5> values;
 
@@ -232,13 +232,13 @@ namespace
         }
         else
         {
-            for (size_t iy = r.min.y; iy < r.max.y; ++iy)
+            for (size_t iy = r.min.y; iy <= r.max.y; ++iy)
             {
                 const size_t level_width = level.get_width();
                 const size_t src_y = (iy * level.get_height() / image_height) * level_width;
                 const size_t dest_y = (iy - origin_y) * color_tile.get_width() - origin_x;
 
-                for (size_t ix = r.min.x; ix < r.max.x; ++ix)
+                for (size_t ix = r.min.x; ix <= r.max.x; ++ix)
                 {
                     Color<float, 5> values;
 