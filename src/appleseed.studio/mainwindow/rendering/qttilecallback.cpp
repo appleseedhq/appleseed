appleseedhq/appleseed

commit 482a42a4d7924e61bfedf00822ea366f480df810

fixed frozen display mode, plus many other changes.
- the render widget is now updated only when its content has changed.
- many simplifications, refactorings and cleanups.

src/appleseed.studio/mainwindow/rendering/qttilecallback.cpp
@@ -52,11 +52,8 @@ namespace
       : public TileCallbackBase
     {
       public:
-        QtTileCallback(
-            RenderWidget*   render_widget,
-            volatile bool&  tile_callbacks_enabled)
+        explicit QtTileCallback(RenderWidget* render_widget)
           : m_render_widget(render_widget)
-          , m_tile_callbacks_enabled(tile_callbacks_enabled)
         {
         }
 
@@ -71,11 +68,9 @@ namespace
             const size_t    width,
             const size_t    height) APPLESEED_OVERRIDE
         {
-            if (!m_tile_callbacks_enabled)
-                return;
-
             assert(m_render_widget);
             m_render_widget->highlight_region(x, y, width, height);
+            m_render_widget->update();
         }
 
         virtual void post_render_tile(
@@ -83,26 +78,21 @@ namespace
             const size_t    tile_x,
             const size_t    tile_y) APPLESEED_OVERRIDE
         {
-            if (!m_tile_callbacks_enabled)
-                return;
-
             assert(m_render_widget);
             m_render_widget->blit_tile(*frame, tile_x, tile_y);
+            m_render_widget->update();
         }
 
         virtual void post_render(
             const Frame*    frame) APPLESEED_OVERRIDE
         {
-            if (!m_tile_callbacks_enabled)
-                return;
-
             assert(m_render_widget);
             m_render_widget->blit_frame(*frame);
+            m_render_widget->update();
         }
 
       private:
-        RenderWidget*       m_render_widget;
-        volatile bool&      m_tile_callbacks_enabled;
+        RenderWidget* m_render_widget;
     };
 }
 
@@ -111,11 +101,8 @@ namespace
 // QtTileCallbackFactory factory.
 //
 
-QtTileCallbackFactory::QtTileCallbackFactory(
-    RenderWidget*   render_widget,
-    volatile bool&  tile_callbacks_enabled)
+QtTileCallbackFactory::QtTileCallbackFactory(RenderWidget* render_widget)
   : m_render_widget(render_widget)
-  , m_tile_callbacks_enabled(tile_callbacks_enabled)
 {
 }
 
@@ -126,10 +113,7 @@ void QtTileCallbackFactory::release()
 
 ITileCallback* QtTileCallbackFactory::create()
 {
-    return
-        new QtTileCallback(
-            m_render_widget,
-            m_tile_callbacks_enabled);
+    return new QtTileCallback(m_render_widget);
 }
 
 }   // namespace studio

src/appleseed.studio/mainwindow/mainwindow.cpp
@@ -782,6 +782,18 @@ void MainWindow::add_render_widget(const QString& label)
     connect(
         render_tab, SIGNAL(signal_entity_picked(renderer::ScenePicker::PickingResult)),
         SLOT(slot_clear_filter()));
+    connect(
+        render_tab, SIGNAL(signal_camera_change_begin()),
+        &m_rendering_manager, SLOT(slot_camera_change_begin()));
+    connect(
+        render_tab, SIGNAL(signal_camera_change_end()),
+        &m_rendering_manager, SLOT(slot_camera_change_end()));
+    connect(
+        render_tab, SIGNAL(signal_camera_changed()),
+        &m_rendering_manager, SLOT(slot_camera_changed()));
+    connect(
+        render_tab, SIGNAL(signal_camera_changed()),
+        &m_rendering_manager, SIGNAL(signal_camera_changed()));
 
     // Add the render tab to the tab bar.
     const int tab_index = m_ui->tab_render_channels->addTab(render_tab, label);
@@ -980,13 +992,14 @@ void MainWindow::start_rendering(const bool interactive)
         i->second->update();
     }
 
+    // Retrieve the right configuration.
     const char* configuration_name = interactive ? "interactive" : "final";
     const ParamArray params = get_project_params(configuration_name);
 
+    // Effectively start rendering.
     m_rendering_manager.start_rendering(
         project,
         params,
-        interactive,
         m_render_tabs["RGB"]);
 }
 

src/appleseed.studio/mainwindow/rendering/frozendisplayrenderer.cpp
@@ -29,9 +29,6 @@
 // Interface header.
 #include "frozendisplayrenderer.h"
 
-// appleseed.studio headers.
-#include "mainwindow/rendering/renderwidget.h"
-
 // appleseed.renderer headers.
 #include "renderer/api/aov.h"
 #include "renderer/api/camera.h"
@@ -47,13 +44,10 @@
 #include "foundation/math/transform.h"
 #include "foundation/platform/types.h"
 
-// Qt headers.
-#include <QImage>
-#include <QMutexLocker>
-
 // Standard headers.
+#include <cassert>
 #include <cstddef>
-#include <cstring>
+#include <limits>
 
 using namespace foundation;
 using namespace renderer;
@@ -65,32 +59,17 @@ namespace studio {
 FrozenDisplayRenderer::FrozenDisplayRenderer(
     const SamplingContext::Mode sampling_mode,
     const Camera&               camera,
-    const Frame&                frame,
-    RenderWidget&               render_widget)
+    const Frame&                frame)
   : m_sampling_mode(sampling_mode)
   , m_camera(camera)
   , m_frame(frame)
   , m_frame_props(frame.image().properties())
   , m_color_image(frame.image())
   , m_depth_image(frame.aov_images().get_image(frame.aov_images().get_index("depth")))
-  , m_render_widget(render_widget)
+  , m_temp_image(m_frame_props.m_pixel_count * 4)
+  , m_camera_transform(camera.transform_sequence().get_earliest_transform())
+  , m_points(m_frame_props.m_pixel_count)
 {
-    const QImage& dest_image = m_render_widget.image();
-    m_zbuffer.resize(dest_image.width() * dest_image.height(), 0.0f);
-    m_points.resize(m_frame_props.m_pixel_count);
-}
-
-namespace
-{
-    // todo: factorize with code in utility/interop.h and foundation/image/pixel.h.
-    Color3b rgb32f_to_color3b(const float r, const float g, const float b)
-    {
-        return
-            Color3b(
-                truncate<uint8>(clamp(r * 256.0f, 0.0f, 255.0f)),
-                truncate<uint8>(clamp(g * 256.0f, 0.0f, 255.0f)),
-                truncate<uint8>(clamp(b * 256.0f, 0.0f, 255.0f)));
-    }
 }
 
 void FrozenDisplayRenderer::capture()
@@ -125,26 +104,23 @@ void FrozenDisplayRenderer::capture()
                             px, py,
                             0.5, 0.5);
 
-                    // Generate a world space ray going through that film point.
-                    ShadingRay ray;
-                    m_camera.generate_ray(sampling_context, point, ray);
-
-                    // Retrieve pixel color.
-                    Color4f pixel;
-                    color_tile.get_pixel(px, py, pixel);
-
-                    // todo: hack.
-                    pixel.rgb() = fast_linear_rgb_to_srgb(pixel.rgb());
-
                     // Retrieve pixel depth.
                     const float depth = depth_tile.get_component<float>(px, py, 0);
 
                     if (depth >= 0.0f)
                     {
+                        // Generate a world space ray going through that film point.
+                        ShadingRay ray;
+                        m_camera.generate_ray(sampling_context, point, ray);
+
+                        // Retrieve pixel color.
+                        Color4f pixel;
+                        color_tile.get_pixel(px, py, pixel);
+
                         // Compute and store world space point and color.
                         RenderPoint point;
                         point.m_position = Vector3f(ray.point_at(depth));
-                        point.m_color = rgb32f_to_color3b(pixel[0], pixel[1], pixel[2]);
+                        point.m_color = pixel.rgb();
                         m_points[point_index++] = point;
                     }
                 }
@@ -153,32 +129,33 @@ void FrozenDisplayRenderer::capture()
     }
 }
 
+void FrozenDisplayRenderer::update()
+{
+    boost::mutex::scoped_lock lock(m_camera_transform_mutex);
+
+    m_camera_transform = m_camera.transform_sequence().get_earliest_transform();
+}
+
 void FrozenDisplayRenderer::render()
 {
-    // Get the camera transform.
-    Transformd tmp;
-    const Transformd& camera_transform = m_camera.transform_sequence().evaluate(0.0, tmp);
+    // Make a local copy of the camera transform to prevent update() from blocking.
+    Transformd camera_transform_copy;
+    {
+        boost::mutex::scoped_lock lock(m_camera_transform_mutex);
+        camera_transform_copy = m_camera_transform;
+    }
 
-    // Get exclusive access to the render widget.
-    QMutexLocker render_widget_locker(&m_render_widget.mutex());
+    // Retrieve pointer to temporary image pixels.
+    float* temp_pixels = &m_temp_image[0];
 
-    // Retrieve destination image and associated information.
-    QImage& dest_image = m_render_widget.image();
-    const int image_width = dest_image.width();
-    const int image_height = dest_image.height();
-    const size_t dest_stride = static_cast<size_t>(dest_image.bytesPerLine());
-
-    // Retrieve pointers to the color and depth buffers.
-    uint8* pixels = static_cast<uint8*>(dest_image.scanLine(0));
-    float* zbuffer = &m_zbuffer[0];
-
-    // Clear image.
-    for (size_t y = 0; y < image_height; ++y)
-        memset(pixels + y * dest_stride, 0, dest_stride);
-
-    // Clear Z-buffer.
-    for (size_t i = 0; i < image_width * image_height; ++i)
-        zbuffer[i] = -1.0e38f;
+    // Clear temporary image.
+    for (size_t i = 0; i < m_frame_props.m_pixel_count * 4; i += 4)
+    {
+        temp_pixels[i + 0] = 0.0f;
+        temp_pixels[i + 1] = 0.0f;
+        temp_pixels[i + 2] = 0.0f;
+        temp_pixels[i + 3] = -numeric_limits<float>::max();
+    }
 
     const size_t point_count = m_points.size();
     for (size_t i = 0; i < point_count; ++i)
@@ -186,36 +163,65 @@ void FrozenDisplayRenderer::render()
         const RenderPoint& point = m_points[i];
 
         // Transform point to camera space.
-        const Vector3f point_camera = camera_transform.point_to_local(point.m_position);
+        const Vector3f point_camera = camera_transform_copy.point_to_local(point.m_position);
 
         // Project point to film plane.
         Vector2d ndc;
-        m_camera.project_camera_space_point(Vector3d(point_camera), ndc);
+        if (!m_camera.project_camera_space_point(Vector3d(point_camera), ndc))
+            continue;
 
         // Compute point coordinates in pixels.
-        const int ix = truncate<int>(ndc.x * image_width);
-        const int iy = truncate<int>(ndc.y * image_height);
+        const int ix = truncate<int>(ndc.x * m_frame_props.m_canvas_width);
+        const int iy = truncate<int>(ndc.y * m_frame_props.m_canvas_height);
 
         // Reject points outside the frame.
         if (ix < 0 ||
             iy < 0 ||
-            ix >= image_width ||
-            iy >= image_height)
+            ix >= m_frame_props.m_canvas_width ||
+            iy >= m_frame_props.m_canvas_height)
             continue;
+
+        const size_t pixel_index = (iy * m_frame_props.m_canvas_width + ix) * 4;
 
         // Check point depth against Z-buffer.
-        const size_t zbuffer_index = iy * image_width + ix;
-        if (point_camera.z <= zbuffer[zbuffer_index])
+        if (point_camera.z <= temp_pixels[pixel_index + 3])
             continue;
 
-        // Write to Z-buffer.
-        zbuffer[zbuffer_index] = point_camera.z;
+        // Set point in temporary image.
+        temp_pixels[pixel_index + 0] = point.m_color[0];
+        temp_pixels[pixel_index + 1] = point.m_color[1];
+        temp_pixels[pixel_index + 2] = point.m_color[2];
+        temp_pixels[pixel_index + 3] = point_camera.z;
+    }
 
-        // Draw point on render widget.
-        const size_t base = iy * dest_stride + ix * 3;
-        pixels[base + 0] = point.m_color[0];
-        pixels[base + 1] = point.m_color[1];
-        pixels[base + 2] = point.m_color[2];
+    // Copy the temporary image to the frame.
+    for (size_t ty = 0; ty < m_frame_props.m_tile_count_y; ++ty)
+    {
+        for (size_t tx = 0; tx < m_frame_props.m_tile_count_x; ++tx)
+        {
+            Tile& color_tile = m_color_image.tile(tx, ty);
+            const size_t tile_width = color_tile.get_width();
+            const size_t tile_height = color_tile.get_height();
+
+            for (size_t py = 0; py < tile_height; ++py)
+            {
+                for (size_t px = 0; px < tile_width; ++px)
+                {
+                    const size_t ix = tx * m_frame_props.m_tile_width + px;
+                    const size_t iy = ty * m_frame_props.m_tile_height + py;
+
+                    assert(ix < m_frame_props.m_canvas_width);
+                    assert(iy < m_frame_props.m_canvas_height);
+
+                    const size_t pixel_index = (iy * m_frame_props.m_canvas_width + ix) * 4;
+
+                    // The third channel becomes an alpha channel.
+                    temp_pixels[pixel_index + 3] = 1.0f;
+
+                    color_tile.set_pixel<float>(px, py, temp_pixels + pixel_index);
+                }
+            }
+        }
     }
 }
 

src/appleseed.studio/mainwindow/rendering/frozendisplayrenderer.h
@@ -34,13 +34,14 @@
 
 // appleseed.foundation headers.
 #include "foundation/image/color.h"
+#include "foundation/math/transform.h"
 #include "foundation/math/vector.h"
+#include "foundation/platform/thread.h"
 
 // Standard headers.
 #include <vector>
 
 // Forward declarations.
-namespace appleseed     { namespace studio { class RenderWidget; } }
 namespace foundation    { class CanvasProperties; }
 namespace foundation    { class Image; }
 namespace renderer      { class Camera; }
@@ -55,10 +56,15 @@ class FrozenDisplayRenderer
     FrozenDisplayRenderer(
         const renderer::SamplingContext::Mode   sampling_mode,
         const renderer::Camera&                 camera,
-        const renderer::Frame&                  frame,
-        RenderWidget&                           render_widget);
+        const renderer::Frame&                  frame);
 
+    // Capture the frame as a point cloud.
     void capture();
+
+    // Capture the camera transform. Thread-safe.
+    void update();
+
+    // Render the point cloud to the frame.
     void render();
 
   private:
@@ -66,18 +72,21 @@ class FrozenDisplayRenderer
     const renderer::Camera&                     m_camera;
     const renderer::Frame&                      m_frame;
     const foundation::CanvasProperties&         m_frame_props;
+
     foundation::Image&                          m_color_image;
     foundation::Image&                          m_depth_image;
-    RenderWidget&                               m_render_widget;
+    std::vector<float>                          m_temp_image;
+
+    foundation::Transformd                      m_camera_transform;
+    boost::mutex                                m_camera_transform_mutex;
 
     struct RenderPoint
     {
         foundation::Vector3f    m_position;
-        foundation::Color3b     m_color;
+        foundation::Color3f     m_color;
     };
 
     std::vector<RenderPoint>                    m_points;
-    std::vector<float>                          m_zbuffer;
 };
 
 }       // namespace studio

src/appleseed.studio/mainwindow/rendering/qttilecallback.h
@@ -47,9 +47,7 @@ class QtTileCallbackFactory
 {
   public:
     // Constructor.
-    QtTileCallbackFactory(
-        RenderWidget*   render_widget,
-        volatile bool&  tile_callbacks_enabled);
+    explicit QtTileCallbackFactory(RenderWidget* render_widget);
 
     // Delete this instance.
     virtual void release() APPLESEED_OVERRIDE;
@@ -58,8 +56,7 @@ class QtTileCallbackFactory
     virtual renderer::ITileCallback* create() APPLESEED_OVERRIDE;
 
   private:
-    RenderWidget*       m_render_widget;
-    volatile bool&      m_tile_callbacks_enabled;
+    RenderWidget* m_render_widget;
 };
 
 }       // namespace studio

src/appleseed.studio/mainwindow/rendering/renderingmanager.cpp
@@ -49,6 +49,9 @@
 // appleseed.foundation headers.
 #include "foundation/image/analysis.h"
 #include "foundation/image/image.h"
+#include "foundation/platform/defaulttimers.h"
+#include "foundation/platform/types.h"
+#include "foundation/utility/job/iabortswitch.h"
 #include "foundation/utility/foreach.h"
 #include "foundation/utility/string.h"
 
@@ -57,7 +60,6 @@
 
 // Qt headers.
 #include <QApplication>
-#include <QTimerEvent>
 
 // Standard headers.
 #include <cassert>
@@ -102,9 +104,6 @@ RenderingManager::RenderingManager(StatusBar& status_bar)
   : m_status_bar(status_bar)
   , m_project(0)
   , m_render_tab(0)
-  , m_allow_camera_changes(false)
-  , m_camera_changed(false)
-  , m_tile_callbacks_enabled(true)
 {
     //
     // The connections below are using the Qt::BlockingQueuedConnection connection type.
@@ -162,40 +161,15 @@ RenderingManager::~RenderingManager()
 void RenderingManager::start_rendering(
     Project*                    project,
     const ParamArray&           params,
-    const bool                  interactive,
     RenderTab*                  render_tab)
 {
     m_project = project;
     m_params = params;
     m_render_tab = render_tab;
 
-    m_allow_camera_changes = false;
-    m_camera_changed = false;
-    m_tile_callbacks_enabled = true;
-
-    if (interactive)
-    {
-        connect(
-            m_render_tab, SIGNAL(signal_camera_change_begin()),
-            SLOT(slot_camera_change_begin()));
-
-        connect(
-            m_render_tab, SIGNAL(signal_camera_change_end()),
-            SLOT(slot_camera_change_end()));
-
-        connect(
-            m_render_tab, SIGNAL(signal_camera_changed()),
-            SLOT(slot_camera_changed()));
-
-        connect(
-            m_render_tab, SIGNAL(signal_camera_changed()),
-            SIGNAL(signal_camera_changed()));
-    }
-
     m_tile_callback_factory.reset(
         new QtTileCallbackFactory(
-            m_render_tab->get_render_widget(),
-            m_tile_callbacks_enabled));
+            m_render_tab->get_render_widget()));
 
     m_master_renderer.reset(
         new MasterRenderer(
@@ -219,6 +193,33 @@ void RenderingManager::wait_until_rendering_end()
 {
     while (is_rendering())
         QApplication::processEvents();
+}
+
+void RenderingManager::abort_rendering()
+{
+    RENDERER_LOG_DEBUG("aborting rendering...");
+
+    m_renderer_controller.set_status(IRendererController::AbortRendering);
+}
+
+void RenderingManager::restart_rendering()
+{
+    m_renderer_controller.set_status(IRendererController::RestartRendering);
+}
+
+void RenderingManager::reinitialize_rendering()
+{
+    m_renderer_controller.set_status(IRendererController::ReinitializeRendering);
+}
+
+void RenderingManager::pause_rendering()
+{
+    m_renderer_controller.set_status(IRendererController::PauseRendering);
+}
+
+void RenderingManager::resume_rendering()
+{
+    m_renderer_controller.set_status(IRendererController::ResumeRendering);
 }
 
 void RenderingManager::push_delayed_action(auto_ptr<IDelayedAction> action)
@@ -249,23 +250,6 @@ void RenderingManager::clear_permanent_states()
     m_permanent_states.clear();
 }
 
-void RenderingManager::abort_rendering()
-{
-    RENDERER_LOG_DEBUG("aborting rendering...");
-
-    m_renderer_controller.set_status(IRendererController::AbortRendering);
-}
-
-void RenderingManager::restart_rendering()
-{
-    m_renderer_controller.set_status(IRendererController::RestartRendering);
-}
-
-void RenderingManager::reinitialize_rendering()
-{
-    m_renderer_controller.set_status(IRendererController::ReinitializeRendering);
-}
-
 void RenderingManager::slot_abort_rendering()
 {
     abort_rendering();
@@ -279,13 +263,6 @@ void RenderingManager::slot_restart_rendering()
 void RenderingManager::slot_reinitialize_rendering()
 {
     reinitialize_rendering();
-}
-
-void RenderingManager::timerEvent(QTimerEvent* event)
-{
-    if (event->timerId() == m_render_widget_update_timer.timerId())
-        m_render_tab->get_render_widget()->update();
-    else QObject::timerEvent(event);
 }
 
 void RenderingManager::print_final_rendering_time()
@@ -350,21 +327,13 @@ void RenderingManager::slot_rendering_begin()
     consume_delayed_actions();
 
     m_rendering_timer.clear();
-
-    const int IdleUpdateRate = 30;  // hertz
-    m_render_widget_update_timer.start(1000 / IdleUpdateRate, this);
-
-    m_allow_camera_changes = true;
 }
 
 void RenderingManager::slot_rendering_end()
 {
-    m_allow_camera_changes = false;
-
     // Save the controller target point into the camera when rendering ends.
     m_render_tab->get_camera_controller()->save_camera_target();
 
-    m_render_widget_update_timer.stop();
     print_final_rendering_time();
 
     if (m_params.get_optional<bool>("print_final_average_luminance", false))
@@ -376,27 +345,21 @@ void RenderingManager::slot_rendering_end()
 
 void RenderingManager::slot_frame_begin()
 {
-    if (m_camera_changed)
-    {
-        // Update the scene's camera before rendering the frame.
-        if (m_allow_camera_changes)
-            m_render_tab->get_camera_controller()->update_camera_transform();
+    // Update the scene's camera before rendering the frame.
+    m_render_tab->get_camera_controller()->update_camera_transform();
 
-        if (m_frozen_display_renderer.get())
-            m_frozen_display_renderer->render();
-
-        m_camera_changed = false;
-    }
-
+    // Start printing rendering time in the status bar.
     m_status_bar.start_rendering_time_display(&m_rendering_timer);
     m_rendering_timer.start();
 }
 
 void RenderingManager::slot_frame_end()
 {
+    // Stop printing rendering time in the status bar.
     m_rendering_timer.measure();
     m_status_bar.stop_rendering_time_display();
 
+    // Ensure that the render widget is up-to-date.
     m_render_tab->get_render_widget()->update();
 }
 
@@ -404,29 +367,105 @@ void RenderingManager::slot_camera_change_begin()
 {
     if (m_params.get_optional<bool>("freeze_display_during_navigation", false))
     {
-        m_tile_callbacks_enabled = false;
-        m_frozen_display_renderer.reset(
-            new FrozenDisplayRenderer(
+        pause_rendering();
+
+        assert(m_frozen_display_func.get() == 0);
+        assert(m_frozen_display_thread.get() == 0);
+
+        m_frozen_display_abort_switch.clear();
+
+        m_frozen_display_func.reset(
+            new FrozenDisplayFunc(
                 get_sampling_context_mode(m_params),
                 *m_project->get_scene()->get_camera(),
                 *m_project->get_frame(),
-                *m_render_tab->get_render_widget()));
-        m_frozen_display_renderer->capture();
+                *m_tile_callback_factory.get(),
+                m_frozen_display_abort_switch));
+
+        m_frozen_display_thread.reset(
+            new boost::thread(
+                ThreadFunctionWrapper<FrozenDisplayFunc>(m_frozen_display_func.get())));
     }
 }
 
 void RenderingManager::slot_camera_changed()
 {
-    m_camera_changed = true;
-    restart_rendering();
+    if (m_frozen_display_func.get())
+    {
+        m_render_tab->get_camera_controller()->update_camera_transform();
+        m_frozen_display_func->update();
+    }
+    else
+    {
+        restart_rendering();
+    }
 }
 
 void RenderingManager::slot_camera_change_end()
 {
-    if (m_frozen_display_renderer.get())
+    if (m_frozen_display_func.get())
     {
-        m_frozen_display_renderer.reset();
-        m_tile_callbacks_enabled = true;
+        m_frozen_display_abort_switch.abort();
+        m_frozen_display_thread->join();
+        m_frozen_display_thread.reset();
+        m_frozen_display_func.reset();
+
+        m_project->get_frame()->clear_main_image();
+
+        restart_rendering();
+    }
+}
+
+
+//
+// RenderingManager::FrozenDisplayFunc class implementation.
+//
+
+RenderingManager::FrozenDisplayFunc::FrozenDisplayFunc(
+    const SamplingContext::Mode sampling_mode,
+    const Camera&               camera,
+    const Frame&                frame,
+    ITileCallbackFactory&       tile_callback_factory,
+    IAbortSwitch&               abort_switch)
+  : m_renderer(sampling_mode, camera, frame)
+  , m_frame(frame)
+  , m_tile_callback(tile_callback_factory.create())
+  , m_abort_switch(abort_switch)
+{
+    // Start by capturing the current frame as a point cloud.
+    m_renderer.capture();
+}
+
+void RenderingManager::FrozenDisplayFunc::update()
+{
+    m_renderer.update();
+}
+
+void RenderingManager::FrozenDisplayFunc::operator()()
+{
+    const double TargetElapsed = 1.0 / 30.0;
+
+    DefaultWallclockTimer timer;
+    const double rcp_timer_freq = 1.0 / timer.frequency();
+    uint64 last_time = timer.read();
+
+    while (!m_abort_switch.is_aborted())
+    {
+        // Render the point cloud to the frame.
+        m_renderer.render();
+
+        // Display the frame.
+        m_tile_callback->post_render(&m_frame);
+
+        // Limit frame rate.
+        const uint64 time = timer.read();
+        const double elapsed = (time - last_time) * rcp_timer_freq;
+        if (elapsed < TargetElapsed)
+        {
+            const double ms = ceil(1000.0 * (TargetElapsed - elapsed));
+            sleep(truncate<uint32>(ms), m_abort_switch);
+        }
+        last_time = time;
     }
 }
 

commit 3adfaa8b387482605ff7104b756ba65fcb079ba7

fixed regression introduced in 482a42a4d7924e61bfedf00822ea366f480df810 ("fixed frozen display mode, plus many other changes.").
We can't update the UI from another thread than the UI thread.  Doing so leads to crashes and major UI update bugs.

src/appleseed.studio/mainwindow/rendering/qttilecallback.cpp
@@ -33,6 +33,10 @@
 // appleseed.studio headers.
 #include "mainwindow/rendering/renderwidget.h"
 
+// Qt headers.
+#include <QObject>
+#include <Qt>
+
 // Standard headers.
 #include <cassert>
 #include <cstddef>
@@ -49,12 +53,19 @@ namespace studio {
 namespace
 {
     class QtTileCallback
-      : public TileCallbackBase
+      : public QObject
+      , public TileCallbackBase
     {
+        Q_OBJECT
+
       public:
         explicit QtTileCallback(RenderWidget* render_widget)
           : m_render_widget(render_widget)
         {
+            connect(
+                this, SIGNAL(signal_update()),
+                m_render_widget, SLOT(update()),
+                Qt::QueuedConnection);
         }
 
         virtual void release() APPLESEED_OVERRIDE
@@ -70,7 +81,7 @@ namespace
         {
             assert(m_render_widget);
             m_render_widget->highlight_region(x, y, width, height);
-            m_render_widget->update();
+            emit signal_update();
         }
 
         virtual void post_render_tile(
@@ -80,7 +91,7 @@ namespace
         {
             assert(m_render_widget);
             m_render_widget->blit_tile(*frame, tile_x, tile_y);
-            m_render_widget->update();
+            emit signal_update();
         }
 
         virtual void post_render(
@@ -88,8 +99,11 @@ namespace
         {
             assert(m_render_widget);
             m_render_widget->blit_frame(*frame);
-            m_render_widget->update();
+            emit signal_update();
         }
+
+      signals:
+        void signal_update();
 
       private:
         RenderWidget* m_render_widget;
@@ -118,3 +132,5 @@ ITileCallback* QtTileCallbackFactory::create()
 
 }   // namespace studio
 }   // namespace appleseed
+
+#include "mainwindow/rendering/moc_cpp_qttilecallback.cxx"