appleseedhq/appleseed

commit bfa040537da87046588d94983f7b9319ce13eaa2

small code optimizations.

src/appleseed/foundation/image/filteredtile.cpp
@@ -35,9 +35,6 @@
 #include "foundation/math/scalar.h"
 #include "foundation/math/vector.h"
 
-// Standard headers.
-#include <cmath>
-
 using namespace std;
 
 namespace foundation
@@ -98,25 +95,26 @@ void FilteredTile::add(
 
     // Find the pixels affected by this sample.
     AABB2i footprint;
-    footprint.min.x = truncate<int>(ceil(dx - m_filter.get_xradius()));
-    footprint.min.y = truncate<int>(ceil(dy - m_filter.get_yradius()));
-    footprint.max.x = truncate<int>(floor(dx + m_filter.get_xradius()));
-    footprint.max.y = truncate<int>(floor(dy + m_filter.get_yradius()));
+    footprint.min.x = truncate<int>(fast_ceil(dx - m_filter.get_xradius()));
+    footprint.min.y = truncate<int>(fast_ceil(dy - m_filter.get_yradius()));
+    footprint.max.x = truncate<int>(fast_floor(dx + m_filter.get_xradius()));
+    footprint.max.y = truncate<int>(fast_floor(dy + m_filter.get_yradius()));
 
     // Don't affect pixels outside the crop window.
     footprint = AABB2i::intersect(footprint, m_crop_window);
 
     for (int ry = footprint.min.y; ry <= footprint.max.y; ++ry)
     {
+        float* RESTRICT ptr = reinterpret_cast<float*>(pixel(footprint.min.x, ry));
+
         for (int rx = footprint.min.x; rx <= footprint.max.x; ++rx)
         {
-            float* RESTRICT ptr = reinterpret_cast<float*>(pixel(rx, ry));
             const float weight = m_filter.evaluate(rx - dx, ry - dy);
 
             *ptr++ += weight;
 
             for (size_t i = 0; i < m_channel_count - 1; ++i)
-                ptr[i] += values[i] * weight;
+                *ptr++ += values[i] * weight;
         }
     }
 }

src/appleseed/CMakeLists.txt
@@ -345,6 +345,7 @@ set (foundation_meta_benchmarks_sources
     foundation/meta/benchmarks/benchmark_colorspace.cpp
     foundation/meta/benchmarks/benchmark_distance.cpp
     foundation/meta/benchmarks/benchmark_fastmath.cpp
+    foundation/meta/benchmarks/benchmark_filteredtile.cpp
     foundation/meta/benchmarks/benchmark_integerdivision.cpp
     foundation/meta/benchmarks/benchmark_intersection.cpp
     foundation/meta/benchmarks/benchmark_job.cpp

src/appleseed/foundation/meta/benchmarks/benchmark_filteredtile.cpp
@@ -1,0 +1,60 @@
+
+//
+// This source file is part of appleseed.
+// Visit http://appleseedhq.net/ for additional information and resources.
+//
+// This software is released under the MIT license.
+//
+// Copyright (c) 2015 Francois Beaune, The appleseedhq Organization
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+// appleseed.foundation headers.
+#include "foundation/image/filteredtile.h"
+#include "foundation/math/filter.h"
+#include "foundation/utility/benchmark.h"
+
+using namespace foundation;
+
+BENCHMARK_SUITE(Foundation_Image_FilteredTile)
+{
+    struct Fixture
+    {
+        GaussianFilter2<float>  m_filter;
+        FilteredTile            m_tile;
+        const volatile float    m_x;
+        const volatile float    m_y;
+
+        Fixture()
+          : m_filter(2.0f, 2.0f, 8.0f)
+          , m_tile(1024, 1024, 4, m_filter)
+          , m_x(42.42f)
+          , m_y(66.66f)
+        {
+        }
+    };
+
+    BENCHMARK_CASE_F(Add, Fixture)
+    {
+        const float Values[4] = { 1.0f, 2.0f, 3.0f, 4.0f };
+
+        m_tile.add(m_x, m_y, Values);
+    }
+}

commit eccb3a48d2d9503871775397553cf427cd7a1d36

fixed regression introduced in bfa040537da87046588d94983f7b9319ce13eaa2 ("small code optimizations").

src/appleseed/foundation/image/filteredtile.cpp
@@ -103,6 +103,11 @@ void FilteredTile::add(
     // Don't affect pixels outside the crop window.
     footprint = AABB2i::intersect(footprint, m_crop_window);
 
+    // Bail out if the point does not fall inside the crop window.
+    // Only check the x coordinate; the y coordinate is checked in the loop below.
+    if (footprint.min.x > footprint.max.x)
+        return;
+
     for (int ry = footprint.min.y; ry <= footprint.max.y; ++ry)
     {
         float* RESTRICT ptr = reinterpret_cast<float*>(pixel(footprint.min.x, ry));