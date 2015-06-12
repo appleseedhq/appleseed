b8a823729770b5770da75bb68da77f99fdc434b5
Franz Beaune <beaune@aist.enst.fr>
2015-06-12 18:41:09 +0200

    cosmetics.

--- src/appleseed/renderer/kernel/rendering/rendererservices.h
+++ src/appleseed/renderer/kernel/rendering/rendererservices.h
@@ -307,7 +307,7 @@ class RendererServices
     DECLARE_ATTR_GETTER(ray_ior);
     DECLARE_ATTR_GETTER(ray_has_differentials);
 
-    // Appleseed version attributes
+    // appleseed version attributes.
     DECLARE_ATTR_GETTER(appleseed_version_major);
     DECLARE_ATTR_GETTER(appleseed_version_minor);
     DECLARE_ATTR_GETTER(appleseed_version_patch);
