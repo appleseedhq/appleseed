2240337da329188b07bc40943caa4b2ea5816123
Franz Beaune <beaune@aist.enst.fr>
2015-07-19 22:36:45 +0200

    formatting.

--- src/appleseed/renderer/kernel/lighting/imagebasedlighting.cpp
+++ src/appleseed/renderer/kernel/lighting/imagebasedlighting.cpp
@@ -122,10 +122,7 @@ void compute_ibl_bsdf_sampling(
         // includes the contribution of a specular component since these are explicitly rejected
         // afterward. We need a mechanism to indicate that we want the contribution of some of
         // the components only.
-        BSDFSample sample(
-            shading_point,
-            sampling_context,
-            outgoing);
+        BSDFSample sample(shading_point, sampling_context, outgoing);
         bsdf.sample(
             bsdf_data,
             false,              // not adjoint
