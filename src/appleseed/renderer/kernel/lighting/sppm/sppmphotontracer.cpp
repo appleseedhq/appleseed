appleseedhq/appleseed

commit 315085c9dbb4b7b44f726e670e6475e52b40e47c

Take into account shutter times when doing deformation motion blur.

src/appleseed/renderer/kernel/lighting/sppm/sppmphotontracer.cpp
@@ -249,8 +249,11 @@ namespace
           , m_photon_end(photon_end)
           , m_pass_hash(pass_hash)
           , m_abort_switch(abort_switch)
-          , m_ray_dtime(scene.get_camera()->get_shutter_open_time_interval())
         {
+            const Camera *camera = scene.get_camera();
+            m_shutter_open_time = camera->get_shutter_open_time();
+            m_shutter_close_time = camera->get_shutter_close_time();
+            m_ray_dtime = m_shutter_close_time - m_shutter_open_time;
         }
 
         virtual void execute(const size_t thread_index) APPLESEED_OVERRIDE
@@ -302,6 +305,8 @@ namespace
         const size_t                m_pass_hash;
         IAbortSwitch&               m_abort_switch;
         SPPMPhotonVector            m_local_photons;
+        double                      m_shutter_open_time;
+        double                      m_shutter_close_time;
         double                      m_ray_dtime;
 
         void trace_light_photon(
@@ -391,11 +396,15 @@ namespace
 
             // Build the photon ray.
             child_sampling_context.split_in_place(1, 1);
+            ShadingRay::TimeType time;
+            time.m_relative = sampling_context.next_double2();
+            time.m_absolute = lerp(m_shutter_open_time, m_shutter_close_time, time.m_relative);
+            time.m_differential = m_ray_dtime;
+
             const ShadingRay ray(
                 light_sample.m_point,
                 emission_direction,
-                child_sampling_context.next_double2(),
-                m_ray_dtime,
+                time,
                 VisibilityFlags::LightRay,
                 0);
 
@@ -450,11 +459,15 @@ namespace
 
             // Build the photon ray.
             child_sampling_context.split_in_place(1, 1);
+            ShadingRay::TimeType time;
+            time.m_relative = sampling_context.next_double2();
+            time.m_absolute = lerp(m_shutter_open_time, m_shutter_close_time, time.m_relative);
+            time.m_differential = m_ray_dtime;
+
             const ShadingRay ray(
                 emission_position,
                 emission_direction,
-                child_sampling_context.next_double2(),
-                m_ray_dtime,
+                time,
                 VisibilityFlags::LightRay,
                 0);
 
@@ -536,12 +549,16 @@ namespace
           , m_photon_end(photon_end)
           , m_pass_hash(pass_hash)
           , m_abort_switch(abort_switch)
-          , m_ray_dtime(scene.get_camera()->get_shutter_open_time_interval())
         {
             const GAABB3 scene_bbox = m_scene.compute_bbox();
             m_scene_center = scene_bbox.center();
             m_scene_radius = scene_bbox.radius();
             m_safe_scene_diameter = 1.01 * (2.0 * m_scene_radius);
+
+            const Camera *camera = scene.get_camera();
+            m_shutter_open_time = camera->get_shutter_open_time();
+            m_shutter_close_time = camera->get_shutter_close_time();
+            m_ray_dtime = m_shutter_close_time - m_shutter_open_time;
         }
 
         virtual void execute(const size_t thread_index) APPLESEED_OVERRIDE
@@ -594,7 +611,9 @@ namespace
         const size_t                m_pass_hash;
         IAbortSwitch&               m_abort_switch;
         SPPMPhotonVector            m_local_photons;
-        const double                m_ray_dtime;
+        double                      m_shutter_open_time;
+        double                      m_shutter_close_time;
+        double                      m_ray_dtime;
 
         Vector3d                    m_scene_center;         // world space
         double                      m_scene_radius;         // world space
@@ -657,11 +676,15 @@ namespace
 
             // Build the photon ray.
             child_sampling_context.split_in_place(1, 1);
+            ShadingRay::TimeType time;
+            time.m_relative = sampling_context.next_double2();
+            time.m_absolute = lerp(m_shutter_open_time, m_shutter_close_time, time.m_relative);
+            time.m_differential = m_ray_dtime;
+
             const ShadingRay ray(
                 ray_origin,
                 -outgoing,
-                child_sampling_context.next_double2(),
-                m_ray_dtime,
+                time,
                 VisibilityFlags::LightRay,
                 0);
 

src/appleseed/renderer/kernel/intersection/assemblytree.cpp
@@ -655,7 +655,7 @@ bool AssemblyLeafVisitor::visit(
             &item.m_transform_sequence;
         Transformd tmp;
         const Transformd& assembly_instance_transform =
-            assembly_instance_transform_seq->evaluate(ray.m_time, tmp);
+            assembly_instance_transform_seq->evaluate(ray.m_time.m_absolute, tmp);
 
         // Transform the ray to assembly instance space.
         ShadingPoint local_shading_point;
@@ -709,7 +709,7 @@ bool AssemblyLeafVisitor::visit(
                         *triangle_tree,
                         local_shading_point.m_ray,
                         local_ray_info,
-                        local_shading_point.m_ray.m_time,
+                        local_shading_point.m_ray.m_time.m_relative,
                         visitor
 #ifdef FOUNDATION_BVH_ENABLE_TRAVERSAL_STATS
                         , m_triangle_tree_stats
@@ -816,7 +816,7 @@ bool AssemblyLeafProbeVisitor::visit(
         // Evaluate the transformation of the assembly instance.
         Transformd tmp;
         const Transformd& assembly_instance_transform =
-            item.m_transform_sequence.evaluate(ray.m_time, tmp);
+            item.m_transform_sequence.evaluate(ray.m_time.m_absolute, tmp);
 
         // Transform the ray to assembly instance space.
         ShadingRay local_ray;
@@ -869,14 +869,14 @@ bool AssemblyLeafProbeVisitor::visit(
             {
                 // Check the intersection between the ray and the triangle tree.
                 TriangleTreeProbeIntersector intersector;
-                TriangleLeafProbeVisitor visitor(*triangle_tree, local_ray.m_time, local_ray.m_flags);
+                TriangleLeafProbeVisitor visitor(*triangle_tree, local_ray.m_time.m_relative, local_ray.m_flags);
                 if (triangle_tree->get_moving_triangle_count() > 0)
                 {
                     intersector.intersect_motion(
                         *triangle_tree,
                         local_ray,
                         local_ray_info,
-                        local_ray.m_time,
+                        local_ray.m_time.m_relative,
                         visitor
 #ifdef FOUNDATION_BVH_ENABLE_TRAVERSAL_STATS
                         , m_triangle_tree_stats

src/appleseed/renderer/kernel/intersection/regiontree.cpp
@@ -528,7 +528,7 @@ double RegionLeafVisitor::visit(
                 *triangle_tree,
                 ray,
                 ray_info,
-                ray.m_time,
+                ray.m_time.m_relative,
                 visitor
 #ifdef FOUNDATION_BVH_ENABLE_TRAVERSAL_STATS
                 , m_triangle_tree_stats
@@ -576,14 +576,14 @@ double RegionLeafProbeVisitor::visit(
     {
         // Check the intersection between the ray and the triangle tree.
         TriangleTreeProbeIntersector intersector;
-        TriangleLeafProbeVisitor visitor(*triangle_tree, ray.m_time, ray.m_flags);
+        TriangleLeafProbeVisitor visitor(*triangle_tree, ray.m_time.m_relative, ray.m_flags);
         if (triangle_tree->get_moving_triangle_count() > 0)
         {
             intersector.intersect_motion(
                 *triangle_tree,
                 ray,
                 ray_info,
-                ray.m_time,
+                ray.m_time.m_relative,
                 visitor
 #ifdef FOUNDATION_BVH_ENABLE_TRAVERSAL_STATS
                 , m_triangle_tree_stats

src/appleseed/renderer/kernel/intersection/triangletree.cpp
@@ -1430,7 +1430,7 @@ bool TriangleLeafVisitor::visit(
             }
 
             // Advance to the motion step immediately before the ray time.
-            const double base_time = m_shading_point.m_ray.m_time * motion_segment_count;
+            const double base_time = m_shading_point.m_ray.m_time.m_relative * motion_segment_count;
             const size_t base_index = truncate<size_t>(base_time);
             reader += base_index * 3 * sizeof(GVector3);
 

src/appleseed/renderer/kernel/lighting/directlightingintegrator.h
@@ -172,7 +172,7 @@ class DirectLightingIntegrator
     const foundation::Vector3d&         m_point;
     const foundation::Vector3d&         m_geometric_normal;
     const foundation::Basis3d&          m_shading_basis;
-    const double                        m_time;
+    const ShadingRay::TimeType&         m_time;
     const foundation::Dual3d&           m_outgoing;
     const BSDF&                         m_bsdf;
     const void*                         m_bsdf_data;

src/appleseed/renderer/kernel/lighting/lightsampler.cpp
@@ -80,7 +80,13 @@ void LightSample::make_shading_point(
 
     intersector.manufacture_hit(
         shading_point,
-        ShadingRay(m_point, direction, 0.0, 0.0, 0.0, 0.0, VisibilityFlags::CameraRay, 0),
+        ShadingRay(
+            m_point,
+            direction,
+            0.0,
+            0.0,
+            ShadingRay::TimeType(0.0, 0.0, 0.0),
+            VisibilityFlags::CameraRay, 0),
         ShadingPoint::PrimitiveTriangle,    // note: we assume light samples are always on triangles (and not on curves)
         m_triangle->m_assembly_instance,
         m_triangle->m_assembly_instance->transform_sequence().get_earliest_transform(),
@@ -99,6 +105,11 @@ LightSampler::LightSampler(const Scene& scene, const ParamArray& params)
   : m_params(params)
   , m_emitting_triangle_hash_table(m_triangle_key_hasher)
 {
+    const Camera *camera = scene.get_camera();
+    m_shutter_open_time = camera->get_shutter_open_time();
+    m_shutter_close_time = camera->get_shutter_close_time();
+    m_ray_dtime = m_shutter_close_time - m_shutter_open_time;
+
     RENDERER_LOG_INFO("collecting light emitters...");
 
     // Collect all non-physical lights.
@@ -389,7 +400,7 @@ void LightSampler::build_emitting_triangle_hash_table()
 }
 
 void LightSampler::sample_non_physical_lights(
-    const double                        time,
+    const ShadingRay::TimeType&         time,
     const Vector3d&                     s,
     LightSample&                        light_sample) const
 {
@@ -412,7 +423,7 @@ void LightSampler::sample_non_physical_lights(
 }
 
 void LightSampler::sample_emitting_triangles(
-    const double                        time,
+    const ShadingRay::TimeType&         time,
     const Vector3d&                     s,
     LightSample&                        light_sample) const
 {
@@ -435,7 +446,7 @@ void LightSampler::sample_emitting_triangles(
 }
 
 void LightSampler::sample(
-    const double                        time,
+    const ShadingRay::TimeType&         time,
     const Vector3d&                     s,
     LightSample&                        light_sample) const
 {
@@ -482,7 +493,7 @@ double LightSampler::evaluate_pdf(const ShadingPoint& shading_point) const
 }
 
 void LightSampler::sample_non_physical_light(
-    const double                        time,
+    const ShadingRay::TimeType&         time,
     const Vector2d&                     s,
     const size_t                        light_index,
     const double                        light_prob,
@@ -495,14 +506,14 @@ void LightSampler::sample_non_physical_light(
     // Evaluate and store the transform of the light.
     light_sample.m_light_transform =
           light_info.m_light->get_transform()
-        * light_info.m_transform_sequence.evaluate(time);
+        * light_info.m_transform_sequence.evaluate(time.m_absolute);
 
     // Store the probability density of this light.
     light_sample.m_probability = light_prob;
 }
 
 void LightSampler::sample_emitting_triangle(
-    const double                        time,
+    const ShadingRay::TimeType&         time,
     const Vector2d&                     s,
     const size_t                        triangle_index,
     const double                        triangle_prob,

src/appleseed/renderer/kernel/lighting/lightsampler.h
@@ -32,6 +32,7 @@
 
 // appleseed.renderer headers.
 #include "renderer/kernel/intersection/intersectionsettings.h"
+#include "renderer/kernel/shading/shadingray.h"
 #include "renderer/modeling/scene/containers.h"
 #include "renderer/utility/transformsequence.h"
 
@@ -188,26 +189,26 @@ class LightSampler
 
     // Sample the set of non-physical lights.
     void sample_non_physical_lights(
-        const double                        time,
+        const ShadingRay::TimeType&         time,
         const foundation::Vector3d&         s,
         LightSample&                        light_sample) const;
 
     // Sample a single given non-physical light.
     void sample_non_physical_light(
-        const double                        time,
+        const ShadingRay::TimeType&         time,
         const foundation::Vector2d&         s,
         const size_t                        light_index,
         LightSample&                        light_sample) const;
 
     // Sample the set of emitting triangles.
     void sample_emitting_triangles(
-        const double                        time,
+        const ShadingRay::TimeType&         time,
         const foundation::Vector3d&         s,
         LightSample&                        light_sample) const;
 
     // Sample the sets of non-physical lights and emitting triangles.
     void sample(
-        const double                        time,
+        const ShadingRay::TimeType&         time,
         const foundation::Vector3d&         s,
         LightSample&                        light_sample) const;
     void sample(
@@ -242,6 +243,10 @@ class LightSampler
     EmittingTriangleKeyHasher   m_triangle_key_hasher;
     EmittingTriangleHashTable   m_emitting_triangle_hash_table;
 
+    double                      m_shutter_open_time;
+    double                      m_shutter_close_time;
+    double                      m_ray_dtime;
+
     // Recursively collect non-physical lights from a given set of assembly instances.
     void collect_non_physical_lights(
         const AssemblyInstanceContainer&    assembly_instances,
@@ -268,7 +273,7 @@ class LightSampler
 
     // Sample a given non-physical light.
     void sample_non_physical_light(
-        const double                        time,
+        const ShadingRay::TimeType&         time,
         const foundation::Vector2d&         s,
         const size_t                        light_index,
         const double                        light_prob,
@@ -276,7 +281,7 @@ class LightSampler
 
     // Sample a given emitting triangle.
     void sample_emitting_triangle(
-        const double                        time,
+        const ShadingRay::TimeType&         time,
         const foundation::Vector2d&         s,
         const size_t                        triangle_index,
         const double                        triangle_prob,
@@ -349,7 +354,7 @@ inline bool LightSampler::has_lights_or_emitting_triangles() const
 }
 
 inline void LightSampler::sample_non_physical_light(
-    const double                            time,
+    const ShadingRay::TimeType&             time,
     const foundation::Vector2d&             s,
     const size_t                            light_index,
     LightSample&                            sample) const
@@ -361,7 +366,11 @@ inline void LightSampler::sample(
     const foundation::Vector4d&             s,
     LightSample&                            light_sample) const
 {
-    sample(s[0], foundation::Vector3d(s[1], s[2], s[3]), light_sample);
+    ShadingRay::TimeType time;
+    time.m_relative = s[0];
+    time.m_absolute = foundation::lerp(m_shutter_open_time, m_shutter_close_time, time.m_relative);
+    time.m_differential = m_ray_dtime;
+    sample(time, foundation::Vector3d(s[1], s[2], s[3]), light_sample);
 }
 
 }       // namespace renderer

src/appleseed/renderer/kernel/lighting/lighttracing/lighttracingsamplegenerator.cpp
@@ -204,13 +204,17 @@ namespace
                 m_params.m_max_iterations)
           , m_light_sample_count(0)
           , m_path_count(0)
-          , m_ray_dtime(scene.get_camera()->get_shutter_open_time_interval())
         {
             const GAABB3 scene_bbox = m_scene.compute_bbox();
             m_scene_center = scene_bbox.center();
             m_scene_radius = scene_bbox.radius();
             m_safe_scene_diameter = 1.01 * (2.0 * m_scene_radius);
             m_disk_point_prob = 1.0 / (Pi * m_scene_radius * m_scene_radius);
+
+            const Camera *camera = scene.get_camera();
+            m_shutter_open_time = camera->get_shutter_open_time();
+            m_shutter_close_time = camera->get_shutter_close_time();
+            m_ray_dtime = m_shutter_close_time - m_shutter_open_time;
         }
 
         virtual void release() APPLESEED_OVERRIDE
@@ -306,7 +310,7 @@ namespace
             void visit_area_light_vertex(
                 const LightSample&          light_sample,
                 const Spectrum&             light_particle_flux,
-                const double                time)
+                const ShadingRay::TimeType& time)
             {
                 // Compute the vertex-to-camera direction vector.
                 const Vector3d vertex_to_camera = m_camera_position - light_sample.m_point;
@@ -346,7 +350,7 @@ namespace
             void visit_non_physical_light_vertex(
                 const Vector3d&             light_vertex,
                 const Spectrum&             light_particle_flux,
-                const double                time)
+                const ShadingRay::TimeType& time)
             {
                 // Compute the transmission factor between the vertex and the camera.
                 Vector2d sample_position;
@@ -441,12 +445,12 @@ namespace
 
             double vertex_visible_to_camera(
                 const Vector3d&             vertex_position,
-                const double                time,
+                const ShadingRay::TimeType& time,
                 const ShadingRay::DepthType ray_depth,
                 Vector2d&                   sample_position) const
             {
                 // Compute the position of the vertex on the image plane.
-                if (!m_camera.project_point(time, vertex_position, sample_position))
+                if (!m_camera.project_point(time.m_absolute, vertex_position, sample_position))
                     return 0.0;
 
                 // Reject vertices that don't belong on the image plane of the camera.
@@ -524,7 +528,9 @@ namespace
         uint64                          m_path_count;
         Population<uint64>              m_path_length;
 
-        const double                    m_ray_dtime;
+        double                          m_shutter_open_time;
+        double                          m_shutter_close_time;
+        double                          m_ray_dtime;
 
         virtual size_t generate_samples(
             const size_t                sequence_index,
@@ -637,11 +643,15 @@ namespace
 
             // Build the light ray.
             sampling_context.split_in_place(1, 1);
+            ShadingRay::TimeType time;
+            time.m_relative = sampling_context.next_double2();
+            time.m_absolute = lerp(m_shutter_open_time, m_shutter_close_time, time.m_relative);
+            time.m_differential = m_ray_dtime;
+
             const ShadingRay light_ray(
                 light_sample.m_point,
                 emission_direction,
-                sampling_context.next_double2(),
-                m_ray_dtime,
+                time,
                 VisibilityFlags::LightRay,
                 0);
 
@@ -710,11 +720,15 @@ namespace
 
             // Build the light ray.
             sampling_context.split_in_place(1, 1);
+            ShadingRay::TimeType time;
+            time.m_relative = sampling_context.next_double2();
+            time.m_absolute = lerp(m_shutter_open_time, m_shutter_close_time, time.m_relative);
+            time.m_differential = m_ray_dtime;
+
             const ShadingRay light_ray(
                 emission_position,
                 emission_direction,
-                sampling_context.next_double2(),
-                m_ray_dtime,
+                time,
                 VisibilityFlags::LightRay,
                 0);
 
@@ -794,11 +808,15 @@ namespace
 
             // Build the light ray.
             sampling_context.split_in_place(1, 1);
+            ShadingRay::TimeType time;
+            time.m_relative = sampling_context.next_double2();
+            time.m_absolute = lerp(m_shutter_open_time, m_shutter_close_time, time.m_relative);
+            time.m_differential = m_ray_dtime;
+
             const ShadingRay light_ray(
                 ray_origin,
                 -outgoing,
-                sampling_context.next_double2(),
-                m_ray_dtime,
+                time,
                 VisibilityFlags::LightRay,
                 0);
 

commit c95837fa3332450fc6b41cc44af63bb1f2c07889

fixed regression in SPPM photon tracing.
regression introduced in 315085c9dbb4b7b44f726e670e6475e52b40e47c ("Take into account shutter times when doing deformation motion blur.").

reported by Haggi.

src/appleseed/renderer/kernel/lighting/sppm/sppmphotontracer.cpp
@@ -391,15 +391,13 @@ namespace
 
             // Build the photon ray.
             child_sampling_context.split_in_place(1, 1);
-            ShadingRay::Time time = ShadingRay::Time::create_with_normalized_time(
-                sampling_context.next_double2(),
-                m_shutter_open_time,
-                m_shutter_close_time);
-
             const ShadingRay ray(
                 light_sample.m_point,
                 emission_direction,
-                time,
+                ShadingRay::Time::create_with_normalized_time(
+                    child_sampling_context.next_double2(),
+                    m_shutter_open_time,
+                    m_shutter_close_time),
                 VisibilityFlags::LightRay,
                 0);
 
@@ -454,15 +452,13 @@ namespace
 
             // Build the photon ray.
             child_sampling_context.split_in_place(1, 1);
-            ShadingRay::Time time = ShadingRay::Time::create_with_normalized_time(
-                sampling_context.next_double2(),
-                m_shutter_open_time,
-                m_shutter_close_time);
-
             const ShadingRay ray(
                 emission_position,
                 emission_direction,
-                time,
+                ShadingRay::Time::create_with_normalized_time(
+                    child_sampling_context.next_double2(),
+                    m_shutter_open_time,
+                    m_shutter_close_time),
                 VisibilityFlags::LightRay,
                 0);
 
@@ -669,15 +665,13 @@ namespace
 
             // Build the photon ray.
             child_sampling_context.split_in_place(1, 1);
-            ShadingRay::Time time = ShadingRay::Time::create_with_normalized_time(
-                sampling_context.next_double2(),
-                m_shutter_open_time,
-                m_shutter_close_time);
-
             const ShadingRay ray(
                 ray_origin,
                 -outgoing,
-                time,
+                ShadingRay::Time::create_with_normalized_time(
+                    child_sampling_context.next_double2(),
+                    m_shutter_open_time,
+                    m_shutter_close_time),
                 VisibilityFlags::LightRay,
                 0);
 