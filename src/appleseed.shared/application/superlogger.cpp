6f0ba41af391fca970227a6c4c1db0099a96498a
Alexey Dokuchaev <danfe@nsu.ru>
2015-09-04 19:40:35 +0800

    Change some preprocessor checks to be more generic to simplify porting
    Particularly, avoid hard-to-keep-exhaustive #ifdef list when a single
    #ifndef would do and use #else; gain initial knowledge of the FreeBSD.

--- src/appleseed.shared/application/superlogger.cpp
+++ src/appleseed.shared/application/superlogger.cpp
@@ -34,7 +34,7 @@
 #include <cstdio>
 
 // Platform headers.
-#if defined __APPLE__ || defined __linux__
+#ifndef _WIN32
 #include <unistd.h>
 #endif
 
@@ -74,7 +74,7 @@ ILogTarget& SuperLogger::get_log_target() const
 
 void SuperLogger::enable_message_coloring()
 {
-#if defined __APPLE__ || defined __linux__
+#ifndef _WIN32
     const int StdErrFileDesc = 2;
     if (!isatty(StdErrFileDesc))
         return;
