appleseedhq/appleseed

commit 87b796984375d28168c2d1f1f7d56370972dbf30

Cache some scene information
In particular, this allows to avoid repeatedly computing the scene's bounding box.

src/appleseed/renderer/kernel/lighting/sppm/sppmpasscallback.cpp
@@ -83,13 +83,14 @@ SPPMPassCallback::SPPMPassCallback(
   , m_pass_number(0)
 {
     // Compute the initial lookup radius.
-    const GAABB3 scene_bbox = scene.compute_bbox();
-    const float scene_diameter =
-        scene_bbox.is_valid()
-            ? static_cast<float>(scene_bbox.diameter())
-            : 0.0f;
+    const Scene::CachedInfo* scene_info = scene.get_cached_info();
+    assert(scene_info);
+    const float diameter = static_cast<float>(scene_info->m_diameter);
     const float diameter_factor = m_params.m_initial_radius_percents / 100.0f;
-    m_initial_lookup_radius = m_lookup_radius = scene_diameter * diameter_factor;
+    m_initial_lookup_radius = diameter * diameter_factor;
+
+    // Start with the initial lookup radius.
+    m_lookup_radius = m_initial_lookup_radius;
 }
 
 void SPPMPassCallback::release()

src/appleseed/renderer/kernel/lighting/lighttracing/lighttracingsamplegenerator.cpp
@@ -206,10 +206,11 @@ namespace
           , m_light_sample_count(0)
           , m_path_count(0)
         {
-            const GAABB3 scene_bbox = m_scene.compute_bbox();
-            m_scene_center = scene_bbox.center();
-            m_scene_radius = scene_bbox.radius();
-            m_safe_scene_diameter = 1.01 * (2.0 * m_scene_radius);
+            const Scene::CachedInfo* scene_info = m_scene.get_cached_info();
+            assert(scene_info);
+            m_scene_center = scene_info->m_center;
+            m_scene_radius = scene_info->m_radius;
+            m_safe_scene_diameter = scene_info->m_safe_diameter;
             m_disk_point_prob = 1.0 / (Pi * m_scene_radius * m_scene_radius);
 
             const Camera* camera = scene.get_camera();
@@ -788,7 +789,7 @@ namespace
             const Basis3d basis(-outgoing);
             const Vector3d ray_origin =
                   m_scene_center
-                - m_safe_scene_diameter * basis.get_normal()
+                - m_safe_scene_diameter * basis.get_normal()    // a safe radius would have been sufficient
                 + p[0] * basis.get_tangent_u() +
                 + p[1] * basis.get_tangent_v();
 

src/appleseed/renderer/kernel/lighting/sppm/sppmphotontracer.cpp
@@ -542,10 +542,11 @@ namespace
           , m_pass_hash(pass_hash)
           , m_abort_switch(abort_switch)
         {
-            const GAABB3 scene_bbox = m_scene.compute_bbox();
-            m_scene_center = scene_bbox.center();
-            m_scene_radius = scene_bbox.radius();
-            m_safe_scene_diameter = 1.01 * (2.0 * m_scene_radius);
+            const Scene::CachedInfo* scene_info = m_scene.get_cached_info();
+            assert(scene_info);
+            m_scene_center = scene_info->m_center;
+            m_scene_radius = scene_info->m_radius;
+            m_safe_scene_diameter = scene_info->m_safe_diameter;
 
             const Camera* camera = scene.get_camera();
             m_shutter_open_time = camera->get_shutter_open_time();

src/appleseed/renderer/modeling/light/directionallight.cpp
@@ -104,10 +104,11 @@ namespace
 
             check_non_zero_emission("irradiance", "irradiance_multiplier");
 
-            const GAABB3 scene_bbox = project.get_scene()->compute_bbox();
-            m_scene_center = scene_bbox.center();
-            m_scene_radius = scene_bbox.radius();
-            m_safe_scene_diameter = 1.01 * (2.0 * m_scene_radius);
+            const Scene::CachedInfo* scene_info = project.get_scene()->get_cached_info();
+            assert(scene_info);
+            m_scene_center = scene_info->m_center;
+            m_scene_radius = scene_info->m_radius;
+            m_safe_scene_diameter = scene_info->m_safe_diameter;
 
             m_inputs.evaluate_uniforms(&m_values);
             m_values.m_irradiance *= static_cast<float>(m_values.m_irradiance_multiplier);

src/appleseed/renderer/modeling/light/sunlight.cpp
@@ -127,10 +127,11 @@ namespace
             // Apply turbidity bias.
             m_values.m_turbidity += BaseTurbidity;
 
-            const GAABB3 scene_bbox = project.get_scene()->compute_bbox();
-            m_scene_center = scene_bbox.center();
-            m_scene_radius = scene_bbox.radius();
-            m_safe_scene_diameter = 1.01 * (2.0 * m_scene_radius);
+            const Scene::CachedInfo* scene_info = project.get_scene()->get_cached_info();
+            assert(scene_info);
+            m_scene_center = scene_info->m_center;
+            m_scene_radius = scene_info->m_radius;
+            m_safe_scene_diameter = scene_info->m_safe_diameter;
 
             precompute_constants();
 

src/appleseed/renderer/modeling/scene/scene.cpp
@@ -95,6 +95,7 @@ Scene::Scene()
   : Entity(g_class_uid)
   , BaseGroup(this)
   , impl(new Impl(this))
+  , m_has_cached_info(false)
 {
     set_name("scene");
 }
@@ -244,6 +245,8 @@ bool Scene::on_frame_begin(
     const Project&          project,
     IAbortSwitch*           abort_switch)
 {
+    cache_scene_info();
+
     bool success = true;
 
     if (impl->m_camera.get())
@@ -279,6 +282,32 @@ void Scene::on_frame_end(const Project& project)
 
     if (impl->m_camera.get())
         impl->m_camera->on_frame_end(project);
+
+    m_has_cached_info = false;
+}
+
+void Scene::cache_scene_info()
+{
+    assert(!m_has_cached_info);
+
+    m_cached_info.m_bbox = compute_bbox();
+
+    if (m_cached_info.m_bbox.is_valid())
+    {
+        m_cached_info.m_center = m_cached_info.m_bbox.center();
+        m_cached_info.m_radius = m_cached_info.m_bbox.radius();
+        m_cached_info.m_diameter = m_cached_info.m_bbox.diameter();
+        m_cached_info.m_safe_diameter = m_cached_info.m_diameter * GScalar(1.01);
+    }
+    else
+    {
+        m_cached_info.m_center = GVector3(0.0);
+        m_cached_info.m_radius = GScalar(0.0);
+        m_cached_info.m_diameter = GScalar(0.0);
+        m_cached_info.m_safe_diameter = GScalar(0.0);
+    }
+
+    m_has_cached_info = true;
 }
 
 

src/appleseed/renderer/modeling/scene/scene.h
@@ -107,22 +107,41 @@ class APPLESEED_DLLSYMBOL Scene
     // Perform post-frame rendering actions.
     void on_frame_end(const Project& project);
 
+    struct CachedInfo
+    {
+        GAABB3      m_bbox;
+        GVector3    m_center;
+        GScalar     m_radius;
+        GScalar     m_diameter;
+        GScalar     m_safe_diameter;
+    };
+
+    // Return information cached by on_frame_begin(), or 0 if they are not available.
+    // Cached info are created by on_frame_begin() and destroyed by on_frame_end().
+    const CachedInfo* get_cached_info() const;
+
   private:
     friend class SceneFactory;
 
     struct Impl;
     Impl* impl;
 
+    bool        m_has_cached_info;
+    CachedInfo  m_cached_info;
+
     // Constructor. Initially, the scene is empty.
     Scene();
 
     // Destructor.
     ~Scene();
+
+    // Cache scene information, which are then available through get_cached_info().
+    void cache_scene_info();
 };
 
 
 //
-// SceneFactory.
+// Scene factory.
 //
 
 class APPLESEED_DLLSYMBOL SceneFactory
@@ -131,6 +150,16 @@ class APPLESEED_DLLSYMBOL SceneFactory
     // Create a new scene.
     static foundation::auto_release_ptr<Scene> create();
 };
+
+
+//
+// Scene class implementation.
+//
+
+inline const Scene::CachedInfo* Scene::get_cached_info() const
+{
+    return m_has_cached_info ? &m_cached_info : 0;
+}
 
 }       // namespace renderer
 

commit 0731a39e30b7136126428d1c0619b63855400703

Fix regression in SPPM render engine
Regression introduced in 87b796984375d28168c2d1f1f7d56370972dbf30.

src/appleseed/renderer/kernel/lighting/sppm/sppmpasscallback.cpp
@@ -83,9 +83,13 @@ SPPMPassCallback::SPPMPassCallback(
   , m_pass_number(0)
 {
     // Compute the initial lookup radius.
-    const float diameter = static_cast<float>(scene.get_render_data().m_diameter);
+    const GAABB3 scene_bbox = scene.compute_bbox();
+    const float scene_diameter =
+        scene_bbox.is_valid()
+            ? static_cast<float>(scene_bbox.diameter())
+            : 0.0f;
     const float diameter_factor = m_params.m_initial_radius_percents / 100.0f;
-    m_initial_lookup_radius = diameter * diameter_factor;
+    m_initial_lookup_radius = scene_diameter * diameter_factor;
 
     // Start with the initial lookup radius.
     m_lookup_radius = m_initial_lookup_radius;