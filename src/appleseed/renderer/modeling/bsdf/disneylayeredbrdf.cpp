60ff8504b36c80fc0db7eef8e2ecbd91e302672f
Francois Beaune <beaune@aist.enst.fr>
2016-07-31 11:36:58 +0200

    Add todo note

--- src/appleseed/renderer/modeling/bsdf/disneylayeredbrdf.cpp
+++ src/appleseed/renderer/modeling/bsdf/disneylayeredbrdf.cpp
@@ -132,6 +132,7 @@ void DisneyLayeredBRDF::evaluate_inputs(
     }
 
     // Colors in SeExpr are always in the sRGB color space.
+    // todo: convert colors earlier so that all math is done in linear space.
     values->m_base_color = srgb_to_linear_rgb(Color3f(base_color));
 
     m_brdf->prepare_inputs(shading_point, values);
