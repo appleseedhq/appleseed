2a39710d73d9cca8adef9dfa4afb0cfd3a96a533
est77 <ramenhdr@gmail.com>
2016-09-06 20:58:41 +0200

    Allow negative values for anisotropy on disney brdf OSL closure

--- src/appleseed/renderer/kernel/shading/closures.cpp
+++ src/appleseed/renderer/kernel/shading/closures.cpp
@@ -303,7 +303,7 @@ namespace
             values->m_metallic = saturate(p->metallic);
             values->m_specular = max(p->specular, 0.0f);
             values->m_specular_tint = saturate(p->specular_tint);
-            values->m_anisotropic = saturate(p->anisotropic);
+            values->m_anisotropic = clamp(p->anisotropic, -1.0f, 1.0f);
             values->m_roughness = clamp(p->roughness, 0.0001f, 1.0f);
             values->m_sheen = saturate(p->sheen);
             values->m_sheen_tint = saturate(p->sheen_tint);
