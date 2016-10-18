appleseedhq/appleseed

commit 2448e8df779504d7dae481a64307d78f10afac14

Automatically call on_frame_end() on entities on which on_frame_begin() was called

src/appleseed/renderer/modeling/bsdf/kelemenbrdf.cpp
@@ -135,8 +135,8 @@ namespace
     {
       public:
         KelemenBRDFImpl(
-            const char*         name,
-            const ParamArray&   params)
+            const char*             name,
+            const ParamArray&       params)
           : BSDF(name, Reflective, ScatteringMode::Diffuse | ScatteringMode::Glossy, params)
         {
             m_inputs.declare("matte_reflectance", InputFormatSpectralReflectance);
@@ -162,11 +162,12 @@ namespace
         }
 
         virtual bool on_frame_begin(
-            const Project&      project,
-            const BaseGroup*    parent,
-            IAbortSwitch*       abort_switch) APPLESEED_OVERRIDE
+            const Project&          project,
+            const BaseGroup*        parent,
+            OnFrameBeginRecorder&   recorder,
+            IAbortSwitch*           abort_switch) APPLESEED_OVERRIDE
         {
-            if (!BSDF::on_frame_begin(project, parent, abort_switch))
+            if (!BSDF::on_frame_begin(project, parent, recorder, abort_switch))
                 return false;
 
             // todo: implement proper error handling.
@@ -204,20 +205,18 @@ namespace
         }
 
         virtual void on_frame_end(
-            const Project&      project,
-            const BaseGroup*    parent) APPLESEED_OVERRIDE
+            const Project&          project,
+            const BaseGroup*        parent) APPLESEED_OVERRIDE
         {
             m_mdf.reset();
-
-            BSDF::on_frame_end(project, parent);
         }
 
         APPLESEED_FORCE_INLINE virtual void sample(
-            SamplingContext&    sampling_context,
-            const void*         data,
-            const bool          adjoint,
-            const bool          cosine_mult,
-            BSDFSample&         sample) const APPLESEED_OVERRIDE
+            SamplingContext&        sampling_context,
+            const void*             data,
+            const bool              adjoint,
+            const bool              cosine_mult,
+            BSDFSample&             sample) const APPLESEED_OVERRIDE
         {
             // Define aliases to match the notations in the paper.
             const Vector3d& V = sample.m_outgoing.get_value();
@@ -337,15 +336,15 @@ namespace
         }
 
         APPLESEED_FORCE_INLINE virtual double evaluate(
-            const void*         data,
-            const bool          adjoint,
-            const bool          cosine_mult,
-            const Vector3d&     geometric_normal,
-            const Basis3d&      shading_basis,
-            const Vector3d&     outgoing,
-            const Vector3d&     incoming,
-            const int           modes,
-            Spectrum&           value) const APPLESEED_OVERRIDE
+            const void*             data,
+            const bool              adjoint,
+            const bool              cosine_mult,
+            const Vector3d&         geometric_normal,
+            const Basis3d&          shading_basis,
+            const Vector3d&         outgoing,
+            const Vector3d&         incoming,
+            const int               modes,
+            Spectrum&               value) const APPLESEED_OVERRIDE
         {
             // Define aliases to match the notations in the paper.
             const Vector3d& V = outgoing;
@@ -420,12 +419,12 @@ namespace
         }
 
         APPLESEED_FORCE_INLINE virtual double evaluate_pdf(
-            const void*         data,
-            const Vector3d&     geometric_normal,
-            const Basis3d&      shading_basis,
-            const Vector3d&     outgoing,
-            const Vector3d&     incoming,
-            const int           modes) const APPLESEED_OVERRIDE
+            const void*             data,
+            const Vector3d&         geometric_normal,
+            const Basis3d&          shading_basis,
+            const Vector3d&         outgoing,
+            const Vector3d&         incoming,
+            const int               modes) const APPLESEED_OVERRIDE
         {
             // Define aliases to match the notations in the paper.
             const Vector3d& V = outgoing;

src/appleseed/renderer/modeling/environment/environment.cpp
@@ -60,8 +60,8 @@ UniqueID Environment::get_class_uid()
 }
 
 Environment::Environment(
-    const char*         name,
-    const ParamArray&   params)
+    const char*             name,
+    const ParamArray&       params)
   : ConnectableEntity(g_class_uid, params)
   , m_environment_edf(0)
   , m_environment_shader(0)
@@ -83,11 +83,12 @@ const char* Environment::get_model() const
 }
 
 bool Environment::on_frame_begin(
-    const Project&      project,
-    const BaseGroup*    parent,
-    IAbortSwitch*       abort_switch)
+    const Project&          project,
+    const BaseGroup*        parent,
+    OnFrameBeginRecorder&   recorder,
+    IAbortSwitch*           abort_switch)
 {
-    if (!ConnectableEntity::on_frame_begin(project, parent, abort_switch))
+    if (!ConnectableEntity::on_frame_begin(project, parent, recorder, abort_switch))
         return false;
 
     m_environment_edf = get_uncached_environment_edf();
@@ -97,13 +98,11 @@ bool Environment::on_frame_begin(
 }
 
 void Environment::on_frame_end(
-    const Project&      project,
-    const BaseGroup*    parent)
+    const Project&          project,
+    const BaseGroup*        parent)
 {
     m_environment_edf = 0;
     m_environment_shader = 0;
-
-    ConnectableEntity::on_frame_end(project, parent);
 }
 
 EnvironmentEDF* Environment::get_uncached_environment_edf() const
@@ -152,8 +151,8 @@ DictionaryArray EnvironmentFactory::get_input_metadata()
 }
 
 auto_release_ptr<Environment> EnvironmentFactory::create(
-    const char*         name,
-    const ParamArray&   params)
+    const char*             name,
+    const ParamArray&       params)
 {
     return auto_release_ptr<Environment>(new Environment(name, params));
 }

src/appleseed/renderer/modeling/environmentedf/oslenvironmentedf.cpp
@@ -98,9 +98,10 @@ namespace
         virtual bool on_frame_begin(
             const Project&          project,
             const BaseGroup*        parent,
+            OnFrameBeginRecorder&   recorder,
             IAbortSwitch*           abort_switch) APPLESEED_OVERRIDE
         {
-            if (!EnvironmentEDF::on_frame_begin(project, parent, abort_switch))
+            if (!EnvironmentEDF::on_frame_begin(project, parent, recorder, abort_switch))
                 return false;
 
             m_shader_group =
@@ -114,8 +115,6 @@ namespace
             const BaseGroup*        parent) APPLESEED_OVERRIDE
         {
             m_shader_group = 0;
-
-            EnvironmentEDF::on_frame_end(project, parent);
         }
 
         virtual void sample(

src/appleseed/renderer/modeling/material/disneymaterial.cpp
@@ -678,12 +678,13 @@ void DisneyMaterial::update_asset_paths(const StringDictionary& mappings)
 bool DisneyMaterial::on_frame_begin(
     const Project&          project,
     const BaseGroup*        parent,
+    OnFrameBeginRecorder&   recorder,
     IAbortSwitch*           abort_switch)
 {
-    if (!Material::on_frame_begin(project, parent, abort_switch))
+    if (!Material::on_frame_begin(project, parent, recorder, abort_switch))
         return false;
 
-    if (!impl->m_brdf->on_frame_begin(project, parent, abort_switch))
+    if (!impl->m_brdf->on_frame_begin(project, parent, recorder, abort_switch))
         return false;
 
     const EntityDefMessageContext context("material", this);
@@ -700,10 +701,6 @@ void DisneyMaterial::on_frame_end(
 {
     impl->clear_per_thread_layers();
     impl->m_layers.clear();
-
-    impl->m_brdf->on_frame_end(project, parent);
-
-    Material::on_frame_end(project, parent);
 }
 
 void DisneyMaterial::add_layer(Dictionary layer_values)

src/appleseed/renderer/modeling/material/material.cpp
@@ -78,8 +78,8 @@ UniqueID Material::get_class_uid()
 }
 
 Material::Material(
-    const char*         name,
-    const ParamArray&   params)
+    const char*             name,
+    const ParamArray&       params)
   : ConnectableEntity(g_class_uid, params)
   , m_shade_alpha_cutouts(params.get_optional<bool>("shade_alpha_cutouts", false))
   , m_has_render_data(false)
@@ -149,13 +149,14 @@ const ShaderGroup* Material::get_uncached_osl_surface() const
 #endif
 
 bool Material::on_frame_begin(
-    const Project&      project,
-    const BaseGroup*    parent,
-    IAbortSwitch*       abort_switch)
+    const Project&          project,
+    const BaseGroup*        parent,
+    OnFrameBeginRecorder&   recorder,
+    IAbortSwitch*           abort_switch)
 {
     assert(!m_has_render_data);
 
-    if (!ConnectableEntity::on_frame_begin(project, parent, abort_switch))
+    if (!ConnectableEntity::on_frame_begin(project, parent, recorder, abort_switch))
         return false;
 
     m_render_data.m_surface_shader = get_uncached_surface_shader();
@@ -175,16 +176,14 @@ bool Material::on_frame_begin(
 }
 
 void Material::on_frame_end(
-    const Project&      project,
-    const BaseGroup*    parent)
+    const Project&          project,
+    const BaseGroup*        parent)
 {
     if (m_has_render_data)
     {
         delete m_render_data.m_basis_modifier;
         m_has_render_data = false;
     }
-
-    ConnectableEntity::on_frame_end(project, parent);
 }
 
 bool Material::has_emission() const

src/appleseed/renderer/modeling/object/meshobject.cpp
@@ -102,8 +102,8 @@ struct MeshObject::Impl
 };
 
 MeshObject::MeshObject(
-    const char*         name,
-    const ParamArray&   params)
+    const char*             name,
+    const ParamArray&       params)
   : Object(name, params)
   , impl(new Impl())
 {
@@ -126,11 +126,12 @@ const char* MeshObject::get_model() const
 }
 
 bool MeshObject::on_frame_begin(
-    const Project&      project,
-    const BaseGroup*    parent,
-    IAbortSwitch*       abort_switch)
+    const Project&          project,
+    const BaseGroup*        parent,
+    OnFrameBeginRecorder&   recorder,
+    IAbortSwitch*           abort_switch)
 {
-    if (!Object::on_frame_begin(project, parent, abort_switch))
+    if (!Object::on_frame_begin(project, parent, recorder, abort_switch))
         return false;
 
     m_alpha_map = get_uncached_alpha_map();
@@ -140,13 +141,11 @@ bool MeshObject::on_frame_begin(
 }
 
 void MeshObject::on_frame_end(
-    const Project&      project,
-    const BaseGroup*    parent)
+    const Project&          project,
+    const BaseGroup*        parent)
 {
     m_alpha_map = 0;
     m_shade_alpha_cutouts = false;
-
-    Object::on_frame_end(project, parent);
 }
 
 bool MeshObject::has_alpha_map() const
@@ -308,16 +307,16 @@ size_t MeshObject::get_motion_segment_count() const
 }
 
 void MeshObject::set_vertex_pose(
-    const size_t        vertex_index,
-    const size_t        motion_segment_index,
-    const GVector3&     vertex)
+    const size_t            vertex_index,
+    const size_t            motion_segment_index,
+    const GVector3&         vertex)
 {
     impl->m_tess.set_vertex_pose(vertex_index, motion_segment_index, vertex);
 }
 
 GVector3 MeshObject::get_vertex_pose(
-    const size_t        vertex_index,
-    const size_t        motion_segment_index) const
+    const size_t            vertex_index,
+    const size_t            motion_segment_index) const
 {
     return impl->m_tess.get_vertex_pose(vertex_index, motion_segment_index);
 }
@@ -328,16 +327,16 @@ void MeshObject::clear_vertex_poses()
 }
 
 void MeshObject::set_vertex_normal_pose(
-    const size_t        normal_index,
-    const size_t        motion_segment_index,
-    const GVector3&     normal)
+    const size_t            normal_index,
+    const size_t            motion_segment_index,
+    const GVector3&         normal)
 {
     impl->m_tess.set_vertex_normal_pose(normal_index, motion_segment_index, normal);
 }
 
 GVector3 MeshObject::get_vertex_normal_pose(
-    const size_t        normal_index,
-    const size_t        motion_segment_index) const
+    const size_t            normal_index,
+    const size_t            motion_segment_index) const
 {
     return impl->m_tess.get_vertex_normal_pose(normal_index, motion_segment_index);
 }
@@ -348,16 +347,16 @@ void MeshObject::clear_vertex_normal_poses()
 }
 
 void MeshObject::set_vertex_tangent_pose(
-    const size_t        tangent_index,
-    const size_t        motion_segment_index,
-    const GVector3&     tangent)
+    const size_t            tangent_index,
+    const size_t            motion_segment_index,
+    const GVector3&         tangent)
 {
     impl->m_tess.set_vertex_tangent_pose(tangent_index, motion_segment_index, tangent);
 }
 
 GVector3 MeshObject::get_vertex_tangent_pose(
-    const size_t        tangent_index,
-    const size_t        motion_segment_index) const
+    const size_t            tangent_index,
+    const size_t            motion_segment_index) const
 {
     return impl->m_tess.get_vertex_tangent_pose(tangent_index, motion_segment_index);
 }
@@ -424,8 +423,8 @@ const char* MeshObjectFactory::get_model()
 }
 
 auto_release_ptr<MeshObject> MeshObjectFactory::create(
-    const char*         name,
-    const ParamArray&   params)
+    const char*             name,
+    const ParamArray&       params)
 {
     return auto_release_ptr<MeshObject>(new MeshObject(name, params));
 }

commit 89196f12d3a60808bb417762b9d08b324ac10543

Fix regressions introduced in 2448e8df779504d7dae481a64307d78f10afac14

src/appleseed/renderer/modeling/bsdf/kelemenbrdf.cpp
@@ -209,6 +209,8 @@ namespace
             const BaseGroup*        parent) APPLESEED_OVERRIDE
         {
             m_mdf.reset();
+
+            BSDF::on_frame_end(project, parent);
         }
 
         APPLESEED_FORCE_INLINE virtual void sample(

src/appleseed/renderer/modeling/environment/environment.cpp
@@ -103,6 +103,8 @@ void Environment::on_frame_end(
 {
     m_environment_edf = 0;
     m_environment_shader = 0;
+
+    ConnectableEntity::on_frame_end(project, parent);
 }
 
 EnvironmentEDF* Environment::get_uncached_environment_edf() const

src/appleseed/renderer/modeling/environmentedf/oslenvironmentedf.cpp
@@ -115,6 +115,8 @@ namespace
             const BaseGroup*        parent) APPLESEED_OVERRIDE
         {
             m_shader_group = 0;
+
+            EnvironmentEDF::on_frame_end(project, parent);
         }
 
         virtual void sample(

src/appleseed/renderer/modeling/material/disneymaterial.cpp
@@ -701,6 +701,8 @@ void DisneyMaterial::on_frame_end(
 {
     impl->clear_per_thread_layers();
     impl->m_layers.clear();
+
+    Material::on_frame_end(project, parent);
 }
 
 void DisneyMaterial::add_layer(Dictionary layer_values)

src/appleseed/renderer/modeling/material/material.cpp
@@ -184,6 +184,8 @@ void Material::on_frame_end(
         delete m_render_data.m_basis_modifier;
         m_has_render_data = false;
     }
+
+    ConnectableEntity::on_frame_end(project, parent);
 }
 
 bool Material::has_emission() const

src/appleseed/renderer/modeling/object/meshobject.cpp
@@ -146,6 +146,8 @@ void MeshObject::on_frame_end(
 {
     m_alpha_map = 0;
     m_shade_alpha_cutouts = false;
+
+    Object::on_frame_end(project, parent);
 }
 
 bool MeshObject::has_alpha_map() const