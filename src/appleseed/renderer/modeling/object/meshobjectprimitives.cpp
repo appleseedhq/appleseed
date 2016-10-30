appleseedhq/appleseed

commit 22aa09e52903b68e9e0b4360fa43994eae87e0c6

Refactored mesh primitives. Added disk primitive.

src/appleseed/renderer/modeling/object/meshobjectprimitives.cpp
@@ -38,6 +38,7 @@
 
 // appleseed.foundation headers.
 #include "foundation/math/scalar.h"
+#include "foundation/math/transform.h"
 #include "foundation/math/vector.h"
 #include "foundation/platform/types.h"
 
@@ -53,32 +54,18 @@ namespace renderer
 
 namespace
 {
-    class ParametricSphere
+    class ParametricSurfaceBase
     {
       public:
-        explicit ParametricSphere(const float radius)
-          : m_radius(radius)
+
+        const Transformf& transform() const
         {
+            return Transformf::identity();
         }
-    
-        GVector3 evaluate(const float u, const float v) const
-        {
-            const float theta = static_cast<float>(TwoPi) * u;
-            const float phi = static_cast<float>(Pi) * v;
-            const float sin_phi = sin(phi);
-    
-            return GVector3(
-                m_radius * cos(theta) * sin_phi,
-               -m_radius * cos(phi),
-                m_radius * sin(theta) * sin_phi
-            );
-        }
-    
-      private:
-        const float m_radius;
     };
-    
+
     class ParametricGrid
+      : public ParametricSurfaceBase
     {
       public:
         ParametricGrid(const float width, const float height)
@@ -86,7 +73,7 @@ namespace
           , m_height(height)
         {
         }
-    
+
         GVector3 evaluate(const float u, const float v) const
         {
             return GVector3(
@@ -94,128 +81,246 @@ namespace
                 0.0f,
                 m_height * (v - 0.5f));
         }
-    
+
+        GVector3 evaluate_du(const float u, const float v) const
+        {
+            return GVector3(1.0f, 0.0f, 0.0f);
+        }
+
+        GVector3 evaluate_normal(const float u, const float v) const
+        {
+            return GVector3(0.0f, 1.0f, 0.0f);
+        }
+
       private:
         const float m_width;
         const float m_height;
     };
-    
-    class ParametricTorus
+
+    class ParametricDisk
+      : public ParametricSurfaceBase
     {
       public:
-        ParametricTorus(const float major_radius, const float minor_radius)
-          : m_major_radius(major_radius)
-          , m_minor_radius(minor_radius)
+        explicit ParametricDisk(const float radius)
+          : m_radius(radius)
         {
         }
-    
+
         GVector3 evaluate(const float u, const float v) const
         {
-            const float theta = static_cast<float>(TwoPi) * u;
-            const float phi = static_cast<float>(TwoPi) * v;
+            const float r = u * 2.0f * m_radius;
+            const float theta = static_cast<float>(TwoPi) * v;
+            return GVector3(r * cos(theta), 0.0f, r * sin(theta));
+        }
+
+        GVector3 evaluate_du(const float u, const float v) const
+        {
+            GVector3 dpdu = evaluate(u, v);
+            const float dpdu_norm = norm(dpdu);
+
+            // Return an arbitrary tangent at center of the disk.
+            if (dpdu_norm == 0.0f)
+                return GVector3(-1.0f, 0.0f, 0.0f);
+
+            return dpdu / dpdu_norm;
+        }
+
+        GVector3 evaluate_normal(const float u, const float v) const
+        {
+            return GVector3(0.0f, 1.0f, 0.0f);
+        }
+
+      private:
+        const float m_radius;
+    };
+
+    class ParametricSphere
+      : public ParametricSurfaceBase
+    {
+      public:
+        ParametricSphere(const float radius, const size_t resolution_u)
+          : m_radius(radius)
+          , m_h(1.0f / resolution_u * 4.0f)
+          , m_transform(Matrix4f::make_rotation_x(HalfPi))
+        {
+        }
+
+        const Transformf& transform() const
+        {
+            return m_transform;
+        }
+
+        GVector3 evaluate(const float u, const float v) const
+        {
+            const float theta = static_cast<float>(TwoPi) * (1.0f - u);
+            const float phi = static_cast<float>(Pi) * v;
+            const float sin_phi = sin(phi);
+
+            return GVector3(
+                m_radius * cos(theta) * sin_phi,
+                m_radius * sin(theta) * sin_phi,
+                m_radius * cos(phi));
+        }
+
+        GVector3 evaluate_du(const float u, const float v) const
+        {
+            const GVector3 p = evaluate(u, v);
+
+            // Compute u tangent using forward differencing.
+            GVector3 dpdu = evaluate(u + m_h, v) - p;
+            const float dpdu_norm = norm(dpdu);
+
+            // Return an arbitrary tangent at the poles.
+            if (dpdu_norm == 0.0f)
+                return GVector3(-1.0f, 0.0f, 0.0f);
+
+            return dpdu / dpdu_norm;
+        }
+
+        GVector3 evaluate_normal(const float u, const float v) const
+        {
+            return normalize(evaluate(u, v));
+        }
+
+      private:
+        const float         m_radius;
+        const float         m_h;
+        const Transformf    m_transform;
+    };
+
+    class ParametricTorus
+      : public ParametricSurfaceBase
+    {
+      public:
+        ParametricTorus(
+            const float major_radius,
+            const float minor_radius,
+            const size_t resolution_u,
+            const size_t resolution_v)
+          : m_major_radius(major_radius)
+          , m_minor_radius(minor_radius)
+          , m_hu(1.0f / (resolution_u * 4.0f))
+          , m_hv(1.0f / (resolution_v * 4.0f))
+          , m_transform(Matrix4f::make_rotation_x(HalfPi))
+        {
+        }
+
+        const Transformf& transform() const
+        {
+            return m_transform;
+        }
+
+        GVector3 evaluate(const float u, const float v) const
+        {
+            const float theta = static_cast<float>(TwoPi) * (1.0f - u);
+            const float phi = static_cast<float>(TwoPi) * (1.0f - v);
             const float cos_phi = cos(phi);
-    
+
             return GVector3(
                 (m_major_radius + m_minor_radius * cos_phi) * cos(theta),
-                 m_minor_radius * sin(phi),
-                (m_major_radius + m_minor_radius * cos_phi) * sin(theta));
+                (m_major_radius + m_minor_radius * cos_phi) * sin(theta),
+                m_minor_radius * sin(phi));
         }
-    
+
+        GVector3 evaluate_du(const float u, const float v) const
+        {
+            // Compute u tangent using forward differencing.
+            const GVector3 p = evaluate(u, v);
+            GVector3 dpdu = evaluate(u + m_hu, v) - p;
+
+            return normalize(dpdu);
+        }
+
+        GVector3 evaluate_normal(const float u, const float v) const
+        {
+            // Compute tangents using forward differencing.
+            const GVector3 p = evaluate(u, v);
+            GVector3 dpdu = evaluate(u + m_hu, v) - p;
+            GVector3 dpdv = evaluate(u, v + m_hv) - p;
+
+            // Compute normal.
+            return normalize(cross(dpdu, dpdv));
+        }
+
       private:
-        const float m_major_radius;
-        const float m_minor_radius;
+        const float         m_major_radius;
+        const float         m_minor_radius;
+        const float         m_hu, m_hv;
+        const Transformf    m_transform;
     };
-    
+
     template <typename ParametricSurface>
-    void create_vertices(MeshObject& mesh, ParametricSurface surface, const size_t resolution_u, const size_t resolution_v)
+    void create_vertices(
+        MeshObject&         mesh,
+        ParametricSurface   surface,
+        const size_t        resolution_u,
+        const size_t        resolution_v)
     {
         const size_t num_points = resolution_u * resolution_v;
-    
+
         mesh.reserve_vertices(num_points);
         mesh.reserve_vertex_normals(num_points);
         mesh.reserve_vertex_tangents(num_points);
         mesh.reserve_tex_coords(num_points);
-    
-        // A small step used to calculate the u and v derivatives using forward differencing.
-        const float h = 1.0f / (max(resolution_u, resolution_v) * 4.0f);
-    
+
+        const Transformf& transform = surface.transform();
+
         for (size_t j = 0; j < resolution_v; ++j)
         {
             const float v = fit<size_t, float>(j, 0, resolution_v - 1, 0.0f, 1.0f);
+
             for (size_t i = 0; i < resolution_u; ++i)
             {
                 const float u = fit<size_t, float>(i, 0, resolution_u - 1, 0.0f, 1.0f);
+
+                mesh.push_tex_coords(GVector2(u, v));
+
                 const GVector3 p = surface.evaluate(u, v);
-    
-                // Compute u tangent using forward differencing.
-                GVector3 dpdu = surface.evaluate(u + h, v) - p;
-                const float dpdu_norm = norm(dpdu);
-    
-                if (dpdu_norm == 0.0f)
-                {
-                    // If the u tangent is zero (surface pole), approximate it using a nearby point.
-                    dpdu = normalize(surface.evaluate(u + h, v + h) - p);
-                }
-                else
-                    dpdu /= dpdu_norm;
-    
-                // Compute v tangent using forward differencing.
-                GVector3 dpdv = normalize(surface.evaluate(u, v + h) - p);
-                const float dpdv_norm = norm(dpdv);
-    
-                if (dpdv_norm == 0.0f)
-                {
-                    // If the v tangent is zero (surface pole), approximate it using a nearby point.
-                    dpdu = normalize(surface.evaluate(u + h, v + h) - p);
-                }
-                else
-                    dpdv /= dpdv_norm;
-    
-                const GVector3 n = normalize(cross(dpdv, dpdu));
-    
-                mesh.push_vertex(p);
-                mesh.push_vertex_normal(n);
-                mesh.push_vertex_tangent(dpdu);
-                mesh.push_tex_coords(GVector2(1.0f - u, v));
+                mesh.push_vertex(transform.point_to_parent(p));
+
+                GVector3 dpdu = surface.evaluate_du(u, v);
+                mesh.push_vertex_tangent(transform.vector_to_parent(dpdu));
+
+                const GVector3 n = surface.evaluate_normal(u, v);
+                mesh.push_vertex_normal(transform.normal_to_parent(n));
             }
         }
     }
-    
+
     size_t convert_to_index(const size_t resolution_u, const size_t i, const size_t j)
     {
         return resolution_u * j + i;
     }
-    
-    void create_triangles(MeshObject& mesh, const size_t resolution_u, const size_t resolution_v)
+
+    void create_triangles(
+        MeshObject&     mesh,
+        const size_t    resolution_u,
+        const size_t    resolution_v)
     {
         mesh.reserve_triangles(2 * (resolution_u - 1) * (resolution_v - 1));
-        for (size_t j = 0; j < resolution_v - 1; ++j)
+        for (size_t j = 0, je = resolution_v - 1; j < je; ++j)
         {
-            for (size_t i = 0; i < resolution_u - 1; ++i)
+            for (size_t i = 0, ie = resolution_u - 1; i < ie; ++i)
             {
                 const size_t v0 = convert_to_index(resolution_u, i    , j);
                 const size_t v1 = convert_to_index(resolution_u, i + 1, j);
                 const size_t v2 = convert_to_index(resolution_u, i + 1, j + 1);
                 const size_t v3 = convert_to_index(resolution_u, i    , j + 1);
-    
+
                 mesh.push_triangle(
-                    Triangle(
-                        v3, v1, v0,
-                        v3, v1, v0,
-                        v3, v1, v0,
-                        0));
+                    Triangle(v3, v1, v0, v3, v1, v0, v3, v1, v0, 0));
                 mesh.push_triangle(
-                    Triangle(
-                        v3, v2, v1,
-                        v3, v2, v1,
-                        v3, v2, v1,
-                        0));
+                    Triangle(v3, v2, v1, v3, v2, v1, v3, v2, v1, 0));
             }
         }
     }
-    
+
     template <typename ParametricSurface>
-    void create_primitive(MeshObject& mesh, const ParametricSurface& surface, const size_t resolution_u, const size_t resolution_v)
+    void create_primitive(
+        MeshObject&                 mesh,
+        const ParametricSurface&    surface,
+        const size_t                resolution_u,
+        const size_t                resolution_v)
     {
         create_vertices(mesh, surface, resolution_u, resolution_v);
         create_triangles(mesh, resolution_u, resolution_v);
@@ -245,8 +350,19 @@ auto_release_ptr<MeshObject> create_primitive_mesh(const char* name, const Param
             RENDERER_LOG_ERROR("radius must be greater than zero.");
             return auto_release_ptr<MeshObject>();
         }
-        const ParametricSphere sphere(radius);
+        const ParametricSphere sphere(radius, resolution_u);
         create_primitive(*mesh, sphere, resolution_u, resolution_v);
+    }
+    else if (strcmp(primitive_type, "disk") == 0)
+    {
+        const float radius = params.get_optional<float>("disk", 1.0);
+        if (radius <= 0.0)
+        {
+            RENDERER_LOG_ERROR("radius must be greater than zero.");
+            return auto_release_ptr<MeshObject>();
+        }
+        const ParametricDisk disk(radius);
+        create_primitive(*mesh, disk, resolution_u, resolution_v);
     }
     else if (strcmp(primitive_type, "grid") == 0)
     {
@@ -269,7 +385,7 @@ auto_release_ptr<MeshObject> create_primitive_mesh(const char* name, const Param
             RENDERER_LOG_ERROR("torus radii must be greater than zero.");
             return auto_release_ptr<MeshObject>();
         }
-        const ParametricTorus torus(major_radius, minor_radius);
+        const ParametricTorus torus(major_radius, minor_radius, resolution_u, resolution_v);
         create_primitive(*mesh, torus, resolution_u, resolution_v);
     }
     else

sandbox/tests/test scenes/primitive mesh/01 - sphere.appleseed
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<!-- File generated by appleseed.dll version 1.4.0-beta (SSE 4.2). -->
+<!-- File generated by appleseed.so version 1.5.0-beta (SSE 4.2). -->
 <project format_revision="11">
     <search_paths>
         <search_path>
@@ -8,28 +8,20 @@
     </search_paths>
     <scene>
         <camera name="camera" model="pinhole_camera">
-            <parameter name="controller_target" value="0 0 0" />
+            <parameter name="controller_target" value="-0.0177106 0.0112301 0.00217694" />
             <parameter name="film_dimensions" value="0.024892 0.018669" />
             <parameter name="focal_length" value="0.035" />
             <transform time="0">
                 <matrix>
-                    0.368893078491291 0.016867214764423 0.929318779379449 1.281258439174482
-                    0.000000000000000 0.999835327892967 -0.018147096108279 -0.025019531027630
-                    -0.929471837465352 0.006694338149061 0.368832332090786 0.508511770791162
+                    0.105598935652650 -0.647018810762293 0.755126163835405 0.740576128575671
+                    0.000000000000000 0.759371963106365 0.650656761778424 0.664610188043734
+                    -0.994408801644991 -0.068708661519002 0.080188871068496 0.082701441744182
                     0.000000000000000 0.000000000000000 0.000000000000000 1.000000000000000
                 </matrix>
             </transform>
         </camera>
         <environment_edf name="environment_edf1" model="constant_environment_edf">
-            <parameter name="radiance" value="0.059" />
-            <transform time="0">
-                <matrix>
-                    1.000000000000000 0.000000000000000 0.000000000000000 0.000000000000000
-                    0.000000000000000 1.000000000000000 0.000000000000000 0.000000000000000
-                    0.000000000000000 0.000000000000000 1.000000000000000 0.000000000000000
-                    0.000000000000000 0.000000000000000 0.000000000000000 1.000000000000000
-                </matrix>
-            </transform>
+            <parameter name="radiance" value="0.05" />
         </environment_edf>
         <environment name="environment" model="generic_environment">
             <parameter name="environment_edf" value="environment_edf1" />
@@ -51,7 +43,7 @@
             </texture_instance>
             <bsdf name="bsdf1" model="lambertian_brdf">
                 <parameter name="reflectance" value="UVCheckerMap03-512_inst" />
-                <parameter name="reflectance_multiplier" value="1.0" />
+                <parameter name="reflectance_multiplier" value="1" />
             </bsdf>
             <material name="material1" model="generic_material">
                 <parameter name="bsdf" value="bsdf1" />
@@ -64,42 +56,20 @@
                 <parameter name="cast_indirect_light" value="true" />
                 <parameter name="exposure" value="0.0" />
                 <parameter name="importance_multiplier" value="1.0" />
-                <parameter name="irradiance" value="1.0" />
-                <parameter name="irradiance_multiplier" value="1.0" />
-                <transform>
-                    <matrix>
-                        1.000000000000000 0.000000000000000 0.000000000000000 0.000000000000000
-                        0.000000000000000 1.000000000000000 0.000000000000000 0.000000000000000
-                        0.000000000000000 0.000000000000000 1.000000000000000 0.000000000000000
-                        0.000000000000000 0.000000000000000 0.000000000000000 1.000000000000000
-                    </matrix>
-                </transform>
+                <parameter name="irradiance" value="1" />
+                <parameter name="irradiance_multiplier" value="2" />
             </light>
             <object name="ground_sphere" model="mesh_object">
                 <parameter name="primitive" value="sphere" />
                 <parameter name="radius" value="0.2" />
+                <parameter name="resolution_u" value="48" />
+                <parameter name="resolution_v" value="96" />
             </object>
             <object_instance name="ground_sphere_inst" object="ground_sphere">
-                <transform>
-                    <matrix>
-                        1.000000000000000 0.000000000000000 0.000000000000000 0.000000000000000
-                        0.000000000000000 1.000000000000000 0.000000000000000 0.000000000000000
-                        0.000000000000000 0.000000000000000 1.000000000000000 0.000000000000000
-                        0.000000000000000 0.000000000000000 0.000000000000000 1.000000000000000
-                    </matrix>
-                </transform>
                 <assign_material slot="default" side="front" material="material1" />
             </object_instance>
         </assembly>
         <assembly_instance name="assembly_inst" assembly="assembly">
-            <transform time="0">
-                <matrix>
-                    1.000000000000000 0.000000000000000 0.000000000000000 0.000000000000000
-                    0.000000000000000 1.000000000000000 0.000000000000000 0.000000000000000
-                    0.000000000000000 0.000000000000000 1.000000000000000 0.000000000000000
-                    0.000000000000000 0.000000000000000 0.000000000000000 1.000000000000000
-                </matrix>
-            </transform>
         </assembly_instance>
     </scene>
     <output>

sandbox/tests/test scenes/primitive mesh/03 - torus.appleseed
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<!-- File generated by appleseed.dll version 1.4.0-beta (SSE 4.2). -->
+<!-- File generated by appleseed.so version 1.5.0-beta (SSE 4.2). -->
 <project format_revision="11">
     <search_paths>
         <search_path>
@@ -8,14 +8,14 @@
     </search_paths>
     <scene>
         <camera name="camera" model="pinhole_camera">
-            <parameter name="controller_target" value="0.0952523 0.280562 -0.0138497" />
+            <parameter name="controller_target" value="0.295173 -0.259092 0.200128" />
             <parameter name="film_dimensions" value="0.024892 0.018669" />
             <parameter name="focal_length" value="0.035" />
             <transform time="0">
                 <matrix>
-                    0.748900115327495 -0.243106577927761 0.616480177321800 16.625941354458984
-                    0.000000000000000 0.930279298622774 0.366852049951912 10.117564237259591
-                    -0.662682893443361 -0.274735542517114 0.696686274025376 18.667536267428144
+                    0.765520985000058 -0.339711675135209 0.546418886296375 9.226953758210659
+                    0.000000000000000 0.849253351177511 0.527985554266182 8.371376676383358
+                    -0.643410927420837 -0.404184021567649 0.650121261908008 10.827031539197508
                     0.000000000000000 0.000000000000000 0.000000000000000 1.000000000000000
                 </matrix>
             </transform>
@@ -30,14 +30,6 @@
             <parameter name="sun_theta" value="61.034" />
             <parameter name="turbidity" value="1.0" />
             <parameter name="turbidity_multiplier" value="2.0" />
-            <transform time="0">
-                <matrix>
-                    1.000000000000000 0.000000000000000 0.000000000000000 0.000000000000000
-                    0.000000000000000 1.000000000000000 0.000000000000000 0.000000000000000
-                    0.000000000000000 0.000000000000000 1.000000000000000 0.000000000000000
-                    0.000000000000000 0.000000000000000 0.000000000000000 1.000000000000000
-                </matrix>
-            </transform>
         </environment_edf>
         <environment name="environment" model="generic_environment">
             <parameter name="environment_edf" value="environment_edf1" />
@@ -50,15 +42,15 @@
             <texture_instance name="UVCheckerMap03-512_inst" texture="UVCheckerMap03-512">
                 <transform>
                     <matrix>
-                        0.100000000000000 0.000000000000000 0.000000000000000 0.000000000000000
-                        0.000000000000000 0.400000000000000 0.000000000000000 0.000000000000000
-                        0.000000000000000 0.000000000000000 0.400000000000000 0.000000000000000
+                        0.250000000000000 0.000000000000000 0.000000000000000 0.000000000000000
+                        0.000000000000000 1.000000000000000 0.000000000000000 0.000000000000000
+                        0.000000000000000 0.000000000000000 1.000000000000000 0.000000000000000
                         0.000000000000000 0.000000000000000 0.000000000000000 1.000000000000000
                     </matrix>
                 </transform>
             </texture_instance>
             <bsdf name="bsdf1" model="disney_brdf">
-                <parameter name="anisotropic" value="1" />
+                <parameter name="anisotropic" value="0.75" />
                 <parameter name="base_color" value="UVCheckerMap03-512_inst" />
                 <parameter name="clearcoat" value="0.0" />
                 <parameter name="clearcoat_gloss" value="1.0" />
@@ -98,28 +90,13 @@
                 <parameter name="primitive" value="torus" />
                 <parameter name="resolution_u" value="64" />
                 <parameter name="resolution_v" value="128" />
+                <parameter name="tangents" value="true" />
             </object>
             <object_instance name="ground_torus_inst" object="ground_torus">
-                <transform>
-                    <matrix>
-                        1.000000000000000 0.000000000000000 0.000000000000000 0.000000000000000
-                        0.000000000000000 1.000000000000000 0.000000000000000 0.000000000000000
-                        0.000000000000000 0.000000000000000 1.000000000000000 0.000000000000000
-                        0.000000000000000 0.000000000000000 0.000000000000000 1.000000000000000
-                    </matrix>
-                </transform>
                 <assign_material slot="default" side="front" material="material1" />
             </object_instance>
         </assembly>
         <assembly_instance name="assembly_inst" assembly="assembly">
-            <transform time="0">
-                <matrix>
-                    1.000000000000000 0.000000000000000 0.000000000000000 0.000000000000000
-                    0.000000000000000 1.000000000000000 0.000000000000000 0.000000000000000
-                    0.000000000000000 0.000000000000000 1.000000000000000 0.000000000000000
-                    0.000000000000000 0.000000000000000 0.000000000000000 1.000000000000000
-                </matrix>
-            </transform>
         </assembly_instance>
     </scene>
     <output>

commit bfba1a7fcd9a9d2af729d0ba929b84d9d90bca82

Fix flipped V coordinates in grid primitive
Regression introduced in 22aa09e52903b68e9e0b4360fa43994eae87e0c6 ("Refactored mesh primitives. Added disk primitive").

src/appleseed/renderer/modeling/object/meshobjectprimitives.cpp
@@ -273,7 +273,7 @@ namespace
             {
                 const float u = fit<size_t, float>(i, 0, resolution_u - 1, 0.0f, 1.0f);
 
-                mesh.push_tex_coords(GVector2(u, v));
+                mesh.push_tex_coords(GVector2(u, 1.0f - v));
 
                 const GVector3 p = surface.evaluate(u, v);
                 mesh.push_vertex(transform.point_to_parent(p));

sandbox/tests/test scenes/primitive mesh/02 - plane.appleseed
@@ -15,7 +15,7 @@
                 <matrix>
                     1.000000000000000 0.000000000000000 0.000000000000000 0.000000000000000
                     0.000000000000000 0.000000000000000 1.000000000000000 3.500000000000000
-                    0.000000000000000 1.000000000000000 0.000000000000000 0.000000000000000
+                    0.000000000000000 -1.000000000000000 0.000000000000000 0.000000000000000
                     0.000000000000000 0.000000000000000 0.000000000000000 1.000000000000000
                 </matrix>
             </transform>