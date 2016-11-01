appleseedhq/appleseed

commit bb19e1c09125dda0d7ff1fa0670378d8082eeec1

Decouple renderer::BSDFSample from renderer::ShadingPoint

src/appleseed/renderer/modeling/bsdf/ashikhminbrdf.cpp
@@ -108,7 +108,7 @@ namespace
             BSDFSample&         sample) const APPLESEED_OVERRIDE
         {
             // No reflection below the shading surface.
-            const Vector3d& shading_normal = sample.get_shading_normal();
+            const Vector3d& shading_normal = sample.m_shading_normal;
             const double cos_on = dot(sample.m_outgoing.get_value(), shading_normal);
             if (cos_on < 0.0)
                 return;
@@ -141,15 +141,15 @@ namespace
                 const Vector3d wi = sample_hemisphere_cosine(Vector2d(s[0], s[1]));
 
                 // Transform the incoming direction to parent space.
-                incoming = sample.get_shading_basis().transform_to_parent(wi);
+                incoming = sample.m_shading_basis.transform_to_parent(wi);
 
                 // Compute the halfway vector in world space.
                 h = normalize(incoming + sample.m_outgoing.get_value());
 
                 // Compute the glossy exponent, needed to evaluate the PDF.
-                const double cos_hn = dot(h, sample.get_shading_normal());
-                const double cos_hu = dot(h, sample.get_shading_basis().get_tangent_u());
-                const double cos_hv = dot(h, sample.get_shading_basis().get_tangent_v());
+                const double cos_hn = dot(h, sample.m_shading_normal);
+                const double cos_hu = dot(h, sample.m_shading_basis.get_tangent_u());
+                const double cos_hv = dot(h, sample.m_shading_basis.get_tangent_v());
                 const double exp_den = 1.0 - cos_hn * cos_hn;
                 const double exp_u = values->m_nu * cos_hu * cos_hu;
                 const double exp_v = values->m_nv * cos_hv * cos_hv;
@@ -187,13 +187,13 @@ namespace
                 const double sin_theta = sqrt(1.0 - cos_theta * cos_theta);
 
                 // Compute the halfway vector in world space.
-                h = sample.get_shading_basis().transform_to_parent(
+                h = sample.m_shading_basis.transform_to_parent(
                     Vector3d::make_unit_vector(cos_theta, sin_theta, cos_phi, sin_phi));
 
                 // Compute the incoming direction in world space.
                 incoming =
                     force_above_surface(
-                        reflect(sample.m_outgoing.get_value(), h), sample.get_geometric_normal());
+                        reflect(sample.m_outgoing.get_value(), h), sample.m_geometric_normal);
             }
 
             // No reflection below the shading surface.

src/appleseed/renderer/modeling/bsdf/bsdfsample.cpp
@@ -58,14 +58,13 @@ void BSDFSample::compute_reflected_differentials()
         double ddndx, ddndy;
         compute_normal_derivatives(dndx, dndy, ddndx, ddndy);
 
-        const Vector3d& normal = m_shading_point.get_shading_normal();
-        const double dot_on = dot(m_outgoing.get_value(), normal);
+        const double dot_on = dot(m_outgoing.get_value(), m_shading_normal);
 
         m_incoming =
             Dual3d(
                 m_incoming.get_value(),
-                -m_outgoing.get_dx() + 2.0 * Vector3d(dot_on * dndx + ddndx * normal),
-                -m_outgoing.get_dy() + 2.0 * Vector3d(dot_on * dndy + ddndy * normal));
+                -m_outgoing.get_dx() + 2.0 * Vector3d(dot_on * dndx + ddndx * m_shading_normal),
+                -m_outgoing.get_dy() + 2.0 * Vector3d(dot_on * dndy + ddndy * m_shading_normal));
 
         if (m_probability != BSDF::DiracDelta)
             apply_pdf_differentials_heuristic();
@@ -80,10 +79,8 @@ void BSDFSample::compute_transmitted_differentials(const double eta)
         double ddndx, ddndy;
         compute_normal_derivatives(dndx, dndy, ddndx, ddndy);
 
-        const Vector3d& normal = m_shading_point.get_shading_normal();
-
-        const double dot_on = dot(-m_outgoing.get_value(), normal);
-        const double dot_in = dot(m_incoming.get_value(), normal);
+        const double dot_on = dot(-m_outgoing.get_value(), m_shading_normal);
+        const double dot_in = dot(m_incoming.get_value(), m_shading_normal);
         const double mu = eta * dot_on - dot_in;
 
         const double a = eta - (square(eta) * dot_on) / dot_in;
@@ -93,8 +90,8 @@ void BSDFSample::compute_transmitted_differentials(const double eta)
         m_incoming =
             Dual3d(
                 m_incoming.get_value(),
-                eta * m_outgoing.get_dx() - Vector3d(mu * dndx + dmudx * normal),
-                eta * m_outgoing.get_dy() - Vector3d(mu * dndy + dmudy * normal));
+                eta * m_outgoing.get_dx() - Vector3d(mu * dndx + dmudx * m_shading_normal),
+                eta * m_outgoing.get_dy() - Vector3d(mu * dndy + dmudy * m_shading_normal));
 
         if (m_probability != BSDF::DiracDelta)
             apply_pdf_differentials_heuristic();
@@ -111,18 +108,11 @@ void BSDFSample::compute_normal_derivatives(
     // Physically Based Rendering, first edition, page 513.
     //
 
-    const Vector3d& dndu = m_shading_point.get_dndu(0);
-    const Vector3d& dndv = m_shading_point.get_dndv(0);
-    const Vector2f& duvdx = m_shading_point.get_duvdx(0);
-    const Vector2f& duvdy = m_shading_point.get_duvdy(0);
+    dndx = m_dndu * static_cast<double>(m_duvdx[0]) + m_dndv * static_cast<double>(m_duvdx[1]);
+    dndy = m_dndu * static_cast<double>(m_duvdy[0]) + m_dndv * static_cast<double>(m_duvdy[1]);
 
-    dndx = dndu * static_cast<double>(duvdx[0]) + dndv * static_cast<double>(duvdx[1]);
-    dndy = dndu * static_cast<double>(duvdy[0]) + dndv * static_cast<double>(duvdy[1]);
-
-    const Vector3d& normal = m_shading_point.get_shading_normal();
-
-    ddndx = dot(m_outgoing.get_dx(), normal) + dot(m_outgoing.get_value(), dndx);
-    ddndy = dot(m_outgoing.get_dy(), normal) + dot(m_outgoing.get_value(), dndy);
+    ddndx = dot(m_outgoing.get_dx(), m_shading_normal) + dot(m_outgoing.get_value(), dndx);
+    ddndy = dot(m_outgoing.get_dy(), m_shading_normal) + dot(m_outgoing.get_value(), dndy);
 }
 
 void BSDFSample::apply_pdf_differentials_heuristic()

src/appleseed/renderer/modeling/bsdf/bsdfsample.h
@@ -46,7 +46,13 @@ class BSDFSample
 {
   public:
     // Inputs.
-    const ShadingPoint&             m_shading_point;        // shading point at which the sampling is done
+    const foundation::Vector3d      m_geometric_normal;     // world space geometric normal at the point where sampling is done, unit-length
+    const foundation::Vector3d      m_shading_normal;       // world space shading normal at the point where sampling is done, unit-length
+    foundation::Basis3d             m_shading_basis;        // world space shading basis at the point where sampling is done
+    const foundation::Vector3d      m_dndu;
+    const foundation::Vector3d      m_dndv;
+    const foundation::Vector2f      m_duvdx;
+    const foundation::Vector2f      m_duvdy;
     const foundation::Dual3d        m_outgoing;             // world space outgoing direction, unit-length
 
     // Outputs.
@@ -59,11 +65,6 @@ class BSDFSample
     BSDFSample(
         const ShadingPoint&         shading_point,
         const foundation::Dual3d&   outgoing);
-
-    const foundation::Vector3d& get_geometric_normal() const;
-    const foundation::Vector3d& get_shading_normal() const;
-    const foundation::Basis3d& get_shading_basis() const;
-    void set_shading_basis(const foundation::Basis3d& basis);
 
     void compute_reflected_differentials();
     void compute_transmitted_differentials(const double eta);
@@ -86,30 +87,16 @@ class BSDFSample
 inline BSDFSample::BSDFSample(
     const ShadingPoint&             shading_point,
     const foundation::Dual3d&       outgoing)
-  : m_shading_point(shading_point)
+  : m_geometric_normal(shading_point.get_geometric_normal())
+  , m_shading_normal(shading_point.get_shading_normal())
+  , m_shading_basis(shading_point.get_shading_basis())
+  , m_dndu(shading_point.get_dndu(0))
+  , m_dndv(shading_point.get_dndv(0))
+  , m_duvdx(shading_point.get_duvdx(0))
+  , m_duvdy(shading_point.get_duvdy(0))
   , m_outgoing(outgoing)
   , m_mode(ScatteringMode::Absorption)
 {
-}
-
-inline const foundation::Vector3d& BSDFSample::get_geometric_normal() const
-{
-    return m_shading_point.get_geometric_normal();
-}
-
-inline const foundation::Vector3d& BSDFSample::get_shading_normal() const
-{
-    return m_shading_point.get_shading_normal();
-}
-
-inline const foundation::Basis3d& BSDFSample::get_shading_basis() const
-{
-    return m_shading_point.get_shading_basis();
-}
-
-inline void BSDFSample::set_shading_basis(const foundation::Basis3d& basis)
-{
-    m_shading_point.set_shading_basis(basis);
 }
 
 }       // namespace renderer

src/appleseed/renderer/modeling/bsdf/bsdfwrapper.h
@@ -111,7 +111,7 @@ void BSDFWrapper<BSDFImpl>::sample(
     const bool                          cosine_mult,
     BSDFSample&                         sample) const
 {
-    assert(foundation::is_normalized(sample.get_geometric_normal()));
+    assert(foundation::is_normalized(sample.m_geometric_normal));
     assert(foundation::is_normalized(sample.m_outgoing.get_value()));
 
     BSDFImpl::sample(
@@ -130,14 +130,14 @@ void BSDFWrapper<BSDFImpl>::sample(
         {
             if (adjoint)
             {
-                const double cos_on = std::abs(foundation::dot(sample.m_outgoing.get_value(), sample.get_shading_normal()));
-                const double cos_ig = std::abs(foundation::dot(sample.m_incoming.get_value(), sample.get_geometric_normal()));
-                const double cos_og = std::abs(foundation::dot(sample.m_outgoing.get_value(), sample.get_geometric_normal()));
+                const double cos_on = std::abs(foundation::dot(sample.m_outgoing.get_value(), sample.m_shading_normal));
+                const double cos_ig = std::abs(foundation::dot(sample.m_incoming.get_value(), sample.m_geometric_normal));
+                const double cos_og = std::abs(foundation::dot(sample.m_outgoing.get_value(), sample.m_geometric_normal));
                 sample.m_value *= static_cast<float>(cos_on * cos_ig / cos_og);
             }
             else
             {
-                const double cos_in = std::abs(foundation::dot(sample.m_incoming.get_value(), sample.get_shading_normal()));
+                const double cos_in = std::abs(foundation::dot(sample.m_incoming.get_value(), sample.m_shading_normal));
                 sample.m_value *= static_cast<float>(cos_in);
             }
         }

src/appleseed/renderer/modeling/bsdf/kelemenbrdf.cpp
@@ -220,7 +220,7 @@ namespace
         {
             // Define aliases to match the notations in the paper.
             const Vector3d& V = sample.m_outgoing.get_value();
-            const Vector3d& N = sample.get_shading_normal();
+            const Vector3d& N = sample.m_shading_normal;
 
             // No reflection below the shading surface.
             const double dot_VN = dot(V, N);
@@ -262,7 +262,7 @@ namespace
                 const Vector3d wi = sample_hemisphere_cosine(Vector2d(s[0], s[1]));
 
                 // Transform the incoming direction to parent space.
-                incoming = sample.get_shading_basis().transform_to_parent(wi);
+                incoming = sample.m_shading_basis.transform_to_parent(wi);
 
                 // Compute the halfway vector.
                 H = normalize(incoming + V);
@@ -279,7 +279,7 @@ namespace
                 const Vector3d local_H = m_mdf->sample(Vector2d(s[0], s[1]));
 
                 // Transform the halfway vector to parent space.
-                H = sample.get_shading_basis().transform_to_parent(local_H);
+                H = sample.m_shading_basis.transform_to_parent(local_H);
 
                 dot_HV = dot(H, V);
 

src/appleseed/renderer/modeling/bsdf/microfacethelper.h
@@ -213,14 +213,14 @@ class MicrofacetBRDFHelper
         // Compute the incoming direction by sampling the MDF.
         sampling_context.split_in_place(3, 1);
         const VectorType s = sampling_context.next_vector2<3>();
-        const VectorType wo = sample.get_shading_basis().transform_to_local(sample.m_outgoing.get_value());
+        const VectorType wo = sample.m_shading_basis.transform_to_local(sample.m_outgoing.get_value());
         const VectorType m = mdf.sample(wo, s, alpha_x, alpha_y);
-        const VectorType h = sample.get_shading_basis().transform_to_parent(m);
+        const VectorType h = sample.m_shading_basis.transform_to_parent(m);
         const VectorType incoming = foundation::reflect(sample.m_outgoing.get_value(), h);
         const T cos_oh = foundation::dot(sample.m_outgoing.get_value(), h);
 
         // No reflection below the shading surface.
-        const VectorType& n = sample.get_shading_normal();
+        const VectorType& n = sample.m_shading_normal;
         const T cos_in = foundation::dot(incoming, n);
         if (cos_in < T(0.0))
             return;
@@ -229,13 +229,13 @@ class MicrofacetBRDFHelper
 
         const T G =
             mdf.G(
-                sample.get_shading_basis().transform_to_local(incoming),
+                sample.m_shading_basis.transform_to_local(incoming),
                 wo,
                 m,
                 g_alpha_x,
                 g_alpha_y);
 
-        f(sample.m_outgoing.get_value(), h, sample.get_shading_normal(), sample.m_value);
+        f(sample.m_outgoing.get_value(), h, sample.m_shading_normal, sample.m_value);
         sample.m_value *= static_cast<float>(D * G / (T(4.0) * cos_on * cos_in));
         sample.m_probability = mdf.pdf(wo, m, alpha_x, alpha_y) / (T(4.0) * cos_oh);
         sample.m_mode = ScatteringMode::Glossy;

src/appleseed/renderer/modeling/bsdf/orennayarbrdf.cpp
@@ -102,7 +102,7 @@ namespace
             BSDFSample&         sample) const APPLESEED_OVERRIDE
         {
             // No reflection below the shading surface.
-            const Vector3d& n = sample.get_shading_normal();
+            const Vector3d& n = sample.m_shading_normal;
 
             // Compute the incoming direction in local space.
             sampling_context.split_in_place(2, 1);
@@ -110,7 +110,7 @@ namespace
             const Vector3d wi = sample_hemisphere_cosine(s);
 
             // Transform the incoming direction to parent space.
-            const Vector3d incoming = sample.get_shading_basis().transform_to_parent(wi);
+            const Vector3d incoming = sample.m_shading_basis.transform_to_parent(wi);
 
             // Compute the BRDF value.
             const InputValues* values = static_cast<const InputValues*>(data);

src/appleseed/renderer/modeling/bsdf/oslbsdf.cpp
@@ -208,7 +208,7 @@ namespace
                 const double s = sampling_context.next_double2();
 
                 const size_t closure_index = c->choose_closure(s);
-                sample.set_shading_basis(c->get_closure_shading_basis(closure_index));
+                sample.m_shading_basis = c->get_closure_shading_basis(closure_index);
                 bsdf_from_closure_id(c->get_closure_type(closure_index))
                     .sample(
                         sampling_context,

commit 81751f9f22265073179226f51860817395aaeea3

Fix regression when rendering with OSL
Fix regression introduced in bb19e1c09125dda0d7ff1fa0670378d8082eeec1 ("Decouple renderer::BSDFSample from renderer::ShadingPoint").

src/appleseed/renderer/modeling/bsdf/ashikhminbrdf.cpp
@@ -108,7 +108,7 @@ namespace
             BSDFSample&         sample) const APPLESEED_OVERRIDE
         {
             // No reflection below the shading surface.
-            const float cos_on = dot(sample.m_outgoing.get_value(), sample.m_shading_normal);
+            const float cos_on = dot(sample.m_outgoing.get_value(), sample.m_shading_basis.get_normal());
             if (cos_on < 0.0f)
                 return;
 
@@ -146,7 +146,7 @@ namespace
                 h = normalize(incoming + sample.m_outgoing.get_value());
 
                 // Compute the glossy exponent, needed to evaluate the PDF.
-                const float cos_hn = dot(h, sample.m_shading_normal);
+                const float cos_hn = dot(h, sample.m_shading_basis.get_normal());
                 const float cos_hu = dot(h, sample.m_shading_basis.get_tangent_u());
                 const float cos_hv = dot(h, sample.m_shading_basis.get_tangent_v());
                 const float exp_den = 1.0f - cos_hn * cos_hn;
@@ -196,13 +196,13 @@ namespace
             }
 
             // No reflection below the shading surface.
-            const float cos_in = dot(incoming, sample.m_shading_normal);
+            const float cos_in = dot(incoming, sample.m_shading_basis.get_normal());
             if (cos_in < 0.0f)
                 return;
 
             // Compute dot products.
             const float cos_oh = abs(dot(sample.m_outgoing.get_value(), h));
-            const float cos_hn = dot(h, sample.m_shading_normal);
+            const float cos_hn = dot(h, sample.m_shading_basis.get_normal());
 
             // Evaluate the diffuse component of the BRDF (equation 5).
             const float a = 1.0f - pow5(1.0f - 0.5f * cos_in);

src/appleseed/renderer/modeling/bsdf/bsdfsample.cpp
@@ -58,13 +58,14 @@ void BSDFSample::compute_reflected_differentials()
         float ddndx, ddndy;
         compute_normal_derivatives(dndx, dndy, ddndx, ddndy);
 
-        const float dot_on = dot(m_outgoing.get_value(), m_shading_normal);
+        const Vector3f& n = m_shading_basis.get_normal();
+        const float dot_on = dot(m_outgoing.get_value(), n);
 
         m_incoming =
             Dual3f(
                 m_incoming.get_value(),
-                -m_outgoing.get_dx() + 2.0f * Vector3f(dot_on * dndx + ddndx * m_shading_normal),
-                -m_outgoing.get_dy() + 2.0f * Vector3f(dot_on * dndy + ddndy * m_shading_normal));
+                -m_outgoing.get_dx() + 2.0f * Vector3f(dot_on * dndx + ddndx * n),
+                -m_outgoing.get_dy() + 2.0f * Vector3f(dot_on * dndy + ddndy * n));
 
         if (m_probability != BSDF::DiracDelta)
             apply_pdf_differentials_heuristic();
@@ -79,8 +80,9 @@ void BSDFSample::compute_transmitted_differentials(const float eta)
         float ddndx, ddndy;
         compute_normal_derivatives(dndx, dndy, ddndx, ddndy);
 
-        const float dot_on = dot(-m_outgoing.get_value(), m_shading_normal);
-        const float dot_in = dot(m_incoming.get_value(), m_shading_normal);
+        const Vector3f& n = m_shading_basis.get_normal();
+        const float dot_on = dot(-m_outgoing.get_value(), n);
+        const float dot_in = dot(m_incoming.get_value(), n);
         const float mu = eta * dot_on - dot_in;
 
         const float a = eta - (square(eta) * dot_on) / dot_in;
@@ -90,8 +92,8 @@ void BSDFSample::compute_transmitted_differentials(const float eta)
         m_incoming =
             Dual3f(
                 m_incoming.get_value(),
-                eta * m_outgoing.get_dx() - Vector3f(mu * dndx + dmudx * m_shading_normal),
-                eta * m_outgoing.get_dy() - Vector3f(mu * dndy + dmudy * m_shading_normal));
+                eta * m_outgoing.get_dx() - Vector3f(mu * dndx + dmudx * n),
+                eta * m_outgoing.get_dy() - Vector3f(mu * dndy + dmudy * n));
 
         if (m_probability != BSDF::DiracDelta)
             apply_pdf_differentials_heuristic();
@@ -108,11 +110,19 @@ void BSDFSample::compute_normal_derivatives(
     // Physically Based Rendering, first edition, page 513.
     //
 
-    dndx = m_dndu * m_duvdx[0] + m_dndv * m_duvdx[1];
-    dndy = m_dndu * m_duvdy[0] + m_dndv * m_duvdy[1];
+    const Vector3f dndu(m_shading_point.get_dndu(0));
+    const Vector3f dndv(m_shading_point.get_dndv(0));
 
-    ddndx = dot(m_outgoing.get_dx(), m_shading_normal) + dot(m_outgoing.get_value(), dndx);
-    ddndy = dot(m_outgoing.get_dy(), m_shading_normal) + dot(m_outgoing.get_value(), dndy);
+    const Vector2f duvdx(m_shading_point.get_duvdx(0));
+    const Vector2f duvdy(m_shading_point.get_duvdy(0));
+
+    dndx = dndu * duvdx[0] + dndv * duvdx[1];
+    dndy = dndu * duvdy[0] + dndv * duvdy[1];
+
+    const Vector3f& n = m_shading_basis.get_normal();
+
+    ddndx = dot(m_outgoing.get_dx(), n) + dot(m_outgoing.get_value(), dndx);
+    ddndy = dot(m_outgoing.get_dy(), n) + dot(m_outgoing.get_value(), dndy);
 }
 
 void BSDFSample::apply_pdf_differentials_heuristic()

src/appleseed/renderer/modeling/bsdf/bsdfsample.h
@@ -47,12 +47,7 @@ class BSDFSample
   public:
     // Inputs.
     const foundation::Vector3f      m_geometric_normal;     // world space geometric normal at the point where sampling is done, unit-length
-    const foundation::Vector3f      m_shading_normal;       // world space shading normal at the point where sampling is done, unit-length
-    foundation::Basis3f             m_shading_basis;        // world space shading basis at the point where sampling is done
-    const foundation::Vector3f      m_dndu;
-    const foundation::Vector3f      m_dndv;
-    const foundation::Vector2f      m_duvdx;
-    const foundation::Vector2f      m_duvdy;
+    const foundation::Basis3f&      m_shading_basis;        // world space shading basis at the point where sampling is done
     const foundation::Dual3f        m_outgoing;             // world space outgoing direction, unit-length
 
     // Outputs.
@@ -66,10 +61,15 @@ class BSDFSample
         const ShadingPoint&         shading_point,
         const foundation::Dual3d&   outgoing);
 
+    void set_shading_basis(const foundation::Basis3f& basis);
+
     void compute_reflected_differentials();
     void compute_transmitted_differentials(const float eta);
 
   private:
+    const ShadingPoint&             m_shading_point;
+    foundation::Basis3f             m_local_shading_basis;
+
     void compute_normal_derivatives(
         foundation::Vector3f&       dndx,
         foundation::Vector3f&       dndy,
@@ -88,15 +88,18 @@ inline BSDFSample::BSDFSample(
     const ShadingPoint&             shading_point,
     const foundation::Dual3d&       outgoing)
   : m_geometric_normal(shading_point.get_geometric_normal())
-  , m_shading_normal(shading_point.get_shading_normal())
-  , m_shading_basis(shading_point.get_shading_basis())
-  , m_dndu(shading_point.get_dndu(0))
-  , m_dndv(shading_point.get_dndv(0))
-  , m_duvdx(shading_point.get_duvdx(0))
-  , m_duvdy(shading_point.get_duvdy(0))
+  , m_shading_basis(m_local_shading_basis)
   , m_outgoing(outgoing)
   , m_mode(ScatteringMode::Absorption)
+  , m_shading_point(shading_point)
+  , m_local_shading_basis(shading_point.get_shading_basis())
 {
+}
+
+inline void BSDFSample::set_shading_basis(const foundation::Basis3f& basis)
+{
+    m_local_shading_basis = basis;
+    m_shading_point.set_shading_basis(foundation::Basis3d(basis));
 }
 
 }       // namespace renderer

src/appleseed/renderer/modeling/bsdf/bsdfwrapper.h
@@ -130,14 +130,14 @@ void BSDFWrapper<BSDFImpl>::sample(
         {
             if (adjoint)
             {
-                const float cos_on = std::abs(foundation::dot(sample.m_outgoing.get_value(), sample.m_shading_normal));
+                const float cos_on = std::abs(foundation::dot(sample.m_outgoing.get_value(), sample.m_shading_basis.get_normal()));
                 const float cos_ig = std::abs(foundation::dot(sample.m_incoming.get_value(), sample.m_geometric_normal));
                 const float cos_og = std::abs(foundation::dot(sample.m_outgoing.get_value(), sample.m_geometric_normal));
                 sample.m_value *= cos_on * cos_ig / cos_og;
             }
             else
             {
-                const float cos_in = std::abs(foundation::dot(sample.m_incoming.get_value(), sample.m_shading_normal));
+                const float cos_in = std::abs(foundation::dot(sample.m_incoming.get_value(), sample.m_shading_basis.get_normal()));
                 sample.m_value *= cos_in;
             }
         }

src/appleseed/renderer/modeling/bsdf/kelemenbrdf.cpp
@@ -222,7 +222,7 @@ namespace
         {
             // Define aliases to match the notations in the paper.
             const Vector3f& V = sample.m_outgoing.get_value();
-            const Vector3f& N = sample.m_shading_normal;
+            const Vector3f& N = sample.m_shading_basis.get_normal();
 
             // No reflection below the shading surface.
             const float dot_VN = dot(V, N);

src/appleseed/renderer/modeling/bsdf/microfacethelper.h
@@ -224,8 +224,7 @@ class MicrofacetBRDFHelper
         const T cos_oh = foundation::dot(sample.m_outgoing.get_value(), h);
 
         // No reflection below the shading surface.
-        const VectorType& n = sample.m_shading_normal;
-        const T cos_in = foundation::dot(incoming, n);
+        const T cos_in = foundation::dot(incoming, sample.m_shading_basis.get_normal());
         if (cos_in <= T(0.0))
             return;
 
@@ -239,7 +238,7 @@ class MicrofacetBRDFHelper
                 g_alpha_x,
                 g_alpha_y);
 
-        f(sample.m_outgoing.get_value(), h, sample.m_shading_normal, sample.m_value);
+        f(sample.m_outgoing.get_value(), h, sample.m_shading_basis.get_normal(), sample.m_value);
         sample.m_value *= static_cast<float>(D * G / (T(4.0) * cos_on * cos_in));
         sample.m_probability = mdf.pdf(wo, m, alpha_x, alpha_y) / (T(4.0) * cos_oh);
         sample.m_mode = ScatteringMode::Glossy;

src/appleseed/renderer/modeling/bsdf/orennayarbrdf.cpp
@@ -101,9 +101,6 @@ namespace
             const bool          cosine_mult,
             BSDFSample&         sample) const APPLESEED_OVERRIDE
         {
-            // No reflection below the shading surface.
-            const Vector3f& n = sample.m_shading_normal;
-
             // Compute the incoming direction in local space.
             sampling_context.split_in_place(2, 1);
             const Vector2f s = sampling_context.next2<Vector2f>();
@@ -116,6 +113,9 @@ namespace
             const InputValues* values = static_cast<const InputValues*>(data);
             if (values->m_roughness != 0.0f)
             {
+                const Vector3f& n = sample.m_shading_basis.get_normal();
+
+                // No reflection below the shading surface.
                 const float cos_on = dot(sample.m_outgoing.get_value(), n);
                 if (cos_on < 0.0f)
                     return;

src/appleseed/renderer/modeling/bsdf/oslbsdf.cpp
@@ -208,7 +208,7 @@ namespace
                 const float s = sampling_context.next2<float>();
 
                 const size_t closure_index = c->choose_closure(s);
-                sample.m_shading_basis = c->get_closure_shading_basis(closure_index);
+                sample.set_shading_basis(c->get_closure_shading_basis(closure_index));
                 bsdf_from_closure_id(c->get_closure_type(closure_index))
                     .sample(
                         sampling_context,

src/appleseed/renderer/modeling/bsdf/sheenbrdf.cpp
@@ -99,8 +99,7 @@ namespace
             BSDFSample&         sample) const APPLESEED_OVERRIDE
         {
             // No reflection below the shading surface.
-            const Vector3f& n = sample.m_shading_normal;
-            const float cos_on = dot(sample.m_outgoing.get_value(), n);
+            const float cos_on = dot(sample.m_outgoing.get_value(), sample.m_shading_basis.get_normal());
             if (cos_on < 0.0f)
                 return;
 

src/appleseed/renderer/modeling/bsdf/specularbrdf.cpp
@@ -86,7 +86,7 @@ namespace
             BSDFSample&         sample) const APPLESEED_OVERRIDE
         {
             // No reflection below the shading surface.
-            const Vector3f& shading_normal = sample.m_shading_normal;
+            const Vector3f& shading_normal = sample.m_shading_basis.get_normal();
             const float cos_on = dot(sample.m_outgoing.get_value(), shading_normal);
             if (cos_on < 0.0f)
                 return;

src/appleseed/renderer/modeling/bsdf/specularbtdf.cpp
@@ -117,7 +117,7 @@ namespace
         {
             const InputValues* values = static_cast<const InputValues*>(data);
 
-            const Vector3f& shading_normal = sample.m_shading_normal;
+            const Vector3f& shading_normal = sample.m_shading_basis.get_normal();
             const float cos_theta_i = dot(sample.m_outgoing.get_value(), shading_normal);
             const float sin_theta_i2 = 1.0f - square(cos_theta_i);
             const float sin_theta_t2 = sin_theta_i2 * square(values->m_eta);

src/appleseed/renderer/modeling/bsdf/velvetbrdf.cpp
@@ -103,7 +103,7 @@ namespace
             const bool          cosine_mult,
             BSDFSample&         sample) const APPLESEED_OVERRIDE
         {
-            const Vector3f& n = sample.m_shading_normal;
+            const Vector3f& n = sample.m_shading_basis.get_normal();
             const float cos_on = min(dot(sample.m_outgoing.get_value(), n), 1.0f);
             if (cos_on < 0.0f)
                 return;