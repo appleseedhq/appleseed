b6025d8a7a02a9902f8112752af40e6f98283d6e
Franz Beaune <franz@appleseedhq.net>
2016-12-25 19:46:31 +0100

    Add poisoning facilities

---
+++ src/appleseed/foundation/utility/poison.h
@@ -0,0 +1,81 @@
+
+//
+// This source file is part of appleseed.
+// Visit http://appleseedhq.net/ for additional information and resources.
+//
+// This software is released under the MIT license.
+//
+// Copyright (c) 2016 Francois Beaune, The appleseedhq Organization
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+#ifndef APPLESEED_FOUNDATION_UTILITY_POISON_H
+#define APPLESEED_FOUNDATION_UTILITY_POISON_H
+
+// appleseed.foundation headers.
+#include "foundation/math/fp.h"
+#include "foundation/platform/arch.h"
+
+// Standard headers.
+#include <cstring>
+
+namespace foundation
+{
+
+// Set x to a remarkable value that will help detect cases where it is used without being initialized.
+template <typename T>
+void poison(T& x);
+
+
+//
+// Implementation.
+//
+
+template <typename T>
+inline void poison(T& x)
+{
+    std::memset(&x, 0xADU, sizeof(x));
+}
+
+template <>
+inline void poison(void*& p)
+{
+#ifdef APPLESEED_ARCH32
+    p = reinterpret_cast<void*>(0xDEADBEEFUL);
+#else
+    p = reinterpret_cast<void*>(0xDEADBEEFDEADBEEFULL);
+#endif
+}
+
+template <>
+inline void poison(float& x)
+{
+    x = FP<float>::snan();
+}
+
+template <>
+inline void poison(double& x)
+{
+    x = FP<double>::snan();
+}
+
+}       // namespace foundation
+
+#endif  // !APPLESEED_FOUNDATION_UTILITY_POISON_H
