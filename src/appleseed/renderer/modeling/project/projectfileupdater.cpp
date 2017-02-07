appleseedhq/appleseed

commit c31f86fd396e0e0b38a04fd26a03648b250c2e96

Rename specular BTDF inputs

src/appleseed/renderer/modeling/project/projectfileupdater.cpp
@@ -1235,6 +1235,11 @@ namespace
             {
                 move_if_exist(bsdf, "anisotropy", "anisotropic");
             }
+            else if (strcmp(bsdf.get_model(), SpecularBTDFFactory().get_model()) == 0)
+            {
+                move_if_exist(bsdf, "volume_density", "density");
+                move_if_exist(bsdf, "volume_scale", "scale");
+            }
         }
     };
 }

src/appleseed/renderer/modeling/bsdf/specularbtdf.cpp
@@ -75,8 +75,8 @@ namespace
             m_inputs.declare("transmittance_multiplier", InputFormatFloat, "1.0");
             m_inputs.declare("fresnel_multiplier", InputFormatFloat, "1.0");
             m_inputs.declare("ior", InputFormatFloat);
-            m_inputs.declare("density", InputFormatFloat, "0.0");
-            m_inputs.declare("scale", InputFormatFloat, "1.0");
+            m_inputs.declare("volume_density", InputFormatFloat, "0.0");
+            m_inputs.declare("volume_scale", InputFormatFloat, "1.0");
         }
 
         virtual void release() APPLESEED_OVERRIDE
@@ -233,7 +233,7 @@ namespace
             Spectrum&               absorption) const APPLESEED_OVERRIDE
         {
             const InputValues* values = static_cast<const InputValues*>(data);
-            const float d = values->m_density * values->m_scale * distance;
+            const float d = values->m_volume_density * values->m_volume_scale * distance;
 
             absorption.resize(values->m_transmittance.size());
 
@@ -347,8 +347,8 @@ DictionaryArray SpecularBTDFFactory::get_input_metadata() const
 
     metadata.push_back(
         Dictionary()
-            .insert("name", "density")
-            .insert("label", "Density")
+            .insert("name", "volume_density")
+            .insert("label", "Volume Density")
             .insert("type", "numeric")
             .insert("min_value", "0.0")
             .insert("max_value", "10.0")
@@ -357,8 +357,8 @@ DictionaryArray SpecularBTDFFactory::get_input_metadata() const
 
     metadata.push_back(
         Dictionary()
-            .insert("name", "scale")
-            .insert("label", "Scale")
+            .insert("name", "volume_scale")
+            .insert("label", "Volume Scale")
             .insert("type", "numeric")
             .insert("min_value", "0.0")
             .insert("max_value", "10.0")

src/appleseed/renderer/modeling/bsdf/specularbtdf.h
@@ -63,8 +63,8 @@ APPLESEED_DECLARE_INPUT_VALUES(SpecularBTDFInputValues)
     float       m_transmittance_multiplier;     // specular transmittance multiplier
     float       m_fresnel_multiplier;           // Fresnel multiplier
     float       m_ior;                          // index of refraction inside the medium
-    float       m_density;                      // medium density
-    float       m_scale;                        // distance scaling factor
+    float       m_volume_density;               // volume density
+    float       m_volume_scale;                 // volume scaling factor
 
     struct Precomputed
     {

commit d3af5076e6c9fe285f14a348682ce3b7d2fc5c73

Fix regression introduced in c31f86fd396e0e0b38a04fd26a03648b250c2e96
c31f86fd396e0e0b38a04fd26a03648b250c2e96: Rename specular BTDF inputs

src/appleseed/renderer/modeling/project/projectfileupdater.cpp
@@ -1238,13 +1238,13 @@ namespace
                 move_if_exist(bsdf, "anisotropy", "anisotropic");
             }
             else if (strcmp(bsdf.get_model(), GlossyBRDFFactory().get_model()) == 0 ||
-                     strcmp(bsdf.get_model(), MetalBRDFFactory().get_model()) == 0 ||
-                     strcmp(bsdf.get_model(), SpecularBTDFFactory().get_model()) == 0)
+                     strcmp(bsdf.get_model(), MetalBRDFFactory().get_model()) == 0)
             {
                 move_if_exist(bsdf, "anisotropy", "anisotropic");
             }
             else if (strcmp(bsdf.get_model(), SpecularBTDFFactory().get_model()) == 0)
             {
+                move_if_exist(bsdf, "anisotropy", "anisotropic");
                 move_if_exist(bsdf, "volume_density", "density");
                 move_if_exist(bsdf, "volume_scale", "scale");
             }