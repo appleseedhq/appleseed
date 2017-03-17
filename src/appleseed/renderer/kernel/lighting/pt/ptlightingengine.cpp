appleseedhq/appleseed

commit bfcbd5d06fc6a55793fa3e71a8ea3b5d35b096be

Fix for OSL bump pt nee + test scene.

src/appleseed/renderer/kernel/lighting/pt/ptlightingengine.cpp
@@ -446,6 +446,22 @@ namespace
                         vertex_aovs);
                 }
 
+                // If we have an OSL shader and this is not the last vertex of the path,
+                // we need to choose one of the closures and set its shading basis into the shading point
+                // for the DirectLightingIntegrator to use it.
+                if (!last_vertex && (m_params.m_enable_dl || m_params.m_enable_ibl))
+                {
+                    const Material::RenderData& material_data =
+                        vertex.m_shading_point->get_material()->get_render_data();
+
+                    if (material_data.m_shader_group)
+                    {
+                        m_shading_context.choose_bsdf_closure_shading_basis(
+                            *vertex.m_shading_point,
+                            m_sampling_context.next2<Vector2f>());
+                    }
+                }
+
                 // Direct lighting.
                 if (m_params.m_enable_dl || vertex.m_path_length > 1)
                 {

sandbox/tests/test scenes/osl/24 - osl bump pt nee.appleseed
@@ -1,0 +1,292 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!-- File generated by appleseed.so version 1.7.0-beta (SSE 4.2). -->
+<project format_revision="15">
+    <search_paths>
+        <search_path>
+            _geometry
+        </search_path>
+        <search_path>
+            ../../../shaders/maya
+        </search_path>
+        <search_path>
+            _textures
+        </search_path>
+    </search_paths>
+    <scene>
+        <camera name="|camera1|cameraShape1" model="pinhole_camera">
+            <parameter name="controller_target" value="-3.57628e-07 8.24052 6.58574" />
+            <parameter name="film_dimensions" value="0.0359999 0.0359999" />
+            <parameter name="focal_length" value="0.035" />
+            <parameter name="shutter_close_time" value="0" />
+            <parameter name="shutter_open_time" value="0" />
+            <transform time="0">
+                <matrix>
+                    1.000000000000000 0.000000000000000 0.000000000000000 0.000000000000000
+                    0.000000000000000 1.000000000000000 0.000000000000000 5.102296347038383
+                    0.000000000000000 0.000000000000000 1.000000000000000 15.616288400597384
+                    0.000000000000000 0.000000000000000 0.000000000000000 1.000000000000000
+                </matrix>
+            </transform>
+        </camera>
+        <environment name="environment" model="generic_environment" />
+        <assembly name="assembly">
+            <shader_group name="areaLightShape1_shader_group">
+                <shader type="surface" name="as_maya_areaLight" layer="areaLightShape1">
+                    <parameter name="in_color" value="color 1 1 1" />
+                    <parameter name="in_exposure" value="float 0" />
+                    <parameter name="in_intensity" value="float 40" />
+                    <parameter name="in_intensity_scale" value="float 1" />
+                    <parameter name="in_normalize" value="int 0" />
+                </shader>
+            </shader_group>
+            <shader_group name="lambert12_shader_group">
+                <shader type="shader" name="as_maya_place2dTexture" layer="place2dTexture8">
+                    <parameter name="in_coverage" value="float[] 1 1 " />
+                    <parameter name="in_fast" value="int 0" />
+                    <parameter name="in_mirrorU" value="int 0" />
+                    <parameter name="in_mirrorV" value="int 0" />
+                    <parameter name="in_noiseUV" value="float[] 0 0 " />
+                    <parameter name="in_offset" value="float[] 0 0 " />
+                    <parameter name="in_repeatUV" value="float[] 4 2 " />
+                    <parameter name="in_rotateFrame" value="float 0" />
+                    <parameter name="in_rotateUV" value="float 0" />
+                    <parameter name="in_stagger" value="int 0" />
+                    <parameter name="in_translateFrame" value="float[] 0 0 " />
+                    <parameter name="in_uvFilterSize" value="float[] 0 0 " />
+                    <parameter name="in_wrapU" value="int 1" />
+                    <parameter name="in_wrapV" value="int 1" />
+                </shader>
+                <shader type="shader" name="as_maya_file" layer="file5">
+                    <parameter name="in_alphaGain" value="float 1" />
+                    <parameter name="in_alphaIsLuminance" value="int 1" />
+                    <parameter name="in_alphaOffset" value="float 0" />
+                    <parameter name="in_colorGain" value="color 1 1 1" />
+                    <parameter name="in_colorOffset" value="color 0 0 0" />
+                    <parameter name="in_defaultColor" value="color 0.5 0.5 0.5" />
+                    <parameter name="in_disableFileLoad" value="int 0" />
+                    <parameter name="in_exposure" value="float 0" />
+                    <parameter name="in_fileTextureName" value="string polkadot_heightmap.tx" />
+                    <parameter name="in_filter" value="float 1" />
+                    <parameter name="in_filterOffset" value="float 0" />
+                    <parameter name="in_filterType" value="int 3" />
+                    <parameter name="in_filterWidth" value="float 0.707" />
+                    <parameter name="in_frameExtension" value="int 1" />
+                    <parameter name="in_frameOffset" value="int 0" />
+                    <parameter name="in_invert" value="int 0" />
+                    <parameter name="in_preFilter" value="int 0" />
+                    <parameter name="in_preFilterRadius" value="float 2" />
+                    <parameter name="in_ptexFilterBlur" value="float 0" />
+                    <parameter name="in_ptexFilterInterpolateLevels" value="int 0" />
+                    <parameter name="in_ptexFilterType" value="int 3" />
+                    <parameter name="in_ptexFilterWidth" value="float 1" />
+                    <parameter name="in_useFrameExtension" value="int 0" />
+                    <parameter name="in_useMaximumRes" value="int 0" />
+                    <parameter name="in_uvFilterSize" value="float[] 0 0 " />
+                </shader>
+                <shader type="shader" name="as_maya_bump2d" layer="bump2d4">
+                    <parameter name="in_adjustEdges" value="int 0" />
+                    <parameter name="in_bumpDepth" value="float 1" />
+                    <parameter name="in_bumpFilter" value="float 1" />
+                    <parameter name="in_bumpFilterOffset" value="float 0" />
+                    <parameter name="in_bumpInterp" value="int 0" />
+                    <parameter name="in_bumpValue" value="float 1" />
+                    <parameter name="in_infoBits" value="int 0" />
+                    <parameter name="in_normalMapFlipG" value="int 0" />
+                    <parameter name="in_normalMapFlipR" value="int 0" />
+                    <parameter name="in_normalMapMode" value="int 0" />
+                    <parameter name="in_normalMapSwapRG" value="int 0" />
+                    <parameter name="in_rayOrigin" value="point 0 0 0" />
+                    <parameter name="in_refPointCamera" value="point 0 0 0" />
+                    <parameter name="in_refPointObj" value="point 0 0 0" />
+                    <parameter name="in_tangentUCamera" value="vector 0 0 0" />
+                    <parameter name="in_tangentVCamera" value="vector 0 1 0" />
+                    <parameter name="in_uvFilterSize" value="float[] 0 0 " />
+                    <parameter name="in_xPixelAngle" value="float 0.002053" />
+                </shader>
+                <shader type="shader" name="as_maya_lambert" layer="lambert12">
+                    <parameter name="in_ambientColor" value="color 0 0 0" />
+                    <parameter name="in_color" value="color 0.5 0.5 0.5" />
+                    <parameter name="in_diffuse" value="float 0.8" />
+                    <parameter name="in_glowIntensity" value="float 0" />
+                    <parameter name="in_hideSource" value="int 0" />
+                    <parameter name="in_incandescence" value="color 0 0 0" />
+                    <parameter name="in_matteOpacity" value="float 1" />
+                    <parameter name="in_matteOpacityMode" value="int 2" />
+                    <parameter name="in_shadowAttenuation" value="float 0.5" />
+                    <parameter name="in_translucence" value="float 0" />
+                    <parameter name="in_translucenceDepth" value="float 0.5" />
+                    <parameter name="in_translucenceFocus" value="float 0.5" />
+                    <parameter name="in_transparency" value="color 0 0 0" />
+                </shader>
+                <shader type="surface" name="as_maya_closure2Surface" layer="closureToSurface">
+                </shader>
+                <connect_shaders src_layer="place2dTexture8" src_param="out_outUV" dst_layer="file5" dst_param="in_uvCoord" />
+                <connect_shaders src_layer="place2dTexture8" src_param="out_outUvFilterSize" dst_layer="file5" dst_param="in_uvFilterSize" />
+                <connect_shaders src_layer="file5" src_param="out_outAlpha" dst_layer="bump2d4" dst_param="in_bumpValue" />
+                <connect_shaders src_layer="file5" src_param="out_outColor" dst_layer="bump2d4" dst_param="in_normalMap" />
+                <connect_shaders src_layer="bump2d4" src_param="out_outNormal" dst_layer="lambert12" dst_param="in_normalCamera" />
+                <connect_shaders src_layer="lambert12" src_param="out_outColor" dst_layer="closureToSurface" dst_param="in_input" />
+            </shader_group>
+            <material name="lambert12SG_material" model="osl_material">
+                <parameter name="osl_surface" value="lambert12_shader_group" />
+            </material>
+            <material name="|areaLight1|areaLightShape1_area_light_back_material" model="generic_material" />
+            <material name="|areaLight1|areaLightShape1_area_light_material" model="osl_material">
+                <parameter name="osl_surface" value="areaLightShape1_shader_group" />
+            </material>
+            <object name="|areaLight1|areaLightShape1" model="mesh_object">
+                <parameter name="height" value="2" />
+                <parameter name="primitive" value="grid" />
+                <parameter name="resolution_u" value="1" />
+                <parameter name="resolution_v" value="1" />
+                <parameter name="width" value="2" />
+            </object>
+            <object name="|pSphere1|pSphereShape1" model="mesh_object">
+                <parameter name="primitive" value="sphere" />
+                <parameter name="radius" value="1.0" />
+                <parameter name="resolution_u" value="32" />
+                <parameter name="resolution_v" value="32" />
+            </object>
+            <object_instance name="|areaLight1|areaLightShape1_instance" object="|areaLight1|areaLightShape1">
+                <parameters name="visibility">
+                    <parameter name="camera" value="true" />
+                    <parameter name="diffuse" value="true" />
+                    <parameter name="glossy" value="true" />
+                    <parameter name="light" value="true" />
+                    <parameter name="shadow" value="true" />
+                    <parameter name="specular" value="true" />
+                </parameters>
+                <transform>
+                    <matrix>
+                        3.237800869858885 0.000000000000000 0.000000000000000 0.000000000000000
+                        0.000000000000000 -3.237800869858885 -0.000000000000001 14.846208121768621
+                        0.000000000000000 0.000000000000001 -3.237800869858885 9.015488882157184
+                        0.000000000000000 0.000000000000000 0.000000000000000 1.000000000000000
+                    </matrix>
+                </transform>
+                <assign_material slot="default" side="front" material="|areaLight1|areaLightShape1_area_light_material" />
+                <assign_material slot="default" side="back" material="|areaLight1|areaLightShape1_area_light_back_material" />
+            </object_instance>
+            <object_instance name="|pSphere1|pSphereShape1_instance" object="|pSphere1|pSphereShape1">
+                <parameters name="visibility">
+                    <parameter name="camera" value="true" />
+                    <parameter name="diffuse" value="true" />
+                    <parameter name="glossy" value="true" />
+                    <parameter name="light" value="true" />
+                    <parameter name="shadow" value="true" />
+                    <parameter name="specular" value="true" />
+                </parameters>
+                <transform>
+                    <matrix>
+                        3.268470455432697 0.000000000000000 0.000000000000000 0.000000000000000
+                        0.000000000000000 3.268470455432697 0.000000000000000 4.903301191582202
+                        0.000000000000000 0.000000000000000 3.268470455432697 4.186653171429538
+                        0.000000000000000 0.000000000000000 0.000000000000000 1.000000000000000
+                    </matrix>
+                </transform>
+                <assign_material slot="default" side="front" material="lambert12SG_material" />
+                <assign_material slot="default" side="back" material="lambert12SG_material" />
+            </object_instance>
+        </assembly>
+        <assembly_instance name="assembly_inst" assembly="assembly">
+        </assembly_instance>
+    </scene>
+    <output>
+        <frame name="beauty">
+            <parameter name="camera" value="|camera1|cameraShape1" />
+            <parameter name="color_space" value="linear_rgb" />
+            <parameter name="pixel_format" value="float" />
+            <parameter name="resolution" value="512 512" />
+            <parameter name="tile_size" value="64 64" />
+        </frame>
+    </output>
+    <configurations>
+        <configuration name="final" base="base_final">
+            <parameter name="frame_renderer" value="generic" />
+            <parameter name="lighting_engine" value="pt" />
+            <parameter name="pixel_renderer" value="uniform" />
+            <parameter name="sample_generator" value="generic" />
+            <parameter name="sample_renderer" value="generic" />
+            <parameter name="sampling_mode" value="qmc" />
+            <parameter name="shading_result_framebuffer" value="ephemeral" />
+            <parameter name="tile_renderer" value="generic" />
+            <parameters name="adaptive_pixel_renderer">
+                <parameter name="enable_diagnostics" value="false" />
+                <parameter name="max_samples" value="256" />
+                <parameter name="min_samples" value="16" />
+                <parameter name="quality" value="2" />
+            </parameters>
+            <parameters name="drt">
+                <parameter name="dl_light_samples" value="1" />
+                <parameter name="enable_ibl" value="true" />
+                <parameter name="ibl_env_samples" value="1" />
+                <parameter name="max_path_length" value="0" />
+                <parameter name="rr_min_path_length" value="3" />
+            </parameters>
+            <parameters name="generic_frame_renderer">
+                <parameter name="passes" value="1" />
+            </parameters>
+            <parameters name="pt">
+                <parameter name="dl_light_samples" value="1" />
+                <parameter name="enable_caustics" value="false" />
+                <parameter name="enable_dl" value="true" />
+                <parameter name="enable_ibl" value="true" />
+                <parameter name="ibl_env_samples" value="1" />
+                <parameter name="max_path_length" value="0" />
+                <parameter name="max_ray_intensity" value="1" />
+                <parameter name="next_event_estimation" value="true" />
+                <parameter name="rr_min_path_length" value="3" />
+            </parameters>
+            <parameters name="sppm">
+                <parameter name="alpha" value="0.7" />
+                <parameter name="dl_mode" value="rt" />
+                <parameter name="enable_caustics" value="true" />
+                <parameter name="enable_ibl" value="true" />
+                <parameter name="env_photons_per_pass" value="1000000" />
+                <parameter name="initial_radius" value="0.1" />
+                <parameter name="light_photons_per_pass" value="1000000" />
+                <parameter name="max_photons_per_estimate" value="100" />
+                <parameter name="path_tracing_max_path_length" value="0" />
+                <parameter name="path_tracing_rr_min_path_length" value="6" />
+                <parameter name="photon_tracing_max_path_length" value="0" />
+                <parameter name="photon_tracing_rr_min_path_length" value="6" />
+                <parameter name="photon_type" value="poly" />
+            </parameters>
+            <parameters name="uniform_pixel_renderer">
+                <parameter name="decorrelate_pixels" value="true" />
+                <parameter name="force_antialiasing" value="false" />
+                <parameter name="samples" value="16" />
+            </parameters>
+        </configuration>
+        <configuration name="interactive" base="base_interactive">
+            <parameter name="frame_renderer" value="progressive" />
+            <parameter name="lighting_engine" value="pt" />
+            <parameter name="pixel_renderer" value="uniform" />
+            <parameter name="sample_generator" value="generic" />
+            <parameter name="sample_renderer" value="generic" />
+            <parameter name="sampling_mode" value="qmc" />
+            <parameter name="tile_renderer" value="generic" />
+            <parameters name="drt">
+                <parameter name="dl_light_samples" value="1" />
+                <parameter name="enable_ibl" value="true" />
+                <parameter name="ibl_env_samples" value="1" />
+                <parameter name="max_path_length" value="0" />
+                <parameter name="rr_min_path_length" value="3" />
+            </parameters>
+            <parameters name="progressive_frame_renderer">
+                <parameter name="max_fps" value="5" />
+            </parameters>
+            <parameters name="pt">
+                <parameter name="dl_light_samples" value="1" />
+                <parameter name="enable_caustics" value="false" />
+                <parameter name="enable_dl" value="true" />
+                <parameter name="enable_ibl" value="true" />
+                <parameter name="ibl_env_samples" value="1" />
+                <parameter name="max_path_length" value="0" />
+                <parameter name="max_ray_intensity" value="1" />
+                <parameter name="next_event_estimation" value="true" />
+                <parameter name="rr_min_path_length" value="3" />
+            </parameters>
+        </configuration>
+    </configurations>
+</project>

src/appleseed/renderer/kernel/shading/oslshadergroupexec.cpp
@@ -157,18 +157,7 @@ void OSLShaderGroupExec::execute_bump(
             shading_point,
             VisibilityFlags::CameraRay);
 
-        CompositeSurfaceClosure c(
-            Basis3f(shading_point.get_shading_basis()),
-            shading_point.get_osl_shader_globals().Ci,
-            m_arena);
-
-        // Pick a shading basis from one of the BSDF closures.
-        if (c.get_closure_count() > 0)
-        {
-            const size_t index = c.choose_closure(s[1]);
-            shading_point.set_shading_basis(
-                Basis3d(c.get_closure_shading_basis(index)));
-        }
+        choose_bsdf_closure_shading_basis(shading_point, s);
     }
 }
 
@@ -232,6 +221,23 @@ void OSLShaderGroupExec::do_execute(
 #endif
         *shader_group.shader_group_ref(),
         shading_point.get_osl_shader_globals());
+}
+
+void OSLShaderGroupExec::choose_bsdf_closure_shading_basis(
+    const ShadingPoint&             shading_point,
+    const Vector2f&                 s) const
+{
+    CompositeSurfaceClosure c(
+        Basis3f(shading_point.get_shading_basis()),
+        shading_point.get_osl_shader_globals().Ci,
+        m_arena);
+
+    if (c.get_closure_count() > 0)
+    {
+        const size_t index = c.choose_closure(s[1]);
+        shading_point.set_shading_basis(
+            Basis3d(c.get_closure_shading_basis(index)));
+    }
 }
 
 }   // namespace renderer

src/appleseed/renderer/kernel/shading/oslshadergroupexec.h
@@ -120,6 +120,10 @@ class OSLShaderGroupExec
         const ShaderGroup&              shader_group,
         const ShadingPoint&             shading_point,
         const VisibilityFlags::Type     ray_flags) const;
+
+    void choose_bsdf_closure_shading_basis(
+        const ShadingPoint&             shading_point,
+        const foundation::Vector2f&     s) const;
 };
 
 }       // namespace renderer

src/appleseed/renderer/kernel/shading/shadingcontext.cpp
@@ -158,4 +158,11 @@ void ShadingContext::execute_osl_surface_shader(
         alpha);
 }
 
+void ShadingContext::choose_bsdf_closure_shading_basis(
+    const ShadingPoint&     shading_point,
+    const Vector2f&         s) const
+{
+    m_shadergroup_exec.choose_bsdf_closure_shading_basis(shading_point, s);
+}
+
 }   // namespace renderer

src/appleseed/renderer/kernel/shading/shadingcontext.h
@@ -138,6 +138,11 @@ class ShadingContext
         const foundation::Color3f&  color,
         const float                 alpha) const;
 
+    // Choose one of the bsdf closures and set its shading basis in shading point.
+    void choose_bsdf_closure_shading_basis(
+        const ShadingPoint&         shading_point,
+        const foundation::Vector2f& s) const;
+
   private:
     const Intersector&              m_intersector;
     Tracer&                         m_tracer;

commit f4d24cd72578970f34758532887227a09318e418

Fixed bug introduced in bfcbd5d06fc6a557

src/appleseed/renderer/kernel/lighting/pt/ptlightingengine.cpp
@@ -456,6 +456,7 @@ namespace
 
                     if (material_data.m_shader_group)
                     {
+                        m_sampling_context.split_in_place(2, 1);
                         m_shading_context.choose_bsdf_closure_shading_basis(
                             *vertex.m_shading_point,
                             m_sampling_context.next2<Vector2f>());