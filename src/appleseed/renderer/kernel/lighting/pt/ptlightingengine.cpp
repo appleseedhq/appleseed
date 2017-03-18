2acce8b5a0806922c2fdeb2e8f5caa9bedf856ed
est77 <ramenhdr@gmail.com>
2017-03-18 09:57:51 +0100

    Fix broken OSL subsurface.

--- src/appleseed/renderer/kernel/lighting/pt/ptlightingengine.cpp
+++ src/appleseed/renderer/kernel/lighting/pt/ptlightingengine.cpp
@@ -446,20 +446,23 @@ namespace
                         vertex_aovs);
                 }
 
-                // If we have an OSL shader and this is not the last vertex of the path,
-                // we need to choose one of the closures and set its shading basis into the shading point
-                // for the DirectLightingIntegrator to use it.
-                if (!last_vertex && (m_params.m_enable_dl || m_params.m_enable_ibl))
+                if (vertex.m_bssrdf == 0)
                 {
-                    const Material::RenderData& material_data =
-                        vertex.m_shading_point->get_material()->get_render_data();
-
-                    if (material_data.m_shader_group)
+                    // If we have an OSL shader and this is not the last vertex of the path,
+                    // we need to choose one of the closures and set its shading basis into the shading point
+                    // for the DirectLightingIntegrator to use it.
+                    if (!last_vertex && (m_params.m_enable_dl || m_params.m_enable_ibl))
                     {
-                        m_sampling_context.split_in_place(2, 1);
-                        m_shading_context.choose_bsdf_closure_shading_basis(
-                            *vertex.m_shading_point,
-                            m_sampling_context.next2<Vector2f>());
+                        const Material::RenderData& material_data =
+                            vertex.m_shading_point->get_material()->get_render_data();
+
+                        if (material_data.m_shader_group)
+                        {
+                            m_sampling_context.split_in_place(2, 1);
+                            m_shading_context.choose_bsdf_closure_shading_basis(
+                                *vertex.m_shading_point,
+                                m_sampling_context.next2<Vector2f>());
+                        }
                     }
                 }
 
