79c54aba5a0f2ae3f258d910d0643589b7617bbe
Luis Barrancos <luis.b.barrancos@gmail.com>
2017-05-27 21:36:26 +0800

    Fix metadata for external apps (Gaffer, etc)

--- src/appleseed.shaders/include/appleseed/maya/as_maya_helpers.h
+++ src/appleseed.shaders/include/appleseed/maya/as_maya_helpers.h
@@ -42,6 +42,7 @@
     color in_defaultColor = color(0.5)                                      \
     [[                                                                      \
         string as_maya_attribute_name = "defaultColor",                     \
+        string as_maya_attribute_short_name = "dc",                         \
         int as_maya_attribute_hidden = 1,                                   \
         string label = "Default Color",                                     \
         string page = "Color Balance"                                       \
@@ -49,6 +50,7 @@
     color in_colorGain = color(1.0)                                         \
     [[                                                                      \
         string as_maya_attribute_name = "colorGain",                        \
+        string as_maya_attribute_short_name = "cg",                         \
         int as_maya_attribute_hidden = 1,                                   \
         string label = "Color Gain",                                        \
         string page = "Color Balance"                                       \
@@ -56,6 +58,7 @@
     color in_colorOffset = color(0.0)                                       \
     [[                                                                      \
         string as_maya_attribute_name = "colorOffset",                      \
+        string as_maya_attribute_short_name = "co",                         \
         int as_maya_attribute_hidden = 1,                                   \
         string label = "Color Offset",                                      \
         string page = "Color Balance"                                       \
@@ -63,6 +66,7 @@
     float in_alphaGain = 1.0                                                \
     [[                                                                      \
         string as_maya_attribute_name = "alphaGain",                        \
+        string as_maya_attribute_short_name = "ag",                         \
         int as_maya_attribute_hidden = 1,                                   \
         string label = "Alpha Gain",                                        \
         string page = "Color Balance"                                       \
@@ -70,6 +74,7 @@
     float in_alphaOffset = 0.0                                              \
     [[                                                                      \
         string as_maya_attribute_name = "alphaOffset",                      \
+        string as_maya_attribute_short_name = "ao",                         \
         int as_maya_attribute_hidden = 1,                                   \
         string label = "Alpha Offset",                                      \
         string page = "Color Balance"                                       \
@@ -77,6 +82,7 @@
     int in_alphaIsLuminance = 1                                             \
     [[                                                                      \
         string as_maya_attribute_name = "alphaIsLuminance",                 \
+        string as_maya_attribute_short_name = "ail",                        \
         int as_maya_attribute_hidden = 1,                                   \
         string label = "Alpha Is Luminance",                                \
         string widget = "checkBox",                                         \
@@ -111,6 +117,7 @@
     int in_wrap = 1                                                         \
     [[                                                                      \
         string as_maya_attribute_name = "wrap",                             \
+        string as_maya_attribute_short_name = "w",                          \
         int as_maya_attribute_hidden = 1,                                   \
         string label = "Wrap",                                              \
         string widget = "checkBox",                                         \
@@ -119,6 +126,7 @@
     int in_local = 0                                                        \
     [[                                                                      \
         string as_maya_attribute_name = "local",                            \
+        string as_maya_attribute_short_name = "lo",                         \
         int as_maya_attribute_hidden = 1,                                   \
         string label = "Local",                                             \
         string widget = "checkBox",                                         \
@@ -127,6 +135,7 @@
     float in_blend = 0.0                                                    \
     [[                                                                      \
         string as_maya_attribute_name = "blend",                            \
+        string as_maya_attribute_short_name = "b",                          \
         int as_maya_attribute_hidden = 1,                                   \
         string label = "Blend",                                             \
         string widget = "slider",                                           \
@@ -141,6 +150,7 @@
     float in_uvCoord[2] = {u, v}                                            \
     [[                                                                      \
         string as_maya_attribute_name = "uvCoord",                          \
+        string as_maya_attribute_short_name = "uv",                         \
         string label = "UV Coordinates",                                    \
         string page = "UV Coordinates"                                      \
     ]],                                                                     \
@@ -148,6 +158,7 @@
         UNDEFINED_UVFILTER, UNDEFINED_UVFILTER}                             \
     [[                                                                      \
         string as_maya_attribute_name = "uvFilterSize",                     \
+        string as_maya_attribute_short_name = "fs",                         \
         int as_maya_attribute_hidden = 1,                                   \
         string label = "UV Filter Size",                                    \
         string page = "UV Coordinates"                                      \
@@ -158,49 +169,51 @@
     [[                                                                      \
         string as_maya_attribute_name = "colorProfile",                     \
         int as_maya_attribute_hidden = 1,                                   \
-        string widget = "string",                                           \
-        string label = "Color Profile"                                      \
+        string widget = "null",                                             \
+        string label = "Color Profile",                                     \
     ]],                                                                     \
     int in_ignoreColorSpaceFileRules = 0                                    \
     [[                                                                      \
         string as_maya_attribute_name = "ignoreColorSpaceFileRules",        \
         int as_maya_attribute_hidden = 1,                                   \
         string widget = "checkBox",                                         \
-        string label = "Ignore Color Space File Rules"                      \
+        string label = "Ignore Color Space File Rules",                     \
+        string page = "File Attributes"                                     \
     ]],                                                                     \
     string in_colorSpace = ""                                               \
     [[                                                                      \
         string as_maya_attribute_name = "colorSpace",                       \
         int as_maya_attribute_hidden = 1,                                   \
         string widget = "string",                                           \
-        string label = "Color Space"                                        \
+        string label = "Color Space",                                       \
+        string page = "File Attributes"                                     \
     ]],                                                                     \
     string in_workingSpace = ""                                             \
     [[                                                                      \
         string as_maya_attribute_name = "workingSpace",                     \
         int as_maya_attribute_hidden = 1,                                   \
-        string widget = "string",                                           \
+        string widget = "null",                                             \
         string label = "Working Space"                                      \
     ]],                                                                     \
     int in_colorManagementEnabled = 0                                       \
     [[                                                                      \
         string as_maya_attribute_name = "colorManagementEnabled",           \
         int as_maya_attribute_hidden = 1,                                   \
-        string widget = "checkBox",                                         \
+        string widget = "null",                                             \
         string label = "Color Management Enabled"                           \
     ]],                                                                     \
     int in_colorManagementConfigFileEnabled = 0                             \
     [[                                                                      \
         string as_maya_attribute_name = "colorManagementConfigFileEnabled", \
         int as_maya_attribute_hidden = 1,                                   \
-        string widget = "checkBox",                                         \
+        string widget = "null",                                             \
         string label = "Enable CMS Config"                                  \
     ]],                                                                     \
     string in_colorManagementConfigFilePath = ""                            \
     [[                                                                      \
         string as_maya_attribute_name = "colorManagementConfigFilePath",    \
         int as_maya_attribute_hidden = 1,                                   \
-        string widget = "filename",                                         \
+        string widget = "null",                                             \
         string label = "Color Management Config File Path"                  \
     ]]
 
