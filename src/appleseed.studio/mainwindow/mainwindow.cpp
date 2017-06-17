appleseedhq/appleseed

commit b889755e3e5951f7f032942f6e2ced6391a372b9

Move colors to stylesheet

src/appleseed.studio/mainwindow/mainwindow.cpp
@@ -502,7 +502,6 @@ void MainWindow::build_log_panel()
     LogWidget* log_widget = new LogWidget(m_ui->log_contents);
     m_ui->log_contents->layout()->addWidget(log_widget);
 
-    log_widget->setObjectName("textedit_log");
     log_widget->setUndoRedoEnabled(false);
     log_widget->setLineWrapMode(QTextEdit::NoWrap);
     log_widget->setReadOnly(true);

sandbox/stylesheets/default.qss
@@ -42,6 +42,7 @@
 #define ACTIVE_BACKGROUND_COLOR             rgb(44, 44, 44)
 #define HOVER_BACKGROUND_COLOR              rgb(190, 140, 50)
 #define SELECTED_BACKGROUND_COLOR           rgb(60, 60, 60)
+#define EDITOR_BACKGROUND_COLOR             rgb(48, 48, 48)
 
 #define DEFAULT_TEXT_COLOR                  rgb(170, 170, 170)
 #define DISABLED_TEXT_COLOR                 rgb(100, 100, 100)
@@ -56,7 +57,7 @@
     #define FIXED_WIDTH_FONT_STYLE          font-family: "Monaco"; font-size: 10px
 #endif
 #ifdef __linux__
-    #define FIXED_WIDTH_FONT_STYLE          font-family: "Courier New"
+    #define FIXED_WIDTH_FONT_STYLE          font-family: "Monospace"
 #endif
 #ifdef __FreeBSD__
     #define FIXED_WIDTH_FONT_STYLE          font-family: "Courier New"
@@ -1151,6 +1152,12 @@ QLabel#a_label
 QWidget#textedit_log
 {
     FIXED_WIDTH_FONT_STYLE;
+}
+
+QWidget#python_editor
+{
+    FIXED_WIDTH_FONT_STYLE;
+    background-color: EDITOR_BACKGROUND_COLOR
 }
 
 QPushButton#bind_entity_button,

src/appleseed.studio/mainwindow/pythonconsole/pythoneditor.cpp
@@ -48,6 +48,7 @@ namespace studio {
 PythonEditor::PythonEditor(QWidget* parent)
   : QPlainTextEdit(parent)
 {
+    setObjectName("python_editor");
     setUndoRedoEnabled(true);
     setLineWrapMode(QPlainTextEdit::WidgetWidth);
     setReadOnly(false);
@@ -58,12 +59,6 @@ PythonEditor::PythonEditor(QWidget* parent)
         Qt::TextEditorInteraction);
 
     new PythonSyntaxHighlighter(this->document());
-
-    setStyleSheet("background-color:#303030");
-
-    QFont font("Monospace");
-    font.setStyleHint(QFont::Monospace);
-    setFont(font);
 
     line_number_area = new LineNumberArea(this);
 

commit 91d09552077cc3cd7863986172d27a80ab8ca432

Fix UI regression
(Introduced in b889755e3e5951f7f032942f6e2ced6391a372b9.)

src/appleseed.studio/mainwindow/mainwindow.cpp
@@ -502,6 +502,7 @@ void MainWindow::build_log_panel()
     LogWidget* log_widget = new LogWidget(m_ui->log_contents);
     m_ui->log_contents->layout()->addWidget(log_widget);
 
+    log_widget->setObjectName("textedit_log");
     log_widget->setUndoRedoEnabled(false);
     log_widget->setLineWrapMode(QTextEdit::NoWrap);
     log_widget->setReadOnly(true);