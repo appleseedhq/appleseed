appleseedhq/appleseed

commit f1fdd0424b8a6104f6712d04538d70e68c8e6736

Fix small bugs in numeric and colormap input widgets

src/appleseed.studio/mainwindow/project/entityinputwidget.cpp
@@ -168,12 +168,12 @@ ColorMapInputWidget::ColorMapInputWidget(QWidget* parent)
     m_line_edit->setMaximumWidth(120);
     connect(m_line_edit, SIGNAL(returnPressed()), SIGNAL(signal_changed()));
 
-    DoubleSlider* slider = new DoubleSlider(Qt::Horizontal, this);
-    slider->setRange(0.0, 1.0);
-    slider->setPageStep(0.1);
-    new MouseWheelFocusEventFilter(slider);
-    new LineEditDoubleSliderAdaptor(m_line_edit, slider);
-    connect(slider, SIGNAL(valueChanged(int)), SIGNAL(signal_changed()));
+    m_slider = new DoubleSlider(Qt::Horizontal, this);
+    m_slider->setRange(0.0, 1.0);
+    m_slider->setPageStep(0.1);
+    new MouseWheelFocusEventFilter(m_slider);
+    new LineEditDoubleSliderAdaptor(m_line_edit, m_slider);
+    connect(m_slider, SIGNAL(valueChanged(int)), SIGNAL(signal_changed()));
 
     QWidget* bind_button = new QPushButton("Bind", this);
     bind_button->setObjectName("bind_entity_button");
@@ -185,7 +185,7 @@ ColorMapInputWidget::ColorMapInputWidget(QWidget* parent)
     line_edit_button_group_layout->setMargin(0);
     line_edit_button_group_layout->setSpacing(6);
     line_edit_button_group_layout->addWidget(m_line_edit);
-    line_edit_button_group_layout->addWidget(slider);
+    line_edit_button_group_layout->addWidget(m_slider);
     line_edit_button_group_layout->addWidget(bind_button);
     line_edit_button_group->setLayout(line_edit_button_group_layout);
 
@@ -229,28 +229,19 @@ void ColorMapInputWidget::set_default_value(const QString& default_value)
     m_default_value = default_value;
 }
 
-namespace
-{
-    // todo: ask the InputBinder instead.
-    bool is_numeric_value(const QString& s)
-    {
-        try
-        {
-            from_string<double>(s);
-            return true;
-        }
-        catch (const ExceptionStringConversionError&)
-        {
-            return false;
-        }
-    }
-}
-
 void ColorMapInputWidget::set_value(const QString& value)
 {
-    m_line_edit->setText(value);
-    m_entity_button->setText(value);
-    setCurrentIndex(value.isEmpty() || is_numeric_value(value) ? 0 : 1);
+    try
+    {
+        const double x = from_string<double>(value);
+        m_slider->setValue(x);
+        setCurrentIndex(0);
+    }
+    catch (const ExceptionStringConversionError&)
+    {
+        m_entity_button->setText(value);
+        setCurrentIndex(1);
+    }
 }
 
 QString ColorMapInputWidget::get_value() const

src/appleseed.studio/mainwindow/project/entityeditor.cpp
@@ -300,7 +300,7 @@ auto_ptr<IInputWidgetProxy> EntityEditor::create_numeric_input_widgets(const Dic
     slider->setRange(min_value, max_value);
     slider->setPageStep((max_value - min_value) / 10.0);
     new MouseWheelFocusEventFilter(slider);
-    new LineEditDoubleSliderAdaptor(line_edit, slider);
+    LineEditDoubleSliderAdaptor* adaptor = new LineEditDoubleSliderAdaptor(line_edit, slider);
     connect(slider, SIGNAL(valueChanged(int)), SLOT(slot_apply()));
 
     if (should_be_focused(metadata))
@@ -323,11 +323,10 @@ auto_ptr<IInputWidgetProxy> EntityEditor::create_numeric_input_widgets(const Dic
         slider->hide();
     }
 
-    auto_ptr<IInputWidgetProxy> widget_proxy(new LineEditProxy(line_edit));
+    const double value = metadata.strings().get<double>("value");
+    adaptor->slot_set_line_edit_value(value);
 
-    // Setting the value via the slider allows to benefit from the string
-    // formatting in LineEditDoubleSliderAdaptor::slot_set_line_edit_value().
-    slider->setValue(metadata.strings().get<double>("value"));
+    auto_ptr<IInputWidgetProxy> widget_proxy(new LineEditProxy(line_edit));
 
     return widget_proxy;
 }

src/appleseed.studio/mainwindow/project/entityinputwidget.h
@@ -37,6 +37,7 @@
 #include <QStackedWidget>
 
 // Forward declarations.
+namespace appleseed { namespace studio { class DoubleSlider; }}
 class QLineEdit;
 class QPushButton;
 class QString;
@@ -123,6 +124,7 @@ class ColorMapInputWidget
   private:
     QString         m_default_value;
     QLineEdit*      m_line_edit;
+    DoubleSlider*   m_slider;
     QPushButton*    m_entity_button;
 };
 

commit 82093cdc659f21b0f504d0c4d107dc4b03ec6ddd

Fix regression in colormap input widget
Regression introduced in f1fdd0424b8a6104f6712d04538d70e68c8e6736: Fix small bugs in numeric and colormap input widgets

src/appleseed.studio/mainwindow/project/entityinputwidget.cpp
@@ -239,6 +239,7 @@ void ColorMapInputWidget::set_value(const QString& value)
     }
     catch (const ExceptionStringConversionError&)
     {
+        m_line_edit->setText(value);
         m_entity_button->setText(value);
         setCurrentIndex(1);
     }