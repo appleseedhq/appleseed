5f4f31e3f7aa48581b7b800820637acdd3fc5dff
gospodnetic <petra.gospodnetic@itwm.fraunhofer.de>
2017-06-29 11:29:56 +0200

    Revert bbox of a solomn NPL to be really small

--- src/appleseed/renderer/kernel/lighting/lighttypes.cpp
+++ src/appleseed/renderer/kernel/lighting/lighttypes.cpp
@@ -78,8 +78,8 @@ foundation::AABB3d NonPhysicalLightSource::get_bbox() const
                                           .extract_translation();
     // Non physical light has no real size - hence we are fixing some small value for the bbox
     return foundation::AABB3d(
-                foundation::Vector3d(position[0] - 1.0, position[1] - 1.0, position[2] - 1.0),
-                foundation::Vector3d(position[0] + 1.0, position[1] + 1.0, position[2] + 1.0));
+                foundation::Vector3d(position[0] - 0.001, position[1] - 0.001, position[2] - 0.001),
+                foundation::Vector3d(position[0] + 0.001, position[1] + 0.001, position[2] + 0.001));
 }
 
 Spectrum NonPhysicalLightSource::get_intensity() const
