f0f9b01ed65ee5b898bfac325d2da5270954e4d8
glebmish <gleb.mishchenko.96@gmail.com>
2017-08-14 20:58:36 +0300

    Process relative paths of textures correctly

--- src/appleseed.python/studio/helpers.py
+++ src/appleseed.python/studio/helpers.py
@@ -2,6 +2,7 @@ import appleseed as asr
 import appleseed.studio as studio
 from appleseed.textureconverter import *
 
+import os
 import sys
 import logging
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
@@ -18,8 +19,16 @@ def get_textures(container):
     return textures
 
 
+def get_full_path(texture_path, project):
+    if os.path.isabs(texture_path):
+        return texture_path
+    else:
+        return project.qualify_path(texture_path)
+
+
 def convert_all_textures_to_tx(maketx_path):
-    scene = studio.current_project().get_scene()
+    project = studio.current_project()
+    scene = project.get_scene()
     textures = get_textures(scene)
 
     tx_converter = TextureConverter(maketx_path)
@@ -28,14 +37,23 @@ def convert_all_textures_to_tx(maketx_path):
         texture_parameters = texture.get_parameters()
         texture_path = texture_parameters['filename']
 
-        if texture.get_model() != 'disk_texture_2d' or texture_path.endswith('.tx'):
-            logging.debug('Skipped converting {}'.format(texture_path))
+        logging.debug("Processing {}".format(texture_path))
+        print texture_path
+
+        texture_full_path = get_full_path(texture_path, project)
+
+        if texture.get_model() != 'disk_texture_2d' or texture_full_path.endswith('.tx'):
+            logging.debug('Skipped converting {}'.format(texture_full_path))
             continue
 
-        new_texture_path = tx_converter.convert(texture_path)
-        if new_texture_path is None:
-            logging.info('Skipped converting of {}'.format(texture_path))
+        new_texture_full_path = tx_converter.convert(texture_full_path)
+
+        if new_texture_full_path is None:
+            logging.info('Skipped converting of {}'.format(texture_full_path))
         else:
+            new_texture_path = os.path.join(os.path.dirname(texture_path),
+                                            os.path.basename(new_texture_full_path))
+
             texture_parameters['filename'] = new_texture_path
             texture.set_parameters(texture_parameters)
             logging.info('{} converted to {}'.format(texture_path, new_texture_path))
