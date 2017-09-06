f116c371f33636983373b9a45168d8fe90452a8e
Roger Leigh <rleigh@codelibre.net>
2017-09-06 19:05:22 +0100

    platform/thread.h: Correct spinlock initialisation with C++11
    This matches the way it's initialised internally by Boost.
    The previous initialisation method broke with clang/llvm 4.0.0
    on FreeBSD with C++11 enabled; copy and assignment operators
    are deleted which this logic was attempting to use.

--- src/appleseed/foundation/platform/thread.h
+++ src/appleseed/foundation/platform/thread.h
@@ -296,9 +296,8 @@ class APPLESEED_DLLSYMBOL ThreadFlag
 
 inline Spinlock::Spinlock()
 {
-    // todo: is there a simpler way to initialize m_sp in a platform-independent manner?
     boost::detail::spinlock initialized_sp = BOOST_DETAIL_SPINLOCK_INIT;
-    m_sp = initialized_sp;
+    std::memcpy(&m_sp, &initialized_sp, sizeof(initialized_sp));
 }
 
 inline bool Spinlock::try_lock()
