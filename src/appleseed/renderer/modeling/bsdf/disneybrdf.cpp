appleseedhq/appleseed

commit 9be29ed9c46ef9ea4e5e9538181b62fbcd461e91

Consider sheen as glossy, not diffuse

src/appleseed/renderer/modeling/bsdf/disneybrdf.cpp
@@ -266,10 +266,10 @@ namespace
                     sample.m_shading_basis,
                     sample.m_outgoing.get_value(),
                     incoming,
-                    sample.m_value.m_diffuse);
+                    sample.m_value.m_glossy);
             assert(sample.m_probability > 0.0f);
 
-            sample.m_mode = ScatteringMode::Diffuse;
+            sample.m_mode = ScatteringMode::Glossy;
             sample.m_incoming = Dual3f(incoming);
             sample.compute_reflected_differentials();
         }
@@ -496,7 +496,6 @@ namespace
 
             if (weights[SheenComponent] > 0.0f)
             {
-                Spectrum sheen;
                 sample.m_probability +=
                     weights[SheenComponent] *
                     DisneySheenComponent().evaluate(
@@ -504,8 +503,7 @@ namespace
                         sample.m_shading_basis,
                         outgoing,
                         incoming,
-                        sheen);
-                sample.m_value.m_diffuse += sheen;
+                        sample.m_value.m_glossy);
             }
 
             if (weights[SpecularComponent] > 0.0f)
@@ -585,7 +583,6 @@ namespace
             float weights[NumComponents];
             compute_component_weights(values, modes, weights);
 
-            value.set(0.0f);
             float pdf = 0.0f;
 
             if (weights[DiffuseComponent] > 0.0f)
@@ -602,7 +599,6 @@ namespace
 
             if (weights[SheenComponent] > 0.0f)
             {
-                Spectrum sheen;
                 pdf +=
                     weights[SheenComponent] *
                     DisneySheenComponent().evaluate(
@@ -610,8 +606,7 @@ namespace
                         shading_basis,
                         outgoing,
                         incoming,
-                        sheen);
-                value.m_diffuse += sheen;
+                        value.m_glossy);
             }
 
             if (weights[SpecularComponent] > 0.0f)
@@ -622,6 +617,8 @@ namespace
                     values->m_anisotropic,
                     alpha_x,
                     alpha_y);
+
+                Spectrum spec;
                 const GGXMDF ggx_mdf;
                 pdf +=
                     weights[SpecularComponent] *
@@ -636,7 +633,8 @@ namespace
                         DisneySpecularFresnelFun(*values),
                         cos_in,
                         cos_on,
-                        value.m_glossy);
+                        spec);
+                value.m_glossy += spec;
             }
 
             if (weights[ClearcoatComponent] > 0.0f)
@@ -752,23 +750,19 @@ namespace
             float                       weights[NumComponents])
         {
             if (ScatteringMode::has_diffuse(modes))
-            {
                 weights[DiffuseComponent] = lerp(values->m_precomputed.m_base_color_luminance, 0.0f, values->m_metallic);
-                weights[SheenComponent] = lerp(values->m_sheen, 0.0f, values->m_metallic);
-            }
             else
-            {
                 weights[DiffuseComponent] = 0.0f;
-                weights[SheenComponent] = 0.0f;
-            }
 
             if (ScatteringMode::has_glossy(modes))
             {
+                weights[SheenComponent] = lerp(values->m_sheen, 0.0f, values->m_metallic);
                 weights[SpecularComponent] = lerp(values->m_specular, 1.0f, values->m_metallic);
                 weights[ClearcoatComponent] = values->m_clearcoat * 0.25f;
             }
             else
             {
+                weights[SheenComponent] = 0.0f;
                 weights[SpecularComponent] = 0.0f;
                 weights[ClearcoatComponent] = 0.0f;
             }

src/appleseed/renderer/modeling/bsdf/sheenbrdf.cpp
@@ -76,7 +76,7 @@ namespace
         SheenBRDFImpl(
             const char*                 name,
             const ParamArray&           params)
-          : BSDF(name, Reflective, ScatteringMode::Diffuse, params)
+          : BSDF(name, Reflective, ScatteringMode::Glossy, params)
         {
             m_inputs.declare("reflectance", InputFormatSpectralReflectance);
             m_inputs.declare("reflectance_multiplier", InputFormatFloat, "1.0");
@@ -100,7 +100,7 @@ namespace
             const int                   modes,
             BSDFSample&                 sample) const override
         {
-            if (!ScatteringMode::has_diffuse(modes))
+            if (!ScatteringMode::has_glossy(modes))
                 return;
 
             // No reflection below the shading surface.
@@ -121,13 +121,13 @@ namespace
             const float fh = pow_int<5>(saturate(1.0f - cos_ih));
 
             const InputValues* values = static_cast<const InputValues*>(data);
-            sample.m_value.m_diffuse = values->m_reflectance;
-            sample.m_value.m_diffuse *= fh * values->m_reflectance_multiplier;
-            sample.m_value.m_beauty = sample.m_value.m_diffuse;
+            sample.m_value.m_glossy = values->m_reflectance;
+            sample.m_value.m_glossy *= fh * values->m_reflectance_multiplier;
+            sample.m_value.m_beauty = sample.m_value.m_glossy;
 
             sample.m_probability = RcpTwoPi<float>();
 
-            sample.m_mode = ScatteringMode::Diffuse;
+            sample.m_mode = ScatteringMode::Glossy;
             sample.m_incoming = Dual3f(incoming);
             sample.compute_reflected_differentials();
         }
@@ -143,7 +143,7 @@ namespace
             const int                   modes,
             DirectShadingComponents&    value) const override
         {
-            if (!ScatteringMode::has_diffuse(modes))
+            if (!ScatteringMode::has_glossy(modes))
                 return 0.0f;
 
             // No reflection below the shading surface.
@@ -160,10 +160,9 @@ namespace
 
             const InputValues* values = static_cast<const InputValues*>(data);
 
-            value.set(0.0f);
-            value.m_diffuse = values->m_reflectance;
-            value.m_diffuse *= fh * values->m_reflectance_multiplier;
-            value.m_beauty = value.m_diffuse;
+            value.m_glossy = values->m_reflectance;
+            value.m_glossy *= fh * values->m_reflectance_multiplier;
+            value.m_beauty = value.m_glossy;
 
             return RcpTwoPi<float>();
         }
@@ -176,7 +175,7 @@ namespace
             const Vector3f&             incoming,
             const int                   modes) const override
         {
-            if (!ScatteringMode::has_diffuse(modes))
+            if (!ScatteringMode::has_glossy(modes))
                 return 0.0f;
 
             // No reflection below the shading surface.

commit 7344d8a52da41729f420549d8b0fb345486ff3a5

Fixed regression introduced in 9be29ed9c46ef9ea4e5e9538181b62fbcd461e91

src/appleseed/renderer/modeling/bsdf/disneybrdf.cpp
@@ -496,6 +496,7 @@ namespace
 
             if (weights[SheenComponent] > 0.0f)
             {
+                Spectrum sheen;
                 sample.m_probability +=
                     weights[SheenComponent] *
                     DisneySheenComponent().evaluate(
@@ -503,7 +504,8 @@ namespace
                         sample.m_shading_basis,
                         outgoing,
                         incoming,
-                        sample.m_value.m_glossy);
+                        sheen);
+                sample.m_value.m_glossy += sheen;
             }
 
             if (weights[SpecularComponent] > 0.0f)