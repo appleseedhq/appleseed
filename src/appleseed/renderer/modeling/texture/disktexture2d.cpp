55d40adee7dd680a8cc1ac276347e28f9d2be06d
Franz Beaune <franz@appleseedhq.net>
2017-10-19 16:36:35 +0200

    Don't collect empty asset paths

--- src/appleseed/renderer/modeling/texture/disktexture2d.cpp
+++ src/appleseed/renderer/modeling/texture/disktexture2d.cpp
@@ -125,7 +125,11 @@ namespace
         void collect_asset_paths(StringArray& paths) const override
         {
             if (m_params.strings().exist("filename"))
-                paths.push_back(m_params.get("filename"));
+            {
+                const char* filename = m_params.get("filename");
+                if (filename[0] != '\0')
+                    paths.push_back(filename);
+            }
         }
 
         void update_asset_paths(const StringDictionary& mappings) override
