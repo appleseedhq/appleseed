c494692632b036ae61e76886fbd8d24ed517ebad
Franz Beaune <franz@appleseedhq.net>
2017-11-15 22:47:53 +0100

    Move textureprocessor Python plugin to samples

--- sandbox/samples/python/studio/plugins/textureprocessor/__init__.py
+++ sandbox/samples/python/studio/plugins/textureprocessor/__init__.py
@@ -1,3 +1,31 @@
+
+#
+# This source file is part of appleseed.
+# Visit http://appleseedhq.net/ for additional information and resources.
+#
+# This software is released under the MIT license.
+#
+# Copyright (c) 2017 Esteban Tovagliari, The appleseedhq Organization
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
 import appleseed as asr
 import appleseed.studio as studio
 from appleseed.studio import ui
@@ -14,7 +42,7 @@ logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 def register():
     menu = ui.find_or_create_menu("Plugins")
 
-    act = QtWidgets.QAction("Convert textures", menu)
+    act = QtWidgets.QAction("Convert Textures", menu)
     act.triggered.connect(convert_all_textures_to_tx)
 
     menu.addAction(act)
@@ -28,7 +56,7 @@ def convert_all_textures_to_tx():
         if os.path.exists(maketx_path):
             return maketx_path
         else:
-            raise Exception('maketx binary is not found')
+            raise Exception('maketx binary not found')
 
     project = studio.current_project()
 
