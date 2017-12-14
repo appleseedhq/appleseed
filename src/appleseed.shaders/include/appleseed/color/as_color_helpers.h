2a765e9d06f5f6da17bcd0d011066257e4e8b3b3
Luis Barrancos <luis.b.barrancos@gmail.com>
2017-12-14 21:43:01 +0800

    Ongoing work with texture layering

--- src/appleseed.shaders/include/appleseed/color/as_color_helpers.h
+++ src/appleseed.shaders/include/appleseed/color/as_color_helpers.h
@@ -215,4 +215,243 @@ float as_luminance(color in_C, string colorspace)
     return as_luminance_D65(in_C, colorspace);
 }
 
+//
+// Reference:
+//
+//      https://help.thefoundry.co.uk/nuke/8.0/content/user_guide/merging/merge_operations.html
+//      https://support.solidangle.com/display/A5AFMUG/Composite
+//      https://renderman.pixar.com/resources/RenderMan_20/PxrBlend.html
+
+color texture_blend(
+    color A,
+    float A_alpha,
+    color B,
+    float B_alpha,
+    int mode,
+    int clamp_output,
+    output float out_alpha)
+{
+    // NUke modes, or prman
+    // burn, dodge, darken, darker color
+
+    color out_rgb = color(0);
+
+    if (mode == 0)
+    {
+        // passthrough A
+        out_rgb = A;
+        //out_alpha = A_alpha;
+    }
+    else if (mode == 1)
+    {
+        // passthrough B
+        out_rgb = B;
+        //out_alpha = B_alpha;
+    }
+    else if (mode == 2)
+    {
+        // Atop: Ab + B(1-a)
+        //out_rgb = A * B_alpha + B * (1.0 - A_alpha);
+    }
+    else if (mode == 3)
+    {
+        // average: (A+B)/2
+        out_rgb = (A + B) / 2.0;
+        //out_alpha = (A_alpha + B_alpha) / 2.0;
+    }
+    else if (mode == 4)
+    {
+        // color-burn:
+        out_rgb = 1.0 - (1.0 - B) / A;
+        //out_alpha = 1.0 - (1.0 - B_alpha) / A_alpha;
+    }
+    else if (mode == 5)
+    {
+        // color dodge:
+        out_rgb = B / (1.0 - A);
+        //out_alpha = B_alpha / (1.0 - A_alpha);
+    }
+    else if (mode == 6)
+    {
+        // conjoint-over
+        out_rgb = (A_alpha > B_alpha)
+            ? A + B * (1.0 - A_alpha) / B_alpha
+            : A;
+    }
+    else if (mode == 7)
+    {
+        // difference
+        out_rgb = abs(A - B);
+        //out_alpha = abs(A_alpha - B_alpha);
+    }
+    else if (mode == 8)
+    {
+        // disjoint over
+        out_rgb = (A_alpha + B_alpha < 1.0)
+            ? A + B
+            : A + B * (1.0 - A_alpha) / B_alpha;
+    }
+    else if (mode == 9)
+    {
+        // divide
+        out_rgb = (B != 0) ? A / B : color(0);
+        //out_alpha = (B_alpha != 0) ? A_alpha / B_alpha : 0.0;
+    }
+    else if (mode == 10)
+    {
+        // exclusion
+        out_rgb = A + B - 2.0 * A * B;
+        //out_alpha = A_alpha + B_alpha - 2.0 * A_alpha * B_alpha;
+    }
+    else if (mode == 11)
+    {
+        // from
+        out_rgb = B - A;
+        //out_alpha = B_alpha - A_alpha;
+    }
+    else if (mode == 12)
+    {
+        // geometric
+        out_rgb = 2.0 * A * B / (A + B);
+        //out_alpha = 2.0 * A_alpha * B_alpha / (A_alpha + B_alpha);
+    }
+    else if (mode == 13)
+    {
+        // hard light, if A<0.5 A * B, else screen
+        // screen A + B - A * B, if A and B in [0,1] else max(A, B)
+
+        if (max(A) < 0.5)
+        {
+            out_rgb = A * B;
+        }
+        else
+        {
+            out_rgb =
+                (min(A) < 0.0 && min(B) < 0.0 && max(A) > 1.0 && max(B) > 1.0)
+                ? max(A, B)
+                : A + B - A * B;
+        }
+    }
+    else if (mode == 14)
+    {
+        // hypot diagonal
+        out_rgb = color(hypot(A[0], B[0]),
+                        hypot(A[1], B[1]),
+                        hypot(A[2], B[2]));
+
+        //out_alpha = hypot(A_alpha, B_alpha);
+    }
+    else if (mode == 15)
+    {
+        // In
+        out_rgb = A * B_alpha;
+    }
+    else if (mode == 16)
+    {
+        // Mask
+        out_rgb = B * A_alpha;
+    }
+    else if (mode == 17)
+    {
+        // Matte
+        out_rgb = mix(B, A, A_alpha);
+    }
+    else if (mode == 18)
+    {
+        // Max
+        out_rgb = max(A, B);
+        //out_alpha = max(A_alpha, B_alpha);
+    }
+    else if (mode == 19)
+    {
+        // Min
+        out_rgb = min(A, B);
+        //out_alpha = min(A_alpha, B_alpha);
+    }
+    else if (mode == 20)
+    {
+        // Minus
+        out_rgb = A - B;
+        //out_alpha = A_alpha - B_alpha;
+    }
+    else if (mode == 21)
+    {
+        // Multiply
+        out_rgb = A * B;
+        //out_alpha = A_alpha * B_alpha;
+    }
+    else if (mode == 22)
+    {
+        // Out
+        out_rgb = A * (1 - B_alpha);
+    }
+    else if (mode == 23)
+    {
+        // Over
+        out_rgb = A + B * (1.0 - A_alpha);
+    }
+    else if (mode == 24)
+    {
+        // Overlay
+        //  if B < 0.5 A * B, else screen
+        // screen A + B - A * B, if A and B in [0,1] else max(A, B)
+
+        if (max(B) < 0.5)
+        {
+            out_rgb = A * B;
+        }
+        else
+        {
+            out_rgb = (min(A) < 0.0 && min(B) < 0.0 &&
+                       max(A) > 1.0 && max(B) > 1.0)
+                ? max(A, B)
+                : A + B - A * B;
+        }
+
+        // Alpha is (or should be) in [0,1].
+        // out_alpha = (B_alpha < 0.5)
+        //    ? A_alpha * B_alpha
+        //    : A_alpha + B_alpha - A_alpha * B_alpha;
+    }
+    else if (mode == 25)
+    {
+        // Plus
+        out_rgb = A + B;
+    }
+    else if (mode == 26)
+    {
+        // Screen
+        out_rgb = (min(A) < 0.0 && min(B) < 0.0 &&
+                   max(A) > 1.0 && max(B) > 1.0)
+            ? max(A, B)
+            : A + B - A * B;
+
+        //out_alpha = A_alpha + B_alpha - A_alpha * B_alpha;
+    }
+    else if (mode == 27)
+    {
+        // Soft light ????
+        ;
+    }
+    else if (mode == 28)
+    {
+        // Stencil
+        out_rgb = B * (1.0 - A_alpha);
+    }
+    else if (mode == 29)
+    {
+        // Under
+        out_rgb = A * (1.0 - B_alpha) + B;
+    }
+    else if (mode == 30)
+    {
+        // Xor
+        out_rgb = A * (1.0 - B_alpha) + B * (1.0 - A_alpha);
+    }
+
+    out_alpha = clamp(B_alpha, 0.0, 1.0);
+
+    return (clamp_output) ? clamp(out_rgb, color(0), color(1)) : out_rgb;
+}
+
 #endif // !AS_COLOR_HELPERS_H
