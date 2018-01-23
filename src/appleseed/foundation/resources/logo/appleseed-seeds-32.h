08f4c697af320d80abc25dbe79fd2a21c2255606
Franz Beaune <franz@appleseedhq.net>
2018-01-23 10:14:42 +0100

    Add appleseed logo to foundation/resources/logo/

---
+++ src/appleseed/foundation/resources/logo/appleseed-seeds-32.h
@@ -0,0 +1,55 @@
+
+//
+// This source file is part of appleseed.
+// Visit http://appleseedhq.net/ for additional information and resources.
+//
+// This software is released under the MIT license.
+//
+// Copyright (c) 2018 Francois Beaune, The appleseedhq Organization
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+#ifndef APPLESEED_FOUNDATION_RESOURCES_LOGO_APPLESEED_SEEDS_32_H
+#define APPLESEED_FOUNDATION_RESOURCES_LOGO_APPLESEED_SEEDS_32_H
+
+// appleseed.foundation headers.
+#include "foundation/platform/types.h"
+
+// appleseed.main headers.
+#include "main/dllsymbol.h"
+
+// Standard headers.
+#include <cstddef>
+
+namespace foundation
+{
+
+//
+// appleseed seeds logo, 32x30, 8-bit per channel, RGBA.
+//
+
+const size_t appleseed_seeds_32_width = 32;
+const size_t appleseed_seeds_32_height = 30;
+
+APPLESEED_DLLSYMBOL extern const uint8 appleseed_seeds_32[];
+
+}       // namespace foundation
+
+#endif  // !APPLESEED_FOUNDATION_RESOURCES_LOGO_APPLESEED_SEEDS_32_H
