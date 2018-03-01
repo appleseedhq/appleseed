b81a995f74327c46f21f615f814660e93561f96f
Fedor Matantsev <fedormatantsev@gmail.com>
2018-03-01 22:04:38 +0300

    Change default values for shutter open end and shutter close start parameters

--- src/appleseed/renderer/modeling/camera/camera.cpp
+++ src/appleseed/renderer/modeling/camera/camera.cpp
@@ -80,9 +80,9 @@ bool Camera::on_render_begin(
     IAbortSwitch*           abort_switch)
 {
     m_shutter_open_time = m_params.get_optional<float>("shutter_open_time", 0.0f);
-    m_shutter_open_end_time = m_params.get_optional<float>("shutter_open_end_time", 0.0f);
-    m_shutter_close_start_time = m_params.get_optional<float>("shutter_close_start_time", 1.0f);
+    m_shutter_open_end_time = m_params.get_optional<float>("shutter_open_end_time", m_shutter_open_time);
     m_shutter_close_time = m_params.get_optional<float>("shutter_close_time", 1.0f);
+    m_shutter_close_start_time = m_params.get_optional<float>("shutter_close_start_time", m_shutter_close_time);
 
     check_shutter_times_for_consistency();
 
