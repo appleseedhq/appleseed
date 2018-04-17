48dd948fbe4a97f194658b6570f3d0290596172e
Franz Beaune <franz@appleseedhq.net>
2018-04-17 14:50:16 +0200

    Update copyright notices

--- cmake/modules/FindAlembic.cmake
+++ cmake/modules/FindAlembic.cmake
@@ -4,7 +4,7 @@
 #
 # This software is released under the MIT license.
 #
-# Copyright (c) 2013-2017 Esteban Tovagliari, The appleseedhq Organization
+# Copyright (c) 2013-2018 Esteban Tovagliari, The appleseedhq Organization
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
