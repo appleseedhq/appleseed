427ca1a2ccb972922d59c96a32712a5dc8de5766
Franz Beaune <franz@appleseedhq.net>
2018-04-17 14:50:16 +0200

    Update copyright notices

--- scripts/rendernode.py
+++ scripts/rendernode.py
@@ -7,7 +7,7 @@
 # This software is released under the MIT license.
 #
 # Copyright (c) 2012-2013 Jonathan Topf, Jupiter Jazz Limited
-# Copyright (c) 2014-2017 Jonathan Topf, The appleseedhq Organization
+# Copyright (c) 2014-2018 Jonathan Topf, The appleseedhq Organization
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
