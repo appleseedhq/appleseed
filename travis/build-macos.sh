ee9cb1e2189e7bf746cd20a686da2bff890db96f
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2018-05-31 13:37:35 +0200

    debug

--- travis/build-macos.sh
+++ travis/build-macos.sh
@@ -8,7 +8,6 @@ mkdir local
 
 echo "Setting up the deps:"
 echo "------------------------"
-brew install boost --without-single --without-static
 brew install boost-python
 brew install zlib
 brew install xerces-c
@@ -32,7 +31,7 @@ git checkout Release-1.8.12
 mkdir build
 cd build
 cmake -DLLVM_DIRECTORY=/usr/local/opt/llvm@3.9/ -DLLVM_STATIC=ON -DENABLERTTI=ON -DUSE_LIBCPLUSPLUS=ON -DCMAKE_INSTALL_PREFIX=$THISDIR/local   ..
-make install
+make install -j 2
 cd ../..
 
 #SeExpr
@@ -42,5 +41,26 @@ git checkout db9610a24401fa7198c54c8768d0484175f54172
 mkdir build
 cd build
 cmake -Wno-dev -DCMAKE_POLICY_DEFAULT_CMP0042=OLD -DCMAKE_INSTALL_PREFIX=$THISDIR/local  ..
-make doc install
+make doc install -j 2
 cd ../..
+
+
+#Main build
+mkdir build
+cd build
+cmake -DWITH_DISNEY_MATERIAL=ON -DUSE_STATIC_BOOST=OFF -DUSE_EXTERNAL_ZLIB=ON -DUSE_EXTERNAL_PNG=ON -DUSE_EXTERNAL_EXR=ON -DUSE_EXTERNAL_XERCES=ON -DUSE_EXTERNAL_SEEXPR=ON -DUSE_EXTERNAL_OIIO=ON -DUSE_EXTERNAL_OCIO=ON -DUSE_EXTERNAL_OSL=ON -DZLIB_INCLUDE_DIR=/usr/local/opt/zlib/include -DZLIB_LIBRARY=/usr/local/opt/zlib/lib/libz.dylib -DPYTHON_INCLUDE_DIR=/usr/local/Cellar/python@2/2.7.15/Frameworks/Python.framework/Versions/2.7/include/python2.7/ -DPYTHON_LIBRARY=/usr/local/Cellar/python@2/2.7.15/Frameworks/Python.framework/Versions/2.7/lib/libpython2.7.dylib -DBoost_PYTHON_LIBRARY_RELEASE=/usr/local/lib/libboost_python27.dylib ..
+make -j 2
+
+
+echo "Running appleseed tests:"
+echo "------------------------"
+
+../sandbox/bin/Debug/appleseed.cli --run-unit-tests --verbose-unit-tests
+
+echo "Running appleseed.python tests:"
+echo "-------------------------------"
+
+export PYTHONPATH=$PYTHONPATH:../sandbox/lib/Debug/python2.7
+python ../sandbox/lib/Debug/python2.7/appleseed/test/runtests.py
+
+set +e
