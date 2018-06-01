a744309542a7b846a4b6cd73bf3dd07b747d833f
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2018-06-01 10:30:55 +0200

    Removing the register deprec. warning in appleseed build

--- travis/build-macos.sh
+++ travis/build-macos.sh
@@ -20,13 +20,6 @@ mkdir -p $HOME/Library/Python/2.7/lib/python/site-packages
 echo 'import site; site.addsitedir("/usr/local/lib/python2.7/site-packages")' >> $HOME/Library/Python/2.7/lib/python/site-packages/homebrew.pth
 
 
-
-echo "Patching OpenEXR:"
-echo "-----------------"
-#Patching openEXR to avoid the "register" issue in Debug mode ([-Werror,-Wdeprecated-register])
-cat /usr/local/include/OpenEXR/half.h | sed 's/register int/int/g' >! /usr/local/include/OpenEXR/half.h
-grep "register" /usr/local/include/OpenEXR/half.h
-
 echo "Installing OSL:"
 echo "---------------"
 #OSL
