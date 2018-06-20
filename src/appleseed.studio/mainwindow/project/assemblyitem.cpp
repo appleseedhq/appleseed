appleseedhq/appleseed

commit 2beed1a532b8f9b3741ab402c3c025335c11bfcd

Simplify updates of the project item registry

src/appleseed.studio/mainwindow/project/assemblyitem.cpp
@@ -365,7 +365,6 @@ namespace
         {
             assembly_instances.remove(*i);
             delete item_registry.get_item(*i);
-            item_registry.remove(*i);
         }
 
         // Recurse into child assemblies.
@@ -408,7 +407,6 @@ void AssemblyItem::do_delete()
 
     // Remove and delete the assembly item.
     ItemBase* assembly_item = m_editor_context.m_item_registry.get_item(assembly_uid);
-    m_editor_context.m_item_registry.remove(assembly_uid);
     delete assembly_item;
 
     // At this point 'this' no longer exists.

src/appleseed.studio/mainwindow/project/assemblycollectionitem.cpp
@@ -34,7 +34,6 @@
 #include "mainwindow/project/assemblyitem.h"
 #include "mainwindow/project/basegroupitem.h"
 #include "mainwindow/project/entityeditorcontext.h"
-#include "mainwindow/project/itemregistry.h"
 #include "mainwindow/project/projectbuilder.h"
 #include "mainwindow/project/tools.h"
 
@@ -122,16 +121,12 @@ ItemBase* AssemblyCollectionItem::create_item(Assembly* assembly)
 {
     assert(assembly);
 
-    ItemBase* item =
+    return
         new AssemblyItem(
             m_editor_context,
             *assembly,
             m_parent,
             m_parent_item);
-
-    m_editor_context.m_item_registry.insert(*assembly, item);
-
-    return item;
 }
 
 }   // namespace studio

src/appleseed.studio/mainwindow/project/assemblyinstanceitem.cpp
@@ -90,7 +90,6 @@ void AssemblyInstanceItem::do_delete()
 
     // Remove and delete the assembly instance item.
     ItemBase* assembly_instance_item = m_editor_context.m_item_registry.get_item(assembly_instance_uid);
-    m_editor_context.m_item_registry.remove(assembly_instance_uid);
     delete assembly_instance_item;
 
     // At this point 'this' no longer exists.

src/appleseed.studio/mainwindow/project/collectionitembase.h
@@ -31,9 +31,7 @@
 #define APPLESEED_STUDIO_MAINWINDOW_PROJECT_COLLECTIONITEMBASE_H
 
 // appleseed.studio headers.
-#include "mainwindow/project/entityeditorcontext.h"
 #include "mainwindow/project/itembase.h"
-#include "mainwindow/project/itemregistry.h"
 #include "utility/treewidget.h"
 
 // appleseed.foundation headers.
@@ -176,15 +174,11 @@ ItemBase* CollectionItemBase<Entity>::create_item(Entity* entity)
 {
     assert(entity);
 
-    ItemBase* item =
+    return
         new ItemBase(
             m_editor_context,
             entity->get_class_uid(),
             entity->get_name());
-
-    m_editor_context.m_item_registry.insert(*entity, item);
-
-    return item;
 }
 
 }       // namespace studio

src/appleseed.studio/mainwindow/project/entityitem.h
@@ -100,7 +100,6 @@ class EntityItem
     friend class EntityInstantiationAction<EntityItem>;
     friend class EntityDeletionAction<EntityItem>;
 
-    foundation::UniqueID        m_entity_uid;
     bool                        m_fixed_position;
 };
 
@@ -118,7 +117,6 @@ EntityItem<Entity, ParentEntity, CollectionItem>::EntityItem(
   : Base(editor_context, entity)
   , m_parent(parent)
   , m_collection_item(collection_item)
-  , m_entity_uid(entity->get_uid())
   , m_fixed_position(false)
 {
 }
@@ -158,7 +156,7 @@ void EntityItem<Entity, ParentEntity, CollectionItem>::slot_edit_accepted(founda
 template <typename Entity, typename ParentEntity, typename CollectionItem>
 void EntityItem<Entity, ParentEntity, CollectionItem>::edit(const foundation::Dictionary& values)
 {
-    Base::m_editor_context.m_item_registry.remove(m_entity_uid);
+    Base::m_editor_context.m_item_registry.remove(Base::m_entity_uid);
 
     const std::string old_entity_name = Base::m_entity->get_name();
 
@@ -170,8 +168,8 @@ void EntityItem<Entity, ParentEntity, CollectionItem>::edit(const foundation::Di
 
     const std::string new_entity_name = Base::m_entity->get_name();
 
-    m_entity_uid = Base::m_entity->get_uid();
-    Base::m_editor_context.m_item_registry.insert(m_entity_uid, this);
+    Base::m_entity_uid = Base::m_entity->get_uid();
+    Base::m_editor_context.m_item_registry.insert(Base::m_entity_uid, this);
 
     Base::update();
 

src/appleseed.studio/mainwindow/project/entityitembase.h
@@ -31,7 +31,9 @@
 #define APPLESEED_STUDIO_MAINWINDOW_PROJECT_ENTITYITEMBASE_H
 
 // appleseed.studio headers.
+#include "mainwindow/project/entityeditorcontext.h"
 #include "mainwindow/project/itembase.h"
+#include "mainwindow/project/itemregistry.h"
 
 // appleseed.foundation headers.
 #include "foundation/utility/containers/dictionary.h"
@@ -85,10 +87,13 @@ class EntityItemBase
         EntityEditorContext&        editor_context,
         Entity*                     entity);
 
+    ~EntityItemBase() override;
+
     void update();
 
   protected:
-    Entity* m_entity;
+    Entity*                 m_entity;
+    foundation::UniqueID    m_entity_uid;
 };
 
 
@@ -102,10 +107,19 @@ EntityItemBase<Entity>::EntityItemBase(
     Entity*                 entity)
   : EntityItemBaseSlots(editor_context, entity->get_class_uid())
   , m_entity(entity)
+  , m_entity_uid(entity->get_uid())
 {
     assert(m_entity != nullptr);
 
     update();
+
+    m_editor_context.m_item_registry.insert(*m_entity, this);
+}
+
+template <typename Entity>
+EntityItemBase<Entity>::~EntityItemBase()
+{
+    m_editor_context.m_item_registry.remove(m_entity_uid);
 }
 
 template <typename Entity>

src/appleseed.studio/mainwindow/project/instancecollectionitem.h
@@ -32,8 +32,6 @@
 
 // appleseed.studio headers.
 #include "mainwindow/project/collectionitembase.h"
-#include "mainwindow/project/entityeditorcontext.h"
-#include "mainwindow/project/itemregistry.h"
 
 // appleseed.foundation headers.
 #include "foundation/platform/compiler.h"
@@ -43,6 +41,7 @@
 #include <cassert>
 
 // Forward declarations.
+namespace appleseed { namespace studio { class EntityEditorContext; } }
 namespace appleseed { namespace studio { class ItemBase; } }
 class QString;
 
@@ -89,16 +88,12 @@ ItemBase* InstanceCollectionItem<Entity, EntityItem, ParentEntity>::create_item(
 {
     assert(entity);
 
-    ItemBase* item =
+    return
         new EntityItem(
             Base::m_editor_context,
             entity,
             m_parent,
             this);
-
-    Base::m_editor_context.m_item_registry.insert(*entity, item);
-
-    return item;
 }
 
 }       // namespace studio

src/appleseed.studio/mainwindow/project/materialcollectionitem.cpp
@@ -136,16 +136,12 @@ ItemBase* MaterialCollectionItem::create_item(Material* material)
 {
     assert(material);
 
-    ItemBase* item =
+    return
         new MaterialItem(
             m_editor_context,
             material,
             m_parent,
             this);
-
-    m_editor_context.m_item_registry.insert(*material, item);
-
-    return item;
 }
 
 void MaterialCollectionItem::slot_create_generic()

commit 57a5acd102052ea7f5a205257701359b005fd0e4

Fix deletion of assembly items
Regression introduced in 2beed1a532b8f9b3741ab402c3c025335c11bfcd: "Simplify updates of the project item registry"

src/appleseed.studio/mainwindow/project/assemblyitem.cpp
@@ -95,6 +95,7 @@ AssemblyItem::AssemblyItem(
     BaseGroupItem*          parent_item)
   : BaseGroupItem(editor_context, g_class_uid, assembly)
   , m_assembly(assembly)
+  , m_assembly_uid(assembly.get_uid())
   , m_parent(parent)
   , m_parent_item(parent_item)
 {
@@ -152,6 +153,13 @@ AssemblyItem::AssemblyItem(
     insertChild(
         11,
         m_volume_collection_item = add_multi_model_collection_item<Volume>(assembly.volumes()));
+
+    m_editor_context.m_item_registry.insert(m_assembly, this);
+}
+
+AssemblyItem::~AssemblyItem()
+{
+    m_editor_context.m_item_registry.remove(m_assembly_uid);
 }
 
 QMenu* AssemblyItem::get_single_item_context_menu() const
@@ -406,10 +414,7 @@ void AssemblyItem::do_delete()
     m_editor_context.m_project_builder.slot_notify_project_modification();
 
     // Remove and delete the assembly item.
-    ItemBase* assembly_item = m_editor_context.m_item_registry.get_item(assembly_uid);
-    delete assembly_item;
-
-    // At this point 'this' no longer exists.
+    delete this;
 }
 
 }   // namespace studio

src/appleseed.studio/mainwindow/project/assemblyitem.h
@@ -35,7 +35,7 @@
 #include "mainwindow/project/entityactions.h"
 
 // appleseed.foundation headers.
-#include "foundation/platform/compiler.h"
+#include "foundation/utility/uid.h"
 
 // Qt headers.
 #include <QList>
@@ -76,6 +76,8 @@ class AssemblyItem
         renderer::BaseGroup&        parent,
         BaseGroupItem*              parent_item);
 
+    ~AssemblyItem() override;
+
     QMenu* get_single_item_context_menu() const override;
 
     void add_item(renderer::BSDF* bsdf);
@@ -101,6 +103,7 @@ class AssemblyItem
     friend class EntityDeletionAction<AssemblyItem>;
 
     renderer::Assembly&             m_assembly;
+    const foundation::UniqueID      m_assembly_uid;
     renderer::BaseGroup&            m_parent;
     BaseGroupItem*                  m_parent_item;
 