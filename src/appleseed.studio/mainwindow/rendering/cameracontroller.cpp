appleseedhq/appleseed

commit 0e2a58bfd3c4282517a6e64f76680b32930a4882

Changing viewpoint in light paths viz no longer automatically affects camera

src/appleseed.studio/mainwindow/rendering/cameracontroller.cpp
@@ -75,9 +75,10 @@ namespace studio {
 //     controller target if the user wishes so (by pressing 'f').
 //
 
-CameraController::CameraController(QWidget* widget, Project& project)
+CameraController::CameraController(QWidget* widget, Project& project, Camera* camera)
   : m_widget(widget)
   , m_project(project)
+  , m_camera(camera)
   , m_enabled(true)
 {
     configure_controller();
@@ -101,20 +102,20 @@ Transformd CameraController::get_transform() const
 
 void CameraController::update_camera_transform()
 {
-    if (Camera* camera = m_project.get_uncached_active_camera())
+    if (m_camera != nullptr)
     {
         // Moving the camera kills camera motion blur.
-        camera->transform_sequence().clear();
+        m_camera->transform_sequence().clear();
 
         // Set the scene camera orientation and position based on the controller.
-        camera->transform_sequence().set_transform(0.0f, get_transform());
+        m_camera->transform_sequence().set_transform(0.0f, get_transform());
     }
 }
 
 void CameraController::save_camera_target()
 {
-    if (Camera* camera = m_project.get_uncached_active_camera())
-        camera->get_parameters().insert("controller_target", m_controller.get_target());
+    if (m_camera != nullptr)
+        m_camera->get_parameters().insert("controller_target", m_controller.get_target());
 }
 
 void CameraController::slot_entity_picked(ScenePicker::PickingResult result)
@@ -174,14 +175,12 @@ void CameraController::configure_controller()
     // By default, the pivot point is the scene's center.
     m_pivot = Vector3d(m_project.get_scene()->compute_bbox().center());
 
-    Camera* camera = m_project.get_uncached_active_camera();
-
     // Set the controller orientation and position.
-    if (camera)
+    if (m_camera != nullptr)
     {
         // Use the scene's camera.
         m_controller.set_transform(
-            camera->transform_sequence().get_earliest_transform().get_local_to_parent());
+            m_camera->transform_sequence().get_earliest_transform().get_local_to_parent());
     }
     else
     {
@@ -195,7 +194,8 @@ void CameraController::configure_controller()
 
     // Check whether the camera has a controller target.
     const bool has_target =
-        camera && camera->get_parameters().strings().exist("controller_target");
+        m_camera != nullptr &&
+        m_camera->get_parameters().strings().exist("controller_target");
 
     // Retrieve the controller target from the camera.
     Vector3d controller_target;
@@ -203,7 +203,7 @@ void CameraController::configure_controller()
     {
         // The scene's camera has a controller target, retrieve it.
         controller_target =
-            camera->get_parameters().get<Vector3d>("controller_target");
+            m_camera->get_parameters().get<Vector3d>("controller_target");
     }
     else
     {

src/appleseed.studio/mainwindow/rendering/cameracontroller.h
@@ -43,6 +43,7 @@
 #include <QObject>
 
 // Forward declarations.
+namespace renderer  { class Camera; }
 namespace renderer  { class Project; }
 class QEvent;
 class QKeyEvent;
@@ -62,7 +63,8 @@ class CameraController
     // The camera controller is disabled by default.
     CameraController(
         QWidget*            widget,
-        renderer::Project&  project);
+        renderer::Project&  project,
+        renderer::Camera*   camera);
 
     // Destructor.
     ~CameraController() override;
@@ -89,6 +91,7 @@ class CameraController
 
     QWidget*                m_widget;
     renderer::Project&      m_project;
+    renderer::Camera*       m_camera;
     bool                    m_enabled;
 
     ControllerType          m_controller;

src/appleseed.studio/mainwindow/rendering/rendertab.cpp
@@ -438,7 +438,8 @@ void RenderTab::recreate_handlers()
     m_camera_controller.reset(
         new CameraController(
             m_render_widget,
-            m_project));
+            m_project,
+            m_project.get_uncached_active_camera()));
     connect(
         m_camera_controller.get(), SIGNAL(signal_camera_change_begin()),
         SIGNAL(signal_camera_change_begin()));

src/appleseed.studio/mainwindow/rendering/lightpathstab.cpp
@@ -167,7 +167,7 @@ void LightPathsTab::slot_save_light_paths()
 
 void LightPathsTab::slot_camera_changed()
 {
-    m_camera_controller->update_camera_transform();
+    m_light_paths_widget->set_transform(m_camera_controller->get_transform());
     m_light_paths_widget->update();
 }
 
@@ -246,6 +246,15 @@ void LightPathsTab::create_toolbar()
         m_light_paths_widget, SLOT(slot_toggle_backface_culling(const bool)));
     m_toolbar->addWidget(backface_culling_button);
 
+    // Synchronize Camera button.
+    QToolButton* sync_camera_button = new QToolButton();
+    sync_camera_button->setIcon(load_icons("lightpathstab_toggle_backface_culling"));
+    sync_camera_button->setToolTip("Synchronize Camera");
+    connect(
+        sync_camera_button, SIGNAL(clicked()),
+        m_light_paths_widget, SLOT(slot_synchronize_camera()));
+    m_toolbar->addWidget(sync_camera_button);
+
     // Add stretchy spacer.
     // This places interactive widgets on the left and info on the right.
     QWidget* spacer = new QWidget();
@@ -292,6 +301,7 @@ void LightPathsTab::recreate_handlers()
     m_screen_space_paths_picking_handler->set_enabled(false);
 
     // The world-space paths picking handler is used to pick paths in the light paths widget.
+    // Commented out because we don't want to allow that.
     // m_world_space_paths_picking_handler.reset(
     //     new LightPathsPickingHandler(
     //         m_light_paths_widget,
@@ -303,7 +313,8 @@ void LightPathsTab::recreate_handlers()
     m_camera_controller.reset(
         new CameraController(
             m_light_paths_widget,
-            m_project));
+            m_project,
+            m_project.get_uncached_active_camera()));
     connect(
         m_camera_controller.get(), SIGNAL(signal_camera_changed()),
         SLOT(slot_camera_changed()));

src/appleseed.studio/mainwindow/rendering/lightpathswidget.cpp
@@ -41,9 +41,7 @@
 // appleseed.foundation headers.
 #include "foundation/image/color.h"
 #include "foundation/image/colorspace.h"
-#include "foundation/math/matrix.h"
 #include "foundation/math/scalar.h"
-#include "foundation/math/transform.h"
 #include "foundation/platform/opengl.h"
 #include "foundation/utility/api/apistring.h"
 #include "foundation/utility/string.h"
@@ -79,11 +77,19 @@ LightPathsWidget::LightPathsWidget(
     setFocusPolicy(Qt::StrongFocus);
     setFixedWidth(static_cast<int>(width));
     setFixedHeight(static_cast<int>(height));
+
+    const float time = m_camera.get_shutter_middle_time();
+    m_camera_matrix = m_camera.transform_sequence().evaluate(time).get_parent_to_local();
 }
 
 QImage LightPathsWidget::capture()
 {
     return grabFrameBuffer();
+}
+
+void LightPathsWidget::set_transform(const Transformd& transform)
+{
+    m_camera_matrix = transform.get_parent_to_local();
 }
 
 void LightPathsWidget::set_light_paths(const LightPathArray& light_paths)
@@ -127,12 +133,6 @@ void LightPathsWidget::set_selected_light_path_index(const int selected_light_pa
         static_cast<int>(m_light_paths.size()));
 }
 
-void LightPathsWidget::slot_toggle_backface_culling(const bool checked)
-{
-    m_backface_culling_enabled = checked;
-    update();
-}
-
 void LightPathsWidget::slot_display_all_light_paths()
 {
     if (m_selected_light_path_index > -1)
@@ -149,6 +149,19 @@ void LightPathsWidget::slot_display_next_light_path()
 {
     if (m_selected_light_path_index < static_cast<int>(m_light_paths.size()) - 1)
         set_selected_light_path_index(m_selected_light_path_index + 1);
+}
+
+void LightPathsWidget::slot_toggle_backface_culling(const bool checked)
+{
+    m_backface_culling_enabled = checked;
+    update();
+}
+
+void LightPathsWidget::slot_synchronize_camera()
+{
+    m_camera.transform_sequence().clear();
+    m_camera.transform_sequence().set_transform(0.0f,
+        Transformd::from_local_to_parent(inverse(m_camera_matrix)));
 }
 
 void LightPathsWidget::initializeGL()
@@ -331,8 +344,9 @@ void LightPathsWidget::render_geometry() const
 {
     glEnable(GL_LIGHTING);
 
+    glMultMatrixd(m_camera_matrix);
+
     const float time = m_camera.get_shutter_middle_time();
-    glMultMatrixd(m_camera.transform_sequence().evaluate(time).get_parent_to_local());
 
     for (const auto& assembly_instance : m_project.get_scene()->assembly_instances())
         draw(assembly_instance, time);

src/appleseed.studio/mainwindow/rendering/lightpathswidget.h
@@ -36,6 +36,8 @@
 #include "renderer/api/lighting.h"
 
 // On Windows, <QGLWidget> requires that <windows.h> is included first.
+#include "foundation/math/matrix.h"
+#include "foundation/math/transform.h"
 #ifdef _WIN32
 #include "foundation/platform/windows.h"
 #endif
@@ -74,6 +76,9 @@ class LightPathsWidget
 
     QImage capture() override;
 
+    void set_transform(
+        const foundation::Transformd&       transform);
+
     void set_light_paths(
         const renderer::LightPathArray&     light_paths);
 
@@ -86,14 +91,16 @@ class LightPathsWidget
         const int                           total_light_paths) const;
 
   public slots:
-    void slot_toggle_backface_culling(const bool checked);
     void slot_display_all_light_paths();
     void slot_display_previous_light_path();
     void slot_display_next_light_path();
+    void slot_toggle_backface_culling(const bool checked);
+    void slot_synchronize_camera();
 
   private:
     const renderer::Project&                m_project;
-    const renderer::Camera&                 m_camera;
+    renderer::Camera&                       m_camera;
+    foundation::Matrix4d                    m_camera_matrix;
 
     bool                                    m_backface_culling_enabled;
 

commit 08408ff4d9aaac4fc58e5e4a5597ae787cd247e7

Allow camera controller to use either the project's camera or a given one
This fixes a regression introduced in 0e2a58bfd3c4282517a6e64f76680b32930a4882 that caused crashes when editing the camera in interactive mode.

src/appleseed.studio/mainwindow/rendering/cameracontroller.cpp
@@ -75,10 +75,20 @@ namespace studio {
 //     controller target if the user wishes so (by pressing 'f').
 //
 
+CameraController::CameraController(QWidget* widget, Project& project)
+  : m_widget(widget)
+  , m_project(project)
+  , m_custom_camera(nullptr)
+  , m_enabled(true)
+{
+    configure_controller();
+    m_widget->installEventFilter(this);
+}
+
 CameraController::CameraController(QWidget* widget, Project& project, Camera* camera)
   : m_widget(widget)
   , m_project(project)
-  , m_camera(camera)
+  , m_custom_camera(camera)
   , m_enabled(true)
 {
     configure_controller();
@@ -102,20 +112,22 @@ Transformd CameraController::get_transform() const
 
 void CameraController::update_camera_transform()
 {
-    if (m_camera != nullptr)
+    if (Camera* camera = fetch_camera())
     {
         // Moving the camera kills camera motion blur.
-        m_camera->transform_sequence().clear();
+        camera->transform_sequence().clear();
 
         // Set the scene camera orientation and position based on the controller.
-        m_camera->transform_sequence().set_transform(0.0f, get_transform());
+        camera->transform_sequence().set_transform(0.0f, get_transform());
     }
 }
 
 void CameraController::save_camera_target()
 {
-    if (m_camera != nullptr)
-        m_camera->get_parameters().insert("controller_target", m_controller.get_target());
+    if (Camera* camera = fetch_camera())
+    {
+        camera->get_parameters().insert("controller_target", m_controller.get_target());
+    }
 }
 
 void CameraController::slot_entity_picked(ScenePicker::PickingResult result)
@@ -149,17 +161,17 @@ bool CameraController::eventFilter(QObject* object, QEvent* event)
             if (handle_mouse_button_press_event(static_cast<QMouseEvent*>(event)))
                 return true;
             break;
-    
+
           case QEvent::MouseButtonRelease:
             if (handle_mouse_button_release_event(static_cast<QMouseEvent*>(event)))
                 return true;
             break;
-    
+
           case QEvent::MouseMove:
             if (handle_mouse_move_event(static_cast<QMouseEvent*>(event)))
                 return true;
             break;
-    
+
           case QEvent::KeyPress:
             if (handle_key_press_event(static_cast<QKeyEvent*>(event)))
                 return true;
@@ -170,17 +182,27 @@ bool CameraController::eventFilter(QObject* object, QEvent* event)
     return QObject::eventFilter(object, event);
 }
 
+Camera* CameraController::fetch_camera()
+{
+    if (m_custom_camera != nullptr)
+        return m_custom_camera;
+    else
+        return m_project.get_uncached_active_camera();
+}
+
 void CameraController::configure_controller()
 {
     // By default, the pivot point is the scene's center.
     m_pivot = Vector3d(m_project.get_scene()->compute_bbox().center());
 
+    Camera* camera = fetch_camera();
+
     // Set the controller orientation and position.
-    if (m_camera != nullptr)
+    if (camera != nullptr)
     {
         // Use the scene's camera.
         m_controller.set_transform(
-            m_camera->transform_sequence().get_earliest_transform().get_local_to_parent());
+            camera->transform_sequence().get_earliest_transform().get_local_to_parent());
     }
     else
     {
@@ -194,8 +216,8 @@ void CameraController::configure_controller()
 
     // Check whether the camera has a controller target.
     const bool has_target =
-        m_camera != nullptr &&
-        m_camera->get_parameters().strings().exist("controller_target");
+        camera != nullptr &&
+        camera->get_parameters().strings().exist("controller_target");
 
     // Retrieve the controller target from the camera.
     Vector3d controller_target;
@@ -203,7 +225,7 @@ void CameraController::configure_controller()
     {
         // The scene's camera has a controller target, retrieve it.
         controller_target =
-            m_camera->get_parameters().get<Vector3d>("controller_target");
+            camera->get_parameters().get<Vector3d>("controller_target");
     }
     else
     {

src/appleseed.studio/mainwindow/rendering/cameracontroller.h
@@ -61,6 +61,14 @@ class CameraController
   public:
     // Constructor.
     // The camera controller is disabled by default.
+    // The camera controller controls the project's camera.
+    CameraController(
+        QWidget*            widget,
+        renderer::Project&  project);
+
+    // Constructor.
+    // The camera controller is disabled by default.
+    // The camera controller controls the given camera.
     CameraController(
         QWidget*            widget,
         renderer::Project&  project,
@@ -91,7 +99,7 @@ class CameraController
 
     QWidget*                m_widget;
     renderer::Project&      m_project;
-    renderer::Camera*       m_camera;
+    renderer::Camera*       m_custom_camera;
     bool                    m_enabled;
 
     ControllerType          m_controller;
@@ -100,6 +108,8 @@ class CameraController
     void configure_controller();
 
     bool eventFilter(QObject* object, QEvent* event) override;
+
+    renderer::Camera* fetch_camera();
 
     bool handle_mouse_button_press_event(const QMouseEvent* event);
     bool handle_mouse_button_release_event(const QMouseEvent* event);

src/appleseed.studio/mainwindow/rendering/rendertab.cpp
@@ -438,8 +438,7 @@ void RenderTab::recreate_handlers()
     m_camera_controller.reset(
         new CameraController(
             m_render_widget,
-            m_project,
-            m_project.get_uncached_active_camera()));
+            m_project));
     connect(
         m_camera_controller.get(), SIGNAL(signal_camera_change_begin()),
         SIGNAL(signal_camera_change_begin()));