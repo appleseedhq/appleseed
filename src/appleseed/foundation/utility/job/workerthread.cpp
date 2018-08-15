50b5879720e4b421b0b49b2364ec01e867c4abe3
Franz Beaune <franz@appleseedhq.net>
2018-08-16 00:13:30 +0200

    Fix FP modes configuration in worker threads

--- src/appleseed/foundation/utility/job/workerthread.cpp
+++ src/appleseed/foundation/utility/job/workerthread.cpp
@@ -83,11 +83,6 @@ void WorkerThread::start()
     assert(m_pause_flag.is_clear());
     assert(!m_abort_switch.is_aborted());
 
-#ifdef APPLESEED_USE_SSE
-    _MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
-    _MM_SET_DENORMALS_ZERO_MODE(_MM_DENORMALS_ZERO_ON);
-#endif
-
     // Start the thread.
     m_thread = new boost::thread(m_thread_func);
 }
@@ -144,6 +139,11 @@ void WorkerThread::set_thread_name()
 void WorkerThread::run()
 {
     set_thread_name();
+
+#if defined APPLESEED_WITH_EMBREE && defined APPLESEED_USE_SSE
+    _MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
+    _MM_SET_DENORMALS_ZERO_MODE(_MM_DENORMALS_ZERO_ON);
+#endif
 
     while (!m_abort_switch.is_aborted())
     {
