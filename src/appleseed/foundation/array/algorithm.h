85204f07ae2dd680c286c0bd7a3c4cba71bf82dd
est77 <ramenhdr@gmail.com>
2018-09-11 18:41:52 +0200

    Add array class

---
+++ src/appleseed/foundation/array/algorithm.h
@@ -0,0 +1,108 @@
+
+//
+// This source file is part of appleseed.
+// Visit https://appleseedhq.net/ for additional information and resources.
+//
+// This software is released under the MIT license.
+//
+// Copyright (c) 2018 Esteban Tovagliari, The appleseedhq Organization
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+#ifndef APPLESEED_FOUNDATION_ARRAY_ALGORITHM_H
+#define APPLESEED_FOUNDATION_ARRAY_ALGORITHM_H
+
+// appleseed.foundation headers.
+#include "foundation/array/applyvisitor.h"
+#include "foundation/array/array.h"
+#include "foundation/array/arrayref.h"
+#include "foundation/array/arrayview.h"
+
+namespace foundation
+{
+namespace detail
+{
+
+template <typename RandomAccessIter>
+class CopyIndexedVisitor
+{
+  public:
+    CopyIndexedVisitor(
+        const Array&        src,
+        RandomAccessIter    first_index,
+        RandomAccessIter    last_index)
+      : m_src(src)
+      , m_first_index(first_index)
+      , m_last_index(last_index)
+    {
+    }
+
+    template <typename T>
+    void operator()(ArrayRef<T>& dst)
+    {
+        const ArrayView<T> src(m_src);
+
+        for (RandomAccessIter it = m_first_index; it != m_last_index; ++it)
+            dst.push_back(src[*it]);
+    }
+
+    const Array&        m_src;
+    RandomAccessIter    m_first_index;
+    RandomAccessIter    m_last_index;
+};
+
+}       // namespace detail
+
+//
+// Return a new array, where each element is taken
+// from src as specified by a range of indices:
+//  new_array[i] = src[indices[i]];
+//
+
+template <typename RandomAccessIter>
+Array copy_indexed(
+    const Array&        src,
+    RandomAccessIter    first_index,
+    RandomAccessIter    last_index)
+{
+    Array dst(src.type());
+
+    if (first_index == last_index)
+        return dst;
+
+    dst.reserve(last_index - first_index);
+
+    detail::CopyIndexedVisitor<RandomAccessIter> v(src, first_index, last_index);
+    apply_visitor(dst, v);
+
+    return dst;
+}
+
+//
+// If the array contains integers, convert the array in place
+// to the smallest type that can represent the array elements,
+// otherwise leave the array unchanged.
+//
+
+void convert_to_smallest_type(Array& array);
+
+}       // namespace foundation
+
+#endif  // !APPLESEED_FOUNDATION_ARRAY_ALGORITHM_H
