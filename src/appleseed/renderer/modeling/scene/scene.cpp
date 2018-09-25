appleseedhq/appleseed

commit 154b5dcd519cb330dc3d79a5cb24640a87a27628

Generalize on_render_begin/end() to all entities

src/appleseed/renderer/modeling/scene/scene.cpp
@@ -298,36 +298,6 @@ void Scene::update_asset_paths(const StringDictionary& mappings)
     invoke_update_asset_paths(environment_shaders(), mappings);
 }
 
-bool Scene::on_render_begin(
-    const Project&          project,
-    IAbortSwitch*           abort_switch)
-{
-    assert(!m_has_render_data);
-    m_render_data.m_bbox = compute_bbox();
-    m_render_data.m_center = m_render_data.m_bbox.center();
-    m_render_data.m_radius = m_render_data.m_bbox.radius();
-    m_render_data.m_diameter = m_render_data.m_bbox.diameter();
-    m_render_data.m_safe_diameter = m_render_data.m_diameter * GScalar(1.01);
-    m_has_render_data = true;
-
-    for (each<CameraContainer> i = cameras(); i; ++i)
-    {
-        if (!i->on_render_begin(project, abort_switch))
-            return false;
-    }
-
-    return true;
-}
-
-void Scene::on_render_end(const Project& project)
-{
-    assert(m_has_render_data);
-    m_has_render_data = false;
-
-    for (each<CameraContainer> i = cameras(); i; ++i)
-        i->on_render_end(project);
-}
-
 namespace
 {
     bool invoke_procedural_expand(
@@ -366,6 +336,53 @@ bool Scene::expand_procedural_assemblies(
     }
 
     return true;
+}
+
+bool Scene::on_render_begin(
+    const Project&          project,
+    const BaseGroup*        parent,
+    OnRenderBeginRecorder&  recorder,
+    IAbortSwitch*           abort_switch)
+{
+    if (!Entity::on_render_begin(project, parent, recorder, abort_switch))
+        return false;
+
+    if (!BaseGroup::on_render_begin(project, parent,recorder,  abort_switch))
+        return false;
+
+    bool success = true;
+    success = success && impl->m_default_surface_shader->on_render_begin(project, this, recorder, abort_switch);
+    success = success && invoke_on_render_begin(environment_edfs(), project, this, recorder, abort_switch);
+    success = success && invoke_on_render_begin(environment_shaders(), project, this, recorder, abort_switch);
+    if (impl->m_environment.get())
+        success = success && impl->m_environment->on_render_begin(project, this, recorder, abort_switch);
+    success = success && invoke_on_render_begin(cameras(), project, this, recorder, abort_switch);
+    if (!success)
+        return false;
+
+    assert(!m_has_render_data);
+    m_render_data.m_bbox = compute_bbox();
+    m_render_data.m_center = m_render_data.m_bbox.center();
+    m_render_data.m_radius = m_render_data.m_bbox.radius();
+    m_render_data.m_diameter = m_render_data.m_bbox.diameter();
+    m_render_data.m_safe_diameter = m_render_data.m_diameter * GScalar(1.01);
+    m_has_render_data = true;
+
+    return true;
+}
+
+void Scene::on_render_end(
+    const Project&          project,
+    const BaseGroup*        parent)
+{
+    // `m_has_render_data` may be false if `on_frame_begin()` failed.
+    if (m_has_render_data)
+        m_has_render_data = false;
+
+    for (each<CameraContainer> i = cameras(); i; ++i)
+        i->on_render_end(project, this);
+
+    Entity::on_render_end(project, parent);
 }
 
 bool Scene::on_frame_begin(

src/appleseed/CMakeLists.txt
@@ -1628,6 +1628,8 @@ set (renderer_modeling_entity_sources
     renderer/modeling/entity/entityvector.h
     renderer/modeling/entity/onframebeginrecorder.cpp
     renderer/modeling/entity/onframebeginrecorder.h
+    renderer/modeling/entity/onrenderbeginrecorder.cpp
+    renderer/modeling/entity/onrenderbeginrecorder.h
     renderer/modeling/entity/registerentityfactories.h
 )
 list (APPEND appleseed_sources

src/appleseed/renderer/kernel/rendering/masterrenderer.cpp
@@ -46,6 +46,7 @@
 #include "renderer/kernel/texturing/texturestore.h"
 #include "renderer/modeling/display/display.h"
 #include "renderer/modeling/entity/onframebeginrecorder.h"
+#include "renderer/modeling/entity/onrenderbeginrecorder.h"
 #include "renderer/modeling/frame/frame.h"
 #include "renderer/modeling/input/inputbinder.h"
 #include "renderer/modeling/project/project.h"
@@ -354,7 +355,19 @@ struct MasterRenderer::Impl
     {
         while (true)
         {
+            // Construct an abort switch that will allow to abort initialization or rendering.
+            RendererControllerAbortSwitch abort_switch(*m_renderer_controller);
+
             m_renderer_controller->on_rendering_begin();
+
+            // Perform pre-render actions. Don't proceed if that failed.
+            OnRenderBeginRecorder recorder;
+            if (!m_project.get_scene()->on_render_begin(m_project, nullptr, recorder, &abort_switch))
+            {
+                recorder.on_render_end(m_project);
+                m_renderer_controller->on_rendering_abort();
+                return RenderingResult::Aborted;
+            }
 
             const IRendererController::Status status = initialize_and_render_frame();
 
@@ -362,10 +375,27 @@ struct MasterRenderer::Impl
             {
               case IRendererController::TerminateRendering:
                 m_renderer_controller->on_rendering_success();
-                return RenderingResult::Succeeded;
+                break;
 
               case IRendererController::AbortRendering:
                 m_renderer_controller->on_rendering_abort();
+                break;
+
+              case IRendererController::ReinitializeRendering:
+                break;
+
+              assert_otherwise;
+            }
+
+            // Perform post-render actions.
+            recorder.on_render_end(m_project);
+
+            switch (status)
+            {
+              case IRendererController::TerminateRendering:
+                return RenderingResult::Succeeded;
+
+              case IRendererController::AbortRendering:
                 return RenderingResult::Aborted;
 
               case IRendererController::ReinitializeRendering:
@@ -387,6 +417,7 @@ struct MasterRenderer::Impl
             return IRendererController::AbortRendering;
 
         // Expand all procedural assemblies.
+        // todo: could this be done in Scene::on_render_begin()?
         if (!m_project.get_scene()->expand_procedural_assemblies(m_project, &abort_switch))
             return IRendererController::AbortRendering;
 
@@ -410,10 +441,6 @@ struct MasterRenderer::Impl
         // Build or update ray tracing acceleration structures.
         m_project.update_trace_context();
 
-        // Perform pre-render rendering actions.
-        if (!m_project.get_scene()->on_render_begin(m_project, &abort_switch))
-            return IRendererController::AbortRendering;
-
         // Create renderer components.
         RendererComponents components(
             m_project,
@@ -430,9 +457,6 @@ struct MasterRenderer::Impl
 
         // Execute the main rendering loop.
         const auto status = render_frame(components, abort_switch);
-
-        // Perform post-render rendering actions.
-        m_project.get_scene()->on_render_end(m_project);
 
         const CanvasProperties& props = m_project.get_frame()->image().properties();
         m_project.get_light_path_recorder().finalize(
@@ -455,12 +479,12 @@ struct MasterRenderer::Impl
             // Discard recorded light paths.
             m_project.get_light_path_recorder().clear();
 
-            // The on_frame_begin() method of the renderer controller might alter the scene
-            // (e.g. transform the camera), thus it needs to be called before the on_frame_begin()
+            // The `on_frame_begin()` method of the renderer controller might alter the scene
+            // (e.g. transform the camera), thus it needs to be called before the `on_frame_begin()`
             // of the scene which assumes the scene is up-to-date and ready to be rendered.
             m_renderer_controller->on_frame_begin();
 
-            // Perform pre-frame rendering actions. Don't proceed if that failed.
+            // Perform pre-frame actions. Don't proceed if that failed.
             OnFrameBeginRecorder recorder;
             if (!components.get_shading_engine().on_frame_begin(m_project, recorder, &abort_switch) ||
                 !m_project.on_frame_begin(m_project, nullptr, recorder, &abort_switch) ||
@@ -501,7 +525,7 @@ struct MasterRenderer::Impl
 
             assert(!frame_renderer.is_rendering());
 
-            // Perform post-frame rendering actions
+            // Perform post-frame actions.
             recorder.on_frame_end(m_project);
             m_renderer_controller->on_frame_end();
 

src/appleseed/renderer/meta/tests/test_pinholecamera.cpp
@@ -31,6 +31,7 @@
 #include "renderer/modeling/camera/camera.h"
 #include "renderer/modeling/camera/pinholecamera.h"
 #include "renderer/modeling/entity/onframebeginrecorder.h"
+#include "renderer/modeling/entity/onrenderbeginrecorder.h"
 #include "renderer/modeling/frame/frame.h"
 #include "renderer/modeling/project/project.h"
 #include "renderer/modeling/scene/scene.h"
@@ -67,11 +68,12 @@ TEST_SUITE(Renderer_Modeling_Camera_PinholeCamera)
                     .insert("resolution", "512 512")
                     .insert("camera", "camera")));
 
-        bool success = project->get_scene()->on_render_begin(project.ref());
+        OnRenderBeginRecorder render_begin_recorder;
+        bool success = project->get_scene()->on_render_begin(project.ref(), nullptr, render_begin_recorder);
         ASSERT_TRUE(success);
 
-        OnFrameBeginRecorder recorder;
-        success = project->get_scene()->on_frame_begin(project.ref(), nullptr, recorder);
+        OnFrameBeginRecorder frame_begin_recorder;
+        success = project->get_scene()->on_frame_begin(project.ref(), nullptr, frame_begin_recorder);
         ASSERT_TRUE(success);
 
         const Camera* camera = project->get_scene()->get_active_camera();
@@ -82,7 +84,7 @@ TEST_SUITE(Renderer_Modeling_Camera_PinholeCamera)
         ASSERT_TRUE(success);
         EXPECT_FEQ(Vector2d(0.5, 0.5), projected);
 
-        recorder.on_frame_end(project.ref());
-        project->get_scene()->on_render_end(project.ref());
+        frame_begin_recorder.on_frame_end(project.ref());
+        render_begin_recorder.on_render_end(project.ref());
     }
 }

src/appleseed/renderer/meta/tests/test_sphericalcamera.cpp
@@ -33,6 +33,7 @@
 #include "renderer/modeling/camera/camera.h"
 #include "renderer/modeling/camera/sphericalcamera.h"
 #include "renderer/modeling/entity/onframebeginrecorder.h"
+#include "renderer/modeling/entity/onrenderbeginrecorder.h"
 #include "renderer/modeling/frame/frame.h"
 #include "renderer/modeling/project/project.h"
 #include "renderer/modeling/scene/scene.h"
@@ -63,11 +64,12 @@ TEST_SUITE(Renderer_Modeling_Camera_SphericalCamera)
                     .insert("resolution", "512 512")
                     .insert("camera", "camera")));
 
-        bool success = project->get_scene()->on_render_begin(project.ref());
+        OnRenderBeginRecorder render_begin_recorder;
+        bool success = project->get_scene()->on_render_begin(project.ref(), nullptr, render_begin_recorder);
         ASSERT_TRUE(success);
 
-        OnFrameBeginRecorder recorder;
-        success = project->get_scene()->on_frame_begin(project.ref(), nullptr, recorder);
+        OnFrameBeginRecorder frame_begin_recorder;
+        success = project->get_scene()->on_frame_begin(project.ref(), nullptr, frame_begin_recorder);
         ASSERT_TRUE(success);
 
         const Camera* camera = project->get_scene()->get_active_camera();
@@ -86,7 +88,7 @@ TEST_SUITE(Renderer_Modeling_Camera_SphericalCamera)
         ASSERT_TRUE(success);
         EXPECT_FEQ(Vector2d(1.0, 1.0), projected);
 
-        recorder.on_frame_end(project.ref());
-        project->get_scene()->on_render_end(project.ref());
+        frame_begin_recorder.on_frame_end(project.ref());
+        render_begin_recorder.on_render_end(project.ref());
     }
 }

src/appleseed/renderer/modeling/camera/camera.cpp
@@ -124,6 +124,8 @@ Camera::~Camera()
 
 bool Camera::on_render_begin(
     const Project&          project,
+    const BaseGroup*        parent,
+    OnRenderBeginRecorder&  recorder,
     IAbortSwitch*           abort_switch)
 {
     m_shutter_open_begin_time = m_params.get_optional<float>(
@@ -280,10 +282,6 @@ void Camera::initialize_shutter_curve_linear()
     impl->m_inverse_cdf_close_point =
         (2.0f * impl->m_normalized_close_begin_time - impl->m_normalized_open_end_time) /
         cbt_minus_oet_plus_one;
-}
-
-void Camera::on_render_end(const Project& project)
-{
 }
 
 bool Camera::on_frame_begin(

src/appleseed/renderer/modeling/camera/camera.h
@@ -49,6 +49,7 @@ namespace foundation    { class IAbortSwitch; }
 namespace renderer      { class BaseGroup; }
 namespace renderer      { class Frame; }
 namespace renderer      { class OnFrameBeginRecorder; }
+namespace renderer      { class OnRenderBeginRecorder; }
 namespace renderer      { class ParamArray; }
 namespace renderer      { class Project; }
 namespace renderer      { class RasterizationCamera; }
@@ -106,12 +107,11 @@ class APPLESEED_DLLSYMBOL Camera
 
     // This method is called once before rendering.
     // Returns true on success, false otherwise.
-    virtual bool on_render_begin(
+    bool on_render_begin(
         const Project&                  project,
-        foundation::IAbortSwitch*       abort_switch = nullptr);
-
-    // This method is called once after rendering.
-    virtual void on_render_end(const Project& project);
+        const BaseGroup*                parent,
+        OnRenderBeginRecorder&          recorder,
+        foundation::IAbortSwitch*       abort_switch = nullptr) override;
 
     // This method is called once before rendering each frame.
     // Returns true on success, false otherwise.

src/appleseed/renderer/modeling/camera/orthographiccamera.cpp
@@ -57,6 +57,7 @@
 
 // Forward declarations.
 namespace foundation    { class IAbortSwitch; }
+namespace renderer      { class OnRenderBeginRecorder; }
 
 using namespace foundation;
 using namespace std;
@@ -77,8 +78,8 @@ namespace
     {
       public:
         OrthographicCamera(
-            const char*         name,
-            const ParamArray&   params)
+            const char*             name,
+            const ParamArray&       params)
           : Camera(name, params)
         {
         }
@@ -117,10 +118,12 @@ namespace
         }
 
         bool on_render_begin(
-            const Project&      project,
-            IAbortSwitch*       abort_switch) override
+            const Project&          project,
+            const BaseGroup*        parent,
+            OnRenderBeginRecorder&  recorder,
+            IAbortSwitch*           abort_switch) override
         {
-            if (!Camera::on_render_begin(project, abort_switch))
+            if (!Camera::on_render_begin(project, parent, recorder, abort_switch))
                 return false;
 
             // Extract the film dimensions from the camera parameters.
@@ -144,9 +147,9 @@ namespace
         }
 
         void spawn_ray(
-            SamplingContext&    sampling_context,
-            const Dual2d&       ndc,
-            ShadingRay&         ray) const override
+            SamplingContext&        sampling_context,
+            const Dual2d&           ndc,
+            ShadingRay&             ray) const override
         {
             // Initialize the ray.
             initialize_ray(sampling_context, ray);
@@ -177,12 +180,12 @@ namespace
         }
 
         bool connect_vertex(
-            SamplingContext&    sampling_context,
-            const float         time,
-            const Vector3d&     point,
-            Vector2d&           ndc,
-            Vector3d&           outgoing,
-            float&              importance) const override
+            SamplingContext&        sampling_context,
+            const float             time,
+            const Vector3d&         point,
+            Vector2d&               ndc,
+            Vector3d&               outgoing,
+            float&                  importance) const override
         {
             // Retrieve the camera transform.
             Transformd scratch;
@@ -211,8 +214,8 @@ namespace
         }
 
         bool project_camera_space_point(
-            const Vector3d&     point,
-            Vector2d&           ndc) const override
+            const Vector3d&         point,
+            Vector2d&               ndc) const override
         {
             // Cannot project the point if it is behind the near plane.
             if (point.z > m_near_z)
@@ -226,11 +229,11 @@ namespace
         }
 
         bool project_segment(
-            const float         time,
-            const Vector3d&     a,
-            const Vector3d&     b,
-            Vector2d&           a_ndc,
-            Vector2d&           b_ndc) const override
+            const float             time,
+            const Vector3d&         a,
+            const Vector3d&         b,
+            Vector2d&               a_ndc,
+            Vector2d&               b_ndc) const override
         {
             // Retrieve the camera transform.
             Transformd scratch;

commit c2b12b76b9978d828e777426e053b70f744e69db

Fix regression that broke orthographic cameras
Regression introduced in 154b5dcd519cb330dc3d79a5cb24640a87a27628: "Generalize on_render_begin/end() to all entities"

src/appleseed/renderer/modeling/scene/scene.cpp
@@ -356,6 +356,16 @@ bool Scene::on_render_begin(
     if (!BaseGroup::on_render_begin(project, parent,recorder,  abort_switch))
         return false;
 
+    // The scene's render data must be computed before `on_render_begin()` is called on child entities,
+    // because child entities such as cameras may retrieve the scene's bounding box or diameter.
+    assert(!m_has_render_data);
+    m_render_data.m_bbox = compute_bbox();
+    m_render_data.m_center = m_render_data.m_bbox.center();
+    m_render_data.m_radius = m_render_data.m_bbox.radius();
+    m_render_data.m_diameter = m_render_data.m_bbox.diameter();
+    m_render_data.m_safe_diameter = m_render_data.m_diameter * GScalar(1.01);
+    m_has_render_data = true;
+
     bool success = true;
     success = success && impl->m_default_surface_shader->on_render_begin(project, this, recorder, abort_switch);
     success = success && invoke_on_render_begin(environment_edfs(), project, this, recorder, abort_switch);
@@ -365,14 +375,6 @@ bool Scene::on_render_begin(
     success = success && invoke_on_render_begin(cameras(), project, this, recorder, abort_switch);
     if (!success)
         return false;
-
-    assert(!m_has_render_data);
-    m_render_data.m_bbox = compute_bbox();
-    m_render_data.m_center = m_render_data.m_bbox.center();
-    m_render_data.m_radius = m_render_data.m_bbox.radius();
-    m_render_data.m_diameter = m_render_data.m_bbox.diameter();
-    m_render_data.m_safe_diameter = m_render_data.m_diameter * GScalar(1.01);
-    m_has_render_data = true;
 
     return true;
 }