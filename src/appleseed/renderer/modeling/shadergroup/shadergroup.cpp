appleseedhq/appleseed

commit e7fab8550b37b9101eb64c26327ab4b256caeeeb

Non photorealistic rendering, part I

src/appleseed/renderer/modeling/shadergroup/shadergroup.cpp
@@ -71,6 +71,10 @@ namespace
     const OIIO::ustring g_randomwalk_glass_str("as_randomwalk_glass");
     const OIIO::ustring g_holdout_str("holdout");
     const OIIO::ustring g_debug_str("debug");
+
+    const OIIO::ustring g_npr_shading_str("as_npr_shading");
+    const OIIO::ustring g_npr_contour_str("as_npr_contour");
+
     const OIIO::ustring g_dPdtime_str("dPdtime");
 
     bool is_subsurface_closure(const OIIO::ustring& closure_name)
@@ -217,14 +221,13 @@ bool ShaderGroup::create_optimized_osl_shader_group(
 
         get_shadergroup_closures_info(shading_system);
         report_has_closure("bsdf", HasBSDFs);
-        report_has_closure("emission", HasEmission);
-        report_has_closure("transparent", HasTransparency);
-        report_has_closure("subsurface", HasSubsurface);
-        report_has_closure("holdout", HasHoldout);
-        report_has_closure("debug", HasDebug);
+        report_has_closure(g_emission_str.c_str(), HasEmission);
+        report_has_closure(g_transparent_str.c_str(), HasTransparency);
+        report_has_closure(g_subsurface_str.c_str(), HasSubsurface);
+        report_has_closure(g_holdout_str.c_str(), HasHoldout);
+        report_has_closure(g_debug_str.c_str(), HasDebug);
 
-        get_shadergroup_globals_info(shading_system);
-        report_uses_global("dPdtime", UsesdPdTime);
+        report_has_closure("NPR", HasNPR);
 
         return true;
     }
@@ -339,6 +342,10 @@ void ShaderGroup::get_shadergroup_closures_info(OSLShadingSystem& shading_system
                 m_flags |= HasHoldout;
             else if (closures[i] == g_debug_str)
                 m_flags |= HasDebug;
+            else if (closures[i] == g_npr_shading_str)
+                m_flags |= HasNPR;
+            else if (closures[i] == g_npr_contour_str)
+                m_flags |= HasNPR;
             else
                 m_flags |= HasBSDFs;
         }

src/appleseed.shaders/CMakeLists.txt
@@ -477,6 +477,18 @@ source_group ("src\\appleseed" FILES
     ${src_appleseed_sources}
 )
 
+set (src_npr_sources
+     src/npr/as_toon.osl
+     src/npr/as_xtoon.osl
+)
+
+list (APPEND osl_sources
+    ${src_npr_sources}
+)
+source_group ("src\\npr" FILES
+    ${src_npr_sources}
+)
+
 
 #--------------------------------------------------------------------------------------------------
 # Prepare the sandbox.

src/appleseed.shaders/as_osl_extensions.h.in
@@ -167,4 +167,19 @@ closure color as_plastic(
     float       diffuse_weight,
     float       scattering) BUILTIN;
 
+
+closure color as_npr_shading() BUILTIN;
+
+closure color as_npr_contour(
+    color       contour_color,
+    float       contour_opacity,
+    float       contour_width,
+    int         object_id,
+    int         material_id,
+    int         occlusion,
+    float       occlusion_threshold,
+    int         creases,
+    float       creases_threshold
+) BUILTIN;
+
 #endif  // AS_OSL_EXTENSIONS_H

src/appleseed.shaders/src/npr/as_toon.osl
@@ -1,0 +1,316 @@
+
+//
+// This source file is part of appleseed.
+// Visit https://appleseedhq.net/ for additional information and resources.
+//
+// This software is released under the MIT license.
+//
+// Copyright (c) 2018 Esteban Tovagliari, The appleseedhq Organization
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+#include "appleseed/color/as_color_blend_modes.h"
+
+//
+// Reference:
+//
+//
+
+shader as_toon
+[[
+    string help = "XToon material node.",
+    string icon = "asDisneyMaterial.png",
+
+    string as_maya_node_name = "asToon",
+    string as_maya_classification = "drawdb/shader/surface:rendernode/appleseed/surface:shader/surface:swatch/AppleseedRenderSwatch",
+    int as_maya_type_id = 0x00127a01,
+
+    string as_blender_node_name = "asToon",
+    string as_blender_category = "shader"
+]]
+(
+    //
+    // Shading.
+    //
+
+    float in_diffuse_weight = 1.0
+    [[
+        string as_maya_attribute_name = "diffuseWeight",
+        float min = 0.0,
+        float max = 1.0,
+        string label = "Diffuse Weight",
+        string page = "Shading",
+        int divider = 1
+    ]],
+    float in_specular_weight = 1.0
+    [[
+        string as_maya_attribute_name = "specularWeight",
+        float min = 0.0,
+        float max = 1.0,
+        string label = "Specular Weight",
+        string page = "Shading"
+    ]],
+    float in_specular_roughness = 0.1
+    [[
+        string as_maya_attribute_name = "specularRoughness",
+        float min = 0.0,
+        float max = 1.0,
+        string label = "Specular Roughness",
+        string page = "Shading"
+    ]],
+
+    //
+    // Toon Remapping
+    //
+
+    color in_shadow_color = color(0.2)
+    [[
+        string as_maya_attribute_name = "shadowColor",
+        string label = "Shadow Color",
+        string page = "Toon"
+    ]],
+    float in_shadow_level = 0.3
+    [[
+        string as_maya_attribute_name = "shadowLevel",
+        float min = 0.0,
+        float max = 1.0,
+        string label = "Shadow Level",
+        string page = "Toon"
+    ]],
+    color in_midtone_color = color(0.5)
+    [[
+        string as_maya_attribute_name = "midtoneColor",
+        string label = "Midtone Color",
+        string page = "Toon"
+    ]],
+    float in_midtone_level = 0.7
+    [[
+        string as_maya_attribute_name = "midtoneLevel",
+        float min = 0.0,
+        float max = 1.0,
+        string label = "Midtone Level",
+        string page = "Toon"
+    ]],
+    color in_highlight_color = color(0.8)
+    [[
+        string as_maya_attribute_name = "highlightColor",
+        string label = "Highlight Color",
+        string page = "Toon",
+        int divider = 1
+    ]],
+    color in_glossy_color = color(1.0)
+    [[
+        string as_maya_attribute_name = "glossyColor",
+        string label = "Glossy Color",
+        string page = "Toon"
+    ]],
+    float in_glossy_level = 0.7
+    [[
+        string as_maya_attribute_name = "glossyLevel",
+        float min = 0.0,
+        float max = 1.0,
+        string label = "Glossy Level",
+        string page = "Toon"
+    ]],
+    float in_softness = 0.05
+    [[
+        string as_maya_attribute_name = "softness",
+        float min = 0.0,
+        float max = 1.0,
+        string label = "Softness",
+        string page = "Toon"
+    ]],
+
+    //
+    // Contours
+    //
+
+    color in_contour_color = color(0.0)
+    [[
+        string as_maya_attribute_name = "contourColor",
+        string label = "Color",
+        string page = "Contours"
+    ]],
+    float in_contour_opacity = 1.0
+    [[
+        string as_maya_attribute_name = "contourOpacity",
+        float min = 0.0,
+        float max = 1.0,
+        string label = "Opacity",
+        string page = "Contours"
+    ]],
+    float in_contour_width = 1.0
+    [[
+        string as_maya_attribute_name = "contourWidth",
+        float min = 0.0,
+        string label = "Width",
+        string page = "Contours"
+    ]],
+    int in_contour_object = 0
+    [[
+        string as_maya_attribute_name = "contourObj",
+        string widget = "checkBox",
+        string label = "Object",
+        string page = "Contours",
+        int as_maya_attribute_connectable = 0,
+        int as_maya_attribute_keyable = 0,
+        int as_maya_attribute_hidden = 1
+    ]],
+    int in_contour_material = 0
+    [[
+        string as_maya_attribute_name = "contourMtl",
+        string widget = "checkBox",
+        string label = "Material",
+        string page = "Contours",
+        int as_maya_attribute_connectable = 0,
+        int as_maya_attribute_keyable = 0,
+        int as_maya_attribute_hidden = 1
+    ]],
+    int in_contour_occlusion = 0
+    [[
+        string as_maya_attribute_name = "contourOcc",
+        string widget = "checkBox",
+        string label = "Occlusion",
+        string page = "Contours",
+        int as_maya_attribute_connectable = 0,
+        int as_maya_attribute_keyable = 0,
+        int as_maya_attribute_hidden = 1
+    ]],
+    int in_contour_creases = 0
+    [[
+        string as_maya_attribute_name = "contourCrease",
+        string widget = "checkBox",
+        string label = "Creases",
+        string page = "Contours",
+        int as_maya_attribute_connectable = 0,
+        int as_maya_attribute_keyable = 0,
+        int as_maya_attribute_hidden = 1
+    ]],
+    float in_occlusion_threshold = 0.1
+    [[
+        string as_maya_attribute_name = "occlusionThreshold",
+        float min = 0.0,
+        string label = "Occlusion threshold",
+        string page = "Contours"
+    ]],
+    float in_crease_threshold = 15
+    [[
+        string as_maya_attribute_name = "creaseThreshold",
+        float min = 0.0,
+        string label = "Crease threshold",
+        string page = "Contours"
+    ]],
+
+    //
+    // Output
+    //
+
+    output closure color out_outColor = 0
+    [[
+        string as_maya_attribute_name = "outColor",
+        string as_maya_attribute_short_name = "oc",
+        string widget = "null"
+    ]],
+    output closure color out_outTransparency = 0
+    [[
+        string as_maya_attribute_name = "outTransparency",
+        string as_maya_attribute_short_name = "ot",
+        string widget = "null"
+    ]]
+)
+{
+    if (raytype("npr"))
+    {
+        // Diffuse
+
+        color diffuse_shading;
+        getattribute("surface_shader:diffuse", diffuse_shading);
+        {
+            float lum = clamp(luminance(diffuse_shading), 0.0, 1.0);
+
+            if (in_softness > 0.0 && lum > in_shadow_level && lum < in_shadow_level + in_softness)
+            {
+                diffuse_shading = mix(
+                    in_shadow_color,
+                    in_midtone_color,
+                    (lum - in_shadow_level) / in_softness);
+            }
+            else if (in_softness > 0.0 && lum > in_midtone_level && lum < in_midtone_level + in_softness)
+            {
+                diffuse_shading = mix(
+                    in_midtone_color,
+                    in_highlight_color,
+                    (lum - in_midtone_level) / in_softness);
+            }
+            else if (lum < in_shadow_level)
+                diffuse_shading = in_shadow_color;
+            else if (lum < in_midtone_level)
+                diffuse_shading = in_midtone_color;
+            else
+                diffuse_shading = in_highlight_color;
+        }
+
+        // Glossy
+
+        color glossy_shading;
+        getattribute("surface_shader:glossy", glossy_shading);
+        {
+            float lum = clamp(luminance(glossy_shading), 0.0, 1.0);
+
+            if (in_softness > 0.0 && lum > in_glossy_level && lum < in_glossy_level + in_softness)
+            {
+                glossy_shading = mix(
+                    color(0),
+                    in_glossy_color,
+                    (lum - in_glossy_level) / in_softness);
+            }
+            else if (lum < in_glossy_level)
+                glossy_shading = color(0);
+            else
+                glossy_shading = in_glossy_color;
+        }
+
+        // Write shading result.
+        out_outColor = (diffuse_shading + glossy_shading) * as_npr_shading();
+
+        // Contours
+        out_outColor += as_npr_contour(
+            in_contour_color,
+            in_contour_opacity,
+            in_contour_width,
+            in_contour_object,
+            in_contour_material,
+            in_contour_occlusion,
+            in_occlusion_threshold,
+            in_contour_creases,
+            in_crease_threshold);
+    }
+    else
+    {
+        // Shading.
+
+        vector T = vector(0);
+        float anisotropy = 0.0;
+
+        out_outColor =
+            (in_diffuse_weight * diffuse(N)) +
+            (in_specular_weight * as_glossy("beckmann", N, T, in_specular_roughness, 0.0, 0.0, 1.5));
+    }
+}

src/appleseed.shaders/src/npr/as_xtoon.osl
@@ -1,0 +1,346 @@
+
+//
+// This source file is part of appleseed.
+// Visit https://appleseedhq.net/ for additional information and resources.
+//
+// This software is released under the MIT license.
+//
+// Copyright (c) 2018 Esteban Tovagliari, The appleseedhq Organization
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+#include "appleseed/color/as_color_blend_modes.h"
+
+//
+// Reference:
+//
+//   X-Toon: An extended toon shader.
+//   http://hal.inria.fr/inria-00362888/PDF/x-toon.pdf
+//
+
+shader as_xtoon
+[[
+    string help = "XToon material node.",
+    string icon = "asDisneyMaterial.png",
+
+    string as_maya_node_name = "asXToon",
+    string as_maya_classification = "drawdb/shader/surface:rendernode/appleseed/surface:shader/surface:swatch/AppleseedRenderSwatch",
+    int as_maya_type_id = 0x00127a02,
+
+    string as_blender_node_name = "asXToon",
+    string as_blender_category = "shader"
+]]
+(
+    //
+    // Shading.
+    //
+
+    color in_color = color(0.5)
+    [[
+        string as_maya_attribute_name = "color",
+        string as_maya_attribute_short_name = "c",
+        string label = "Surface Color",
+        string page = "Shading"
+    ]],
+    float in_diffuse_weight = 1.0
+    [[
+        string as_maya_attribute_name = "diffuseWeight",
+        float min = 0.0,
+        float max = 1.0,
+        string label = "Diffuse Weight",
+        string page = "Shading",
+        int divider = 1
+    ]],
+    float in_specular_weight = 1.0
+    [[
+        string as_maya_attribute_name = "specularWeight",
+        float min = 0.0,
+        float max = 1.0,
+        string label = "Specular Weight",
+        string page = "Shading"
+    ]],
+    float in_specular_roughness = 0.1
+    [[
+        string as_maya_attribute_name = "specularRoughness",
+        float min = 0.0,
+        float max = 1.0,
+        string label = "Specular Roughness",
+        string page = "Shading"
+    ]],
+    float in_metallic_amount = 0.0
+    [[
+        string as_maya_attribute_name = "metallicAmount",
+        string as_maya_attribute_short_name = "ma",
+        float min = 0.0,
+        float max = 1.0,
+        string label = "Metallicness",
+        string page = "Shading"
+    ]],
+
+    //
+    // XToon Remapping
+    //
+
+    string in_xtoon_tex = ""
+    [[
+        string as_maya_attribute_name = "xtoonTex",
+        string label = "Texture",
+        string page = "XToon",
+        string widget = "filename",
+        int as_maya_attribute_connectable = 0,
+        int as_maya_attribute_keyable = 0,
+        int as_maya_attribute_hidden = 1,
+        int as_blender_input_socket = 0,
+        int gafferNoduleLayoutVisible = 0
+    ]],
+    string in_xtoon_detail_mode = "None"
+    [[
+        string as_maya_attribute_name = "xtoonDetailMode",
+        string widget = "popup",
+        string options = "None|Depth|Orientation|Texture",
+        int as_maya_attribute_connectable = 0,
+        int as_maya_attribute_keyable = 0,
+        int as_maya_attribute_hidden = 1,
+        int as_blender_input_socket = 0,
+        int gafferNoduleLayoutVisible = 0,
+        string label = "Detail Mode",
+        string page = "XToon",
+    ]],
+    float in_min_depth = 0.0
+    [[
+        string as_maya_attribute_name = "xtoonDetailZmin",
+        float min = 0.0,
+        string label = "Min Depth",
+        string page = "XToon"
+    ]],
+    float in_max_depth = 100.0
+    [[
+        string as_maya_attribute_name = "xtoonDetailZmax",
+        float min = 0.0,
+        string label = "Max Depth",
+        string page = "XToon"
+    ]],
+    float in_orientation_magnitude = 1.0
+    [[
+        string as_maya_attribute_name = "xtoonDetailOrientMag",
+        float min = 1.0,
+        string label = "Orientation Magnitude",
+        string page = "XToon"
+    ]],
+    float in_texture_detail = 1.0
+    [[
+        string as_maya_attribute_name = "xtoonTexDetail",
+        float min = 1.0,
+        string label = "Texture Detail",
+        string page = "XToon"
+    ]],
+    string in_xtoon_blend_mode = "Replace"
+    [[
+        string as_maya_attribute_name = "xtoonBlendMode",
+        string widget = "popup",
+        string options = "Source|Replace|Darken|Multiply|Color Burn|Linear Burn|Lighten|Screen|Color Dodge|Linear Dodge|Overlay|Soft Light|Hard Light|Vivid Light|Linear Light|Pin Light|Difference|Exclusion|Subtract|Divide|Hue|Saturation|Color|Luminosity",
+        int as_maya_attribute_connectable = 0,
+        int as_maya_attribute_keyable = 0,
+        int as_maya_attribute_hidden = 1,
+        int as_blender_input_socket = 0,
+        int gafferNoduleLayoutVisible = 0,
+        string label = "Overlay Blend",
+        string page = "XToon",
+    ]],
+
+    //
+    // Contours
+    //
+
+    color in_contour_color = color(0.0)
+    [[
+        string as_maya_attribute_name = "contourColor",
+        string label = "Color",
+        string page = "Contours"
+    ]],
+    float in_contour_opacity = 1.0
+    [[
+        string as_maya_attribute_name = "contourOpacity",
+        float min = 0.0,
+        float max = 1.0,
+        string label = "Opacity",
+        string page = "Contours"
+    ]],
+    float in_contour_width = 1.0
+    [[
+        string as_maya_attribute_name = "contourWidth",
+        float min = 0.0,
+        string label = "Width",
+        string page = "Contours"
+    ]],
+    int in_contour_object = 0
+    [[
+        string as_maya_attribute_name = "contourObj",
+        string widget = "checkBox",
+        string label = "Object",
+        string page = "Contours",
+        int as_maya_attribute_connectable = 0,
+        int as_maya_attribute_keyable = 0,
+        int as_maya_attribute_hidden = 1
+    ]],
+    int in_contour_material = 0
+    [[
+        string as_maya_attribute_name = "contourMtl",
+        string widget = "checkBox",
+        string label = "Material",
+        string page = "Contours",
+        int as_maya_attribute_connectable = 0,
+        int as_maya_attribute_keyable = 0,
+        int as_maya_attribute_hidden = 1
+    ]],
+    int in_contour_occlusion = 0
+    [[
+        string as_maya_attribute_name = "contourOcc",
+        string widget = "checkBox",
+        string label = "Occlusion",
+        string page = "Contours",
+        int as_maya_attribute_connectable = 0,
+        int as_maya_attribute_keyable = 0,
+        int as_maya_attribute_hidden = 1
+    ]],
+    int in_contour_creases = 0
+    [[
+        string as_maya_attribute_name = "contourCrease",
+        string widget = "checkBox",
+        string label = "Creases",
+        string page = "Contours",
+        int as_maya_attribute_connectable = 0,
+        int as_maya_attribute_keyable = 0,
+        int as_maya_attribute_hidden = 1
+    ]],
+    float in_occlusion_threshold = 0.1
+    [[
+        string as_maya_attribute_name = "occlusionThreshold",
+        float min = 0.0,
+        string label = "Occlusion threshold",
+        string page = "Contours"
+    ]],
+    float in_crease_threshold = 15
+    [[
+        string as_maya_attribute_name = "creaseThreshold",
+        float min = 0.0,
+        string label = "Crease threshold",
+        string page = "Contours"
+    ]],
+
+    //
+    // Output
+    //
+
+    output closure color out_outColor = 0
+    [[
+        string as_maya_attribute_name = "outColor",
+        string as_maya_attribute_short_name = "oc",
+        string widget = "null"
+    ]],
+    output closure color out_outTransparency = 0
+    [[
+        string as_maya_attribute_name = "outTransparency",
+        string as_maya_attribute_short_name = "ot",
+        string widget = "null"
+    ]]
+)
+{
+    if (raytype("npr"))
+    {
+        color diffuse_shading;
+        getattribute("surface_shader:diffuse", diffuse_shading);
+
+        color glossy_shading;
+        getattribute("surface_shader:glossy", glossy_shading);
+
+        color shading = diffuse_shading + glossy_shading;
+
+        if (in_xtoon_tex != "" && in_xtoon_blend_mode != "Source")
+        {
+            // Shading Axis.
+            float s = clamp(luminance(shading), 0.0, 1.0);
+
+            // Detail Axis.
+            float t;
+
+            if (in_xtoon_detail_mode == "Depth")
+            {
+                float zmin = max(in_min_depth, 0.0001);
+                float zmax = max(in_max_depth, zmin);
+
+                float z;
+                getattribute("path:ray_length", z);
+                z = clamp(z, zmin, zmax);
+
+                t = 1.0 - log(z / zmin) / log(zmax / zmin);
+            }
+            else if (in_xtoon_detail_mode == "Orientation")
+                t = pow(abs(dot(N, I)), in_orientation_magnitude);
+            else if (in_xtoon_detail_mode == "Texture")
+                t = in_texture_detail;
+            else if (in_xtoon_detail_mode == "None")
+                t = 0.0;
+
+            t = clamp(t, 0.0, 1.0);
+            color remapped_shading = (color) texture(
+                in_xtoon_tex,
+                s,
+                t,
+                "swrap", "clamp",
+                "twrap", "clamp");
+
+            if (in_xtoon_blend_mode == "Replace")
+                shading = remapped_shading;
+            else
+                shading = blend_color(in_xtoon_blend_mode, shading, remapped_shading);
+        }
+
+        // Write shading result.
+        out_outColor = shading * as_npr_shading();
+
+        // Contours
+        out_outColor += as_npr_contour(
+            in_contour_color,
+            in_contour_opacity,
+            in_contour_width,
+            in_contour_object,
+            in_contour_material,
+            in_contour_occlusion,
+            in_occlusion_threshold,
+            in_contour_creases,
+            in_crease_threshold);
+    }
+    else
+    {
+        // Shading.
+
+        vector T = vector(0);
+        float anisotropy = 0.0;
+
+        closure color plastic =
+            (in_color * in_diffuse_weight * diffuse(N)) +
+            (in_specular_weight * as_glossy("beckmann", N, T, in_specular_roughness, 0.0, 0.0, 1.5));
+
+        closure color metal = as_metal("beckmann", N, T, in_color, color(1), in_specular_roughness, 0.0, 0.0);
+
+        out_outColor = mix(plastic, metal, in_metallic_amount);
+    }
+}

src/appleseed.shaders/stdosl.h
@@ -299,131 +299,8 @@ point rotate (point p, float angle, point a, point b)
 float luminance (color c) BUILTIN;
 color blackbody (float temperatureK) BUILTIN;
 color wavelength_color (float wavelength_nm) BUILTIN;
-
-
-color transformc (string to, color x)
-{
-    color rgb_to_hsv (color rgb) {  // See Foley & van Dam
-        float r = rgb[0], g = rgb[1], b = rgb[2];
-        float mincomp = min (r, min (g, b));
-        float maxcomp = max (r, max (g, b));
-        float delta = maxcomp - mincomp;  // chroma
-        float h, s, v;
-        v = maxcomp;
-        if (maxcomp > 0)
-            s = delta / maxcomp;
-        else s = 0;
-        if (s <= 0)
-            h = 0;
-        else {
-            if      (r >= maxcomp) h = (g-b) / delta;
-            else if (g >= maxcomp) h = 2 + (b-r) / delta;
-            else                   h = 4 + (r-g) / delta;
-            h /= 6;
-            if (h < 0)
-                h += 1;
-        }
-        return color (h, s, v);
-    }
-
-    color rgb_to_hsl (color rgb) {  // See Foley & van Dam
-        // First convert rgb to hsv, then to hsl
-        float minval = min (rgb[0], min (rgb[1], rgb[2]));
-        color hsv = rgb_to_hsv (rgb);
-        float maxval = hsv[2];   // v == maxval
-        float h = hsv[0], s, l = (minval+maxval) / 2;
-        if (minval == maxval)
-            s = 0;  // special 'achromatic' case, hue is 0
-        else if (l <= 0.5)
-            s = (maxval - minval) / (maxval + minval);
-        else
-            s = (maxval - minval) / (2 - maxval - minval);
-        return color (h, s, l);
-    }
-
-    color r;
-    if (to == "rgb" || to == "RGB")
-        r = x;
-    else if (to == "hsv")
-        r = rgb_to_hsv (x);
-    else if (to == "hsl")
-        r = rgb_to_hsl (x);
-    else if (to == "YIQ")
-        r = color (dot (vector(0.299,  0.587,  0.114), (vector)x),
-                   dot (vector(0.596, -0.275, -0.321), (vector)x),
-                   dot (vector(0.212, -0.523,  0.311), (vector)x));
-    else if (to == "XYZ")
-        r = color (dot (vector(0.412453, 0.357580, 0.180423), (vector)x),
-                   dot (vector(0.212671, 0.715160, 0.072169), (vector)x),
-                   dot (vector(0.019334, 0.119193, 0.950227), (vector)x));
-    else {
-        error ("Unknown color space \"%s\"", to);
-        r = x;
-    }
-    return r;
-}
-
-
-color transformc (string from, string to, color x)
-{
-    color hsv_to_rgb (color c) { // Reference: Foley & van Dam
-        float h = c[0], s = c[1], v = c[2];
-        color r;
-        if (s < 0.0001) {
-            r = v;
-        } else {
-            h = 6 * (h - floor(h));  // expand to [0..6)
-            int hi = (int)h;
-            float f = h - hi;
-            float p = v * (1-s);
-            float q = v * (1-s*f);
-            float t = v * (1-s*(1-f));
-            if      (hi == 0) r = color (v, t, p);
-            else if (hi == 1) r = color (q, v, p);
-            else if (hi == 2) r = color (p, v, t);
-            else if (hi == 3) r = color (p, q, v);
-            else if (hi == 4) r = color (t, p, v);
-            else              r = color (v, p, q);
-        }
-        return r;
-    }
-
-    color hsl_to_rgb (color c) {
-        float h = c[0], s = c[1], l = c[2];
-        // Easiest to convert hsl -> hsv, then hsv -> RGB (per Foley & van Dam)
-        float v = (l <= 0.5) ? (l * (1 + s)) : (l * (1 - s) + s);
-        color r;
-        if (v <= 0) {
-            r = 0;
-        } else {
-            float min = 2 * l - v;
-            s = (v - min) / v;
-            r = hsv_to_rgb (color (h, s, v));
-        }
-        return r;
-    }
-
-    color r;
-    if (from == "rgb" || from == "RGB")
-        r = x;
-    else if (from == "hsv")
-        r = hsv_to_rgb (x);
-    else if (from == "hsl")
-        r = hsl_to_rgb (x);
-    else if (from == "YIQ")
-        r = color (dot (vector(1,  0.9557,  0.6199), (vector)x),
-                   dot (vector(1, -0.2716, -0.6469), (vector)x),
-                   dot (vector(1, -1.1082,  1.7051), (vector)x));
-    else if (from == "XYZ")
-        r = color (dot (vector( 3.240479, -1.537150, -0.498535), (vector)x),
-                   dot (vector(-0.969256,  1.875991,  0.041556), (vector)x),
-                   dot (vector( 0.055648, -0.204043,  1.057311), (vector)x));
-    else {
-        error ("Unknown color space \"%s\"", to);
-        r = x;
-    }
-    return transformc (to, r);
-}
+color transformc (string from, string to, color c) BUILTIN;
+color transformc (string to, color c) { return transformc ("rgb", to, c); }
 
 
 
@@ -541,7 +418,6 @@ string concat (string a, string b, string c, string d, string e, string f) {
 
 
 // Texture
-
 
 // Closures
 //

commit 13aed8b38bddd35ae76b4a3252c6cf42ae17d2f4

Add back registration of dPdtime OSL global
Fix a regression introduced in e7fab8550b37b9101eb64c26327ab4b256caeeeb: "Non photorealistic rendering, part I".

src/appleseed/renderer/modeling/shadergroup/shadergroup.cpp
@@ -226,8 +226,10 @@ bool ShaderGroup::create_optimized_osl_shader_group(
         report_has_closure(g_subsurface_str.c_str(), HasSubsurface);
         report_has_closure(g_holdout_str.c_str(), HasHoldout);
         report_has_closure(g_debug_str.c_str(), HasDebug);
-
         report_has_closure("NPR", HasNPR);
+
+        get_shadergroup_globals_info(shading_system);
+        report_uses_global("dPdtime", UsesdPdTime);
 
         return true;
     }