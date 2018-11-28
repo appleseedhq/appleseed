appleseedhq/appleseed

commit 0df82d8f0d1b476f98f7a8ec9f2548377864ea77

Migrate to spectral transmission in Tracer and do small fixes
- Change Tracer::trace and Tracer::trace_between methods so they now work only with spectra.
- Fix the usage of these functions.
- Fix bug in Tracer when media list was never updated
- Update tracer tests
- Fix integration of inscattered radiance in PTLightingEngine

src/appleseed/renderer/kernel/lighting/directlightingintegrator.cpp
@@ -377,8 +377,8 @@ bool DirectLightingIntegrator::compute_incoming_radiance(
         incoming_prob = sample.m_probability / g;
 
         // Compute and return the incoming radiance.
-        transmission *= g / sample.m_probability;
         radiance *= transmission;
+        radiance *= g / sample.m_probability;
     }
     else
     {
@@ -444,7 +444,7 @@ void DirectLightingIntegrator::take_single_material_sample(
         return;
 
     // Trace a ray in the direction of the reflection.
-    float weight;
+    Spectrum weight;
     const ShadingPoint& light_shading_point =
         m_material_sampler.trace(
             m_shading_context,
@@ -714,8 +714,8 @@ void DirectLightingIntegrator::add_non_physical_light_sample_contribution(
     // Add the contribution of this sample to the illumination.
     const float attenuation = light->compute_distance_attenuation(
         m_material_sampler.get_point(), emission_position);
-    transmission *= attenuation / sample.m_probability;
     light_value *= transmission;
+    light_value *= attenuation / sample.m_probability;
     light_value *= material_value;
     radiance += light_value;
 }

src/appleseed/renderer/kernel/lighting/imagebasedlighting.cpp
@@ -56,8 +56,8 @@ void compute_ibl_combined_sampling(
     SamplingContext&            sampling_context,
     const ShadingContext&       shading_context,
     const EnvironmentEDF&       environment_edf,
-    const IMaterialSampler&     material_sampler,
     const Dual3d&               outgoing,
+    const IMaterialSampler&     material_sampler,
     const int                   env_sampling_modes,
     const size_t                material_sample_count,
     const size_t                env_sample_count,
@@ -124,12 +124,12 @@ void compute_ibl_material_sampling(
             continue;
 
         // Discard occluded samples.
-        float transmission;
+        Spectrum transmission;
         material_sampler.trace(
             shading_context,
             incoming.get_value(),
             transmission);
-        if (transmission == 0.0f)
+        if (max_value(transmission) == 0.0f)
             continue;
 
         // Evaluate the environment's EDF.
@@ -205,9 +205,9 @@ void compute_ibl_environment_sampling(
             continue;
 
         // Discard occluded samples.
-        float transmission;
+        Spectrum transmission;
         material_sampler.trace(shading_context, incoming, transmission);
-        if (transmission == 0.0f)
+        if (max_value(transmission) == 0.0f)
             continue;
 
         // Evaluate the BSDF.

src/appleseed/renderer/kernel/lighting/materialsamplers.cpp
@@ -72,17 +72,6 @@ bool BSDFSampler::contributes_to_light_sampling() const
     return !m_bsdf.is_purely_specular();
 }
 
-float BSDFSampler::trace_between(
-    const ShadingContext&   shading_context,
-    const Vector3d&         target_position) const
-{
-    return
-        shading_context.get_tracer().trace_between(
-            m_shading_point,
-            target_position,
-            VisibilityFlags::ShadowRay);
-}
-
 bool BSDFSampler::sample(
     SamplingContext&        sampling_context,
     const Dual3d&           outgoing,
@@ -132,31 +121,25 @@ float BSDFSampler::evaluate(
 const ShadingPoint& BSDFSampler::trace(
     const ShadingContext&   shading_context,
     const Vector3f&         direction,
-    float&                  transmission) const
+    Spectrum&               transmission) const
 {
-    return
-        shading_context.get_tracer().trace(
-            m_shading_point,
-            Vector3d(direction),
-            VisibilityFlags::ShadowRay,
-            transmission);
-}
+    ShadingRay ray(
+        m_shading_point.get_point(),
+        Vector3d(direction),
+        m_shading_point.get_ray().m_time,
+        VisibilityFlags::ShadowRay,
+        m_shading_point.get_ray().m_depth + 1);
+    ray.copy_media_from(m_shading_point.get_ray());
 
-const ShadingPoint& BSDFSampler::trace(
-    const ShadingContext&     shading_context,
-    const Vector3f&           direction,
-    Spectrum&                 transmission) const
-{
-    return
-        shading_context.get_tracer().trace(
+    const ShadingPoint& shading_point =
+        shading_context.get_tracer().trace<const ShadingPoint&>(
             shading_context,
             m_shading_point,
-            Vector3d(direction),
-            VisibilityFlags::ShadowRay,
-            m_shading_point.get_ray(),
+            ray,
             transmission);
+    if (shading_point.hit()) transmission.set(0.0f);
+    return shading_point;
 }
-
 
 void BSDFSampler::trace_between(
     const ShadingContext&   shading_context,
@@ -167,8 +150,8 @@ void BSDFSampler::trace_between(
         shading_context,
         m_shading_point,
         target_position,
-        VisibilityFlags::ShadowRay,
         m_shading_point.get_ray(),
+        VisibilityFlags::ShadowRay,
         transmission);
 }
 
@@ -194,12 +177,12 @@ bool BSDFSampler::cull_incoming_direction(const Vector3d& incoming) const
 
 PhaseFunctionSampler::PhaseFunctionSampler(
     const ShadingRay&       volume_ray,
-    const PhaseFunction&    phasefunction,
-    const void*             phasefunction_data,
+    const PhaseFunction&    phase_function,
+    const void*             phase_function_data,
     const float             distance)
   : m_volume_ray(volume_ray)
-  , m_phasefunction(phasefunction)
-  , m_phasefunction_data(phasefunction_data)
+  , m_phase_function(phase_function)
+  , m_phase_function_data(phase_function_data)
   , m_distance(distance)
   , m_point(m_volume_ray.point_at(distance))
 {
@@ -225,22 +208,17 @@ bool PhaseFunctionSampler::sample(
     Vector3f incoming_direction;
 
     pdf =
-        m_phasefunction.sample(
+        m_phase_function.sample(
             sampling_context,
             m_volume_ray,
-            m_phasefunction_data,
+            m_phase_function_data,
             m_distance,
             incoming_direction);
 
     incoming = Dual3f(incoming_direction);
 
-    m_phasefunction.scattering_coefficient(
-        m_volume_ray, m_phasefunction_data, m_distance, value);
-
-    Spectrum transmission;
-    m_phasefunction.evaluate_transmission(
-        m_volume_ray, m_phasefunction_data, m_distance, transmission);
-    value *= transmission;
+    m_phase_function.scattering_coefficient(
+        m_volume_ray, m_phase_function_data, m_distance, value);
 
     return true;
 }
@@ -252,65 +230,39 @@ float PhaseFunctionSampler::evaluate(
     Spectrum&               value) const
 {
     const float pdf =
-        m_phasefunction.evaluate(
+        m_phase_function.evaluate(
             m_volume_ray,
-            m_phasefunction_data,
+            m_phase_function_data,
             m_distance,
             incoming);
 
-    m_phasefunction.scattering_coefficient(
-        m_volume_ray, m_phasefunction_data, m_distance, value);
+    m_phase_function.scattering_coefficient(
+        m_volume_ray, m_phase_function_data, m_distance, value);
     value *= pdf;
 
-    Spectrum transmission;
-    m_phasefunction.evaluate_transmission(
-        m_volume_ray, m_phasefunction_data, m_distance, transmission);
-    value *= transmission;
-
     return pdf;
-}
-
-float PhaseFunctionSampler::trace_between(
-    const ShadingContext&   shading_context,
-    const Vector3d&         target_position) const
-{
-    return
-        shading_context.get_tracer().trace_between(
-            m_point,
-            target_position,
-            m_volume_ray.m_time,
-            VisibilityFlags::ShadowRay,
-            m_volume_ray.m_depth + 1);
 }
 
 const ShadingPoint& PhaseFunctionSampler::trace(
     const ShadingContext&   shading_context,
     const Vector3f&         direction,
-    float&                  transmission) const
+    Spectrum&               transmission) const
 {
-    return
-        shading_context.get_tracer().trace(
-            m_point,
-            Vector3d(direction),
-            m_volume_ray.m_time,
-            VisibilityFlags::ShadowRay,
-            m_volume_ray.m_depth + 1,
-            transmission);
-}
-
-const ShadingPoint& PhaseFunctionSampler::trace(
-    const ShadingContext&     shading_context,
-    const Vector3f&           direction,
-    Spectrum&                 transmission) const
-{
-    return
-        shading_context.get_tracer().trace(
-        shading_context,
+    ShadingRay ray(
         m_point,
         Vector3d(direction),
+        m_volume_ray.m_time,
         VisibilityFlags::ShadowRay,
-        m_volume_ray,
-        transmission);
+        m_volume_ray.m_depth + 1);
+    ray.copy_media_from(m_volume_ray);
+
+    const ShadingPoint& shading_point =
+        shading_context.get_tracer().trace<const ShadingPoint&>(
+            shading_context,
+            ray,
+            transmission);
+    if (shading_point.hit()) transmission.set(0.0f);
+    return shading_point;
 }
 
 void PhaseFunctionSampler::trace_between(
@@ -322,8 +274,8 @@ void PhaseFunctionSampler::trace_between(
         shading_context,
         m_point,
         target_position,
-        VisibilityFlags::ShadowRay,
         m_volume_ray,
+        VisibilityFlags::ShadowRay,
         transmission);
 }
 

src/appleseed/renderer/kernel/lighting/materialsamplers.h
@@ -62,15 +62,6 @@ class IMaterialSampler
     virtual const ShadingPoint& trace(
         const ShadingContext&           shading_context,
         const foundation::Vector3f&     direction,
-        float&                          transmission) const = 0;
-
-    virtual float trace_between(
-        const ShadingContext&           shading_context,
-        const foundation::Vector3d&     target_position) const = 0;
-
-    virtual const ShadingPoint& trace(
-        const ShadingContext&           shading_context,
-        const foundation::Vector3f&     direction,
         Spectrum&                       transmission) const = 0;
 
     virtual void trace_between(
@@ -108,15 +99,6 @@ class BSDFSampler
     virtual const foundation::Vector3d& get_point() const override;
 
     virtual bool contributes_to_light_sampling() const override;
-
-    virtual const ShadingPoint& trace(
-        const ShadingContext&           shading_context,
-        const foundation::Vector3f&     direction,
-        float&                          transmission) const override;
-
-    virtual float trace_between(
-        const ShadingContext&           shading_context,
-        const foundation::Vector3d&     target_position) const override;
 
     virtual const ShadingPoint& trace(
         const ShadingContext&           shading_context,
@@ -159,22 +141,13 @@ class PhaseFunctionSampler
   public:
     PhaseFunctionSampler(
         const ShadingRay&               volume_ray,
-        const PhaseFunction&            phasefunction,
-        const void*                     phasefunction_data,
+        const PhaseFunction&            phase_function,
+        const void*                     phase_function_data,
         const float                     distance);
 
     virtual const foundation::Vector3d& get_point() const override;
 
     virtual bool contributes_to_light_sampling() const override;
-
-    virtual const ShadingPoint& trace(
-        const ShadingContext&           shading_context,
-        const foundation::Vector3f&     direction,
-        float&                          transmission) const override;
-
-    virtual float trace_between(
-        const ShadingContext&           shading_context,
-        const foundation::Vector3d&     target_position) const override;
 
     virtual const ShadingPoint& trace(
         const ShadingContext&           shading_context,
@@ -204,8 +177,8 @@ class PhaseFunctionSampler
 
   private:
     const ShadingRay&               m_volume_ray;
-    const PhaseFunction&            m_phasefunction;
-    const void*                     m_phasefunction_data;
+    const PhaseFunction&            m_phase_function;
+    const void*                     m_phase_function_data;
     const float                     m_distance;
     const foundation::Vector3d      m_point;
 };

src/appleseed/renderer/kernel/lighting/imagebasedlighting.h
@@ -59,8 +59,8 @@ void compute_ibl_combined_sampling(
     SamplingContext&                sampling_context,
     const ShadingContext&           shading_context,
     const EnvironmentEDF&           environment_edf,
-    const IMaterialSampler&         material_sampler,
     const foundation::Dual3d&       outgoing,               // world space outgoing direction, unit-length
+    const IMaterialSampler&         material_sampler,
     const int                       env_sampling_modes,     // permitted scattering modes during environment sampling
     const size_t                    material_sample_count,  // number of samples in BSDF sampling
     const size_t                    env_sample_count,       // number of samples in environment sampling

commit 1cc24edde2b06b95c0bfe5999867780151165f3f

Fix regression by tracing probe rays whenever possible when computing IBL
Regression presumably introduced in 0df82d8f0d1b476f98f7a8ec9f2548377864ea77: "Migrate to spectral transmission in Tracer and do small fixes"

src/appleseed/renderer/kernel/lighting/directlightingintegrator.cpp
@@ -134,8 +134,7 @@ void DirectLightingIntegrator::compute_outgoing_radiance_light_sampling_low_vari
     if (!m_light_sampler.has_lights())
         return;
 
-    // Check if PDF of the sampler is Dirac delta and therefore
-    // cannot contribute to the light sampling.
+    // Check if PDF of the sampler is Dirac delta and therefore cannot contribute to the light sampling.
     if (!m_material_sampler.contributes_to_light_sampling())
         return;
 
@@ -144,9 +143,11 @@ void DirectLightingIntegrator::compute_outgoing_radiance_light_sampling_low_vari
         // Add contributions from non-physical light sources that don't belong to the lightset.
         for (size_t i = 0, e = m_light_sampler.get_non_physical_light_count(); i < e; ++i)
         {
+            // Sample the set of non-physical lights.
             LightSample sample;
             m_light_sampler.sample_non_physical_light(m_time, i, sample);
 
+            // Add the contribution of the chosen light.
             add_non_physical_light_sample_contribution(
                 sampling_context,
                 sample,
@@ -248,7 +249,7 @@ void DirectLightingIntegrator::take_single_material_sample(
     // Trace a ray in the direction of the reflection.
     Spectrum weight;
     const ShadingPoint& light_shading_point =
-        m_material_sampler.trace(
+        m_material_sampler.trace_full(
             m_shading_context,
             incoming.get_value(),
             weight);

src/appleseed/renderer/kernel/lighting/imagebasedlighting.cpp
@@ -126,12 +126,7 @@ void compute_ibl_material_sampling(
 
         // Discard occluded samples.
         Spectrum transmission;
-        const ShadingPoint& shading_point = material_sampler.trace(
-            shading_context,
-            incoming.get_value(),
-            transmission);
-        if (shading_point.hit_surface())
-            continue;
+        material_sampler.trace_simple(shading_context, incoming.get_value(), transmission);
         if (is_zero(transmission))
             continue;
 
@@ -180,9 +175,9 @@ void compute_ibl_environment_sampling(
 
     radiance.set(0.0f);
 
-    // todo: if we had a way to know that a BSDF is purely specular, we could
-    // immediately return black here since there will be no contribution from
-    // such a BSDF.
+    // Check if PDF of the sampler is Dirac delta and therefore cannot contribute to the light sampling.
+    if (!material_sampler.contributes_to_light_sampling())
+        return;
 
     sampling_context.split_in_place(2, env_sample_count);
 
@@ -205,22 +200,18 @@ void compute_ibl_environment_sampling(
 
         // Discard occluded samples.
         Spectrum transmission;
-        const ShadingPoint& shading_point = material_sampler.trace(
-            shading_context,
-            incoming,
-            transmission);
-        if (shading_point.hit_surface())
-            continue;
+        material_sampler.trace_simple(shading_context, incoming, transmission);
         if (is_zero(transmission))
             continue;
 
         // Evaluate the BSDF.
         DirectShadingComponents material_value;
-        const float material_prob = material_sampler.evaluate(
-            env_sampling_modes,
-            Vector3f(outgoing.get_value()),
-            incoming,
-            material_value);
+        const float material_prob =
+            material_sampler.evaluate(
+                env_sampling_modes,
+                Vector3f(outgoing.get_value()),
+                incoming,
+                material_value);
         if (material_prob == 0.0f)
             continue;
 

src/appleseed/renderer/kernel/lighting/materialsamplers.cpp
@@ -78,6 +78,63 @@ bool BSDFSampler::contributes_to_light_sampling() const
     return !m_bsdf.is_purely_specular();
 }
 
+const ShadingPoint& BSDFSampler::trace_full(
+    const ShadingContext&       shading_context,
+    const Vector3f&             direction,
+    Spectrum&                   transmission) const
+{
+    ShadingRay ray(
+        m_shading_point.get_point(),
+        Vector3d(direction),
+        m_shading_point.get_ray().m_time,
+        VisibilityFlags::ShadowRay,
+        m_shading_point.get_ray().m_depth + 1);
+    ray.copy_media_from(m_shading_point.get_ray());
+
+    const ShadingPoint& shading_point =
+        shading_context.get_tracer().trace_full(
+            shading_context,
+            m_shading_point,
+            ray,
+            transmission);
+
+    return shading_point;
+}
+
+void BSDFSampler::trace_simple(
+    const ShadingContext&       shading_context,
+    const Vector3f&             direction,
+    Spectrum&                   transmission) const
+{
+    ShadingRay ray(
+        m_shading_point.get_point(),
+        Vector3d(direction),
+        m_shading_point.get_ray().m_time,
+        VisibilityFlags::ShadowRay,
+        m_shading_point.get_ray().m_depth + 1);
+    ray.copy_media_from(m_shading_point.get_ray());
+
+    shading_context.get_tracer().trace_simple(
+        shading_context,
+        m_shading_point,
+        ray,
+        transmission);
+}
+
+void BSDFSampler::trace_between(
+    const ShadingContext&       shading_context,
+    const Vector3d&             target_position,
+    Spectrum&                   transmission) const
+{
+    shading_context.get_tracer().trace_between_simple(
+        shading_context,
+        m_shading_point,
+        target_position,
+        m_shading_point.get_ray(),
+        VisibilityFlags::ShadowRay,
+        transmission);
+}
+
 bool BSDFSampler::sample(
     SamplingContext&            sampling_context,
     const Dual3d&               outgoing,
@@ -123,43 +180,6 @@ float BSDFSampler::evaluate(
             value);
 }
 
-const ShadingPoint& BSDFSampler::trace(
-    const ShadingContext&       shading_context,
-    const Vector3f&             direction,
-    Spectrum&                   transmission) const
-{
-    ShadingRay ray(
-        m_shading_point.get_point(),
-        Vector3d(direction),
-        m_shading_point.get_ray().m_time,
-        VisibilityFlags::ShadowRay,
-        m_shading_point.get_ray().m_depth + 1);
-    ray.copy_media_from(m_shading_point.get_ray());
-
-    const ShadingPoint& shading_point =
-        shading_context.get_tracer().trace_full(
-            shading_context,
-            m_shading_point,
-            ray,
-            transmission);
-
-    return shading_point;
-}
-
-void BSDFSampler::trace_between(
-    const ShadingContext&       shading_context,
-    const Vector3d&             target_position,
-    Spectrum&                   transmission) const
-{
-    shading_context.get_tracer().trace_between_simple(
-        shading_context,
-        m_shading_point,
-        target_position,
-        m_shading_point.get_ray(),
-        VisibilityFlags::ShadowRay,
-        transmission);
-}
-
 
 //
 // VolumeSampler class implementation.
@@ -193,6 +213,61 @@ const ShadingPoint& VolumeSampler::get_shading_point() const
 bool VolumeSampler::contributes_to_light_sampling() const
 {
     return true;
+}
+
+const ShadingPoint& VolumeSampler::trace_full(
+    const ShadingContext&       shading_context,
+    const Vector3f&             direction,
+    Spectrum&                   transmission) const
+{
+    ShadingRay ray(
+        m_point,
+        Vector3d(direction),
+        m_volume_ray.m_time,
+        VisibilityFlags::ShadowRay,
+        m_volume_ray.m_depth + 1);
+    ray.copy_media_from(m_volume_ray);
+
+    const ShadingPoint& shading_point =
+        shading_context.get_tracer().trace_full(
+            shading_context,
+            ray,
+            transmission);
+
+    return shading_point;
+}
+
+void VolumeSampler::trace_simple(
+    const ShadingContext&       shading_context,
+    const Vector3f&             direction,
+    Spectrum&                   transmission) const
+{
+    ShadingRay ray(
+        m_point,
+        Vector3d(direction),
+        m_volume_ray.m_time,
+        VisibilityFlags::ShadowRay,
+        m_volume_ray.m_depth + 1);
+    ray.copy_media_from(m_volume_ray);
+
+    shading_context.get_tracer().trace_simple(
+        shading_context,
+        ray,
+        transmission);
+}
+
+void VolumeSampler::trace_between(
+    const ShadingContext&       shading_context,
+    const Vector3d&             target_position,
+    Spectrum&                   transmission) const
+{
+    shading_context.get_tracer().trace_between_simple(
+        shading_context,
+        m_point,
+        target_position,
+        m_volume_ray,
+        VisibilityFlags::ShadowRay,
+        transmission);
 }
 
 bool VolumeSampler::sample(
@@ -241,42 +316,6 @@ float VolumeSampler::evaluate(
     value.m_beauty = value.m_volume;
 
     return pdf;
-}
-
-const ShadingPoint& VolumeSampler::trace(
-    const ShadingContext&       shading_context,
-    const Vector3f&             direction,
-    Spectrum&                   transmission) const
-{
-    ShadingRay ray(
-        m_point,
-        Vector3d(direction),
-        m_volume_ray.m_time,
-        VisibilityFlags::ShadowRay,
-        m_volume_ray.m_depth + 1);
-    ray.copy_media_from(m_volume_ray);
-
-    const ShadingPoint& shading_point =
-        shading_context.get_tracer().trace_full(
-            shading_context,
-            ray,
-            transmission);
-
-    return shading_point;
-}
-
-void VolumeSampler::trace_between(
-    const ShadingContext&       shading_context,
-    const Vector3d&             target_position,
-    Spectrum&                   transmission) const
-{
-    shading_context.get_tracer().trace_between_simple(
-        shading_context,
-        m_point,
-        target_position,
-        m_volume_ray,
-        VisibilityFlags::ShadowRay,
-        transmission);
 }
 
 }   // namespace renderer

src/appleseed/renderer/kernel/lighting/materialsamplers.h
@@ -56,13 +56,20 @@ namespace renderer
 class IMaterialSampler
 {
   public:
+    virtual ~IMaterialSampler() {}
+
     virtual const foundation::Vector3d& get_point() const = 0;
 
     virtual const ShadingPoint& get_shading_point() const = 0;
 
     virtual bool contributes_to_light_sampling() const = 0;
 
-    virtual const ShadingPoint& trace(
+    virtual const ShadingPoint& trace_full(
+        const ShadingContext&           shading_context,
+        const foundation::Vector3f&     direction,
+        Spectrum&                       transmission) const = 0;
+
+    virtual void trace_simple(
         const ShadingContext&           shading_context,
         const foundation::Vector3f&     direction,
         Spectrum&                       transmission) const = 0;
@@ -102,7 +109,12 @@ class BSDFSampler
 
     bool contributes_to_light_sampling() const override;
 
-    const ShadingPoint& trace(
+    const ShadingPoint& trace_full(
+        const ShadingContext&           shading_context,
+        const foundation::Vector3f&     direction,
+        Spectrum&                       transmission) const override;
+
+    void trace_simple(
         const ShadingContext&           shading_context,
         const foundation::Vector3f&     direction,
         Spectrum&                       transmission) const override;
@@ -151,7 +163,12 @@ class VolumeSampler
 
     bool contributes_to_light_sampling() const override;
 
-    const ShadingPoint& trace(
+    const ShadingPoint& trace_full(
+        const ShadingContext&           shading_context,
+        const foundation::Vector3f&     direction,
+        Spectrum&                       transmission) const override;
+
+    void trace_simple(
         const ShadingContext&           shading_context,
         const foundation::Vector3f&     direction,
         Spectrum&                       transmission) const override;
@@ -175,12 +192,12 @@ class VolumeSampler
         DirectShadingComponents&        value) const override;
 
   private:
-    const ShadingRay&               m_volume_ray;
-    const Volume&                   m_volume;
-    const void*                     m_volume_data;
-    const float                     m_distance;
-    const ShadingPoint&             m_shading_point;
-    const foundation::Vector3d      m_point;
+    const ShadingRay&                   m_volume_ray;
+    const Volume&                       m_volume;
+    const void*                         m_volume_data;
+    const float                         m_distance;
+    const ShadingPoint&                 m_shading_point;
+    const foundation::Vector3d          m_point;
 };
 
 }       // namespace renderer