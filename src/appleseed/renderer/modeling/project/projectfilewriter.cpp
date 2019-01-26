2ea39659b92fe19e75b952ae8c4bea6e59839164
Franz Beaune <franz@appleseedhq.net>
2019-01-26 23:38:49 +0100

    Code tweaks

--- src/appleseed/renderer/modeling/project/projectfilewriter.cpp
+++ src/appleseed/renderer/modeling/project/projectfilewriter.cpp
@@ -244,6 +244,7 @@ namespace
                 float time;
                 Transformd transform;
                 transform_sequence.get_transform(0, time, transform);
+
                 if (transform.get_local_to_parent() == Matrix4d::identity())
                     return;
             }
@@ -331,6 +332,7 @@ namespace
             {
                 XMLElement element("aovs", m_file, m_indenter);
                 element.write(XMLElement::HasChildElements);
+
                 write_collection(aovs);
             }
         }
@@ -348,7 +350,7 @@ namespace
 
             // Don't write the content of the assembly if it was
             // generated procedurally.
-            if (dynamic_cast<const ProceduralAssembly*>(&assembly))
+            if (dynamic_cast<const ProceduralAssembly*>(&assembly) != nullptr)
             {
                 element.write(
                     !assembly.get_parameters().empty()
@@ -356,46 +358,46 @@ namespace
                         : XMLElement::HasNoContent);
 
                 write_params(assembly.get_parameters());
-                return;
             }
+            else
+            {
+                element.write(
+                    !assembly.get_parameters().empty() ||
+                    !assembly.colors().empty() ||
+                    !assembly.textures().empty() ||
+                    !assembly.texture_instances().empty() ||
+                    !assembly.bsdfs().empty() ||
+                    !assembly.bssrdfs().empty() ||
+                    !assembly.edfs().empty() ||
+                    !assembly.shader_groups().empty() ||
+                    !assembly.surface_shaders().empty() ||
+                    !assembly.materials().empty() ||
+                    !assembly.lights().empty() ||
+                    !assembly.objects().empty() ||
+                    !assembly.object_instances().empty() ||
+                    !assembly.volumes().empty() ||
+                    !assembly.assemblies().empty() ||
+                    !assembly.assembly_instances().empty()
+                        ? XMLElement::HasChildElements
+                        : XMLElement::HasNoContent);
 
-            element.write(
-                !assembly.get_parameters().empty() ||
-                !assembly.colors().empty() ||
-                !assembly.textures().empty() ||
-                !assembly.texture_instances().empty() ||
-                !assembly.bsdfs().empty() ||
-                !assembly.bssrdfs().empty() ||
-                !assembly.edfs().empty() ||
-                !assembly.shader_groups().empty() ||
-                !assembly.surface_shaders().empty() ||
-                !assembly.materials().empty() ||
-                !assembly.lights().empty() ||
-                !assembly.objects().empty() ||
-                !assembly.object_instances().empty() ||
-                !assembly.volumes().empty() ||
-                !assembly.assemblies().empty() ||
-                !assembly.assembly_instances().empty()
-                    ? XMLElement::HasChildElements
-                    : XMLElement::HasNoContent);
-
-            write_params(assembly.get_parameters());
-
-            write_collection(assembly.colors());
-            write_collection(assembly.textures());
-            write_collection(assembly.texture_instances());
-            write_collection(assembly.bsdfs());
-            write_collection(assembly.bssrdfs());
-            write_collection(assembly.edfs());
-            write_collection(assembly.shader_groups());
-            write_collection(assembly.surface_shaders());
-            write_collection(assembly.materials());
-            write_collection(assembly.lights());
-            write_object_collection(assembly.objects());
-            write_collection(assembly.object_instances());
-            write_collection(assembly.volumes());
-            write_collection(assembly.assemblies());
-            write_collection(assembly.assembly_instances());
+                write_params(assembly.get_parameters());
+                write_collection(assembly.colors());
+                write_collection(assembly.textures());
+                write_collection(assembly.texture_instances());
+                write_collection(assembly.bsdfs());
+                write_collection(assembly.bssrdfs());
+                write_collection(assembly.edfs());
+                write_collection(assembly.shader_groups());
+                write_collection(assembly.surface_shaders());
+                write_collection(assembly.materials());
+                write_collection(assembly.lights());
+                write_object_collection(assembly.objects());
+                write_collection(assembly.object_instances());
+                write_collection(assembly.volumes());
+                write_collection(assembly.assemblies());
+                write_collection(assembly.assembly_instances());
+            }
         }
 
         // Write an <assembly_instance> element.
@@ -466,7 +468,6 @@ namespace
             element.write(XMLElement::HasChildElements);
 
             write_params(color_entity.get_parameters());
-
             write_value_array("values", color_entity.get_values());
             write_value_array("alpha", color_entity.get_alpha());
         }
@@ -482,6 +483,7 @@ namespace
                 !configuration.get_parameters().empty()
                     ? XMLElement::HasChildElements
                     : XMLElement::HasNoContent);
+
             write_params(configuration.get_parameters());
         }
 
@@ -489,9 +491,9 @@ namespace
         {
             size_t count = 0;
 
-            for (const_each<ConfigurationContainer> i = configurations; i; ++i)
+            for (const Configuration& configuration : configurations)
             {
-                if (!BaseConfigurationFactory::is_base_configuration(i->get_name()))
+                if (!BaseConfigurationFactory::is_base_configuration(configuration.get_name()))
                     ++count;
             }
 
@@ -569,6 +571,7 @@ namespace
                 !frame.post_processing_stages().empty()
                     ? XMLElement::HasChildElements
                     : XMLElement::HasNoContent);
+
             write_params(frame.get_parameters());
             write_aovs(frame);
             write_post_processing_stages(frame);
@@ -737,7 +740,6 @@ namespace
 
             write_params(object_instance.get_parameters());
             write_transform(object_instance.get_transform());
-
             write_assign_materials(ObjectInstance::FrontSide, object_instance.get_front_material_mappings());
             write_assign_materials(ObjectInstance::BackSide, object_instance.get_back_material_mappings());
         }
@@ -764,11 +766,12 @@ namespace
         // Write an <post_processing_stages> element.
         void write_post_processing_stages(const Frame& frame)
         {
-            PostProcessingStageContainer& stages = frame.post_processing_stages();
+            const PostProcessingStageContainer& stages = frame.post_processing_stages();
             if (!stages.empty())
             {
                 XMLElement element("post_processing_stages", m_file, m_indenter);
                 element.write(XMLElement::HasChildElements);
+
                 write_collection(stages);
             }
         }
@@ -792,17 +795,14 @@ namespace
                     : XMLElement::HasNoContent);
 
             write_params(scene.get_parameters());
-
             write_collection(scene.cameras());
             write_collection(scene.colors());
             write_collection(scene.textures());
             write_collection(scene.texture_instances());
             write_collection(scene.environment_edfs());
             write_collection(scene.environment_shaders());
-
             if (scene.get_environment())
                 write(*scene.get_environment());
-
             write_collection(scene.shader_groups());
             write_collection(scene.assemblies());
             write_collection(scene.assembly_instances());
@@ -858,6 +858,7 @@ namespace
             element.add_attribute("name", shader.get_shader());
             element.add_attribute("layer", shader.get_layer());
             element.write(XMLElement::HasChildElements);
+
             write_collection(shader.shader_params());
 
             if (shader.get_source_code())
@@ -882,11 +883,13 @@ namespace
             element.add_attribute("name", shader_group.get_name());
             element.write(XMLElement::HasChildElements);
 
-            for (const_each<ShaderContainer> i = shader_group.shaders(); i; ++i)
-                write(*i);
+            // Write shaders in the original order.
+            for (const Shader& shader : shader_group.shaders())
+                write(shader);
 
-            for (const_each<ShaderConnectionContainer> i = shader_group.shader_connections(); i; ++i)
-                write(*i);
+            // Write shader connections in the original order.
+            for (const ShaderConnection& shader_connection : shader_group.shader_connections())
+                write(shader_connection);
         }
 
         // Write a <surface_shader> element.
