appleseedhq/appleseed

commit 361ac517296d000acde2fce8e5df84847826675d

Improve handling of PYTHONHOME
This fixes issue #2262.

src/appleseed.studio/main/main.cpp
@@ -40,6 +40,7 @@
 // appleseed.foundation headers.
 #include "foundation/core/appleseed.h"
 #include "foundation/platform/path.h"
+#include "foundation/platform/python.h"
 #include "foundation/utility/log.h"
 #include "foundation/utility/preprocessor.h"
 
@@ -77,6 +78,10 @@ namespace bf = boost::filesystem;
 
 namespace
 {
+    // On Windows, messages emitted through this logger won't be visible unless appleseed.studio's subsystem
+    // is set to Console (in appleseed.studio properties under Linker / System / SubSystem).
+    SuperLogger g_logger;
+
     void check_compatibility()
     {
         const char* missing_feature = nullptr;
@@ -110,31 +115,67 @@ namespace
 
             exit(EXIT_FAILURE);
         }
+    }
 
-        // If the PYTHONHOME environment variable is defined, use the Python installation it points to.
-        // Otherwise use the Python installation bundled with appleseed.
-        if (getenv("PYTHONHOME") == nullptr)
+    void configure_python()
+    {
+        const char* python_home_var = getenv("PYTHONHOME");
+
+        if (python_home_var != nullptr && strlen(python_home_var) > 0)
         {
+            // PYTHONHOME is set: the embedded Python interpreter will use the Python installation
+            // the variable points to.
+            LOG_INFO(
+                g_logger,
+                "PYTHONHOME environment variable set to %s: embedded Python interpreter will use "
+                "the Python installation expected to exist at this path.",
+                python_home_var);
+        }
+        else
+        {
+            // PYTHONHOME not set or empty: use the Python installation bundled with appleseed.studio.
+
 #if defined _WIN32 || __APPLE__
-            // On Windows, Python's standard libraries are located in python27\Lib\.
-            const string python_path =
-                bf::canonical(
-                    bf::path(Application::get_root_path()) / "python27"
-                ).make_preferred().string();
+            // On Windows and macOS, Python's standard libraries are located in python27/Lib/.
+            bf::path python_path = bf::path(Application::get_root_path()) / "python27";
 #else
-            // On Linux and macOS, Python's standard libraries are located in lib/python2.7/.
-            const string python_path =
-                bf::canonical(
-                    bf::path(Application::get_root_path())
-                ).make_preferred().string();
+            // On Linux, Python's standard libraries are located in lib/python2.7/.
+            bf::path python_path = bf::path(Application::get_root_path());
 #endif
 
-            static char python_home[FOUNDATION_MAX_PATH_LENGTH + 1];
+            if (bf::is_directory(python_path))
+            {
+                const string python_path_str = bf::canonical(python_path).make_preferred().string();
 
-            assert(python_path.size() <= FOUNDATION_MAX_PATH_LENGTH);
-            strncpy(python_home, python_path.c_str(), sizeof(python_home) - 1);
+                char python_home[FOUNDATION_MAX_PATH_LENGTH + 1];
+                assert(python_path_str.size() <= FOUNDATION_MAX_PATH_LENGTH);
+                strncpy(python_home, python_path_str.c_str(), sizeof(python_home) - 1);
 
-            Py_SetPythonHome(python_home);
+                LOG_INFO(
+                    g_logger,
+                    "PYTHONHOME environment variable not set or empty: embedded Python interpreter "
+                    "will use Python installation expected to exist in %s.",
+                    python_home);
+
+                Py_SetPythonHome(python_home);
+            }
+            else
+            {
+                const string python_path_str = python_path.make_preferred().string();
+
+                QMessageBox msgbox;
+                msgbox.setWindowTitle("Python 2.7 Installation Not Found");
+                msgbox.setIcon(QMessageBox::Critical);
+                msgbox.setText(
+                    QString(
+                        "No Python 2.7 installation could be found in %1 where appleseed.studio expects one "
+                        "to be, and the PYTHONHOME environment variable is not defined or is empty. "
+                        "appleseed.studio may not work satisfactorily.").arg(QString::fromStdString(python_path_str)));
+                msgbox.setStandardButtons(QMessageBox::Ok);
+                msgbox.setDefaultButton(QMessageBox::Ok);
+                set_minimum_width(msgbox, 600);
+                msgbox.exec();
+            }
         }
     }
 
@@ -315,6 +356,9 @@ int main(int argc, char* argv[])
 
     // Make sure appleseed is correctly installed.
     check_installation();
+
+    // Configure the embedded Python interpreter.
+    configure_python();
 
     // Parse the command line.
     CommandLineHandler cl;

src/appleseed.studio/mainwindow/mainwindow.cpp
@@ -65,6 +65,7 @@
 #include "foundation/math/vector.h"
 #include "foundation/platform/compiler.h"
 #include "foundation/platform/path.h"
+#include "foundation/platform/python.h"
 #include "foundation/platform/system.h"
 #include "foundation/utility/containers/dictionary.h"
 #include "foundation/utility/foreach.h"
@@ -1345,6 +1346,11 @@ void MainWindow::print_startup_information()
         Compiler::get_compiler_version());
 
     System::print_information(global_logger());
+
+    char* python_home = Py_GetPythonHome();
+    if (python_home == nullptr)
+        RENDERER_LOG_INFO("Python home not set.");
+    else RENDERER_LOG_INFO("Python home set to %s.", python_home);
 }
 
 namespace

commit 40921fdbbb28fec1b03078fb6317bc017c056b8a

Fix appleseed.studio not starting when packaged
Regression introduced in 361ac517296d000acde2fce8e5df84847826675d ("Improve handling of PYTHONHOME").

This fixes #2398.

src/appleseed.studio/main/main.cpp
@@ -147,7 +147,8 @@ namespace
             {
                 const string python_path_str = safe_canonical(python_path).string();
 
-                char python_home[FOUNDATION_MAX_PATH_LENGTH + 1];
+                // The C string below must be declared static because Python just keeps a pointer to it.
+                static char python_home[FOUNDATION_MAX_PATH_LENGTH + 1];
                 assert(python_path_str.size() <= FOUNDATION_MAX_PATH_LENGTH);
                 strncpy(python_home, python_path_str.c_str(), sizeof(python_home) - 1);
 