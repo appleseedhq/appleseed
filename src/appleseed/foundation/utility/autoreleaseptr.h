5037402ae9f93e3a9b7c0dc4440c3dcbc302dcc7
Uedaki <thiver.96@gmail.com>
2019-02-15 18:57:26 -0800

    the bool conversion function is now explicit

--- src/appleseed/foundation/utility/autoreleaseptr.h
+++ src/appleseed/foundation/utility/autoreleaseptr.h
@@ -148,7 +148,7 @@ class auto_release_ptr
     }
 
     // Implicit bool conversion of the wrapped pointer.
-    operator bool() const
+    explicit operator bool() const
     {
         return static_cast<bool>(m_ptr);
     }
