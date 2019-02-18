082d197ab25bbc49b6f7e89181ae8b4b6b7643e0
Uedaki <thiver.96@gmail.com>
2019-02-18 02:05:42 -0800

    auto_release_ptr<> is now bool-convertible
    Fixes issue #2345.

--- src/appleseed/foundation/utility/autoreleaseptr.h
+++ src/appleseed/foundation/utility/autoreleaseptr.h
@@ -147,6 +147,15 @@ class auto_release_ptr
         return m_ptr;
     }
 
+    // Bool conversion of the wrapped pointer.
+#if !defined(_MSC_VER) || _MSC_VER >= 1800
+    explicit
+#endif
+    operator bool() const
+    {
+        return static_cast<bool>(m_ptr);
+    }
+
     // Return the wrapped pointer.
     T* get() const throw()
     {
