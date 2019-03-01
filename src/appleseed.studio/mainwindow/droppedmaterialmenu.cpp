c9ad2068f1e606245690328c34ce83d15c820734
Uedaki <thiver.96@gmail.com>
2019-02-28 17:24:47 -0800

    Update of the copyright notice

--- src/appleseed.studio/mainwindow/droppedmaterialmenu.cpp
+++ src/appleseed.studio/mainwindow/droppedmaterialmenu.cpp
@@ -5,8 +5,8 @@
 //
 // This software is released under the MIT license.
 //
-// Copyright (c) 2010-2013 Francois Beaune, Jupiter Jazz Limited
-// Copyright (c) 2014-2018 Francois Beaune, The appleseedhq Organization
+// Copyright (c) 2010-2013 Thibault Vergne, Jupiter Jazz Limited
+// Copyright (c) 2014-2018 Thibault Vergne, The appleseedhq Organization
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -27,6 +27,7 @@
 // THE SOFTWARE.
 //
 
+// Interface header.
 #include "mainwindow/droppedmaterialmenu.h"
 
 namespace appleseed {
