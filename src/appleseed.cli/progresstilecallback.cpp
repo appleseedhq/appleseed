fa7c54483ab3de251f8faab1b105ef4e84aeb39a
Achal Pandey <35342732+achalpandeyy@users.noreply.github.com>
2019-03-05 13:08:47 +0530

    Estimate remaining render time in appleseed.cli
    First step towards issue #2438.

--- src/appleseed.cli/progresstilecallback.cpp
+++ src/appleseed.cli/progresstilecallback.cpp
@@ -38,7 +38,9 @@
 #include "foundation/image/canvasproperties.h"
 #include "foundation/image/image.h"
 #include "foundation/image/tile.h"
+#include "foundation/platform/defaulttimers.h"
 #include "foundation/platform/thread.h"
+#include "foundation/utility/stopwatch.h"
 #include "foundation/utility/string.h"
 
 // Standard headers.
@@ -65,6 +67,7 @@ namespace
         explicit ProgressTileCallback(Logger& logger)
           : m_logger(logger)
           , m_rendered_pixels(0)
+          , m_rendered_tiles(0)
         {
         }
 
@@ -72,6 +75,16 @@ namespace
         {
             // The factory always return the same tile callback instance.
             // Prevent this instance from being destroyed by doing nothing here.
+        }
+
+        void on_tiled_frame_begin(const Frame* frame) override
+        {
+            m_stopwatch.start();
+        }
+
+        void on_tiled_frame_end(const Frame* frame) override
+        {
+            m_stopwatch.clear();
         }
 
         void on_tile_end(
@@ -88,14 +101,31 @@ namespace
             // Retrieve the total number of pixels in the frame.
             const size_t total_pixels = frame->image().properties().m_pixel_count;
 
+            // Keep track of the total number of rendered tiles.
+            m_rendered_tiles++;
+
+            // Retrieve the total number of tiles in the frame.
+            const size_t total_tiles = frame->image().properties().m_tile_count;
+
+            // Estimate remaining render time.
+            m_stopwatch.measure();
+            const double elapsed_time = m_stopwatch.get_seconds();
+            const double remaining_time = (elapsed_time / m_rendered_tiles) * (total_tiles - m_rendered_tiles);
+
             // Print a progress message.
-            LOG_INFO(m_logger, "rendering, %s done", pretty_percent(m_rendered_pixels, total_pixels).c_str());
+            LOG_INFO(
+                m_logger,
+                "rendering, %s done; about %s remaining...",
+                pretty_percent(m_rendered_pixels, total_pixels).c_str(),
+                pretty_time(remaining_time).c_str());
         }
 
       private:
-        Logger&             m_logger;
-        boost::mutex        m_mutex;
-        size_t              m_rendered_pixels;
+        Logger&                             m_logger;
+        boost::mutex                        m_mutex;
+        size_t                              m_rendered_pixels;
+        size_t                              m_rendered_tiles;
+        Stopwatch<DefaultWallclockTimer>    m_stopwatch;
     };
 }
 
