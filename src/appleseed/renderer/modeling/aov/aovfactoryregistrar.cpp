023c78fd9c3e27e0e8961532abc2d43493b0efe4
Sergo Pogosyan <usakhelo@users.noreply.github.com>
2019-03-10 14:44:33 -0400

    Add Cryptomatte AOV

--- src/appleseed/renderer/modeling/aov/aovfactoryregistrar.cpp
+++ src/appleseed/renderer/modeling/aov/aovfactoryregistrar.cpp
@@ -32,6 +32,7 @@
 // appleseed.renderer headers.
 #include "renderer/modeling/aov/aovtraits.h"
 #include "renderer/modeling/aov/albedoaov.h"
+#include "renderer/modeling/aov/cryptomatteaov.h"
 #include "renderer/modeling/aov/depthaov.h"
 #include "renderer/modeling/aov/diffuseaov.h"
 #include "renderer/modeling/aov/emissionaov.h"
@@ -89,6 +90,8 @@ AOVFactoryRegistrar::AOVFactoryRegistrar(const SearchPaths& search_paths)
     impl->register_factory(auto_release_ptr<FactoryType>(new PositionAOVFactory()));
     impl->register_factory(auto_release_ptr<FactoryType>(new ScreenSpaceVelocityAOVFactory()));
     impl->register_factory(auto_release_ptr<FactoryType>(new UVAOVFactory()));
+    impl->register_factory(auto_release_ptr<FactoryType>(new CryptomatteAOVFactory(CryptomatteAOV::CryptomatteType::ObjectNames)));
+    impl->register_factory(auto_release_ptr<FactoryType>(new CryptomatteAOVFactory(CryptomatteAOV::CryptomatteType::MaterialNames)));
 }
 
 AOVFactoryRegistrar::~AOVFactoryRegistrar()
