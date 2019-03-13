90999ccb6c7e96698bfb082a61d92045dcbe47d2
François Beaune <franz@appleseedhq.net>
2019-03-13 08:00:40 +0100

    Additions and improvements to sampling functions
    - Add new uniform triangle sampling function due to Eric Heitz: foundation::sample_triangle_uniform_heitz()
    - Add variant of foundation::sample_regular_polygon_uniform() taking two random numbers instead of three
    - Use Heitz's triangle sampling in all foundation::sample_regular_polygon_uniform() functions
    - Improve and add unit tests
    - Add unit benchmarks

--- src/appleseed/foundation/utility/testutils.cpp
+++ src/appleseed/foundation/utility/testutils.cpp
@@ -37,6 +37,7 @@
 #include "foundation/image/genericimagefilewriter.h"
 #include "foundation/image/image.h"
 #include "foundation/image/pixel.h"
+#include "foundation/math/aabb.h"
 #include "foundation/math/scalar.h"
 
 // Standard headers.
@@ -115,6 +116,26 @@ bool are_images_feq(
     }
 
     return true;
+}
+
+void fit_point_cloud_to_image(
+    vector<Vector2d>&           points)
+{
+    AABB2d aabb;
+    aabb.invalidate();
+
+    for (const Vector2d& p : points)
+        aabb.insert(p);
+
+    const Vector2d aabb_extent = aabb.extent();
+    const double scale = 0.8 / max_value(aabb_extent);
+
+    for (Vector2d& p : points)
+    {
+        p -= aabb.min;
+        p *= scale;
+        p += (Vector2d(1.0) - aabb_extent * scale) * 0.5;
+    }
 }
 
 void write_point_cloud_image(
