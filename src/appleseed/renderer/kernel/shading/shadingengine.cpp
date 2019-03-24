appleseedhq/appleseed

commit c403715f33ec52dd263eb2b52032ffaa698e49ca

Improve renderer::SurfaceShader API
This refactoring allows in particular to use AOVs with any surface shader, not just the Physical Surface Shader.

src/appleseed/renderer/kernel/shading/shadingengine.cpp
@@ -183,12 +183,23 @@ bool ShadingEngine::shade_hit_point(
         }
 
         // Execute the surface shader.
+        ShadingComponents shading_components;
+        AOVComponents aov_components;
         surface_shader->evaluate(
             sampling_context,
             pixel_context,
             shading_context,
             shading_point,
-            aov_accumulators,
+            shading_result,
+            shading_components,
+            aov_components);
+
+        // Accumulate shading components and AOV components into the shading result and AOVs.
+        aov_accumulators.write(
+            pixel_context,
+            shading_point,
+            shading_components,
+            aov_components,
             shading_result);
 
         // Apply alpha premultiplication.
@@ -213,25 +224,24 @@ void ShadingEngine::shade_environment(
     if (environment_shader != nullptr)
     {
         // There is an environment shader: execute it.
-        const ShadingRay& ray = shading_point.get_ray();
-        const Vector3d direction = normalize(ray.m_dir);
-        AOVComponents aov_value;
-        ShadingComponents value;
+        ShadingComponents shading_components;
         Alpha alpha;
         environment_shader->evaluate(
             shading_context,
             pixel_context,
-            direction,
-            value.m_emission,
+            normalize(shading_point.get_ray().m_dir),
+            shading_components.m_emission,
             alpha);
-
+        shading_components.m_beauty = shading_components.m_emission;
         shading_result.m_main.a = alpha[0];
-        value.m_beauty = value.m_emission;
+
+        // Accumulate shading components into the shading result and AOVs.
+        AOVComponents aov_components;
         aov_accumulators.write(
             pixel_context,
             shading_point,
-            value,
-            aov_value,
+            shading_components,
+            aov_components,
             shading_result);
 
         // Apply alpha premultiplication.

src/appleseed/renderer/modeling/surfaceshader/constantsurfaceshader.cpp
@@ -47,7 +47,9 @@
 #include "foundation/utility/containers/dictionary.h"
 
 // Forward declarations.
+namespace renderer  { class AOVComponents; }
 namespace renderer  { class PixelContext; }
+namespace renderer  { class ShadingComponents; }
 
 using namespace foundation;
 using namespace std;
@@ -106,8 +108,9 @@ namespace
             const PixelContext&         pixel_context,
             const ShadingContext&       shading_context,
             const ShadingPoint&         shading_point,
-            AOVAccumulatorContainer&    aov_accumulators,
-            ShadingResult&              shading_result) const override
+            ShadingResult&              shading_result,
+            ShadingComponents&          shading_components,
+            AOVComponents&              aov_components) const override
         {
             // Evaluate the shader inputs.
             InputValues values;

src/appleseed/renderer/kernel/aov/aovaccumulator.cpp
@@ -31,10 +31,7 @@
 
 // appleseed.renderer headers.
 #include "renderer/global/globaltypes.h"
-#include "renderer/kernel/shading/shadingcomponents.h"
-#include "renderer/kernel/shading/shadingresult.h"
 #include "renderer/modeling/aov/aov.h"
-#include "renderer/modeling/color/colorspace.h"
 #include "renderer/modeling/frame/frame.h"
 
 // appleseed.foundation headers.
@@ -44,7 +41,6 @@
 
 // Standard headers.
 #include <cassert>
-#include <cstdlib>
 #include <cstring>
 
 using namespace foundation;
@@ -182,32 +178,10 @@ AOVAccumulatorContainer::AOVAccumulatorContainer(const Frame& frame)
     }
 }
 
-namespace
-{
-    class BeautyAOVAccumulator
-      : public AOVAccumulator
-    {
-      public:
-        void write(
-            const PixelContext&         pixel_context,
-            const ShadingPoint&         shading_point,
-            const ShadingComponents&    shading_components,
-            const AOVComponents&        aov_components,
-            ShadingResult&              shading_result) override
-        {
-            shading_result.m_main.rgb() =
-                shading_components.m_beauty.to_rgb(g_std_lighting_conditions);
-        }
-    };
-}
-
 void AOVAccumulatorContainer::init()
 {
     m_size = 0;
     memset(m_accumulators, 0, MaxAOVAccumulatorCount * sizeof(AOVAccumulator*));
-
-    // Create beauty accumulator.
-    insert(auto_release_ptr<AOVAccumulator>(new BeautyAOVAccumulator()));
 }
 
 AOVAccumulatorContainer::~AOVAccumulatorContainer()

src/appleseed/renderer/kernel/lighting/bdpt/bdptlightingengine.cpp
@@ -154,7 +154,7 @@ namespace
             const ShadingContext&       shading_context,
             const ShadingPoint&         shading_point,
             ShadingComponents&          radiance,               // output radiance, in W.sr^-1.m^-2
-            AOVComponents&              components) override
+            AOVComponents&              aov_components) override
         {
             /// TODO:: use arena to alloc BDPTVertices instead
             BDPTVertex* camera_vertices = new BDPTVertex[m_num_max_vertices - 1];

src/appleseed/renderer/kernel/lighting/ilightingengine.h
@@ -64,7 +64,7 @@ class ILightingEngine
         const ShadingContext&     shading_context,
         const ShadingPoint&       shading_point,
         ShadingComponents&        radiance,           // output radiance, in W.sr^-1.m^-2
-        AOVComponents&            components) = 0;
+        AOVComponents&            aov_components) = 0;
 
     // Retrieve performance statistics.
     virtual foundation::StatisticsVector get_statistics() const = 0;

src/appleseed/renderer/kernel/lighting/null/nulllightingengine.h
@@ -72,7 +72,7 @@ class NullLightingEngine
         const ShadingContext&   shading_context,
         const ShadingPoint&     shading_point,
         ShadingComponents&      radiance,
-        AOVComponents&          components) override
+        AOVComponents&          aov_components) override
     {
     }
 };

src/appleseed/renderer/kernel/lighting/pt/ptlightingengine.cpp
@@ -162,7 +162,7 @@ namespace
             const ShadingContext&   shading_context,
             const ShadingPoint&     shading_point,
             ShadingComponents&      radiance,               // output radiance, in W.sr^-1.m^-2
-            AOVComponents&          components) override
+            AOVComponents&          aov_components) override
         {
             if (m_light_path_stream)
             {
@@ -179,7 +179,7 @@ namespace
                     shading_context,
                     shading_point,
                     radiance,
-                    components);
+                    aov_components);
             }
             else
             {
@@ -188,7 +188,7 @@ namespace
                     shading_context,
                     shading_point,
                     radiance,
-                    components);
+                    aov_components);
             }
 
             if (m_light_path_stream)
@@ -201,7 +201,7 @@ namespace
             const ShadingContext&   shading_context,
             const ShadingPoint&     shading_point,
             ShadingComponents&      radiance,               // output radiance, in W.sr^-1.m^-2
-            AOVComponents&          components)
+            AOVComponents&          aov_components)
         {
             PathVisitor path_visitor(
                 m_params,
@@ -210,7 +210,7 @@ namespace
                 shading_context,
                 shading_point.get_scene(),
                 radiance,
-                components,
+                aov_components,
                 m_light_path_stream);
 
             VolumeVisitor volume_visitor(
@@ -393,7 +393,7 @@ namespace
                 const ShadingContext&           shading_context,
                 const Scene&                    scene,
                 ShadingComponents&              path_radiance,
-                AOVComponents&                  components,
+                AOVComponents&                  aov_components,
                 LightPathStream*                light_path_stream)
               : m_params(params)
               , m_light_sampler(light_sampler)
@@ -401,7 +401,7 @@ namespace
               , m_shading_context(shading_context)
               , m_env_edf(scene.get_environment()->get_environment_edf())
               , m_path_radiance(path_radiance)
-              , m_aov_components(components)
+              , m_aov_components(aov_components)
               , m_light_path_stream(light_path_stream)
               , m_omit_emitted_light(false)
             {
@@ -423,7 +423,7 @@ namespace
                 const ShadingContext&           shading_context,
                 const Scene&                    scene,
                 ShadingComponents&              path_radiance,
-                AOVComponents&                  components,
+                AOVComponents&                  aov_components,
                 LightPathStream*                light_path_stream)
               : PathVisitorBase(
                     params,
@@ -432,7 +432,7 @@ namespace
                     shading_context,
                     scene,
                     path_radiance,
-                    components,
+                    aov_components,
                     light_path_stream)
             {
             }
@@ -531,7 +531,7 @@ namespace
                 const ShadingContext&           shading_context,
                 const Scene&                    scene,
                 ShadingComponents&              path_radiance,
-                AOVComponents&                  components,
+                AOVComponents&                  aov_components,
                 LightPathStream*                light_path_stream)
               : PathVisitorBase(
                     params,
@@ -540,7 +540,7 @@ namespace
                     shading_context,
                     scene,
                     path_radiance,
-                    components,
+                    aov_components,
                     light_path_stream)
               , m_is_indirect_lighting(false)
             {

src/appleseed/renderer/kernel/lighting/sppm/sppmlightingengine.cpp
@@ -155,7 +155,7 @@ namespace
             const ShadingContext&       shading_context,
             const ShadingPoint&         shading_point,
             ShadingComponents&          radiance,               // output radiance, in W.sr^-1.m^-2
-            AOVComponents&              components) override
+            AOVComponents&              aov_components) override
         {
             if (m_params.m_view_photons)
             {

commit 60c872b6b5eb8e9ca3af81e4e04f7f1a9e242b95

Improve renderer::EnvironmentShader API and fix regression
Fix regression introduced in c403715f33ec52dd263eb2b52032ffaa698e49ca: Improve renderer::SurfaceShader API

src/appleseed/renderer/kernel/shading/shadingengine.cpp
@@ -225,18 +225,16 @@ void ShadingEngine::shade_environment(
     {
         // There is an environment shader: execute it.
         ShadingComponents shading_components;
-        Alpha alpha;
+        AOVComponents aov_components;
         environment_shader->evaluate(
             shading_context,
             pixel_context,
             normalize(shading_point.get_ray().m_dir),
-            shading_components.m_emission,
-            alpha);
-        shading_components.m_beauty = shading_components.m_emission;
-        shading_result.m_main.a = alpha[0];
+            shading_result,
+            shading_components,
+            aov_components);
 
-        // Accumulate shading components into the shading result and AOVs.
-        AOVComponents aov_components;
+        // Accumulate shading components and AOV components into the shading result and AOVs.
         aov_accumulators.write(
             pixel_context,
             shading_point,

src/appleseed/renderer/modeling/environmentshader/backgroundenvironmentshader.cpp
@@ -30,16 +30,23 @@
 #include "backgroundenvironmentshader.h"
 
 // appleseed.renderer headers.
+#include "renderer/global/globaltypes.h"
 #include "renderer/kernel/rendering/pixelcontext.h"
 #include "renderer/kernel/shading/shadingcontext.h"
+#include "renderer/kernel/shading/shadingresult.h"
+#include "renderer/modeling/color/colorspace.h"
 #include "renderer/modeling/environmentshader/environmentshader.h"
 #include "renderer/modeling/input/sourceinputs.h"
 
 // appleseed.foundation headers.
-#include "foundation/image/colorspace.h"
+#include "foundation/image/color.h"
 #include "foundation/math/vector.h"
 #include "foundation/utility/api/specializedapiarrays.h"
 #include "foundation/utility/containers/dictionary.h"
+
+// Forward declarations.
+namespace renderer  { class AOVComponents; }
+namespace renderer  { class ShadingComponents; }
 
 using namespace foundation;
 
@@ -81,19 +88,21 @@ namespace
             const ShadingContext&   shading_context,
             const PixelContext&     pixel_context,
             const Vector3d&         direction,
-            Spectrum&               value,
-            Alpha&                  alpha) const override
+            ShadingResult&          shading_result,
+            ShadingComponents&      shading_components,
+            AOVComponents&          aov_components) const override
         {
             const Vector2f s(pixel_context.get_sample_position());
+            const Vector2f uv(s[0], 1.0f - s[1]);
 
             InputValues values;
             m_inputs.evaluate(
                 shading_context.get_texture_cache(),
-                SourceInputs(Vector2f(s[0], 1.0f - s[1])),
+                SourceInputs(uv),
                 &values);
 
-            value = values.m_color;
-            alpha = Alpha(values.m_alpha);
+            shading_result.m_main.rgb() = values.m_color.to_rgb(g_std_lighting_conditions);
+            shading_result.m_main.a = values.m_alpha;
         }
 
       private:

src/appleseed/renderer/modeling/environmentshader/backgroundenvironmentshader.h
@@ -32,7 +32,6 @@
 #include "renderer/modeling/environmentshader/ienvironmentshaderfactory.h"
 
 // appleseed.foundation headers.
-#include "foundation/platform/compiler.h"
 #include "foundation/utility/autoreleaseptr.h"
 
 // appleseed.main headers.
@@ -41,7 +40,6 @@
 // Forward declarations.
 namespace foundation    { class Dictionary; }
 namespace foundation    { class DictionaryArray; }
-namespace renderer      { class AOVAccumulatorContainer; }
 namespace renderer      { class EnvironmentShader; }
 namespace renderer      { class ParamArray; }
 

src/appleseed/renderer/modeling/environmentshader/edfenvironmentshader.cpp
@@ -32,14 +32,17 @@
 
 // appleseed.renderer headers.
 #include "renderer/global/globallogger.h"
+#include "renderer/kernel/shading/shadingcomponents.h"
+#include "renderer/kernel/shading/shadingresult.h"
+#include "renderer/modeling/color/colorspace.h"
 #include "renderer/modeling/environmentedf/environmentedf.h"
 #include "renderer/modeling/environmentshader/environmentshader.h"
 #include "renderer/utility/messagecontext.h"
 #include "renderer/utility/paramarray.h"
 
 // appleseed.foundation headers.
+#include "foundation/image/color.h"
 #include "foundation/math/vector.h"
-#include "foundation/utility/api/apistring.h"
 #include "foundation/utility/api/specializedapiarrays.h"
 #include "foundation/utility/containers/dictionary.h"
 
@@ -48,6 +51,7 @@
 
 // Forward declarations.
 namespace foundation    { class IAbortSwitch; }
+namespace renderer      { class AOVComponents; }
 namespace renderer      { class PixelContext; }
 namespace renderer      { class Project; }
 
@@ -125,12 +129,17 @@ namespace
             const ShadingContext&   shading_context,
             const PixelContext&     pixel_context,
             const Vector3d&         direction,
-            Spectrum&               value,
-            Alpha&                  alpha) const override
+            ShadingResult&          shading_result,
+            ShadingComponents&      shading_components,
+            AOVComponents&          aov_components) const override
         {
-            // Evaluate the environment EDF and store the radiance into the shading result.
+            Spectrum value;
             m_env_edf->evaluate(shading_context, Vector3f(direction), value);
-            alpha = Alpha(m_alpha_value);
+
+            shading_components.m_emission = value;
+
+            shading_result.m_main.rgb() = value.to_rgb(g_std_lighting_conditions);
+            shading_result.m_main.a = m_alpha_value;
         }
 
       private:

src/appleseed/renderer/modeling/environmentshader/edfenvironmentshader.h
@@ -33,7 +33,6 @@
 #include "renderer/modeling/environmentshader/ienvironmentshaderfactory.h"
 
 // appleseed.foundation headers.
-#include "foundation/platform/compiler.h"
 #include "foundation/utility/autoreleaseptr.h"
 
 // appleseed.main headers.
@@ -42,7 +41,6 @@
 // Forward declarations.
 namespace foundation    { class Dictionary; }
 namespace foundation    { class DictionaryArray; }
-namespace renderer      { class AOVAccumulatorContainer; }
 namespace renderer      { class EnvironmentShader; }
 namespace renderer      { class ParamArray; }
 

src/appleseed/renderer/modeling/environmentshader/environmentshader.h
@@ -30,7 +30,6 @@
 #pragma once
 
 // appleseed.renderer headers.
-#include "renderer/global/globaltypes.h"
 #include "renderer/modeling/entity/connectableentity.h"
 
 // appleseed.foundation headers.
@@ -41,9 +40,12 @@
 #include "main/dllsymbol.h"
 
 // Forward declarations.
+namespace renderer  { class AOVComponents; }
 namespace renderer  { class ParamArray; }
 namespace renderer  { class PixelContext; }
+namespace renderer  { class ShadingComponents; }
 namespace renderer  { class ShadingContext; }
+namespace renderer  { class ShadingResult; }
 
 namespace renderer
 {
@@ -71,9 +73,10 @@ class APPLESEED_DLLSYMBOL EnvironmentShader
     virtual void evaluate(
         const ShadingContext&           shading_context,
         const PixelContext&             pixel_context,
-        const foundation::Vector3d&     direction,                      // world space direction, pointing toward the environment
-        Spectrum&                       value,
-        Alpha&                          alpha) const = 0;
+        const foundation::Vector3d&     direction,          // world space direction, pointing toward the environment
+        ShadingResult&                  shading_result,
+        ShadingComponents&              shading_components,
+        AOVComponents&                  aov_components) const = 0;
 };
 
 }   // namespace renderer

src/appleseed/renderer/modeling/surfaceshader/constantsurfaceshader.cpp
@@ -42,6 +42,7 @@
 #include "renderer/utility/paramarray.h"
 
 // appleseed.foundation headers.
+#include "foundation/image/color.h"
 #include "foundation/image/colorspace.h"
 #include "foundation/utility/api/specializedapiarrays.h"
 #include "foundation/utility/containers/dictionary.h"