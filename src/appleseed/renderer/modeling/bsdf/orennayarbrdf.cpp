appleseedhq/appleseed

commit 5b494941d33ae3614c55e52813359618277821b4

Fix albedo AOV for Oren Nayar BRDF when roughness is zero.

src/appleseed/renderer/modeling/bsdf/orennayarbrdf.cpp
@@ -146,15 +146,16 @@ namespace
                         incoming,
                         n,
                         sample.m_value.m_diffuse);
-
-                    sample.m_aov_components.m_albedo = values->m_reflectance;
                 }
                 else
                 {
                     // Revert to Lambertian when roughness is zero.
                     sample.m_value.m_diffuse = values->m_reflectance;
-                    sample.m_value.m_diffuse *= values->m_reflectance_multiplier * RcpPi<float>();
+                    sample.m_value.m_diffuse *= values->m_reflectance_multiplier;
                 }
+
+                sample.m_aov_components.m_albedo = values->m_reflectance;
+                sample.m_aov_components.m_albedo *= values->m_reflectance_multiplier;
 
                 sample.m_value.m_beauty = sample.m_value.m_diffuse;
                 sample.m_min_roughness = 1.0f;

src/appleseed/renderer/modeling/bsdf/lambertianbrdf.cpp
@@ -119,10 +119,12 @@ namespace
                 // Compute the BRDF value.
                 const LambertianBRDFInputValues* values = static_cast<const LambertianBRDFInputValues*>(data);
                 sample.m_value.m_diffuse = values->m_reflectance;
-                sample.m_value.m_diffuse *= values->m_reflectance_multiplier * RcpPi<float>();
+                sample.m_value.m_diffuse *= values->m_reflectance_multiplier;
+                sample.m_aov_components.m_albedo = sample.m_value.m_diffuse;
+
+                sample.m_value.m_diffuse *= RcpPi<float>();
                 sample.m_value.m_beauty = sample.m_value.m_diffuse;
 
-                sample.m_aov_components.m_albedo = values->m_reflectance;
                 sample.m_min_roughness = 1.0f;
 
                 sample.compute_reflected_differentials();

commit cfff5cb866fc09e242923e5f6ba8273c5a8f0618

Fix regression in Oren-Nayar BRDF
Regression introduced by 5b494941d33ae3614c55e52813359618277821b4: Fix albedo AOV for Oren Nayar BRDF when roughness is zero.

src/appleseed/renderer/modeling/bsdf/orennayarbrdf.cpp
@@ -151,7 +151,7 @@ namespace
                 {
                     // Revert to Lambertian when roughness is zero.
                     sample.m_value.m_diffuse = values->m_reflectance;
-                    sample.m_value.m_diffuse *= values->m_reflectance_multiplier;
+                    sample.m_value.m_diffuse *= values->m_reflectance_multiplier * RcpPi<float>();
                 }
 
                 sample.m_aov_components.m_albedo = values->m_reflectance;