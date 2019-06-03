c690d821aacc5291ef30fa6271315c542011472b
Franz Beaune <franz@appleseedhq.net>
2019-06-03 13:37:10 +0200

    Tweak formatting

--- src/appleseed/foundation/platform/sharedlibrary.cpp
+++ src/appleseed/foundation/platform/sharedlibrary.cpp
@@ -122,11 +122,7 @@ SharedLibrary::SharedLibrary(const char* path)
 #endif
 
     if (m_handle == nullptr)
-    {
-        throw ExceptionCannotLoadSharedLib(
-            path,
-            get_last_error_message().c_str());
-    }
+        throw ExceptionCannotLoadSharedLib(path, get_last_error_message().c_str());
 }
 
 SharedLibrary::~SharedLibrary()
@@ -147,11 +143,7 @@ void* SharedLibrary::get_symbol(const char* name, const bool no_throw) const
 #endif
 
     if (symbol == nullptr && !no_throw)
-    {
-        throw ExceptionSharedLibCannotGetSymbol(
-            name,
-            get_last_error_message().c_str());
-    }
+        throw ExceptionSharedLibCannotGetSymbol(name, get_last_error_message().c_str());
 
     return symbol;
 }
