appleseedhq/appleseed

commit 4eb5ae72480486a52c783f3e014c457ff3beca6b

Switch to Filter Importance Sampling
This commit implements Filter Importance Sampling (FIS) as a replacement for the sample splatting we were using before.

This removes the need for tile margins and removes many filter evaluations. It also makes noise independent for each pixel, since samples are not shared anymore and will make certain denoisers work better with images rendered by appleseed.

src/appleseed/renderer/kernel/lighting/lighttracing/lighttracingsamplegenerator.cpp
@@ -508,6 +508,7 @@ namespace
                 const Color3f linear_rgb = radiance.to_rgb(g_std_lighting_conditions);
 
                 Sample sample;
+                sample.m_pixel_coords = m_frame.get_pixel_position(position_ndc);
                 sample.m_position = Vector2f(position_ndc);
                 sample.m_color = Color4f(linear_rgb, 1.0f);
                 m_samples.push_back(sample);
@@ -946,8 +947,7 @@ SampleAccumulationBuffer* LightTracingSampleGeneratorFactory::create_sample_accu
     return
         new GlobalSampleAccumulationBuffer(
             props.m_canvas_width,
-            props.m_canvas_height,
-            m_frame.get_filter());
+            props.m_canvas_height);
 }
 
 }   // namespace renderer

src/appleseed/renderer/kernel/rendering/generic/genericsamplegenerator.cpp
@@ -45,6 +45,7 @@
 // appleseed.foundation headers.
 #include "foundation/image/canvasproperties.h"
 #include "foundation/image/image.h"
+#include "foundation/math/filtersamplingtable.h"
 #include "foundation/math/population.h"
 #include "foundation/math/qmc.h"
 #include "foundation/math/scalar.h"
@@ -86,6 +87,7 @@ namespace
           , m_sample_renderer(sample_renderer_factory->create(generator_index))
           , m_window_width_next_pow2(next_power(static_cast<double>(m_window_width), 2.0))
           , m_window_height_next_pow3(next_power(static_cast<double>(m_window_height), 3.0))
+          , m_filter_sampling_table(frame.get_filter_sampling_table())
         {
         }
 
@@ -141,6 +143,8 @@ namespace
         const double                        m_window_width_next_pow2;
         const double                        m_window_height_next_pow3;
 
+        const FilterSamplingTable&          m_filter_sampling_table;
+
         Population<uint64>                  m_total_sampling_dim;
 
         AOVAccumulatorContainer             m_aov_accumulators;
@@ -162,17 +166,6 @@ namespace
             if (x >= m_window_width || y >= m_window_height)
                 return 0;
 
-            // Transform the sample position back to NDC. Full precision divisions are required
-            // to ensure that the sample position indeed lies in the [0,1)^2 interval.
-            const Vector2d sample_position(
-                (m_window_origin_x + t[0]) / m_canvas_width,
-                (m_window_origin_y + t[1]) / m_canvas_height);
-
-            // Create a pixel context that identifies the pixel and sample currently being rendered.
-            const PixelContext pixel_context(
-                Vector2i(m_window_origin_x + x, m_window_origin_y + y),
-                sample_position);
-
             // Create a sampling context. We start with an initial dimension of 2,
             // corresponding to the Halton sequence used for the sample positions.
             SamplingContext sampling_context(
@@ -181,6 +174,23 @@ namespace
                 2,                          // number of dimensions
                 sequence_index,             // number of samples
                 sequence_index);            // initial instance number
+
+            // Sample the pixel filter.
+            sampling_context.split_in_place(2, 1);
+            const Vector2f f = sampling_context.next2<Vector2f>();
+            const Vector2d pf(
+                static_cast<double>(m_filter_sampling_table.sample(f.x)) + 0.5,
+                static_cast<double>(m_filter_sampling_table.sample(f.y)) + 0.5);
+
+            // Transform the sample position back to NDC.
+            const Vector2d sample_position(
+                (m_window_origin_x + x + pf[0]) / m_canvas_width,
+                (m_window_origin_y + y + pf[1]) / m_canvas_height);
+
+            // Create a pixel context that identifies the pixel and sample currently being rendered.
+            const PixelContext pixel_context(
+                Vector2i(m_window_origin_x + x, m_window_origin_y + y),
+                sample_position);
 
             // Render the sample.
             ShadingResult shading_result;
@@ -203,6 +213,7 @@ namespace
 
             // Create a single sample.
             Sample sample;
+            sample.m_pixel_coords = pixel_context.get_pixel_coords();
             sample.m_position = Vector2f(sample_position);
             sample.m_color = shading_result.m_main;
             samples.push_back(sample);
@@ -252,8 +263,7 @@ SampleAccumulationBuffer* GenericSampleGeneratorFactory::create_sample_accumulat
     return
         new LocalSampleAccumulationBuffer(
             props.m_canvas_width,
-            props.m_canvas_height,
-            m_frame.get_filter());
+            props.m_canvas_height);
 }
 
 }   // namespace renderer

src/appleseed/renderer/kernel/rendering/globalsampleaccumulationbuffer.cpp
@@ -53,10 +53,8 @@ namespace renderer
 
 GlobalSampleAccumulationBuffer::GlobalSampleAccumulationBuffer(
     const size_t    width,
-    const size_t    height,
-    const Filter2f& filter)
-  : m_fb(width, height, 3, filter)
-  , m_filter_rcp_norm_factor(1.0f / compute_normalization_factor(filter))
+    const size_t    height)
+  : m_fb(width, height, 3)
 {
 }
 
@@ -85,8 +83,6 @@ void GlobalSampleAccumulationBuffer::store_samples(
             break;
     }
 
-    const float fw = static_cast<float>(m_fb.get_width());
-    const float fh = static_cast<float>(m_fb.get_height());
     size_t counter = 0;
 
     const Sample* sample_end = samples + sample_count;
@@ -95,13 +91,8 @@ void GlobalSampleAccumulationBuffer::store_samples(
         if ((counter++ & 4096) == 0 && abort_switch.is_aborted())
             return;
 
-        const float fx = s->m_position.x * fw;
-        const float fy = s->m_position.y * fh;
-
         Color3f value(s->m_color.rgb());
-        value *= m_filter_rcp_norm_factor;
-
-        m_fb.atomic_add(fx, fy, &value[0]);
+        m_fb.atomic_add(Vector2u(s->m_pixel_coords), &value[0]);
     }
 }
 

src/appleseed/renderer/kernel/rendering/sample.h
@@ -39,6 +39,7 @@ namespace renderer
 class Sample
 {
   public:
+    foundation::Vector2i    m_pixel_coords;
     foundation::Vector2f    m_position;
     foundation::Color4f     m_color;
 };

src/appleseed/renderer/modeling/frame/frame.h
@@ -53,6 +53,7 @@
 
 // Forward declarations.
 namespace foundation    { class DictionaryArray; }
+namespace foundation    { class FilterSamplingTable; }
 namespace foundation    { class IAbortSwitch; }
 namespace foundation    { class Image; }
 namespace foundation    { class ImageAttributes; }
@@ -115,8 +116,8 @@ class APPLESEED_DLLSYMBOL Frame
     // Access the AOV images.
     ImageStack& aov_images() const;
 
-    // Return the reconstruction filter used by the main image and the AOV images.
-    const foundation::Filter2f& get_filter() const;
+    // Return the sampling table for the reconstruction filter used by the main image and the AOV images.
+    const foundation::FilterSamplingTable& get_filter_sampling_table() const;
 
     // Return the number of the first pass to be rendered.
     // 0 by default but can be different if a checkpoint was loaded.
@@ -159,6 +160,9 @@ class APPLESEED_DLLSYMBOL Frame
         const size_t                                    pixel_y,            // y coordinate of the pixel in the tile
         const double                                    sample_x,           // x coordinate of the sample in the pixel, in [0,1)
         const double                                    sample_y) const;    // y coordinate of the sample in the pixel, in [0,1)
+
+    // Return the image space coordinates of a given point in NDC coordinates.
+    foundation::Vector2i get_pixel_position(const foundation::Vector2d& ndc) const;
 
     // Do any post-process needed by AOV images.
     void post_process_aov_images() const;
@@ -315,6 +319,13 @@ inline foundation::Vector2d Frame::get_sample_position(
             tile_y * m_props.m_tile_height + pixel_y,
             sample_x,
             sample_y);
+}
+
+inline foundation::Vector2i Frame::get_pixel_position(const foundation::Vector2d& ndc) const
+{
+    return foundation::Vector2i(
+        static_cast<int>((ndc.x * m_props.m_canvas_width) + 0.5),
+        static_cast<int>((ndc.y * m_props.m_canvas_height) + 0.5));
 }
 
 }   // namespace renderer

src/appleseed.studio/mainwindow/renderingsettingswindow.cpp
@@ -477,7 +477,6 @@ namespace
 
             create_direct_link("uniform_sampler.samples",                   "uniform_pixel_renderer.samples");
             create_direct_link("uniform_sampler.force_antialiasing",        "uniform_pixel_renderer.force_antialiasing");
-            create_direct_link("uniform_sampler.decorrelate_pixels",        "uniform_pixel_renderer.decorrelate_pixels");
 
             create_direct_link("adaptive_tile_sampler.batch_size",          "adaptive_tile_renderer.batch_size");
             create_direct_link("adaptive_tile_sampler.min_samples",         "adaptive_tile_renderer.min_samples");
@@ -517,7 +516,6 @@ namespace
         QSpinBox*   m_image_plane_sampler_passes;
         QGroupBox*  m_uniform_image_plane_sampler;
         QGroupBox*  m_adaptive_tile_image_plane_sampler;
-        QCheckBox*  m_uniform_sampler_decorrelate_pixels;
         QCheckBox*  m_uniform_sampler_force_aa;
 
         void create_image_plane_sampling_general_settings(QVBoxLayout* parent)
@@ -575,14 +573,6 @@ namespace
             connect(
                 samples_spinbox, SIGNAL(valueChanged(const int)),
                 SLOT(slot_changed_uniform_sampler_samples(const int)));
-
-            m_uniform_sampler_decorrelate_pixels = create_checkbox("uniform_sampler.decorrelate_pixels", "Decorrelate Pixels");
-            m_uniform_sampler_decorrelate_pixels->setToolTip(m_params_metadata.get_path("uniform_pixel_renderer.decorrelate_pixels.help"));
-            layout->addWidget(m_uniform_sampler_decorrelate_pixels);
-
-            connect(
-                m_image_plane_sampler_passes, SIGNAL(valueChanged(const int)),
-                SLOT(slot_changed_image_plane_sampler_passes(const int)));
         }
 
         void create_image_plane_sampling_adaptive_tile_sampler_settings(QHBoxLayout* parent)
@@ -642,19 +632,6 @@ namespace
 
             m_uniform_image_plane_sampler->setEnabled(sampler == "uniform");
             m_adaptive_tile_image_plane_sampler->setEnabled(sampler == "adaptive_tile");
-        }
-
-        void slot_changed_image_plane_sampler_passes(const int passes)
-        {
-            if (passes > 1)
-            {
-                m_uniform_sampler_decorrelate_pixels->setChecked(true);
-                m_uniform_sampler_decorrelate_pixels->setDisabled(true);
-            }
-            else
-            {
-                m_uniform_sampler_decorrelate_pixels->setDisabled(false);
-            }
         }
 
         void slot_changed_uniform_sampler_samples(const int samples)

src/appleseed/CMakeLists.txt
@@ -132,6 +132,8 @@ source_group ("foundation\\image\\text" FILES
 )
 
 set (foundation_image_sources
+    foundation/image/accumulatortile.cpp
+    foundation/image/accumulatortile.h
     foundation/image/analysis.cpp
     foundation/image/analysis.h
     foundation/image/canvasproperties.h
@@ -145,8 +147,6 @@ set (foundation_image_sources
     foundation/image/drawing.cpp
     foundation/image/drawing.h
     foundation/image/exceptionunsupportedimageformat.h
-    foundation/image/filteredtile.cpp
-    foundation/image/filteredtile.h
     foundation/image/genericimagefilereader.cpp
     foundation/image/genericimagefilereader.h
     foundation/image/genericimagefilewriter.cpp
@@ -316,6 +316,8 @@ set (foundation_math_sources
     foundation/math/dual.h
     foundation/math/fastmath.h
     foundation/math/filter.h
+    foundation/math/filtersamplingtable.cpp
+    foundation/math/filtersamplingtable.h
     foundation/math/fp.h
     foundation/math/fresnel.h
     foundation/math/frustum.h
@@ -400,7 +402,6 @@ set (foundation_meta_benchmarks_sources
     foundation/meta/benchmarks/benchmark_colorspace.cpp
     foundation/meta/benchmarks/benchmark_distance.cpp
     foundation/meta/benchmarks/benchmark_fastmath.cpp
-    foundation/meta/benchmarks/benchmark_filteredtile.cpp
     foundation/meta/benchmarks/benchmark_half.cpp
     foundation/meta/benchmarks/benchmark_hash.cpp
     foundation/meta/benchmarks/benchmark_imageimportancesampler.cpp
@@ -466,7 +467,7 @@ set (foundation_meta_tests_sources
     foundation/meta/tests/test_dictionary.cpp
     foundation/meta/tests/test_distance.cpp
     foundation/meta/tests/test_fastmath.cpp
-    foundation/meta/tests/test_filteredtile.cpp
+    foundation/meta/tests/test_filtersamplingtable.cpp
     foundation/meta/tests/test_fp.cpp
     foundation/meta/tests/test_fresnel.cpp
     foundation/meta/tests/test_genericimagefilewriter.cpp

src/appleseed/foundation/image/accumulatortile.cpp
@@ -1,0 +1,98 @@
+
+//
+// This source file is part of appleseed.
+// Visit https://appleseedhq.net/ for additional information and resources.
+//
+// This software is released under the MIT license.
+//
+// Copyright (c) 2010-2013 Francois Beaune, Jupiter Jazz Limited
+// Copyright (c) 2014-2018 Francois Beaune, The appleseedhq Organization
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+// Interface header.
+#include "accumulatortile.h"
+
+// appleseed.foundation headers.
+#include "foundation/image/pixel.h"
+#include "foundation/math/scalar.h"
+#include "foundation/platform/atomic.h"
+
+namespace foundation
+{
+
+AccumulatorTile::AccumulatorTile(
+    const size_t            width,
+    const size_t            height,
+    const size_t            channel_count)
+  : Tile(width, height, channel_count + 1, PixelFormatFloat)
+  , m_crop_window(Vector2u(0, 0), Vector2u(width - 1, height - 1))
+{
+}
+
+AccumulatorTile::AccumulatorTile(
+    const size_t            width,
+    const size_t            height,
+    const size_t            channel_count,
+    const AABB2u&           crop_window)
+  : Tile(width, height, channel_count + 1, PixelFormatFloat)
+  , m_crop_window(crop_window)
+{
+}
+
+void AccumulatorTile::clear()
+{
+    float* ptr = reinterpret_cast<float*>(pixel(0));
+
+    for (size_t i = 0, e = m_pixel_count * m_channel_count; i < e; ++i)
+        ptr[i] = 0.0f;
+}
+
+void AccumulatorTile::add(
+    const Vector2u&         pi,
+    const float*            values)
+{
+    // Ignore samples outside the crop window.
+    if (!m_crop_window.contains(pi))
+        return;
+
+    float* APPLESEED_RESTRICT ptr = reinterpret_cast<float*>(pixel(pi.x, pi.y));
+    *ptr++ += 1.0f;
+
+    for (size_t i = 0, e = m_channel_count - 1; i < e; ++i)
+        *ptr++ += values[i];
+}
+
+void AccumulatorTile::atomic_add(
+    const Vector2u&         pi,
+    const float*            values)
+{
+    // Ignore samples outside the crop window.
+    if (!m_crop_window.contains(pi))
+        return;
+
+    float* APPLESEED_RESTRICT ptr = reinterpret_cast<float*>(pixel(pi.x, pi.y));
+    foundation::atomic_add(ptr++, 1.0f);
+
+    for (size_t i = 0, e = m_channel_count - 1; i < e; ++i)
+        foundation::atomic_add(ptr++, values[i]);
+}
+
+}   // namespace foundation

commit a634d040be6918ac9416e4a2aa24635a804376fd

Fix half-pixel shift introduced in switch to FIS
Regression introduced in 4eb5ae72480486a52c783f3e014c457ff3beca6b: `Switch to Filter Importance Sampling`

src/appleseed/renderer/kernel/lighting/lighttracing/lighttracingsamplegenerator.cpp
@@ -279,13 +279,13 @@ namespace
         {
             const Parameters&               m_params;
             const Camera&                   m_camera;
-            const Frame&                    m_frame;
+            const size_t                    m_canvas_width;
+            const size_t                    m_canvas_height;
             const ShadingContext&           m_shading_context;
             SamplingContext&                m_sampling_context;
-
-            const Spectrum                  m_initial_flux;         // initial particle flux (in W)
             SampleVector&                   m_samples;
             size_t                          m_sample_count;         // the number of samples added to m_samples
+            const Spectrum                  m_initial_flux;         // initial particle flux (in W)
 
             PathVisitor(
                 const Parameters&           params,
@@ -297,7 +297,8 @@ namespace
                 const Spectrum&             initial_flux)
               : m_params(params)
               , m_camera(*scene.get_active_camera())
-              , m_frame(frame)
+              , m_canvas_width(frame.image().properties().m_canvas_width)
+              , m_canvas_height(frame.image().properties().m_canvas_height)
               , m_shading_context(shading_context)
               , m_sampling_context(sampling_context)
               , m_samples(samples)
@@ -512,9 +513,10 @@ namespace
                 const Color3f linear_rgb = radiance.to_rgb(g_std_lighting_conditions);
 
                 Sample sample;
-                sample.m_pixel_coords = m_frame.get_pixel_position(position_ndc);
-                sample.m_position = Vector2f(position_ndc);
-                sample.m_color = Color4f(linear_rgb, 1.0f);
+                sample.m_pixel_coords.x = static_cast<int>(position_ndc.x * m_canvas_width);
+                sample.m_pixel_coords.y = static_cast<int>(position_ndc.y * m_canvas_height);
+                sample.m_color.rgb() = linear_rgb;
+                sample.m_color.a = 1.0f;
                 m_samples.push_back(sample);
 
                 ++m_sample_count;

src/appleseed/renderer/kernel/rendering/generic/genericsamplegenerator.cpp
@@ -214,7 +214,6 @@ namespace
             // Create a single sample.
             Sample sample;
             sample.m_pixel_coords = pixel_context.get_pixel_coords();
-            sample.m_position = Vector2f(sample_position);
             sample.m_color = shading_result.m_main;
             samples.push_back(sample);
 

src/appleseed/renderer/kernel/rendering/globalsampleaccumulationbuffer.cpp
@@ -91,8 +91,7 @@ void GlobalSampleAccumulationBuffer::store_samples(
         if ((counter++ & 4096) == 0 && abort_switch.is_aborted())
             return;
 
-        Color3f value(s->m_color.rgb());
-        m_fb.atomic_add(Vector2u(s->m_pixel_coords), &value[0]);
+        m_fb.atomic_add(Vector2u(s->m_pixel_coords), &s->m_color[0]);
     }
 }
 

src/appleseed/renderer/kernel/rendering/sample.h
@@ -40,7 +40,6 @@ class Sample
 {
   public:
     foundation::Vector2i    m_pixel_coords;
-    foundation::Vector2f    m_position;
     foundation::Color4f     m_color;
 };
 

src/appleseed/renderer/modeling/frame/frame.h
@@ -158,9 +158,6 @@ class APPLESEED_DLLSYMBOL Frame
         const double                                    sample_x,           // x coordinate of the sample in the pixel, in [0,1)
         const double                                    sample_y) const;    // y coordinate of the sample in the pixel, in [0,1)
 
-    // Return the image space coordinates of a given point in NDC coordinates.
-    foundation::Vector2i get_pixel_position(const foundation::Vector2d& ndc) const;
-
     // Do any post-process needed by AOV images.
     void post_process_aov_images() const;
 
@@ -318,13 +315,6 @@ inline foundation::Vector2d Frame::get_sample_position(
             tile_y * m_props.m_tile_height + pixel_y,
             sample_x,
             sample_y);
-}
-
-inline foundation::Vector2i Frame::get_pixel_position(const foundation::Vector2d& ndc) const
-{
-    return foundation::Vector2i(
-        static_cast<int>((ndc.x * m_props.m_canvas_width) + 0.5),
-        static_cast<int>((ndc.y * m_props.m_canvas_height) + 0.5));
 }
 
 }   // namespace renderer