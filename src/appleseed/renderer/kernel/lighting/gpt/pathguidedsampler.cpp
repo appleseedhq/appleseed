a61d06c90789b924ee1240ff0dccc499a9351986
Stephen Agyemang <stephen.agyemang@hotmail.com>
2019-08-16 14:50:14 +0200

    Further changes to PathguidedSampler
    Getting rid of unnecessary variables and assignments. Splitting
    context only where it will be used.

--- src/appleseed/renderer/kernel/lighting/gpt/pathguidedsampler.cpp
+++ src/appleseed/renderer/kernel/lighting/gpt/pathguidedsampler.cpp
@@ -49,15 +49,15 @@ bool PathGuidedSampler::sample(
     BSDFSample sample(&m_shading_point, Dual3f(outgoing));
     float d_tree_pdf;
 
-    bool is_path_guided = guide_path_extension(
-        sampling_context,
-        sample,
-        pdf,
-        d_tree_pdf
+    guide_path_extension(
+                    sampling_context,
+                    sample,
+                    pdf,
+                    d_tree_pdf
     );
 
     // Filter scattering modes.
-    if (!is_path_guided && !(m_bsdf_sampling_modes & sample.get_mode()))
+    if (!(m_bsdf_sampling_modes & sample.get_mode()))
         return false;
 
     incoming = sample.m_incoming;
@@ -108,19 +108,12 @@ float PathGuidedSampler::evaluate(
     return guided_path_extension_pdf(incoming, bsdf_pdf, d_tree_pdf, false);
 }
 
-// This was taken from the Mitsuba implementation of "Practical Path Guiding for
-// Efficient Light-Transport Simulation" [Müller et al. 2017] and adapted to work
-// with Appleseed.
-
 bool PathGuidedSampler::guide_path_extension(
     SamplingContext&                sampling_context,
     BSDFSample&                     bsdf_sample,
     float&                          wi_pdf,
     float&                          d_tree_pdf) const
 {
-    sampling_context.split_in_place(2, 1);
-    foundation::Vector2f sample = sampling_context.next2<foundation::Vector2f>();
-
     if (!m_sd_tree_is_built || m_bsdf.is_purely_specular())
     {
         m_bsdf.sample(
@@ -130,14 +123,17 @@ bool PathGuidedSampler::guide_path_extension(
             true, // multiply by |cos(incoming, normal)|
             m_bsdf_sampling_modes,
             bsdf_sample);
-        wi_pdf = bsdf_sample.get_probability();
+            
         d_tree_pdf = 0.0f;
+        wi_pdf = bsdf_sample.get_probability();
         return false;
     }
 
-    if (sample.x < m_bsdf_sampling_fraction)
+    sampling_context.split_in_place(1, 1);
+    const float s = sampling_context.next2<float>();
+    
+    if (s < m_bsdf_sampling_fraction)
     {
-        sample.x /= m_bsdf_sampling_fraction;
         m_bsdf.sample(
             sampling_context,
             m_bsdf_data,
@@ -147,13 +143,8 @@ bool PathGuidedSampler::guide_path_extension(
             bsdf_sample);
 
         if(bsdf_sample.get_mode() == ScatteringMode::None)
-        {
-            wi_pdf = d_tree_pdf = 0.0f;
             return false;
-        }
-        
-        // If we sampled a delta component, then we have a 0 probability
-        // of sampling that direction via guiding, thus we can return early.
+
         if (bsdf_sample.get_mode() == ScatteringMode::Specular)
         {
             d_tree_pdf = 0.0f;
@@ -167,7 +158,6 @@ bool PathGuidedSampler::guide_path_extension(
     }
     else
     {
-        sample.x = (sample.x - m_bsdf_sampling_fraction) / (1.0f - m_bsdf_sampling_fraction);
         DTreeSample d_tree_sample;
         m_d_tree->sample(sampling_context, d_tree_sample);
         bsdf_sample.m_incoming = foundation::Dual3f(d_tree_sample.direction);
@@ -183,9 +173,10 @@ bool PathGuidedSampler::guide_path_extension(
             bsdf_sample.m_incoming.get_value(),
             m_bsdf_sampling_modes,
             bsdf_sample.m_value);
+
         bsdf_sample.set_to_scattering(
-            ScatteringMode::has_diffuse(m_bsdf.get_modes()) ? ScatteringMode::Diffuse : ScatteringMode::Glossy,
-            bsdf_pdf);
+            ScatteringMode::has_diffuse(m_bsdf.get_modes()) ? ScatteringMode::Diffuse : ScatteringMode::Glossy, bsdf_pdf);
+
         wi_pdf = guided_path_extension_pdf(bsdf_sample.m_incoming.get_value(), bsdf_pdf, d_tree_pdf, true);
 
         return true;
