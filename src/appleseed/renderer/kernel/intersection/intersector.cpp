ffa307d81083f1850dd6b039743006871164814e
Kevin Masson <hi@oktomus.com>
2019-09-21 15:59:39 +0200

    Apply review

--- src/appleseed/renderer/kernel/intersection/intersector.cpp
+++ src/appleseed/renderer/kernel/intersection/intersector.cpp
@@ -224,8 +224,8 @@ namespace
         const ShadingPoint&         shading_point,
         const ShadingPoint*         parent_shading_point)
     {
-        static const size_t MaxWarningsPerThread = 100;
-        static size_t warnings_count = 0;
+        constexpr size_t MaxWarningsPerThread = 20;
+        static size_t warning_count = 0;
 
         if (shading_point.hit_surface() &&
             parent_shading_point &&
@@ -237,13 +237,13 @@ namespace
                     "self-intersection detected, distance %e.",
                     shading_point.get_distance());
 
-                warnings_count += 1;
+                ++warning_count;
             }
             else if (warnings_count == MaxWarningsPerThread)
             {
                 RENDERER_LOG_WARNING("more self-intersections detected, omitting warning messages for brevity.");
 
-                warnings_count += 1;
+                ++warning_count;
             }
         }
     }
