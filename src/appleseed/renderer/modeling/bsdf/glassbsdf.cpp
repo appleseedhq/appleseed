fc422566e15520c58d84d635a8e380c987f3bc95
Mango-3 <numen22@hotmail.com>
2019-10-08 00:06:49 +0300

    Fix transmittance default value in glassbsdf

--- src/appleseed/renderer/modeling/bsdf/glassbsdf.cpp
+++ src/appleseed/renderer/modeling/bsdf/glassbsdf.cpp
@@ -5,7 +5,7 @@
 //
 // This software is released under the MIT license.
 //
-// Copyright (c) 2016-2018 Esteban Tovagliari, The appleseedhq Organization
+// Copyright (c) 2016-2019 Esteban Tovagliari, The appleseedhq Organization
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -982,7 +982,7 @@ DictionaryArray GlassBSDFFactory::get_input_metadata() const
                     .insert("color", "Colors")
                     .insert("texture_instance", "Texture Instances"))
             .insert("use", "required")
-            .insert("default", "0.85"));
+            .insert("default", "0.99"));
 
     metadata.push_back(
         Dictionary()
