588958f9148b363185aa06192360c95b0dcfd25a
Franz Beaune <franz@appleseedhq.net>
2019-12-30 16:16:06 +0100

    Fix compilation with Boost 1.72+ and Qt 5.14.0

--- src/appleseed.studio/mainwindow/rendering/pixelcolortracker.cpp
+++ src/appleseed.studio/mainwindow/rendering/pixelcolortracker.cpp
@@ -133,10 +133,10 @@ void PixelColorTracker::set_rgba_labels(const QPoint& point) const
             static_cast<size_t>(pixel.y),
             linear_rgba);
 
-        m_r_label->setText(QString().sprintf("%4.3f", linear_rgba.r));
-        m_g_label->setText(QString().sprintf("%4.3f", linear_rgba.g));
-        m_b_label->setText(QString().sprintf("%4.3f", linear_rgba.b));
-        m_a_label->setText(QString().sprintf("%4.3f", linear_rgba.a));
+        m_r_label->setText(QString::number(linear_rgba.r, 'f', 3));
+        m_g_label->setText(QString::number(linear_rgba.g, 'f', 3));
+        m_b_label->setText(QString::number(linear_rgba.b, 'f', 3));
+        m_a_label->setText(QString::number(linear_rgba.a, 'f', 3));
     }
     else
     {
