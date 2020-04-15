b4b19dfa7b5e9b3fc320261286595cdc1667c36c
est77 <ramenhdr@gmail.com>
2020-04-15 20:10:11 +0200

    Added missing include in bsdfwrapper

--- src/appleseed/renderer/modeling/bsdf/bsdfwrapper.h
+++ src/appleseed/renderer/modeling/bsdf/bsdfwrapper.h
@@ -32,6 +32,7 @@
 // appleseed.renderer headers.
 #include "renderer/global/globaltypes.h"
 #include "renderer/kernel/shading/directshadingcomponents.h"
+#include "renderer/kernel/shading/shadingpoint.h"
 #include "renderer/modeling/bsdf/bsdfsample.h"
 #include "renderer/modeling/scene/objectinstance.h"
 #include "renderer/utility/shadowterminator.h"
@@ -168,7 +169,7 @@ void BSDFWrapper<BSDFImpl, Cull>::sample(
         //                 outgoing.get_value(),
         //                 sample.m_incoming.get_value(),
         //                 modes);
-        // 
+        //
         //         assert(
         //             (sample.m_probability == BSDFImpl::DiracDelta && ref_probability == 0.0f) ||
         //             (sample.m_probability > 0.0f && feq(sample.m_probability, ref_probability, 1.0e-2f)) ||
