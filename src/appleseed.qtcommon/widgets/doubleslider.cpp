appleseedhq/appleseed

commit b6f0bee4a86e2eebdfedaa4940945b57142ff775

Switch to modern connect syntax in appleseed.bench & appleseed.qtcommon
Partially addresses #2797.

src/appleseed.qtcommon/widgets/doubleslider.cpp
@@ -41,7 +41,9 @@ namespace
 DoubleSlider::DoubleSlider(QWidget* parent)
   : QSlider(parent)
 {
-    connect(this, SIGNAL(valueChanged(int)), this, SLOT(setValue(int)));
+    connect(
+        this, &DoubleSlider::valueChanged, 
+        this, QOverload<const int>::of(&DoubleSlider::setValue));
 
     setSingleStep(1);
 }
@@ -49,7 +51,9 @@ DoubleSlider::DoubleSlider(QWidget* parent)
 DoubleSlider::DoubleSlider(const Qt::Orientation orientation, QWidget* parent)
   : QSlider(orientation, parent)
 {
-    connect(this, SIGNAL(valueChanged(int)), this, SLOT(setValue(int)));
+   connect(
+        this, &DoubleSlider::valueChanged, 
+        this, QOverload<const int>::of(&DoubleSlider::setValue));
 
     setSingleStep(1);
 }

src/appleseed.bench/help/about/aboutwindow.cpp
@@ -42,6 +42,7 @@
 #include "foundation/utility/api/apistringpair.h"
 
 // Qt headers.
+#include <QDialogButtonBox>
 #include <QKeySequence>
 #include <QShortcut>
 #include <QString>
@@ -73,11 +74,11 @@ AboutWindow::AboutWindow(QWidget* parent)
 
     setFixedSize(width(), sizeHint().height());
 
-    connect(m_ui->dialogbuttonbox, SIGNAL(accepted()), this, SLOT(close()));
+    connect(m_ui->dialogbuttonbox, &QDialogButtonBox::accepted, this, &QWidget::close);
 
     connect(
-        new QShortcut(QKeySequence(Qt::Key_Escape), this), SIGNAL(activated()),
-        this, SLOT(close()));
+        new QShortcut(QKeySequence(Qt::Key_Escape), this), &QShortcut::activated,
+        this, &QWidget::close);
 
     WindowBase::load_settings();
 }

src/appleseed.bench/mainwindow/applicationsettingswindow.cpp
@@ -47,6 +47,7 @@
 #include "foundation/string/string.h"
 
 // Qt headers.
+#include <QDialogButtonBox>
 #include <QKeySequence>
 #include <QShortcut>
 #include <Qt>
@@ -89,20 +90,20 @@ void ApplicationSettingsWindow::slot_reload_application_settings()
 
 void ApplicationSettingsWindow::build_connections()
 {
-    connect(m_ui->buttonbox, SIGNAL(accepted()), SLOT(slot_save_configuration_and_close()));
-    connect(m_ui->buttonbox, SIGNAL(rejected()), SLOT(slot_restore_configuration_and_close()));
+    connect(m_ui->buttonbox, &QDialogButtonBox::accepted, &ApplicationSettingsWindow::slot_save_configuration_and_close);
+    connect(m_ui->buttonbox, &QDialogButtonBox::rejected, &ApplicationSettingsWindow::slot_restore_configuration_and_close);
 
     connect(
-        create_window_local_shortcut(this, Qt::Key_Return), SIGNAL(activated()),
-        SLOT(slot_save_configuration_and_close()));
+        create_window_local_shortcut(this, Qt::Key_Return), &QShortcut::activated),
+        &ApplicationSettingsWindow::slot_save_configuration_and_close);
 
     connect(
-        create_window_local_shortcut(this, Qt::Key_Enter), SIGNAL(activated()),
-        SLOT(slot_save_configuration_and_close()));
+        create_window_local_shortcut(this, Qt::Key_Enter), &QShortcut::activated),
+        &ApplicationSettingsWindow::slot_save_configuration_and_close);
 
     connect(
-        create_window_local_shortcut(this, Qt::Key_Escape), SIGNAL(activated()),
-        SLOT(slot_restore_configuration_and_close()));
+        create_window_local_shortcut(this, Qt::Key_Escape), &QShortcut::activated),
+        &ApplicationSettingsWindow::slot_save_configuration_and_close);
 }
 
 void ApplicationSettingsWindow::load_settings()

src/appleseed.bench/mainwindow/rendering/qttilecallback.cpp
@@ -53,8 +53,8 @@ QtTileCallback::QtTileCallback(RenderWidget* render_widget)
   : m_render_widget(render_widget)
 {
     connect(
-        this, SIGNAL(signal_update()),
-        m_render_widget, SLOT(update()),
+        this, &QtTileCallback::signal_update,
+        m_render_widget, &QWidget::update,
         Qt::QueuedConnection);
 }
 

src/appleseed.bench/mainwindow/rendering/renderingmanager.cpp
@@ -139,37 +139,37 @@ RenderingManager::RenderingManager(RenderingTimeDisplay& rendering_time_display)
     //
 
     connect(
-        &m_renderer_controller, SIGNAL(signal_frame_begin()),
-        SLOT(slot_frame_begin()),
+        &m_renderer_controller, &QtRendererController::signal_frame_begin,
+        &RenderingManager::slot_frame_begin,
         Qt::BlockingQueuedConnection);
 
     connect(
-        &m_renderer_controller, SIGNAL(signal_frame_end()),
-        SLOT(slot_frame_end()),
+        &m_renderer_controller, &QtRendererController::signal_frame_end,
+        &RenderingManager::slot_frame_end,
         Qt::BlockingQueuedConnection);
 
     connect(
-        &m_renderer_controller, SIGNAL(signal_rendering_begin()),
-        SLOT(slot_rendering_begin()),
+        &m_renderer_controller, &QtRendererController::signal_rendering_begin,
+        &RenderingManager::slot_rendering_begin,
         Qt::BlockingQueuedConnection);
 
     connect(
-        &m_renderer_controller, SIGNAL(signal_rendering_success()),
-        SLOT(slot_rendering_end()),
+        &m_renderer_controller, &QtRendererController::signal_rendering_success,
+        &RenderingManager::slot_rendering_end,
         Qt::BlockingQueuedConnection);
 
     connect(
-        &m_renderer_controller, SIGNAL(signal_rendering_abort()),
-        SLOT(slot_rendering_end()),
+        &m_renderer_controller, &QtRendererController::signal_rendering_abort,
+        &RenderingManager::slot_rendering_end,
         Qt::BlockingQueuedConnection);
 
     connect(
-        &m_renderer_controller, SIGNAL(signal_rendering_success()),
-        SIGNAL(signal_rendering_success()));
+        &m_renderer_controller, &QtRendererController::signal_rendering_success,
+        &QtRendererController::signal_rendering_success);
 
     connect(
-        &m_renderer_controller, SIGNAL(signal_rendering_abort()),
-        SIGNAL(signal_rendering_abort()));
+        &m_renderer_controller, &QtRendererController::signal_rendering_abort,
+        &QtRendererController::signal_rendering_abort);
 }
 
 void RenderingManager::start_rendering(
@@ -191,6 +191,7 @@ void RenderingManager::start_rendering(
     connect(
         tile_callback_factory, &QtTileCallbackFactory::signal_progressive_frame_update,
         this, &RenderingManager::signal_progressive_frame_update);
+    
     tile_callback_collection_factory->insert(tile_callback_factory);
 
     tile_callback_collection_factory->insert(
@@ -213,16 +214,16 @@ void RenderingManager::start_rendering(
             m_renderer_controller));
 
     connect(
-        m_master_renderer_thread.get(), SIGNAL(signal_rendering_failed()),
-        SLOT(slot_frame_end()));
+        m_master_renderer_thread.get(), &MasterRendererThread::signal_rendering_failed,
+        &RenderingManager::slot_frame_end);
 
     connect(
-        m_master_renderer_thread.get(), SIGNAL(signal_rendering_failed()),
-        SLOT(slot_rendering_failed()));
+        m_master_renderer_thread.get(), &MasterRendererThread::signal_rendering_failed,
+        &RenderingManager::slot_rendering_failed);
 
     connect(
-        m_master_renderer_thread.get(), SIGNAL(finished()),
-        SLOT(slot_master_renderer_thread_finished()));
+        m_master_renderer_thread.get(), &QThread::finished,
+        &RenderingManager::slot_master_renderer_thread_finished);
 
     m_master_renderer_thread->start();
 }

src/appleseed.qtcommon/project/projectmanager.cpp
@@ -40,6 +40,7 @@
 #include "foundation/platform/compiler.h"
 
 // Qt headers.
+#include <QFutureWatcher>
 #include <QtConcurrent>
 
 // Boost headers.
@@ -66,9 +67,10 @@ ProjectManager::ProjectManager()
   : m_is_loading(false)
   , m_dirty_flag(false)
 {
+
     connect(
-        &m_async_io_future_watcher, SIGNAL(finished()),
-        this, SLOT(slot_load_project_async_complete()));
+        &m_async_io_future_watcher, &QFutureWatcher<bool>::finished,
+        this, &ProjectManager::slot_load_project_async_complete);
 }
 
 void ProjectManager::create_project()

src/appleseed.qtcommon/widgets/foldablepanelwidget.cpp
@@ -54,7 +54,7 @@ FoldablePanelWidget::FoldablePanelWidget(const QString& title, QWidget* parent)
 
     m_container->setProperty("hasFrame", true);
 
-    connect(m_button, SIGNAL(clicked()), this, SLOT(slot_fold_unfold()));
+    connect(m_button, &QPushButton::clicked, this, &FoldablePanelWidget::slot_fold_unfold);
 }
 
 QFrame* FoldablePanelWidget::container()

src/appleseed.qtcommon/widgets/logwidget.cpp
@@ -52,7 +52,7 @@ LogWidget::LogWidget(QWidget* parent)
 {
     m_action_clear_all = new QAction("Clear All", this);
     m_action_clear_all->setShortcut(QKeySequence("Ctrl+K"));
-    connect(m_action_clear_all, SIGNAL(triggered()), this, SLOT(slot_clear_all()));
+    connect(m_action_clear_all, &QAction::triggered, this, &LogWidget::slot_clear_all);
     addAction(m_action_clear_all);
 }
 

commit fa9dfb90ba501cfc1dd0e4fa161ef555d6829326

Fix double slider regression
Regression introduced in b6f0bee4a86e2eebdfedaa4940945b57142ff775.

src/appleseed.qtcommon/widgets/doubleslider.cpp
@@ -42,7 +42,7 @@ DoubleSlider::DoubleSlider(QWidget* parent)
   : QSlider(parent)
 {
     connect(
-        this, &DoubleSlider::valueChanged, 
+        this, &QSlider::valueChanged,
         this, static_cast<void (DoubleSlider::*)(const int)>(&DoubleSlider::setValue));
 
     setSingleStep(1);
@@ -52,7 +52,7 @@ DoubleSlider::DoubleSlider(const Qt::Orientation orientation, QWidget* parent)
   : QSlider(orientation, parent)
 {
     connect(
-        this, &DoubleSlider::valueChanged, 
+        this, &QSlider::valueChanged,
         this, static_cast<void (DoubleSlider::*)(const int)>(&DoubleSlider::setValue));
 
     setSingleStep(1);