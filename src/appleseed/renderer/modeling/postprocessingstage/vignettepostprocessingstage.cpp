5a2ba7d41654aedd40337b9b76fb79e9b12f3350
Tiago Chaves <tiagoloureirochaves@gmail.com>
2020-05-14 16:23:45 -0300

    Remove commented-out code

--- src/appleseed/renderer/modeling/postprocessingstage/vignettepostprocessingstage.cpp
+++ src/appleseed/renderer/modeling/postprocessingstage/vignettepostprocessingstage.cpp
@@ -153,43 +153,6 @@ namespace
             // Wait until jobs have effectively stopped.
             job_manager.start();
             job_queue.wait_until_completion();
-
-            /*
-
-            // Generate tiles in linear order.
-            std::vector<size_t> tiles;
-            linear_ordering(tiles, props.m_tile_count);
-
-            // Make sure the right number of tiles was created.
-            assert(tiles.size() == props.m_tile_count);
-
-            // Create and schedule one post-processing effect job per tile.
-            for (size_t i = 0; i < props.m_tile_count; ++i)
-            {
-                // Compute coordinates of the tile in the frame.
-                const size_t tile_index = tiles[i];
-                const size_t tile_x = tile_index % props.m_tile_count_x;
-                const size_t tile_y = tile_index / props.m_tile_count_x;
-                assert(tile_x < props.m_tile_count_x);
-                assert(tile_y < props.m_tile_count_y);
-
-                // Create the tile job.
-                job_queue.schedule(
-                    new VignetteJob(
-                        frame, tile_x, tile_y,
-                        thread_count, abort_switch,
-                        // TODO encapsulate effect settings/context:
-                        m_intensity, m_anisotropy,
-                        resolution, normalization_factor),
-                        true);
-            }
-
-            Logger l; // TODO use globallogger() (?)
-            JobManager job_manager(l, job_queue, thread_count);
-            job_manager.start();
-
-            job_queue.wait_until_completion();
-            */
         }
 
       private:
@@ -197,115 +160,8 @@ namespace
         float m_anisotropy;
     };
 }
-/*
-//
-// VignetteEffect class implementation.
-//
 
 
-VignetteEffect::VignetteEffect(
-    const float                     intensity,
-    const float                     anisotropy,
-    const foundation::Vector2f&     resolution,
-    const foundation::Vector2f&     normalization_factor)
-  : m_intensity(intensity)
-  , m_anisotropy(anisotropy)
-  , m_resolution(resolution)
-  , m_normalization_factor(normalization_factor)
-{
-}
-
-void VignetteEffect::apply(
-    const Frame&                frame,
-    const size_t                tile_x,
-    const size_t                tile_y,
-    foundation::IAbortSwitch&   abort_switch) const
-{
-    Image& image = frame.image();
-
-    assert(tile_x < image.properties().m_tile_count_x);
-    assert(tile_y < image.properties().m_tile_count_y);
-
-    Tile& tile = image.tile(tile_x, tile_y);
-    const size_t tile_width = tile.get_width();
-    const size_t tile_height = tile.get_height();
-    const size_t tile_offset_x = tile_x * tile_width;
-    const size_t tile_offset_y = tile_y * tile_height;
-
-    for (size_t y = 0; y < tile_height; ++y)
-    {
-        for (size_t x = 0; x < tile_width; ++x)
-        {
-            const Vector2u pixel_coord = Vector2u(x + tile_offset_x, y + tile_offset_y);
-
-            // Pixel coordinate normalized to be in the [-1, 1] range vertically.
-            const Vector2f coord = (2.0f * static_cast<Vector2f>(pixel_coord) - m_resolution) / m_normalization_factor;
-
-            //
-            // Port of Keijiro Takahashi's natural vignetting effect for Unity.
-            // Recreates natural illumination falloff, which is approximated by the "cosine fourth" law of illumination falloff.
-            //
-            // References:
-            //
-            //   https://github.com/keijiro/KinoVignette
-            //   https://en.wikipedia.org/wiki/Vignetting#Natural_vignetting
-            //
-
-            const float linear_radial_falloff = norm(coord) * m_intensity;
-            const float quadratic_radial_falloff = linear_radial_falloff * linear_radial_falloff + 1.0f;
-
-            // Inversely proportional to the fourth power of the distance from the pixel to the image center.
-            const float inverse_biquadratic_radial_falloff = 1.0f / (quadratic_radial_falloff * quadratic_radial_falloff);
-
-            Color4f pixel;
-            tile.get_pixel(x, y, pixel);
-            pixel.rgb() *= inverse_biquadratic_radial_falloff;
-
-            tile.set_pixel(x, y, pixel);
-        }
-    }
-}
-
-void VignetteEffect::release()
-{
-}
-
-//
-// VignetteJob class implementation.
-//
-
-VignetteJob::VignetteJob(
-    const Frame&                frame,
-    const size_t                tile_x,
-    const size_t                tile_y,
-    const size_t                thread_count,
-    foundation::IAbortSwitch&   abort_switch,
-    const float                 intensity,
-    const float                 anisotropy,
-    const foundation::Vector2f& resolution,
-    const foundation::Vector2f& normalization_factor)
-  : m_frame(frame)
-  , m_tile_x(tile_x)
-  , m_tile_y(tile_y)
-  , m_thread_count(thread_count)
-  , m_abort_switch(abort_switch)
-{
-    // FIXME receive reference to a list of "appliers", and access it with thread_index on execute
-    m_effect_applier = new VignetteEffect(intensity, anisotropy, resolution, normalization_factor);
-}
-
-void VignetteJob::execute(const size_t thread_index)
-{
-    // Apply the Vignette post-processing effect to the tile.
-    //assert(thread_index < m_effect_appliers.size());
-    //m_effect_appliers[thread_index]
-    m_effect_applier->apply(
-        m_frame,
-        m_tile_x,
-        m_tile_y,
-        m_abort_switch);
-}
-*/
 //
 // VignettePostProcessingStageFactory class implementation.
 //
