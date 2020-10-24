92df33e80bf0e39ac52454025a9b0ad7cdca5e24
Kevin Masson <hi@oktomus.com>
2020-10-24 16:39:43 +0200

    Alternative: post process the frame just after rendering
    This is an alternative solution to the bug "False Colors not being applied to all tiles".
    
    The problem is the following:
    1. We emit a signal for the end of the render **before** applying post-process.
        The signal is emitted using `renderer_controller.on_rendering_success()` in `MasterRenderer::do_render()`.
    2. Because of that, appleseed.studio apply False Colors before post-process.
    3. False Colors gets applied on a copy of the frame, to keep the original render.
    4. So if there is a post-process + False Colors, False Colors get replaced by the post-process.
    
    One solution is to call `on_rendering_success` after applygin post-process, which order-correctly the effects:
    1. Post-process is applied first
    2. Then False Colors is applied on the post-processed frame
    3. It works !
    
    The problem with that solution, is that we don't call `on_rendering_success` and `on_rendering_abort` from `do_render` anymore, but `on_rendering_begin()` still is. Trigerring the signals from differents places in the code make it less easy to maintain and understand.
    
    I think we should keep the structure in `do_render()` to make sure interactive rendering + final rendering always work the same way.
    
    Another solution, the one in this commit, is to apply the post-process right after render. Then, no matter when you emit the signal, the frame will always be ready and will never get modified after the signal is being emitted.

--- src/appleseed/renderer/kernel/rendering/masterrenderer.cpp
+++ src/appleseed/renderer/kernel/rendering/masterrenderer.cpp
@@ -212,39 +212,12 @@ struct MasterRenderer::Impl
         try
         {
             IRendererController& controller =
-                    m_serial_renderer_controller != nullptr
-                        ? *m_serial_renderer_controller
-                        : renderer_controller;
+                m_serial_renderer_controller != nullptr
+                    ? *m_serial_renderer_controller
+                    : renderer_controller;
 
             // Render.
             result.m_status = do_render(controller);
-
-            // Retrieve frame's render info. Note that the frame entity may have been replaced during rendering.
-            ParamArray& render_info = m_project.get_frame()->render_info();
-
-            // Insert rendering time into frame's render info.
-            render_info.insert("render_time", m_project.get_rendering_timer().get_seconds());
-
-            // Don't proceed further if rendering failed or aborted.
-            if (result.m_status != RenderingResult::Succeeded)
-            {
-                if (result.m_status == RenderingResult::Aborted)
-                    controller.on_rendering_abort();
-
-                return result;
-            }
-
-            // Post-process.
-            RenderingTimer stopwatch;
-            stopwatch.start();
-            postprocess();
-            stopwatch.measure();
-
-            // Insert post-processing time into frame's render info.
-            render_info.insert("post_processing_time", stopwatch.get_seconds());
-            RENDERER_LOG_INFO("post-processing time: %s.", pretty_time(stopwatch.get_seconds()).c_str());
-
-            controller.on_rendering_success();
         }
         catch (const std::bad_alloc&)
         {
@@ -314,9 +287,11 @@ struct MasterRenderer::Impl
             switch (status)
             {
               case IRendererController::TerminateRendering:
+                renderer_controller.on_rendering_success();
                 return RenderingResult::Succeeded;
 
               case IRendererController::AbortRendering:
+                renderer_controller.on_rendering_abort();
                 return RenderingResult::Aborted;
 
               case IRendererController::ReinitializeRendering:
@@ -421,6 +396,25 @@ struct MasterRenderer::Impl
         m_project.get_light_path_recorder().finalize(
             props.m_canvas_width,
             props.m_canvas_height);
+
+        // Retrieve frame's render info. Note that the frame entity may have been replaced during rendering.
+        ParamArray& render_info = m_project.get_frame()->render_info();
+
+        // Insert rendering time into frame's render info.
+        render_info.insert("render_time", m_project.get_rendering_timer().get_seconds());
+
+        // Post-process the frame if rendering succeeded.
+        if (status == IRendererController::Status::TerminateRendering)
+        {
+            RenderingTimer stopwatch;
+            stopwatch.start();
+            postprocess();
+            stopwatch.measure();
+
+            // Insert post-processing time into frame's render info.
+            render_info.insert("post_processing_time", stopwatch.get_seconds());
+            RENDERER_LOG_INFO("post-processing time: %s.", pretty_time(stopwatch.get_seconds()).c_str());
+        }
 
         return status;
     }
